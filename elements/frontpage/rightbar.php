ea51c27be4ce4e05089cacb5a19f49105ae1ea05
root <root>
2007-08-09 10:58:17 +0000

    Excalibur *not* supported by QLabs. Hooray

--- elements/frontpage/rightbar.php
+++ elements/frontpage/rightbar.php
@@ -3,9 +3,6 @@
 		?><div class="sidebar rightbar"><?php
 			Element( "category/list" );
     		?><br />
-            <div style="text-align:center;color:#666666">
-                Supported by <a href="http://www.qlabs.gr/">QLabs</a>
-            </div>
-        </div><?php
+        	</div><?php
 	}
 ?>
