5a880ebd2719ba2cb9fa6ba2493736e72b30bcd6
Aleksis Brezas <abresas@gmail.com>
2007-08-11 20:53:28 +0000

    fixing pm read

--- units/pm/expandpm.do.php
+++ units/pm/expandpm.do.php
@@ -6,11 +6,9 @@ function UnitPmExpandpm( tInteger $pmid ) {
 	$libs->Load( 'pm' );
 	$pmid = $pmid->Get();
 	$pm = new PM( $pmid );
-	?>alert( '<?php echo $pmid; ?>' );<?php
-	?>alert( '<?php echo "." . $pm->Id . "."; ?>' );<?php
-	if ( !$pm->IsRead ) {	
+	if ( !$pm->IsRead() ) {	
 		$pm->DelId = 1;	
-		//$pm->Save();
+		$pm->Save();
 	}
 }
 ?>
