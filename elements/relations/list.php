8bee7d639f5a50afb33690077477bb044aaf1361
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-08-16 12:56:29 +0000

    Trying a different approach

--- elements/relations/list.php
+++ elements/relations/list.php
@@ -23,7 +23,7 @@
 			<form id="newrform" action="do/relations/new" method="post" style="display: none;" >
 			<fieldset style="width: 40px">
 			<legend><font color="#2412FE"><b>Δημιουργία Σχέσης</b></font></legend>
-			<input type="text" id="type" name="type" class="bigtext" value="Γράψε εδώ την νέα Σχέση!" onfocus="((this.value=='Γράψε εδώ την νέα Σχέση!')?this.value='':this.value=this.value);" onkeypress="return Relations.checkSize( window.event );" />
+			<input type="text" id="type" name="type" class="bigtext" value="Γράψε εδώ την νέα Σχέση!" onfocus="((this.value=='Γράψε εδώ την νέα Σχέση!')?this.value='':this.value=this.value);" onkeypress="return Relations.checkSize( event );" />
 			<a onclick="g( 'newrform' ).submit();return false;" style="cursor: pointer;" alt="Δημιουργία" title="Δημιουργία"><img src="<?php 
 			echo $xc_settings[ 'staticimagesurl' ];
 			?>icons/accept.png" /></a>&nbsp;
