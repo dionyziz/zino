f789b2b6b02f3880c99b2d8085724fa6248028be
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-08-16 11:31:05 +0000

    Putting alerts for YSoD

--- elements/test.php
+++ elements/test.php
@@ -1,12 +1,13 @@
 <?php
     function ElementTest() {
     	global $libs;
-        var_dump(
+        /*var_dump(
             User_ByUsername(
                 array(
                     'izual', 'dionyziz', 'abresas'
                 )
             )
-        );
+        );*/
+        echo mformatsmallstories( array( "Kostis90gr h4x0r3d <script>alert('hey')</script> you !@#$%^&*() [merlin:user kostis90gr] :-)" ) );
     }
 ?>
