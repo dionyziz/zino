3cf8ef9a904009404e0c595550eb42aaf598e582
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-08-16 11:35:18 +0000

    Preventin YSoD

--- elements/test.php
+++ elements/test.php
@@ -8,6 +8,8 @@
                 )
             )
         );*/
-        echo mformatsmallstories( array( "Kostis90gr h4x0r3d <script>alert('hey')</script> you !@#$%^&*() [merlin:user kostis90gr] :-)" ) );
+        echo "<!--";
+        mformatsmallstories( array( "Kostis90gr h4x0r3d <script>alert('hey')</script> you !@#$%^&*() [merlin:user kostis90gr] :-)" ) );
+        echo "-->";
     }
 ?>
