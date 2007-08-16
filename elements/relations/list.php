010e55b9bae4474ed64e048067666536490ecc34
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-08-16 12:44:51 +0000

    SHould work now

--- elements/relations/list.php
+++ elements/relations/list.php
@@ -22,7 +22,7 @@
 		?><br /><a id="newr" onclick="Relations.create();return false;" alt="Νέα Σχέση" title="Νέα Σχέση" style="cursor: pointer;">Δημιούργησε μια νέα σχέση</a>
 			<form id="newrform" action="do/relations/new" method="post" style="display: none;" onsubmit="function () {if( g( 'type' ).value.length > 20 ) {
 			alert( 'Δεν μπορεί μια σχέση να έχει όνομα μεγαλύτερο των 20 χαρακτήρων' );
-		}return true;}" onkeypress="function (e) { return submitenter( this, e ); }">
+		}return true;}" onkeypress="return submitenter( this, e );">
 			<fieldset style="width: 40px">
 			<legend><font color="#2412FE"><b>Δημιουργία Σχέσης</b></font></legend>
 			<input type="text" id="type" name="type" class="bigtext" value="Γράψε εδώ την νέα Σχέση!" onfocus="((this.value=='Γράψε εδώ την νέα Σχέση!')?this.value='':this.value=this.value);" />
