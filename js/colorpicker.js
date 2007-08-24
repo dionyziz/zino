f2910c69224515e29fb8545a5da103b0f3efedd1
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-08-24 15:04:46 +0000

    Proxorame

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -1,16 +1,16 @@
-var numen = new Array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
-
 var ColorPicker = {
 	Create : function () {
 		var table = document.createElement( 'table' );
 		table.border="0";
 		table.cellpadding="0";
 		table.cellspacing="0";
+		var k,l,m;
+		k=l=m=0;
 		for( var y=0;y<40;++y ) {
 			var tr = document.createElement( 'tr' ); 
 			for ( var i=0;i<600;i+=10 ) {
 				var td = document.createElement( 'td' );
-				td.style.backgroundColor = "#00ff00";
+				td.style.backgroundColor = "rgb(" + k + "," + l + "," + m + ")";
 				
 				var img = document.createElement( 'img' );
 				img.src = "http://webringworld.org/pics/blank.gif";
@@ -19,9 +19,22 @@ var ColorPicker = {
 				
 				td.appendChild( img );
 				tr.appendChild( td );
+				
+				if( m < 255 ) {
+					++m;
+					continue;
+				}
+				if( l < 255 ) {
+					++l;
+					continue;
+				}
+				if( k < 255 ) {
+					++k;
+					continue;
+				}
 			}
 			table.appendChild( tr );
 		}
 		return table;
-	}
+	},
 };
