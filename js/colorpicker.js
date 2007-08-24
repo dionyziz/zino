0e4d1f01339859d4e2cabfeb13f2f7e42c37f823
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-08-24 19:57:13 +0000

    Fixing out of bounds

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -18,7 +18,7 @@ var ColorPicker = {
 				td.onmouseover= (function (r, g, b, preview) {
 								return function () {
 									document.body.style.cursor = "pointer";
-									ColorPicker.Preview( r, g, b, "preview" );
+									ColorPicker.Preview( ColorPicker.fix( r ), ColorPicker.fix( g ), ColorPicker.fix( b ), "preview" );
 								}
 							})(r,g,b,"preview");
 				td.onmouseout = function () {
@@ -26,41 +26,57 @@ var ColorPicker = {
 						};
 				td.onclick = (function ( r, g, b ) {
 								return function () {
-									clickaria( r, g, b );
+									clickaria( ColorPicker.fix( r ), ColorPicker.fix( g ), ColorPicker.fix( b ) );
 									Modals.Destroy();
 								}
 							})(r,g,b);
-				td.style.height = "2px";
-				td.style.width = "2px";
+				
+				var img = document.createElement( 'img' );
+				img.src = "http://webringworld.org/pics/blank.gif";
+				img.height="2";
+				img.width="2";
+				
+				td.appendChild( img );
 				tr.appendChild( td );
 				
-				if( b < 253 ) {
+				if( b < 255 ) {
 					b+=26;
 				}
-				else if( g < 253 ) {
+				else if( g < 255 ) {
 					g+=25;
 					b=0;
 				}
-				else if( r < 253 ) {
+				else if( r < 255 ) {
 					r+=24;
 					b=g=0;
 				}
 			}
 			table.appendChild( tr );
 		}
-		var dived = document.createElement( 'div' );
-		dived.style.width = "234px";
-		dived.style.height = "30px";
-		dived.id = "preview";
+		var tabled = document.createElement( 'table' );
+		var trd = document.createElement( 'tr' );
+		var tdd = document.createElement( 'td' );
+		tdd.id = "preview";
+		var imgd = document.createElement( 'img' );
+		imgd.width = "234";
+		imgd.height = "30";
+		imgd.src = "http://webringworld.org/pics/blank.gif";
+		tdd.appendChild(imgd);
+		trd.appendChild(tdd);
+		tabled.appendChild(trd);
 		
 		var div = document.createElement( 'div' );
 		div.appendChild( table );
-		div.appendChild( dived );
+		div.appendChild( tabled );
 		
 		return div;
 	},
 	Preview : function( r, g, b, id ) {
 		var ted = document.getElementById( id );
 		ted.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+	},
+	fix : function( num ) {
+		return ( num > 255 )?255:num;
 	}
 };
+
