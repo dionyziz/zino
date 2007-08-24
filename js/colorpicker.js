96b729fe8bafacc386b519d49c46587010909270
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-08-24 17:24:22 +0000

    Teliopoiisi

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -4,6 +4,9 @@ var ColorPicker = {
 		table.border="0";
 		table.cellpadding="0";
 		table.cellspacing="0";
+		var caption = document.createElement( 'caption' );
+		caption.appendChild( document.createTextNode( "Επέλεξε ένα χρώμα" ) );
+		table.appendChild( caption );
 		var r,g,b;
 		r=g=b=0;
 		for( var y=0;y<27;++y ) {
@@ -13,9 +16,13 @@ var ColorPicker = {
 				td.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 				td.onmouseover= (function (r, g, b, preview) {
 								return function () {
+									document.body.style.cursor = "pointer";
 									ColorPicker.Preview( r, g, b, "preview" );
 								}
 							})(r,g,b,"preview");
+				td.onmouseout = function () {
+							document.body.style.cursor = "default";
+						};
 				td.onclick = (function ( r, g, b ) {
 								return function () {
 									clickaria( r, g, b );
