1996179d3628917f8a8fd5c6adc433bc17840d92
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-08-28 18:07:34 +0000

    Taking care for the undeleted comments

--- units/comments/undodelete.do.php
+++ units/comments/undodelete.do.php
@@ -9,12 +9,34 @@
 		$libs->Load( 'comment' );
 		
 		$comment = New Comment( $commentid );
+		$daddy = $comment->ParentId();
 		$comment->UndoDelete();
 		
-		?>id = <?php echo $commentid; ?>;
+		?>var id = <?php echo $commentid; ?>;
+		var daddy = <?php echo $daddy; ?>;
+		
 		g( 'comment_' + id ).style.display = 'block';
 		
-		g( 'comment_undo_delete_' + id ).style.display = 'none';<?php
+		g( 'comment_undo_delete_' + id ).style.display = 'none';
+		
+		if( daddy != 0 ) {
+			var numcom = g( daddy + "_children" ).firstChild;
+			var num = parseInt( numcom.nodeValue );
+			++num;
+			numcom.nodeValue = num;
+			
+			if( num == 1 ) {
+				var toolbar = g( 'comment_' + daddy + '_toolbar' );
+				for( var i in toolbar.childNodes ) {
+					if( toolbar.childNodes[i].firstChild.firstChild.firstChild.nodeValue == "Διαγραφή" ) {
+						toolbar.removeChild( toolbar.childNodes[i] );
+						break;
+					}
+				}
+			}
+		}
+		
+		<?php
 	}
 
 ?>
