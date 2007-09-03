b11ba5f525ad4d9f303c5cc205430c9f66de1aa6
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-09-03 12:54:11 +0000

    Finished with Show/Hide

--- elements/user/profile/frelations.php
+++ elements/user/profile/frelations.php
@@ -17,10 +17,10 @@
     	
     	?><div id="friend_relations" class="friend_relations">
                 <map id="close" name="close">
-				<area shape="rect" coords="94,20,105,30" onclick="alert('Klino');return false;" alt="Κλείσιμο" title="Κλείσιμο" href=''/>
+				<area shape="rect" coords="94,20,105,30" onclick="Friends.ShowAll( false );return false;" alt="Κλείσιμο" title="Κλείσιμο" href=''/>
 				</map>
 				
-				<img src="https://beta.chit-chat.gr/etc/mockups/frelations/frelations_htmled/top_close.png" usemap="#close" style="border: none;" /><br />
+				<img src="https://beta.chit-chat.gr/etc/mockups/frelations/frelations_htmled/top_close.png" usemap="#close" style="border: none;" />
                 <div class="frelations"><?php
                 foreach( $relations as $relation ) {
                 	?><div id="frel_<?php
