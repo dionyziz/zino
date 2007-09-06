c05bdd09115dd910b046debef939f6c9042a8a59
Aleksis Brezas <abresas@gmail.com>
2007-09-06 13:43:36 +0000

    testing with proc_open()

--- libs/sanitizer.php
+++ libs/sanitizer.php
@@ -31,8 +31,8 @@
                 2 => array( "/tmp/error-output.txt", "a" )
             );
 
-            $cmd = '/srv/www/vhosts/chit-chat.gr/subdomains/beta/httpsdocs/bin/sanitizer/sanitizer';
-
+            $cmd = 'sanitizer';
+            chdir( '/srv/www/vhosts/chit-chat.gr/subdomains/beta/httpsdocs/bin/sanitizer' );
             $proccess = proc_open( $cmd, $descriptorspec, $pipes );
             if ( !is_resource( $proccess ) ) {
                 die( "Error opening sanitizer process" );
