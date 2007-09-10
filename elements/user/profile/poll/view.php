2c06578c864c190c604232e36525d652f7f993ff
Aleksis Brezas <abresas@gmail.com>
2007-09-10 22:32:46 +0000

    userpolls: vote

--- elements/user/profile/poll/view.php
+++ elements/user/profile/poll/view.php
@@ -6,25 +6,29 @@
         $page->AttachStylesheet( "css/poll.css" );
 
         ?><div class="userpoll">
-            <h4>Ποιος είναι ο πιο hot tokio hotel member?!</h4>
-            <ul>
-                <li><dl>
-                    <dt><input type="radio" id="p_4_0" name="option" value="0" /></dt>
-                    <dd><label for="p_4_0">Bill!</label></dd>
-                </dl></li>
-                <li><dl class="l">
-                    <dt><input type="radio" id="p_4_1" name="option" value="1" /></dt>
-                    <dd><label for="p_4_1">Tom!</label></dd>
-                </dl></li>
-                <li><dl>
-                    <dt><input type="radio" id="p_4_2" name="option" value="2" /></dt>
-                    <dd><label for="p_4_2">Georg!</label></dd>
-                </dl></li>
-                <li><dl class="l">
-                    <dt><input type="radio" id="p_4_3" name="option" value="3" /></dt>
-                    <dd><label for="p_4_3">Gustav!</label></dd>
-                </dl></li>
-            </ul>
+            <h4><?php
+                echo $poll->Question;
+            ?></h4>
+            <ul><?php
+                $options = $poll->Options;
+                foreach ( $options as $option ) {
+                    ?><li><dl>
+                        <dt><input type="radio" id="p_<?php
+                        echo $poll->Id;
+                        ?>_<?php
+                        echo $option->Id;
+                        ?>" name="option" value="0" /></dt>
+                        <dd><label for="p_<?php
+                        echo $poll->Id;
+                        ?>_<?php
+                        echo $option->Id;
+                        ?>"><?php
+                        echo $option->Text;
+                        ?></label></dd>
+                    </dl></li><?php
+                }
+
+            ?></ul>
         </div><?php
     }
 
