0ca9dd2c8c7d8ce93c929ede8b03df67b025caf5
Aleksis Brezas <abresas@gmail.com>
2007-09-10 22:53:58 +0000

    userpolls: create

--- elements/user/profile/poll/new.php
+++ elements/user/profile/poll/new.php
@@ -12,7 +12,7 @@
         $page->AttachScript( 'js/newpoll.js' );
 
         ?><div class="userpoll" style="opacity:0.5;filter:progid:DXImageTransform.Microsoft.Alpha(opacity=0);" id="newpoll">
-            <h4 style="height:18px"><a href="" onclick="CreatePoll();return false;" style="background-image:url('http://static.chit-chat.gr/images/icons/add.png');background-repeat:no-repeat;padding-left:20px;height:18px;display:block">δημιουργία δημοσκόπησης</a></h4>
+            <h4 style="height:18px"><a href="" onclick="Poll.CreatePoll();return false;" style="background-image:url('http://static.chit-chat.gr/images/icons/add.png');background-repeat:no-repeat;padding-left:20px;height:18px;display:block">δημιουργία δημοσκόπησης</a></h4>
             <ul>
             </ul>
         </div><?php
