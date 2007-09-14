b3d2f7e1d8db205357f65a3d1c859a586fe106ee
Aleksis Brezas <abresas@gmail.com>
2007-09-14 07:28:46 +0000

    edit and delete poll

--- elements/user/profile/poll/results.php
+++ elements/user/profile/poll/results.php
@@ -1,14 +1,16 @@
 <?php
 
     function ElementUserProfilePollResults( $poll ) {
+        global $xc_settings;
+
         ?><div class="pollresults">
             <h4><a style="float:right;"><img src="<?php
             echo $xc_settings[ 'staticimagesurl' ];
-            ?>/images/icons/delete.png" style="width: 16px; height: 16px;" alt="delete poll" /></a><?php
+            ?>icons/delete.png" style="width: 16px; height: 16px;" alt="delete poll" /></a><?php
             echo htmlspecialchars( $poll->Question );
             ?><a><img src="<?php
             echo $xc_settings[ 'staticimagesurl' ];
-            ?>/images/icons/edit.png" style="width: 16px; height: 16px;" alt="edit poll title" /></a></h4>
+            ?>icons/edit.png" style="width: 16px; height: 16px;" alt="edit poll title" /></a></h4>
             <ul class="results"><?php
 
                 $options = $poll->Options;
