9e9be8fa77f81b3247348519253dd0df8091a71b
Aleksis Brezas <abresas@gmail.com>
2007-09-14 13:32:35 +0000

    userpolls: undo delete

---
+++ units/poll/undodelete.do.php
@@ -0,0 +1,20 @@
+<?php
+
+    function UnitUndoDelete( tInteger $pollid, tCoalaPointer $callback ) {
+        global $user;
+        global $libs;
+
+        $libs->Load( 'poll' );
+
+        $poll = new Poll( $pollid->Get() );
+        if ( !$poll->Exists() || !$user->IsAnonymous() || $poll->UserId != $user->Id() ) {
+            return;
+        }
+        $poll->DelId = 0;
+        $poll->Save();
+
+        echo $callback;
+        ?>();<?php
+    }
+
+?>
