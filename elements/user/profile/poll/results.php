5e0db9aaf13102ecafef774af599923c12b76885
Aleksis Brezas <abresas@gmail.com>
2007-09-14 07:26:42 +0000

    edit and delete poll

--- elements/user/profile/poll/results.php
+++ elements/user/profile/poll/results.php
@@ -4,11 +4,11 @@
         ?><div class="pollresults">
             <h4><a style="float:right;"><img src="<?php
             echo $xc_settings[ 'staticimagesurl' ];
-            ?>/icons/delete.png" style="width: 16px; height: 16px;" alt="delete poll" /></a><?php
+            ?>/images/icons/delete.png" style="width: 16px; height: 16px;" alt="delete poll" /></a><?php
             echo htmlspecialchars( $poll->Question );
             ?><a><img src="<?php
             echo $xc_settings[ 'staticimagesurl' ];
-            ?>/icons/edit.png" style="width: 16px; height: 16px;" alt="edit poll title" /></a></h4>
+            ?>/images/icons/edit.png" style="width: 16px; height: 16px;" alt="edit poll title" /></a></h4>
             <ul class="results"><?php
 
                 $options = $poll->Options;
