2dcef9308f4fbb5a8465e976f02e9969c6b7fcdf
Dionysis Zindros <dionyziz@gmail.com>
2007-09-20 21:28:11 +0000

    -dbg

--- units/pm/expandpm.do.php
+++ units/pm/expandpm.do.php
@@ -7,10 +7,6 @@
     	$pmid = $pmid->Get();
     	$pm = new PM( $pmid );
     	if ( !$pm->IsRead() ) {	
-            ?>alert( 'Mark as read <?php
-            echo $pm->Id;
-            ?>' );<?php
-            
     		$pm->DelId = 1;	
     		$pm->Save();
     	}
