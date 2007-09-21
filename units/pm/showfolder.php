adf28122d900084326546b9340ec8b51e1fe5835
Dionysis Zindros <dionyziz@gmail.com>
2007-09-21 07:58:27 +0000

    PMs move message

--- units/pm/showfolder.php
+++ units/pm/showfolder.php
@@ -65,6 +65,8 @@ function UnitPmShowfolder( tInteger $folderid ) {
             } );
             drag.SetOnEnd( function ( draggable ) {
                 draggable.style.position = 'relative';
+                draggable.style.left = '0';
+                draggable.style.top = '0';
                 Animations.Create( draggable, 'opacity', 500, 0.7, 1 );
                 document.getElementById( 'folders' ).style.border = '1px solid #838080';
             } );
