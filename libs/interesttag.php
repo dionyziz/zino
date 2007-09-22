21f7d0fe49a7439229f2f1c997d4608fa7b4b816
Aleksis Brezas <abresas@gmail.com>
2007-09-22 22:47:40 +0000

    interest tag library

--- libs/interesttag.php
+++ libs/interesttag.php
@@ -27,10 +27,6 @@
             }
         }
 
-        if ( count( $tags ) > 1 ) {
-            die( print_r( $tags ) . "<br />" . print_r( $prevs ) );
-        }
-
         $ret = array();
         $cur = $tags[ $first ];
         while ( isset( $tags[ $cur->NextId ] ) ) {
