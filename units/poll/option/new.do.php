26be1eb00e9c5acc891c37716d3b5a6d1424d1c6
Aleksis Brezas <abresas@gmail.com>
2007-09-22 14:31:41 +0000

    debug poll option create

--- units/poll/option/new.do.php
+++ units/poll/option/new.do.php
@@ -7,23 +7,24 @@
         $libs->Load( 'poll' );
 
         $option         = new PollOption();
+        
+        ob_start();
+        var_dump( $option->Poll );
+        ?>alert( <?php echo w_json_encode( ob_get_clean() ) ?> );<?php
+
         $option->PollId = $pollid->Get();
+        ob_start();
+        var_dump( $option->Poll );
+        ?>alert( <?php echo w_json_encode( ob_get_clean() ) ?> );<?php
         $option->Text   = $text->Get();
         $option->Save();
+        ob_start();
+        var_dump( $option->Poll );
+        ?>alert( <?php echo w_json_encode( ob_get_clean() ) ?> );<?php
 
         ob_start();
         Element( "poll/option/view", $option, $user );
         $html = ob_get_clean();
-
-        $poll = $option->Poll;
-        
-        ob_start();
-        var_dump( $option );
-        $dump = ob_get_clean();
-
-        ?>alert( <?php
-        echo w_json_encode( $dump );
-        ?> );<?php
 
         echo $callback;
         ?>( <?php
