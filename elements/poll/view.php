6d1bfe655520fcad4388236cf4a05c14ddecbb1f
Aleksis Brezas <abresas@gmail.com>
2007-09-25 19:34:04 +0000

    poll page

---
+++ elements/poll/view.php
@@ -0,0 +1,17 @@
+<?php
+
+    function ElementPollView( tInteger $id ) {
+        global $libs;
+        global $page;
+
+        $libs->Load( 'poll' );
+
+        $page->AttachStylesheet( 'css/poll.css' );
+        $page->AttachScript( 'js/poll.js' );
+
+        $poll = new Poll( $id->Get() );
+
+        Element( "poll/box" );
+    }
+
+?>
