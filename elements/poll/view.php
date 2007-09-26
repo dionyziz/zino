8836afa0e3a02e3bf46625c00ffc8d25124f746f
Aleksis Brezas <abresas@gmail.com>
2007-09-26 22:04:06 +0000

    poll page: comments

--- elements/poll/view.php
+++ elements/poll/view.php
@@ -18,25 +18,25 @@
         ?><div class="poll"><?php
 
             Element( "poll/box", $poll );
+        ?></div><?php
 
-			?><div class="comments" id="comments"><?php	
-				$search = New Search_Comments();
-				$search->SetFilter( 'typeid', 3 ); // 0: article, 1: userspace
-				$search->SetFilter( 'page', $poll->Id ); //show all comments of an article 
-				$search->SetFilter( 'delid', 0 ); // do not show deleted comments
-				$search->SetSortMethod( 'date', 'DESC' ); //sort by date, newest shown first
-				if ( $oldcomments ) {
-					$search->SetLimit( 10000 );
-				}
-				else {
-					$search->SetLimit( 50 );  //show no more than 50 comments
-				}
-				$comments = $search->GetParented(); //get comments
-				
-                Element( 'comment/import' );
-				Element( 'comment/list' , $comments , 0 , 0 );
-				Element( 'comment/reply', $poll, 3 );
-			?></div>
+        ?><div class="comments" id="comments"><?php	
+            $search = New Search_Comments();
+            $search->SetFilter( 'typeid', 3 ); // 0: article, 1: userspace
+            $search->SetFilter( 'page', $poll->Id ); //show all comments of an article 
+            $search->SetFilter( 'delid', 0 ); // do not show deleted comments
+            $search->SetSortMethod( 'date', 'DESC' ); //sort by date, newest shown first
+            if ( $oldcomments ) {
+                $search->SetLimit( 10000 );
+            }
+            else {
+                $search->SetLimit( 50 );  //show no more than 50 comments
+            }
+            $comments = $search->GetParented(); //get comments
+            
+            Element( 'comment/import' );
+            Element( 'comment/list' , $comments , 0 , 0 );
+            Element( 'comment/reply', $poll, 3 );
 
         ?></div><?php
     }
