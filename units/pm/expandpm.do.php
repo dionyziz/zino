972262d59811aaa81851ecc010ccc8c202ab4024
Chris Pappas <chrispappas12@gmail.com>
2007-10-03 17:58:48 +0000

    :

--- units/pm/expandpm.do.php
+++ units/pm/expandpm.do.php
@@ -2,13 +2,12 @@
     function UnitPmExpandpm( tInteger $pmid ) {
     	global $libs;
     	global $user;
-		
-		?>alert( 'making pm read' );<?php
     	
     	$libs->Load( 'pm' );
     	$pmid = $pmid->Get();
     	$pm = new PM( $pmid );
     	if ( !$pm->IsRead() ) {	
+			?>alert( 'making read' );<?php
     		$pm->DelId = 1;	
     		$pm->Save();
     	}
