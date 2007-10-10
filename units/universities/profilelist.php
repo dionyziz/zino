a4e186701f0932936a59eafe208e5d81c2f7a509
Chris Pappas <chrispappas12@gmail.com>
2007-10-10 20:04:25 +0000

    :

--- units/universities/profilelist.php
+++ units/universities/profilelist.php
@@ -7,12 +7,12 @@
 		?>var modaluni = document.getElementById( 'modaluni' );
 		var modalunidivlist = modaluni.getElementsByTagName( 'div' );
 		var newdiv = modalunidivlist[ 1 ];
-		newdiv.appendChild( document.createTextNode( 'Διάλεξε ίδρυμα ' ) );
+		newdiv.appendChild( document.createTextNode( 'Ξ”ΞΉΞ¬Ξ»ΞµΞΎΞµ Ξ―Ξ΄ΟΟ…ΞΌΞ± ' ) );
 		var selectlist = <?php
 		ob_start();
     	Element( 'universities/unipertownlist' , $townid );
     	echo w_json_encode( ob_get_clean() );
     	?>;
-		newdiv.appendChild( selectlist );<?php
+		newdiv.innerHTML += selectlist;<?php
 	}
 ?>
