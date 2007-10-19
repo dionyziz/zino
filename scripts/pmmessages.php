e5bf36e597f767c3c3a38e8606ca83496824a6fc
Aleksis Brezas <abresas@gmail.com>
2007-10-19 23:02:33 +0000

    pms 2.0 script

---
+++ scripts/pmmessages.php
@@ -0,0 +1,62 @@
+<?php
+	set_include_path( '../:./' );
+	
+	global $page;
+	global $libs;
+    global $db;
+    
+	require_once 'libs/rabbit/rabbit.php';
+    Rabbit_Construct( 'HTML' );
+    $req = $_GET;
+    
+	Rabbit_ClearPostGet();
+	
+	$libs->Load( 'albums' );
+
+    $ret = GetAllPms();
+    $texts = array();
+    foreach ( $ret as $pm ) {
+        $texts[ $pm[ 'pm_id' ] ] = $pm[ 'pm_text' ];
+    }
+
+    $formatted = mformatpms( $texts );
+
+    foreach ( $ret as $pm ) {
+        ?>Updating pm <?php
+        echo $pm->Id
+        ?>... <?php
+
+        $pmm = array(
+            'pm_id'             => $pm[ 'pm_id' ],
+            'pm_senderid'       => $pm[ 'pm_from' ],
+            'pm_text'           => $pm[ 'pm_text' ],
+            'pm_textformatted'  => $formatted[ 'pm_id' ],
+            'pm_date'           => $pm[ 'pm_created' ]
+        );
+
+        $pmifsend = array(
+            'pmif_id'       => $pm[ 'pm_id' ],
+            'pmif_userid'   => $pm[ 'pm_from' ],
+            'pmif_folderid' => -2,
+            'pmif_delid'    => 0
+        );
+
+        $pmifrecv = array(
+            'pmif_id'       => $pm[ 'pm_id' ],
+            'pmif_userid'   => $pm[ 'pm_to' ],
+            'pmif_folderid' => -1,
+            'pmif_delid'    => $pm[ 'pm_delid' ]
+        );
+
+        $db->Insert( $pmm, $pmmessages );
+        $db->Insert( $pmifsend, $pmmessageinfodler );
+        $db->Insert( $pmifrecv, $pmmessageinfolder );
+
+        ?>OK<br /><?php
+    }
+	
+    $page->Output();
+
+    Rabbit_Destruct();
+	
+?>
