b52db832e543b2389e08e3a472ac3cef54b6c563
Aleksis Brezas <abresas@gmail.com>
2007-10-19 22:11:30 +0000

    pm receivers

--- libs/pm.php
+++ libs/pm.php
@@ -501,7 +501,7 @@
 			$ret = array();
 			while ( $row = $res->FetchArray() ) {
 				$ret[] = new PM( $row );
-                print_r( $ret[ count( $ret ) - 1 ] );
+                die( print_r( $ret[ count( $ret ) - 1 ] ) );
 			}
 
 			return $ret;
