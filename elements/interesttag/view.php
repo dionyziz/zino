8af07e8394a55c7141aaa59f999d25e009dd3f22
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-10-31 13:00:37 +0000

    Debug

--- elements/interesttag/view.php
+++ elements/interesttag/view.php
@@ -6,6 +6,8 @@
 		$libs->Load( 'interesttag' );
 		$page->SetTitle( 'Ενδιαφέροντα' );
 		
+		echo "y0!";
+		
 		$users = InterestTag_List( $text );
 		if( count( $users ) == 0 ) {
 			?><b>Λυπάμε, δεν υπάρχουν χρήστες με τέτοια ενδιαφέροντα</b><?php
