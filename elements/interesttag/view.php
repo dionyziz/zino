608550924e4a95c58bde32051650d7be985dfbcd
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-11-01 20:05:49 +0000

    Fixes

--- elements/interesttag/view.php
+++ elements/interesttag/view.php
@@ -2,10 +2,10 @@
 	function ElementInteresttagView( $text ) {
 		global $page;
 		global $libs;
-		global $user;
 		
 		$libs->Load( 'interesttag' );
 		$page->SetTitle( 'Ενδιαφέροντα' );
+		$page->AttachStyleSheet( 'css/rounded.css' );
 		
 		$tag_users = InterestTag_List( $text ); // Get a list of instances of InterestTag
 		if( count( $tag_users ) == 0 ) {
@@ -14,7 +14,7 @@
 		}
 		$users = array(); // A List of instances of User
 		foreach ( $tag_users as $tag_user ) {
-			$users[] = New User( $tag_user );
+			$users[] = New User( $tag_user->UserId );
 		}
 		Element( 'user/profile/friends' , $users, true );
 	}
