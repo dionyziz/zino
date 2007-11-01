8c64cd38ad6161c5a0e4c8834df2db07f56ca280
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-11-01 10:37:15 +0000

    Ma gtg dn kali to element?

--- elements/interesttag/view.php
+++ elements/interesttag/view.php
@@ -7,13 +7,12 @@
 		$libs->Load( 'interesttag' );
 		$page->SetTitle( 'Ενδιαφέροντα' );
 		
-		echo "y0!dude";
-		
 		$users = InterestTag_List( $text );
 		if( count( $users ) == 0 ) {
 			?><b>Λυπάμε, δεν υπάρχουν χρήστες με τέτοια ενδιαφέροντα</b><?php
 			return;
 		}
 		Element( 'user/profile/friends' , $users, true );
+		echo "y0!dude";
 	}
 ?>
