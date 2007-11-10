ba8948db9b5f2230f56dd3b59261805bb3976bc5
Aleksis Brezas <abresas@gmail.com>
2007-11-10 19:29:47 +0000

    tags user friends

--- tests/interesttag.php
+++ tests/interesttag.php
@@ -138,6 +138,7 @@
             $tags = InterestTag_List( 'Sin City' );
             $test = New User( 'test' );
             $found = false;
+            die( print_r( $tags ) );
             foreach ( $tags as $tag ) { // they should be ordered by order (users who have this tag first show up first), secondarily ordered by reverse creation date (newest users who added it show up first)
                 $this->Assert( $tag instanceof InterestTag, 'InterestTag_List should return an array of InterestTag instances when retrieving a list of tags with a particular text' );
                 $user = $tag->User;
