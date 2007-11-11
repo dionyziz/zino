40e595d68b19e6aaea8b71844a789bbbe9bff940
Chris Pappas <chrispappas12@gmail.com>
2007-11-11 19:56:50 +0000

    frontpage mockup

--- etc/mockups/7/frontpage.php
+++ etc/mockups/7/frontpage.php
@@ -5,17 +5,19 @@
 </div>
 <div>
 	<h2>Γνώρισε νέους φίλους</h2>
-	<ul class="people">
-		<li><a href=""><img src="images/avatars/2400.jpg" alt="dionyziz" title="dionyziz" />dionyziz<span>προβολή προφίλ &raquo;</span></a></li>
-		<li><a href=""><img src="images/avatars/14041.jpg" alt="klio" title="klio" />klio<span>προβολή προφίλ &raquo;</span></a></li>
-		<li><a href=""><img src="images/avatars/20128.png" alt="finlandos" title="finlandos" />finlandos<span>προβολή προφίλ &raquo;</span></a></li>
-		<li><a href=""><img src="images/avatars/28155.jpg" alt="elsa_kaulitz" title="elsa_kaulitz" />elsa_kaulitz<span>προβολή προφίλ &raquo;</span></a></li>
-		<li><a href=""><img src="images/avatars/29203.jpg" alt="izual" title="izual" />izual<span>προβολή προφίλ &raquo;</span></a></li>
-		<li><a href=""><img src="images/avatars/29230.jpg" alt="metalo92" title="metalo92" />metalo92<span>προβολή προφίλ &raquo;</span></a></li>
-		<li><a href=""><img src="images/avatars/29228.jpg" alt="alinangel" title="alinangel" />alinangel<span>προβολή προφίλ &raquo;</span></a></li>
-		<li><a href=""><img src="images/avatars/11400.jpg" alt="effie" title="effie" />effie<span>προβολή προφίλ &raquo;</span></a></li>
-		<li><a href=""><img src="images/avatars/29229.jpg" alt="punkerfly" title="punkerfly" />punkerfly<span>προβολή προφίλ &raquo;</span></a></li>
-	</ul>
+	<div class="people">
+		<ul>
+			<li><a href=""><img src="images/avatars/2400.jpg" alt="dionyziz" title="dionyziz" />dionyziz<span>προβολή προφίλ &raquo;</span></a></li>
+			<li><a href=""><img src="images/avatars/14041.jpg" alt="klio" title="klio" />klio<span>προβολή προφίλ &raquo;</span></a></li>
+			<li><a href=""><img src="images/avatars/20128.png" alt="finlandos" title="finlandos" />finlandos<span>προβολή προφίλ &raquo;</span></a></li>
+			<li><a href=""><img src="images/avatars/28155.jpg" alt="elsa_kaulitz" title="elsa_kaulitz" />elsa_kaulitz<span>προβολή προφίλ &raquo;</span></a></li>
+			<li><a href=""><img src="images/avatars/29203.jpg" alt="izual" title="izual" />izual<span>προβολή προφίλ &raquo;</span></a></li>
+			<li><a href=""><img src="images/avatars/29230.jpg" alt="metalo92" title="metalo92" />metalo92<span>προβολή προφίλ &raquo;</span></a></li>
+			<li><a href=""><img src="images/avatars/11400.jpg" alt="effie" title="effie" />effie<span>προβολή προφίλ &raquo;</span></a></li>
+			<li><a href=""><img src="images/avatars/29229.jpg" alt="punkerfly" title="punkerfly" />punkerfly<span>προβολή προφίλ &raquo;</span></a></li>
+			<li><a href=""><img src="images/avatars/29228.jpg" alt="alinangel" title="alinangel" />alinangel<span>προβολή προφίλ &raquo;</span></a></li>
+		</ul>
+	</div>
 	<div class="eof"></div>
 </div>
 <div>
