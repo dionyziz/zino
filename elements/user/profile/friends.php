102f74ea3a87e293c587104d5d233b183ef4756c
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-11-11 19:32:04 +0000

    Interests Should Appear As Links

--- elements/user/profile/friends.php
+++ elements/user/profile/friends.php
@@ -25,7 +25,7 @@
                             ?><br /><?php
 							if ( $friend->Hobbies() != "" ) { 
 								?><div style="overflow:hidden;width:90%;" title="Ενδιαφέροντα"><b>Ενδιαφέροντα:</b> <?php
-								$hobsar = explode( htmlspecialchars( $friend->Hobbies() ) );
+								$hobsar = explode( ",", htmlspecialchars( $friend->Hobbies() ) );
 								$hobbis = "";
 								foreach ( $hobsar as $hob ) {
 									$hobbis .= "<a href='?p=tag&text=" . $hob . "'>" . $hob . "</a>,";
