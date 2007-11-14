c76510c065c7b0ec3a48a6bbd91abe88401783b9
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-11-14 12:26:53 +0000

    Testing Delete

--- js/interesttag.js
+++ js/interesttag.js
@@ -68,7 +68,7 @@ var InterestTag = {
 			del.alt = "Διαγραφή";
 			del.title = "Διαγραφή";
 			del.onclick = (function( li ) {
-		//			InterestTag.Delete( li );
+					InterestTag.Delete( li );
 				})(li);
     		
 
