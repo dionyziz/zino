314257bb7bd1c59ecc6f00125d62cb5d7a367d4e
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-11-14 14:25:57 +0000

    Does delete work?

--- js/interesttag.js
+++ js/interesttag.js
@@ -117,6 +117,7 @@ var InterestTag = {
 		g( 'interdel_' + id ).style.display = 'none';
 	},
 	Delete : function( li ) {
+		alert( li.firstChild.nodeValue );
 		Coala.Warm( 'interesttag/delete', { 'text' : li.firstChild.nodeValue } );
 		li.parentNode.removeChild( li );
 	}
