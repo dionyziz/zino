ae279fa4ba6d43a71a3c8cf9b7b28762852887fb
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-11-15 20:33:20 +0000

    Valid InterestTag Text

--- elements/interesttag/_view.php
+++ elements/interesttag/_view.php
@@ -15,14 +15,15 @@
 		$page->SetTitle( 'Ενδιαφέροντα: ' . $text );
 		$page->AttachStyleSheet( 'css/rounded.css' );
 		
+		if ( !InterestTag_Valid( $text ) ) {
+			?><b>Παρακαλώ, το ενδιαφέρον να αποτελείται απο μία λέξη, χωρίς κενά ή κόμματα.</b><?php
+			return;
+		}
+		
 		$tags = InterestTag_List( $text, $offset, 20);
 		$all = InterestTag_Count();
 		if ( $all == 0 ) {
 			?><b>Λυπάμε, δεν υπάρχουν χρήστες με τέτοια ενδιαφέροντα</b><?php
-			return;
-		}
-		if ( !InterestTag_Valid( $text ) ) {
-			?><b>Παρακαλώ, το ενδιαφέρον να αποτελείται απο μία λέξη, χωρίς κενά ή κόμματα.</b><?php
 			return;
 		}
         
