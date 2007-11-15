c9fac97dd7a9382810109e50949ee1e5b105e9f3
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-11-15 15:50:42 +0000

    Some creation/editing frontend

--- js/interesttag.js
+++ js/interesttag.js
@@ -16,22 +16,35 @@ var InterestTag = {
     	ul.style.listStyleType = "none";
     	ul.style.textAlign = "left";
     	
-    	var close = document.createElement( 'a' );
+    	var close = d.createElement( 'a' );
 		close.onclick = function() {
 					Modals.Destroy();
 				};
 		close.className = "close";
-		close.onmouseover = function() {
-						document.body.style.cursor = "pointer";
-					};
-		close.onmouseout = function () {
-						document.body.style.cursor = "default";
-					};
+		close.style.cursor = "pointer";
+		close.alt = "Κλείσιμο";
+		close.title = "Κλείσιμο";
 		
-		var closeimg = document.createElement( 'img' );
+		var closeimg = d.createElement( 'img' );
 		closeimg.src = "http://static.chit-chat.gr/images/colorpicker/close.png";
-		closeimg.alt = "Κλείσιμο";
-		closeimg.title = "Κλείσιμο";
+		
+		var newtagimg = d.createElement( 'img' );
+		newtagimg.src = "http://static.chit-chat.gr/images/icons/page_new.gif";
+		
+		var newtag = d.createElement( 'a' );
+		newtag.style.cursor = "pointer";
+		newtag.style.position = "relative";
+		newtag.style.marginLeft = "-300px";
+		newtag.alt = "Νέο Ενδιαφέρον";
+		newtag.title = "Νέο Ενδιαφέρον";
+		newtag.onclick = ( function( ul ) {
+				return function() {
+					InterestTag.NewTag( ul );
+					return false;
+				}
+			} )( ul );
+			
+		newtag.appendChild( newtagimg );
 		
 		var count = 0;
     	
@@ -42,65 +55,71 @@ var InterestTag = {
     			continue;
     		}
     		
-    		var li = d.createElement( 'li' );
-    		li.appendChild( d.createTextNode( allinterests[i] ) ); 
-    		li.onmouseover = ( function( id ) {
-    				return function () {
-	    				InterestTag.showLinks( id );
-	    			};
-    			} )(count);
-    		li.onmouseout = ( function( id ) {
-    				return function() {
-    					InterestTag.hideLinks( id );
-    				};
-    			} )(count);
-    		
-    		var editimage = d.createElement( 'img' );
-			editimage.src = 'http://static.chit-chat.gr/images/icons/edit.png';
-			
-			var deleteimage = d.createElement( 'img' );
-			deleteimage.src = "http://static.chit-chat.gr/images/icons/delete.png";
-			
-			var edit = d.createElement( 'a' );
-			edit.id = "interedit_" + count;
-			edit.style.cursor = "pointer";
-			edit.style.display = "none";
-			edit.alt = "Επεξεργασία";
-			edit.title = "Επεξεργασία";
-			edit.onclick = ( function( li ) {
-					return function() {
-						InterestTag.Edit( li );
-					}
-				} )(li);
-	
-			var del = d.createElement( 'a' );
-			del.id = "interdel_" + count;
-			del.style.cursor = "pointer";
-			del.style.display = "none";
-			del.alt = "Διαγραφή";
-			del.title = "Διαγραφή";
-			del.onclick = (function( li ) {
-					return function() {
-						InterestTag.Delete( li );
-					};
-				})(li);
-			
-			edit.appendChild( editimage );
-			del.appendChild( deleteimage );
-			li.appendChild( d.createTextNode( ' ' ) );
-			li.appendChild( edit );
-			li.appendChild( d.createTextNode( ' ' ) );
-			li.appendChild( del );
+			var li = InterestTag.createLi( allinterests[i], count );
+			li.appendChild( d.createElement( 'br' ) );
 			ul.appendChild( li );
-			
 			++count;
 		}
 		close.appendChild( closeimg );
 		div.appendChild( closeimg );
 		div.appendChild( ul );
+		div.appendChild( newtag );
 		Modals.Create( div, 400, 270 );
     },
-    is_valid : function( text ) {
+	createLi : function ( text, count ) {
+		var li = d.createElement( 'li' );
+		li.appendChild( d.createTextNode( text ) ); 
+		li.onmouseover = ( function( id ) {
+				return function () {
+					InterestTag.showLinks( id );
+				};
+			} )(count);
+		li.onmouseout = ( function( id ) {
+				return function() {
+					InterestTag.hideLinks( id );
+				};
+			} )(count);
+		
+		var editimage = d.createElement( 'img' );
+		editimage.src = 'http://static.chit-chat.gr/images/icons/edit.png';
+		
+		var deleteimage = d.createElement( 'img' );
+		deleteimage.src = "http://static.chit-chat.gr/images/icons/delete.png";
+		
+		var edit = d.createElement( 'a' );
+		edit.id = "interedit_" + count;
+		edit.style.cursor = "pointer";
+		edit.style.display = "none";
+		edit.alt = "Επεξεργασία";
+		edit.title = "Επεξεργασία";
+		edit.onclick = ( function( li ) {
+				return function() {
+					InterestTag.Edit( li );
+				}
+			} )(li);
+
+		var del = d.createElement( 'a' );
+		del.id = "interdel_" + count;
+		del.style.cursor = "pointer";
+		del.style.display = "none";
+		del.alt = "Διαγραφή";
+		del.title = "Διαγραφή";
+		del.onclick = (function( li ) {
+				return function() {
+					InterestTag.Delete( li );
+				};
+			})(li);
+		
+		edit.appendChild( editimage );
+		del.appendChild( deleteimage );
+		li.appendChild( d.createTextNode( ' ' ) );
+		li.appendChild( edit );
+		li.appendChild( d.createTextNode( ' ' ) );
+		li.appendChild( del );
+		
+		return li;
+	},
+	is_valid : function( val ) {
     	if ( val.length === 0 || val.indexOf( ',' ) != -1 || val.indexOf( ' ' ) != -1 ) {
         	alert( "Δεν μπορείς να δημιουργήσεις κενό ενδιαφέρον ή να χρησιμοποιήσεις κόμμα (,) ή κενά" );
         	return false;
@@ -150,6 +169,7 @@ var InterestTag = {
 		input.type = "text";
 		input.value = text;
 		input.className = "bigtext";
+		input.style.width = "100px";
 		
 		var imageaccept = document.createElement( 'img' );
 		imageaccept.src = 'http://static.chit-chat.gr/images/icons/accept.png';
@@ -189,5 +209,63 @@ var InterestTag = {
 		form.appendChild( editcancel );
 		form.appendChild( d.createElement( 'br' ) );
 		li.parentNode.insertBefore( form, li );
-	}
+	},
+	NewTag : function( ul ) {
+		if( InterestTag.onedit ) {
+			return;
+		}
+		InterestTag.onedit = true;
+		var newid = ul.childNodes[ ul.childNodes.length-1 ].getElementsByTagName( 'a' )[ 0 ].id;
+		newid = parseInt( newid.substr( 10 ) );
+		++newid;
+		
+		var li = InterestTag.createLi( '', newid );
+	
+		var form = d.createElement( 'form' );
+		
+		var input = d.createElement( 'input' );
+		input.type = "text";
+		input.className = "bigtext";
+		input.value = "Νέο Ενδιαφέρον";
+		input.style.width = "100px";
+		input.focus();
+		
+		var imageaccept = d.createElement( 'img' );
+		imageaccept.src = 'http://static.chit-chat.gr/images/icons/accept.png';
+		
+		var imagecancel = d.createElement( 'img' );
+		imagecancel.src = 'http://static.chit-chat.gr/images/icons/cancel.png';
+		
+		var editsubmit = d.createElement( 'a' );
+		editsubmit.style.cursor = 'pointer';
+		editsubmit.onclick = function () {
+					alert( "Ginete neo tag" );
+					InterestTag.onedit = false;
+				};
+		editsubmit.alt = 'Δημιουργία';
+		editsubmit.title = 'Δημιουργία';
+		editsubmit.appendChild( imageaccept );
+		
+		var editcancel = d.createElement( 'a' );
+		editcancel.style.cursor = 'pointer';
+	/*	editcancel.onclick = ( function ( li ) {
+				return function() {
+					li.parentNode.removeChild( form );
+					li.style.display = "inline";
+					InterestTag.onedit = false;
+				}
+			} )( li );*/
+		editcancel.alt = 'Ακύρωση';
+		editcancel.title = 'Ακύρωση';
+		editcancel.appendChild( imagecancel );
+		
+		form.appendChild( input );
+		form.appendChild( d.createTextNode( ' ' ) );
+		form.appendChild( editsubmit );
+		form.appendChild( d.createTextNode( ' ' ) );
+		form.appendChild( editcancel );
+		form.appendChild( d.createElement( 'br' ) );
+		ul.appendChild( li );
+		ul.insertBefore( form, li );
+	}	
 };
