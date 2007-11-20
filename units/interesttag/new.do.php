317d67004f71ef1fb77f4070978b3591429481ff
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-11-20 18:07:40 +0000

    Things strangely stopped working

--- units/interesttag/new.do.php
+++ units/interesttag/new.do.php
@@ -3,11 +3,13 @@
     function UnitInterestTagNew( tString $text ) {
         global $libs;
         global $user;
+        global $water;
 
 		$libs->Load( 'interesttag' );
 
 		$text = $text->Get();
 		if ( !InterestTag_Valid( $text ) ) {
+			$water->Trace( "Not valid text" );
 			return;
 		}
 
