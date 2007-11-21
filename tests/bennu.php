8d72fe6003db84a35dcd48781b8363eaf5a25e54
Aleksis Brezas <abresas@gmail.com>
2007-11-21 18:33:18 +0000

    bennu testcases

--- tests/bennu.php
+++ tests/bennu.php
@@ -31,7 +31,7 @@
             $list = $bennu->Get( 20 );
 
             $this->Assert( is_array( $list ), 'List returned from Bennu::Get is not an array' );
-            $this->AssertFalse( empty( $list ), 'List returned from Bennu without rules is empty' ); // well, no users? duh.
+            $this->AssertFalse( empty( $list ), 'List returned from Bennu without rules is empty' ); // no users? duh.
             
             if ( User_Count() >= 20 ) {
                 $this->AssertEquals( count( $list ), 20, 'Bennu without rules did not return the number of users requested' );
@@ -39,6 +39,71 @@
 
             $list = $bennu->Get( User_Count() + 10 );
             $this->AssertEquals( count( $list ), User_Count(), 'Bennu should return all the users when the number of users requested are equal to or more than the number of all users' );
+        }
+        public function TestExclude() {
+            $bennu = New Bennu();
+
+            $bennu->Exclude( new User( 1 ) );
+            $bennu->Exclude( new User( 1 ) );
+            $bennu->Exclude( new User( 2 ) );
+            $bennu->Exclude( new User( 3 ) );
+            $bennu->Exclude( new User( 5 ) );
+            $bennu->Exclude( new User( 8 ) );
+            $bennu->Exclude( new User( 13 ) );
+            $bennu->Exclude( new User( 21 ) );
+            $bennu->Exclude( new User( 34 ) );
+
+            $excluded = array( 1, 2, 3, 5, 8, 13, 21, 34 );
+
+            $users = $bennu->Get(); // everyone
+
+            foreach ( $users as $user ) {
+                $this->Assert( in_array( $user->Id, $excluded ), 'User excluded but he is still in the list returned by Bennu::Get' );
+            }
+        }
+        public function TestOneRule() {
+            $bennu = New Bennu();
+            
+            $age = New BennuRuleAge();
+            $age->Value = 16;
+            $age->Sigma = 2;
+            $age->Score = 10;
+
+            $bennu->AddRule( $age );
+
+            $list = $bennu->Get( 20 );
+
+            $diff = 0;
+            foreach ( $list as $user ) {
+                $curdiff = abs( $user->Age - 16 );
+                $this->Assert( $curdiff >= $diff, 'Bennu did not return the users on the right order when one rule was added' );
+                $diff = $curdiff;
+            }
+        }
+        public function TestTwoRules() {
+            $bennu = New Bennu();
+
+            $now = time();
+
+            $creation = New BennuRuleCreation();
+            $creation->Value = $now;
+            $creation->Sigma = $now - 60 * 60 * 24 * 7 * 2;
+            $creation->Score = 10000;
+
+            $random = New BennuRuleRandom();
+            $random->Score = 1;
+            
+            $bennu->AddRule( $creation );
+            $bennu->AddRule( $random );
+
+            $users = $bennu->Get( 20 );
+
+            $diff = 0;
+            foreach ( $user as $user ) {
+                $curdiff = abs( strtotime( $user->Creation ) - $now );
+                $this->Assert( $curdiff >= $diff, 'Bennu did not return the users on the right order when two rules were added' );
+                $diff = $curdiff;
+            }
         }
     }
 
