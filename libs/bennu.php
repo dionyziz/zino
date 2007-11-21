fd16270b27f8b903784e852b50fde8472ff6f8ab
Aleksis Brezas <abresas@gmail.com>
2007-11-21 18:37:03 +0000

    bennu lib

---
+++ libs/bennu.php
@@ -0,0 +1,34 @@
+<?php
+
+    class Bennu {
+    }
+
+    abstract class BennuRule {
+    }
+
+    /* default bennu rules */
+
+    class BennuRuleSex extends BennuRule {
+    }
+
+    class BennuRuleAge extends BennuRule {
+    }
+
+    class BennuRuleCreation extends BennuRule {
+    }
+
+    class BennuRulePhotos extends BennuRule {
+    }
+
+    class BennuRuleLocation extends BennuRule {
+    }
+
+    class BennuRuleFriends extends BennuRule {
+    }
+
+    class BennuRuleLastActive extends BennuRule {
+    }
+
+    class BennuRuleRandom extends BennuRule {
+    }
+?>
