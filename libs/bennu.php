44d59bfbe9f65e80c88504cb2b93ea09abf7c8d1
Aleksis Brezas <abresas@gmail.com>
2007-11-22 15:34:47 +0000

    bennu lib

--- libs/bennu.php
+++ libs/bennu.php
@@ -43,7 +43,7 @@
         }
         public function Get( $limit ) {
             for ( $i = 0; $i < count( $this->mUsers ); ++$i ) {
-                $user = $this->mUsers[ i ];
+                $user = $this->mUsers[ $i ];
 
                 if ( in_array( $user->Id(), $this->mExclude ) ) {
                     unset( $this->mUsers[ $i ] );
