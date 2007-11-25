f1a380087ebf01e1d6d206b664c4038ce3102755
Aleksis Brezas <abresas@gmail.com>
2007-11-25 19:30:18 +0000

    new search

--- phoenix/elements/developer/abresas/_search.php
+++ phoenix/elements/developer/abresas/_search.php
@@ -1,27 +1,29 @@
 <?php
 
-    global $libs;
+    function ElementDeveloperAbresasSearch() {
+        global $libs;
 
-    $libs->Load( 'search' );
+        $libs->Load( 'search' );
 
-    $comments = new CommentsSearch;
-    $comments->TypeId   = 1;
-    $comments->Page     = $theuser;
-    $comments->DelId    = 0;
+        $comments = new CommentsSearch;
+        $comments->TypeId   = 1;
+        $comments->Page     = $theuser;
+        $comments->DelId    = 0;
 
-    //$comments->OrderBy  = array( 'date', 'DESC' );
+        //$comments->OrderBy  = array( 'date', 'DESC' );
 
-    /*
-    if ( $oldcomments ) {
-        $comments->Limit = 10000;
+        /*
+        if ( $oldcomments ) {
+            $comments->Limit = 10000;
+        }
+        else {
+            $comments->Limit = 50;
+        }
+        */
+
+        $comments = $comments->Get();
+
+        print_r( $comments );
     }
-    else {
-        $comments->Limit = 50;
-    }
-    */
-
-    $comments = $comments->Get();
-
-    print_r( $comments );
 
 ?>
