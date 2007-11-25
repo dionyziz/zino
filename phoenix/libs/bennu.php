29336016d5a9950e2f333509c98e6442cad21291
Aleksis Brezas <abresas@gmail.com>
2007-11-25 07:54:46 +0000

    bennu test element

--- phoenix/libs/bennu.php
+++ phoenix/libs/bennu.php
@@ -71,8 +71,8 @@
             w_assert( count( $this->mUids ) == count( $this->mScores ) );
             array_multisort( $this->mScores, SORT_DESC, SORT_NUMERIC, $this->mUids, SORT_DESC, SORT_STRING );
             
-            $water->Trace( "bennu uids", array_slice( $this->mUids, 0, 50 ) );
-            $water->Trace( "bennu scores", array_slice( $this->mScores, 0, 50 ) );
+            $water->Trace( "bennu uids", array_slice( $this->mUids, 0, 20 ) );
+            $water->Trace( "bennu scores", array_slice( $this->mScores, 0, 20 ) );
 
             if ( $limit > count( $this->mUids ) || $limit === false ) {
                $limit = count( $this->mUids );
