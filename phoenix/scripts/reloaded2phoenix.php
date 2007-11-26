2b77c647f0de4c6eca9dd78d834473399973da0d
Dionysis Zindros <dionyziz@gmail.com>
2007-11-26 09:09:41 +0000

    banner mockup, cleanup old images

--- phoenix/scripts/reloaded2phoenix.php
+++ phoenix/scripts/reloaded2phoenix.php
@@ -1,11 +1,17 @@
 <?php
     $sql = array(
+        // comments
         "ALTER TABLE 
             `merlin_comments` 
         CHANGE 
             `comment_storyid` `comment_itemid` INT( 11 ) NOT NULL DEFAULT '0';", 
         "ALTER TABLE `merlin_comments` 
             DROP `comment_stars`,
-            DROP `comment_votes`;"
+            DROP `comment_votes`;",
+        "ALTER TABLE `merlin_comments` 
+            DROP `comment_textraw`",
+        "ALTER TABLE `merlin_comments` 
+        CHANGE 
+            `comment_text` `comment_bulkid` INT NOT NULL"
     );
 ?>
