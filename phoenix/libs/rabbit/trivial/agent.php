181fd8e4e7546ab8493511c497639aa06c19140e
Aleksis Brezas <abresas@gmail.com>
2007-11-29 13:53:52 +0000

    32-bit IPs

--- phoenix/libs/rabbit/trivial/agent.php
+++ phoenix/libs/rabbit/trivial/agent.php
@@ -40,12 +40,14 @@
 	}
 
 	function UserIp() {
-		if ( isset($_SERVER["HTTP_CLIENT_IP"]) ) {
-			return $_SERVER["HTTP_CLIENT_IP"];
-		} else {
-			return $_SERVER["REMOTE_ADDR"];
-		}	
+		if ( isset( $_SERVER["HTTP_CLIENT_IP"] ) ) {
+			return ip2long( $_SERVER["HTTP_CLIENT_IP"] );
+		} 
+        else {
+			return ip2long( $_SERVER["REMOTE_ADDR"] );
+		}
 	}
+
 	function BrowserByUseragent( $useragent ) {
 		$browser = array (
 			"MSIE",            // parent
