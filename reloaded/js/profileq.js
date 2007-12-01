e5ea7196f20d13ea889d215182533ef321c7e8d4
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-12-01 22:52:12 +0000

    A minor fix

--- reloaded/js/profileq.js
+++ reloaded/js/profileq.js
@@ -212,6 +212,9 @@ var Profileq = {
 
 	},
     ShowNewQuestion : function ( id, question, preid ) {
+    	if ( g( 'newquest' ) === null ) {
+    		return;
+    	}
         var prequestion = g( 'qedit_' + preid );
         var father = prequestion.parentNode.parentNode;
 
