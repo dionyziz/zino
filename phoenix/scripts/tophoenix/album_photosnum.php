031d1883435b935b9274c355f807c99fb53d2706
Aleksis Brezas <abresas@gmail.com>
2007-12-03 00:58:12 +0000

    tophoenix scripts

--- phoenix/scripts/tophoenix/album_photosnum.php
+++ phoenix/scripts/tophoenix/album_photosnum.php
@@ -7,77 +7,65 @@
         It was calculated by counting the number of photos from the database,
         but now it is a database field (album_numphotos).
 
-        Run this script when the new version - Phoenix - is released.
+        Run the ToPhoenix_ function once when the new version - Phoenix - is released.
     */
-	
-    set_include_path( '../:./' );
-	
-	global $page;
-	global $libs;
-    global $user;
-    
-	require_once 'libs/rabbit/rabbit.php';
-    Rabbit_Construct( 'HTML' );
-
-    $req = $_GET;
-    
-	Rabbit_ClearPostGet();
-
-    if ( !$user->CanModifyCategories() ) {
-        return false;
-    }
-   
-    $libs->Load( 'album' );
 
     function Albums_GetPhotosNum() {
-		global $db;
-		global $images;
-		
-		if ( !is_array( $keys ) ) {
-			$keys = array( $keys );
-		}
-		
-		foreach( $keys as $i => $key ) {
-			$keys[ $i ] = myescape( $key );
-		}
-		
-		$sql = "SELECT
-					`image_albumid`, COUNT( * ) AS numphotos
-				FROM 
-					`$images`
-				WHERE	
-					`image_delid` = '0'
-				GROUP BY
-					`image_albumid`;";
-					
-		$res = $db->Query( $sql );
-		
-		$ret = array();
-		while ( $row = $res->FetchArray() ) {
-			$ret[ $row[ 'image_albumid' ] ] = $row[ 'numphotos' ];
-		}
+        global $db;
+        global $images;
+        
+        if ( !is_array( $keys ) ) {
+            $keys = array( $keys );
+        }
+        
+        foreach( $keys as $i => $key ) {
+            $keys[ $i ] = myescape( $key );
+        }
+        
+        $sql = "SELECT
+                    `image_albumid`, COUNT( * ) AS numphotos
+                FROM 
+                    `$images`
+                WHERE	
+                    `image_delid` = '0'
+                GROUP BY
+                    `image_albumid`;";
+                    
+        $res = $db->Query( $sql );
+        
+        $ret = array();
+        while ( $row = $res->FetchArray() ) {
+            $ret[ $row[ 'image_albumid' ] ] = $row[ 'numphotos' ];
+        }
 
-		return $ret;
-	}
+        return $ret;
+    }
 
-    ?>Fetching number of photos for all albums...<?php
+    function ToPhoenix_AlbumPhotosNum() {
+        global $libs;
 
-    $photosnum = Albums_GetPhotosNum();
+        $libs->Load( 'album' );
 
-    ?>OK.<br /><br /><?php
 
-    foreach ( $photosnum as $id => $num ) {
-        ?>Updating album <?php
-        echo $id;
-        ?>....<?php
+        ?>Fetching number of photos for all albums...<?php
 
-        $album = new Album( $id );
-        $album->PhotosNum = $num;
-        $album->Save();
+        $photosnum = Albums_GetPhotosNum();
 
-        ?>OK (<?php
-        echo $num;
-        ?>).<br /><?php
+        ?>OK.<br /><br /><?php
+
+        foreach ( $photosnum as $id => $num ) {
+            ?>Updating album <?php
+            echo $id;
+            ?>....<?php
+
+            $album = new Album( $id );
+            $album->PhotosNum = $num;
+            $album->Save();
+
+            ?>OK (<?php
+            echo $num;
+            ?>).<br /><?php
+        }
     }
 
 ?>
--- phoenix/scripts/tophoenix/ip.php
+++ phoenix/scripts/tophoenix/ip.php
@@ -1,22 +1,37 @@
 <?php
 
-$sql = "ALTER TABLE `merlin_albums` CHANGE `album_submithost` `album_submithost` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_comments` CHANGE `comment_userip` `comment_userip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_faqcategories` CHANGE `faqcategory_creatorip` `faqcategory_creatorip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_faqquestions` CHANGE `faqquestion_creatorip` `faqquestion_creatorip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_friendrel` CHANGE `frel_creatorip` `frel_creatorip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_images` CHANGE `image_userip` `image_userip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_ipban` CHANGE `ipban_ip` `ipban_ip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_logs` CHANGE `log_host` `log_host` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_places` CHANGE `place_updateip` `place_updateip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_pms` CHANGE `pm_userip` `pm_userip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_profileq` CHANGE `profileq_userip` `profileq_userip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_revisions` CHANGE `revision_creatorip` `revision_creatorip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_searches` CHANGE `search_userip` `search_userip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_shoutbox` CHANGE `shout_userip` `shout_userip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_starring` CHANGE `starring_userip` `starring_userip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_templates` CHANGE `template_updateip` `template_updateip` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_users` CHANGE `user_registerhost` `user_registerhost` INT NOT NULL;";
-$sql = "ALTER TABLE `merlin_usershout` CHANGE `usershout_userip` `usershout_userip` INT NOT NULL;";
+   
+    function ToPhoenix_Ip() {
+        global $db;
+
+        $queries = array( 
+            "albums" => "ALTER TABLE `merlin_albums` CHANGE `album_submithost` `album_submithost` INT NOT NULL;",
+            "comments" => "ALTER TABLE `merlin_comments` CHANGE `comment_userip` `comment_userip` INT NOT NULL;",
+            "faq categories" => "ALTER TABLE `merlin_faqcategories` CHANGE `faqcategory_creatorip` `faqcategory_creatorip` INT NOT NULL;",
+            "faq questions" => "ALTER TABLE `merlin_faqquestions` CHANGE `faqquestion_creatorip` `faqquestion_creatorip` INT NOT NULL;",
+            "friend relations" => "ALTER TABLE `merlin_friendrel` CHANGE `frel_creatorip` `frel_creatorip` INT NOT NULL;",
+            "images" => "ALTER TABLE `merlin_images` CHANGE `image_userip` `image_userip` INT NOT NULL;",
+            "ip bans" => "ALTER TABLE `merlin_ipban` CHANGE `ipban_ip` `ipban_ip` INT NOT NULL;",
+            "logs" => "ALTER TABLE `merlin_logs` CHANGE `log_host` `log_host` INT NOT NULL;",
+            "places" => "ALTER TABLE `merlin_places` CHANGE `place_updateip` `place_updateip` INT NOT NULL;",
+            "pms" => "ALTER TABLE `merlin_pms` CHANGE `pm_userip` `pm_userip` INT NOT NULL;",
+            "profile questions" => "ALTER TABLE `merlin_profileq` CHANGE `profileq_userip` `profileq_userip` INT NOT NULL;",
+            "revisions" => "ALTER TABLE `merlin_revisions` CHANGE `revision_creatorip` `revision_creatorip` INT NOT NULL;",
+            "searches" => "ALTER TABLE `merlin_searches` CHANGE `search_userip` `search_userip` INT NOT NULL;",
+            "shoutbox" => "ALTER TABLE `merlin_shoutbox` CHANGE `shout_userip` `shout_userip` INT NOT NULL;",
+            "starring" => "ALTER TABLE `merlin_starring` CHANGE `starring_userip` `starring_userip` INT NOT NULL;",
+            "templates" => "ALTER TABLE `merlin_templates` CHANGE `template_updateip` `template_updateip` INT NOT NULL;",
+            "users" => "ALTER TABLE `merlin_users` CHANGE `user_registerhost` `user_registerhost` INT NOT NULL;",
+            "usershout" => "ALTER TABLE `merlin_usershout` CHANGE `usershout_userip` `usershout_userip` INT NOT NULL;"
+        );
+
+        foreach ( $queries as $table => $sql ) {
+            ?>Altering <?php
+            echo $table;
+            ?> table.... <?php
+            $db->Query( $sql );
+            ?> OK.<?php
+        }
+    }
 
 ?>
