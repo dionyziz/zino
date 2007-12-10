44329b5653bf775751bbca2036f3232133ab60ae
Aleksis Brezas <abresas@gmail.com>
2007-12-10 05:25:41 +0000

    search list comments

--- phoenix/elements/comment/list.php
+++ phoenix/elements/comment/list.php
@@ -4,7 +4,7 @@
 			return;
 		}
 		foreach ( $comments[ $root ] as $comment ) {
-			Element( 'comment/view' , $comment , $indent , isset( $comments[ $comment->Id() ] ) ? count( $comments[ $comment->Id ] ) : 0 );
+			Element( 'comment/view' , $comment , $indent , isset( $comments[ $comment->Id ] ) ? count( $comments[ $comment->Id ] ) : 0 );
 			Element( 'comment/list' , $comments , $comment->Id , $indent + 1 ); // RECURSE!
 		}
 	}
