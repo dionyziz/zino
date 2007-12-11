8462f0bd54130a79c1bf2995beaac4b2a852d692
Aleksis Brezas <abresas@gmail.com>
2007-12-11 18:43:18 +0000

    search test page

---
+++ phoenix/elements/search.php
@@ -0,0 +1,38 @@
+<?php
+
+    function ElementDeveloperAbresasSearch() {
+        global $libs;
+        global $user;
+
+        $libs->Load( 'comment' );
+
+        $comments = new CommentsSearch;
+        $comments->TypeId   = 1;
+        $comments->ItemId   = 832;
+        $comments->DelId    = 0;
+
+        //$comments->OrderBy  = array( 'date', 'DESC' );
+
+        /*
+        if ( $oldcomments ) {
+            $comments->Limit = 10000;
+        }
+        else {
+            $comments->Limit = 50;
+        }
+        */
+
+        $comments = $comments->GetParented();
+    
+        foreach ( $comments as $comment ) {
+            ?>[ <?php
+            $comment->Id;
+            ?> <?php
+            $comment->User->Username();
+            ?> <?php
+            $comment->Since;
+            ?> ]<?php
+        }
+    }
+
+?>
