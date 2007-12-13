41f852f66b3c9b454af585f16110136721f4fceb
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-12-13 10:53:06 +0000

    LoginAttempt

--- reloaded/libs/loginattempt.php
+++ reloaded/libs/loginattempt.php
@@ -9,20 +9,18 @@
 		
 		w_assert( is_string( $ip ), 'LoginAttempt_checkBot accepts only a string argument' );
 		
-		$now = NowDate();
-		
 		$sql = "SELECT * FROM `$loginattempt`
 				WHERE `loginattempt_ip` = '" . $ip . "'
-				AND `loginattempt_time` >= ( '$now' - INTERVAL 15 MINUTE )
+				AND `loginattempt_time` >= ( NOW() - INTERVAL 15 MINUTE )
 				AND `loginattempt_success` = '0'
 				LIMIT 3;";
 				
 		$res = $db->Query( $sql );
 		
 		if ( $res->NumRows() != 3 ) {
-			return true;
+			return false;
 		}
-		return false;
+		return true;
 	}	
 
 	final class LoginAttempt extends Satori {
