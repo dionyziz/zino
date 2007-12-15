ea48cc77417ec0eae9a6be1cf5c4e1bf50f426fd
Aleksis Brezas <abresas@gmail.com>
2007-12-15 08:38:15 +0000

    new search

--- phoenix/libs/_search.php
+++ phoenix/libs/_search.php
@@ -414,7 +414,7 @@
 
             $water->Trace( "Query: " . $this->mQuery );
 
-            $res = $db->Query( $this->mQuery )->ToObjectsArray( $prototype->GetClass() );
+            return $db->Query( $this->mQuery )->ToObjectsArray( $prototype->GetClass() );
         }
         public function Search() {
             $this->mConnections = array();
