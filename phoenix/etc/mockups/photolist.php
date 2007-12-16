ff86b159049e7818ac363816097b60765170dc74
Kostis Roussopoulos <kostis90gr@gmail.com>
2007-12-16 19:44:14 +0000

    Photolist

--- phoenix/etc/mockups/photolist.php
+++ phoenix/etc/mockups/photolist.php
@@ -11,8 +11,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph1.jpg" alt="Ουρανοξύστης 1" title="Ουρανοξύστης 1" />
-					<span>ωραίος ουρανοξύστης</span>
+					<img src="images/ph1.jpg" alt="Ουρανοξύστης 1" title="Ουρανοξύστης 1" /><br />
+					ωραίος ουρανοξύστης
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -23,8 +23,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph2.jpg" alt="Ουρανοξύστης 2" title="Ουρανοξύστης 2" />
-					<span>ουρανοξύστης με αντανάκλαση ηλίου</span>
+					<img src="images/ph2.jpg" alt="Ουρανοξύστης 2" title="Ουρανοξύστης 2" /><br />
+					ουρανοξύστης με αντανάκλαση ηλίου
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -35,8 +35,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph3.jpg" alt="Ουρανοξύστης 3" title="Ουρανοξύστης 3" />					
-					<span>2 skyscrapers</span>
+					<img src="images/ph3.jpg" alt="Ουρανοξύστης 3" title="Ουρανοξύστης 3" /><br />					
+					2 skyscrapers
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -47,8 +47,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph4.jpg" alt="Ουρανοξύστης 4" title="Ουρανοξύστης 4" />
-					<span>kaboom</span>
+					<img src="images/ph4.jpg" alt="Ουρανοξύστης 4" title="Ουρανοξύστης 4" /><br />
+					kaboom
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -59,8 +59,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph5.jpg" alt="Ουρανοξύστης 5" title="Ουρανοξύστης 5" />
-					<span>γαμάτο</span>
+					<img src="images/ph5.jpg" alt="Ουρανοξύστης 5" title="Ουρανοξύστης 5" /><br />
+					γαμάτο
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -71,8 +71,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph6.jpg" alt="Ουρανοξύστης 6" title="Ουρανοξύστης 6" />
-					<span>μαύρη μαυρίλα</span>
+					<img src="images/ph6.jpg" alt="Ουρανοξύστης 6" title="Ουρανοξύστης 6" /><br />
+					μαύρη μαυρίλα
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -83,8 +83,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph7.jpg" alt="Ουρανοξύστης 7" title="Ουρανοξύστης 7" />
-					<span>πορωτικό ε;;</span>
+					<img src="images/ph7.jpg" alt="Ουρανοξύστης 7" title="Ουρανοξύστης 7" /><br />
+					πορωτικό ε;;
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -95,8 +95,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph8.jpg" alt="Ουρανοξύστης 8" title="Ουρανοξύστης 8" />
-					<span>μια πανέμορφη εικόνα</span>
+					<img src="images/ph8.jpg" alt="Ουρανοξύστης 8" title="Ουρανοξύστης 8" /><br />
+					μια πανέμορφη εικόνα
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -107,8 +107,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph9.jpg" alt="Ουρανοξύστης 9" title="Ουρανοξύστης 9" />
-					<span>και δίπλα σε ποτάμι</span>
+					<img src="images/ph9.jpg" alt="Ουρανοξύστης 9" title="Ουρανοξύστης 9" /><br />
+					και δίπλα σε ποτάμι
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -119,8 +119,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph10.jpg" alt="Ουρανοξύστης 10" title="Ουρανοξύστης 10" />
-					<span>ωραία σύννεφα</span>
+					<img src="images/ph10.jpg" alt="Ουρανοξύστης 10" title="Ουρανοξύστης 10" /><br />
+					ωραία σύννεφα
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -131,8 +131,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph11.jpg" alt="Ουρανοξύστης 11" title="Ουρανοξύστης 11" />
-					<span>σαν 2 πόδια</span>
+					<img src="images/ph11.jpg" alt="Ουρανοξύστης 11" title="Ουρανοξύστης 11" /><br />
+					σαν 2 πόδια
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
@@ -143,8 +143,8 @@
 		<li>
 			<div class="photo">
 				<a href="">
-					<img src="images/ph12.jpg" alt="Ουρανοξύστης 12" title="Ουρανοξύστης 12" />
-					<span>στο Dubai, απλά γαμάει</span>
+					<img src="images/ph12.jpg" alt="Ουρανοξύστης 12" title="Ουρανοξύστης 12" /><br />
+					στο Dubai, απλά γαμάει
 				</a>
 				<dl>
 					<dt class="addfav"><a href=""><img src="images/heart_add.png" alt="Προσθήκη στα αγαπημένα" title="Προσθήκη στα αγαπημένα" /></a></dt>
