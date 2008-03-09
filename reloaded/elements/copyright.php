5295a307e2411ec5091de775a03fc5c20a7ed16b
Dionysis Zindros <dionyziz@gmail.com>
2008-03-09 21:22:37 +0000

    2007 -> 2008 copyright

--- reloaded/elements/copyright.php
+++ reloaded/elements/copyright.php
@@ -5,6 +5,6 @@
 		
 		?><div class="copy"><?php
 		echo $rabbit_settings[ 'applicationname' ];
-		?> <span>&copy; 2007 <a href="http://www.kamibu.com/">Kamibu</a></span></div><?php
+		?> <span>&copy; 2008 <a href="http://www.kamibu.com/">Kamibu</a></span></div><?php
 	}
 ?>
