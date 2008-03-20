046be12b0a458786b01073ebb14cf1cb5fb9d0f4
Chris Pappas <chrispappas12@gmail.com>
2008-03-20 22:50:14 +0000

    frontpage mockup

--- phoenix/etc/mockups/frontpage.php
+++ phoenix/etc/mockups/frontpage.php
@@ -67,10 +67,9 @@
 				</form>
 			</div>
 		</div>
-		<div class="eof"></div>
-		<?php
-	} ?>
-	<div class="shoutbox">
+		<div class="eof"></div><?php
+	} 
+	?><div class="shoutbox">
 		<h2>Συζήτηση</h2>
 		<div class="comments">
 			<div class="comment newcomment">
@@ -170,6 +169,146 @@
 			<h2>Συνέβησαν πρόσφατα</h2>
 			<div class="list">
 				<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+				<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+				<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+				<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+				<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+								<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+								<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+								<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+								<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+								<div class="event">
+					<div class="toolbox">
+						<span class="time">πριν λίγο</span>
+					</div>
+					<div class="who">
+						<a href="http://morvena.zino.gr">
+							<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+							Morvena
+						</a> έγραψε σχόλιο στο ημερολόγιο
+					</div>
+					<div class="subject">
+						<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+					</div>					
+				</div>
+								<div class="event">
 					<div class="toolbox">
 						<span class="time">πριν λίγο</span>
 					</div>
