64ee558ef8b2d1ed738d195c61e465b9784af374
Chris Pappas <chrispappas12@gmail.com>
2008-03-20 22:48:29 +0000

    frontpage mockup

--- phoenix/etc/mockups/frontpage.php
+++ phoenix/etc/mockups/frontpage.php
@@ -164,81 +164,6 @@
 				</div>
 			</div>	
 		</div>
-	</div>
-	<div class="latestcomments">
-		<h2>Νεότερα σχόλια</h2>
-		<div class="list">
-			<div class="event">
-				<div class="toolbox">
-					<span class="time">πριν λίγο</span>
-				</div>
-				<div class="who">
-					<a href="http://morvena.zino.gr">
-						<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
-						Morvena
-					</a> έγραψε σχόλιο στο ημερολόγιο
-				</div>
-				<div class="subject">
-					<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
-				</div>
-			</div>
-			<div class="event">
-				<div class="toolbox">
-					<span class="time">πριν λίγο</span>
-				</div>
-				<div class="who">
-					<a href="http://teddy.zino.gr">
-						<img src="images/avatars/teddy.jpg" class="avatar" alt="morvena" />
-						Teddy
-					</a> έγραψε σχόλιο στην εικόνα
-				</div>
-				<div class="subject">
-					<a href="#"><img src="images/dodge.jpg" alt="dodge" /></a>
-				</div>
-			</div>
-			<div class="event">
-				<div class="toolbox">
-					<span class="time">πριν 1 λεπτό</span>
-				</div>
-				<div class="who">
-					<a href="http://elenh.zino.gr">
-						<img src="images/avatars/elenh.jpg" class="avatar" alt="morvena" />
-						_El3nh_
-					</a> έγραψε σχόλιο στη δημοσκόπηση 
-				</div>
-				<div class="subject">
-					<a href="#">All star, Vans ή DC?</a>
-				</div>
-			</div>
-			<div class="event">
-				<div class="toolbox">
-					<span class="time">πριν 1 λεπτό</span>
-				</div>
-				<div class="who">
-					<a href="http://dionyziz.zino.gr">
-						<img src="images/avatars/dionyziz.jpg" class="avatar" alt="morvena" />
-						dionyziz
-					</a> έγραψε σχόλιο στην εικόνα
-				</div>
-				<div class="subject">
-					<a href="#"><img src="images/paris.jpg" alt="paris" /></a>
-				</div>
-			</div>
-			<div class="event">
-				<div class="toolbox">
-					<span class="time">πριν 3 λεπτά</span>
-				</div>
-				<div class="who">
-					<a href="http://izual.zino.gr">
-						<img src="images/avatars/izual.jpg" class="avatar" alt="morvena" />
-						izual
-					</a> έγραψε σχόλιο στo προφίλ της 
-				</div>
-				<div class="subject">
-					<a href="#"><img src="images/avatars/elsa.jpg" alt="elsa" /></a>
-				</div>
-			</div>
-		</div>
 	</div><?php
 	if ( $loggedin) {
 		?><div class="latestevents">
@@ -317,7 +242,82 @@
 			</div>
 		</div><?php
 	}
-	?><div class="eof"></div>
+	?>
+	<div class="latestcomments">
+		<h2>Νεότερα σχόλια</h2>
+		<div class="list">
+			<div class="event">
+				<div class="toolbox">
+					<span class="time">πριν λίγο</span>
+				</div>
+				<div class="who">
+					<a href="http://morvena.zino.gr">
+						<img src="images/avatars/morvena.jpg" class="avatar" alt="morvena" />
+						Morvena
+					</a> έγραψε σχόλιο στο ημερολόγιο
+				</div>
+				<div class="subject">
+					<a href="#">Βάζω τα φτερά μου και το παίζω πεταλούδος</a>
+				</div>
+			</div>
+			<div class="event">
+				<div class="toolbox">
+					<span class="time">πριν λίγο</span>
+				</div>
+				<div class="who">
+					<a href="http://teddy.zino.gr">
+						<img src="images/avatars/teddy.jpg" class="avatar" alt="morvena" />
+						Teddy
+					</a> έγραψε σχόλιο στην εικόνα
+				</div>
+				<div class="subject">
+					<a href="#"><img src="images/dodge.jpg" alt="dodge" /></a>
+				</div>
+			</div>
+			<div class="event">
+				<div class="toolbox">
+					<span class="time">πριν 1 λεπτό</span>
+				</div>
+				<div class="who">
+					<a href="http://elenh.zino.gr">
+						<img src="images/avatars/elenh.jpg" class="avatar" alt="morvena" />
+						_El3nh_
+					</a> έγραψε σχόλιο στη δημοσκόπηση 
+				</div>
+				<div class="subject">
+					<a href="#">All star, Vans ή DC?</a>
+				</div>
+			</div>
+			<div class="event">
+				<div class="toolbox">
+					<span class="time">πριν 1 λεπτό</span>
+				</div>
+				<div class="who">
+					<a href="http://dionyziz.zino.gr">
+						<img src="images/avatars/dionyziz.jpg" class="avatar" alt="morvena" />
+						dionyziz
+					</a> έγραψε σχόλιο στην εικόνα
+				</div>
+				<div class="subject">
+					<a href="#"><img src="images/paris.jpg" alt="paris" /></a>
+				</div>
+			</div>
+			<div class="event">
+				<div class="toolbox">
+					<span class="time">πριν 3 λεπτά</span>
+				</div>
+				<div class="who">
+					<a href="http://izual.zino.gr">
+						<img src="images/avatars/izual.jpg" class="avatar" alt="morvena" />
+						izual
+					</a> έγραψε σχόλιο στo προφίλ της 
+				</div>
+				<div class="subject">
+					<a href="#"><img src="images/avatars/elsa.jpg" alt="elsa" /></a>
+				</div>
+			</div>
+		</div>
+	</div><div class="eof"></div>
 	<div class="nowonline">
 		<h2>Είναι online τώρα</h2>
 		<div class="list">
