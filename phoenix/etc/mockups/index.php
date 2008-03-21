aaf91141c5464b5c6b7f0ac1b4dde68e55eae660
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-03-21 16:54:32 +0000

    It won't listen, so let it be..RIP Albums-ie6

--- phoenix/etc/mockups/index.php
+++ phoenix/etc/mockups/index.php
@@ -60,8 +60,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
 				?><link rel="stylesheet" type="text/css" href="joined.css" /><?php
 				break;
 			case 'albums':
-				?><link rel="stylesheet" type="text/css" href="albums.css" />
-				<!--[if IE 6]><link rel="stylesheet" type="text/css" href="albums-ie6.css" /><![endif]--><?php 
+				?><link rel="stylesheet" type="text/css" href="albums.css" /><?php 
 				break;
 			case 'photoview':
 				?><link rel="stylesheet" type="text/css" href="photoview.css" />
