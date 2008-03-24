d4b65215116d6114ac07c66ea5694d11ce76811b
Chris Pappas <chrispappas12@gmail.com>
2008-03-24 13:59:51 +0000

    banner js->jquery

--- phoenix/js/banner.js
+++ phoenix/js/banner.js
@@ -34,29 +34,34 @@ var Banner = {
 					Banner.isanimating = false;
 				} );
 				*/
+				$( 'div#banner ul input' )[ 0 ].value = '';
+				$( 'div#banner ul input' )[ 1 ].value = '';
+				$( 'div#banner ul input' )[ 0 ].focus();
+				/*
 				menu.getElementsByTagName( 'input' )[ 0 ].value = '';
 				menu.getElementsByTagName( 'input' )[ 0 ].focus();
+				*/
 			} );
 		}
 		else {
-			//$( menu ).animate( { opacity: "1" } , 500 , function() {
-			Animations.Create( menu, 'opacity', 500, 1, 0, function () {
-				/*
+			$( menu ).animate( { opacity: "1" } , 500 , function() {
+			//Animations.Create( menu, 'opacity', 500, 1, 0, function () {
+				
 				$( options[ 0 ] ).show();
 				$( options[ 1 ] ).show();
 				$( options[ 3 ] ).hide();
 				$( options[ 4 ] ).hide();
 				$( options[ 5 ] ).hide();
-				*/
+				/*
 				options[ 0 ].style.display = '';
 				options[ 1 ].style.display = '';
 				//options[ 2 ].style.display = 'none';
 				options[ 3 ].style.display = 'none';
 				options[ 4 ].style.display = 'none';
 				options[ 5 ].style.display = 'none';
-				
-				//$( menu ).animate( { opacity: "1" } , 500 , function() {
-				Animations.Create( menu, 'opacity', 500, 0, 1, function() {
+				*/
+				$( menu ).animate( { opacity: "1" } , 500 , function() {
+				//Animations.Create( menu, 'opacity', 500, 0, 1, function() {
 					Banner.isanimating = false;
 				} );
 			} );
