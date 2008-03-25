7c414fcc5dc76ffedd6c408b7dc3f74f05751e3a
Aleksis Brezas <abresas@gmail.com>
2008-03-25 12:41:38 +0000

    user lastactive

--- phoenix/libs/user/lastactive.php
+++ phoenix/libs/user/lastactive.php
@@ -1,7 +1,19 @@
 <?php
 
+    /*
+        Update user activity
+
+        $lastactive = New UserLastActive( $user->Id ); // $user: logged in user
+        $lastactive->Date = NowDate();
+        $lastactive->Save();
+    */
+
     class UserLastActive extends Satori {
         protected $mDbTableAlias = 'lastactive';
+
+        public function LoadDefaults() {
+            $this->Date = NowDate();
+        }
     }
 
 ?>
