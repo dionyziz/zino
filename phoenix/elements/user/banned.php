598504917650b063a7a6c17c932cdcc72c28f15f
Dionysis Zindros <dionyziz@gmail.com>
2008-04-01 10:56:52 +0000

    ban handling

--- phoenix/elements/user/banned.php
+++ phoenix/elements/user/banned.php
@@ -12,17 +12,19 @@
             ?>zino-150-reflection.jpg" alt="<?php 
             echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] ); 
             ?>" style="float:left;margin:10px;" />
-            <br />Το σύστημά μας παρατήρησε ότι πραγματοποιείς ασυνήθιστη χρήση του λογαριασμού σου. Προκειμένου να προστατευθούν οι χρήστες του <?php 
-            echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
-            ?> από την ενδεχομένως επιβλαβή χρήση του <?php
-            echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
-            ?>, αυτός ο λογαριασμό έχει τεθεί εκτός λειτουργίας για μερικές ώρες. Εάν χρησιμοποιείς οποιοδήποτε λογισμικό τρίτων που αλληλεπιδρά με τον λογαριασμό σου στο <?php 
-            echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
-            ?>, παρακαλούμε απενεργοποίησέ το ή ρυθμισέ το έτσι ώστε η λειτουργία του να συμμορφώνεται με τους Όρους Χρήσης των υπηρεσιών του <?php 
-            echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
-            ?>. Εάν θεωρείς ότι έχεις χρησιμοποιήσει τον λογαριασμό σου στο <?php 
-            echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
-            ?> σύμφωνα με τους Όρους Χρήσης, παρακαλώ <a href="mailto:webmaster@zino.gr">επικοινώνησε μαζί μας</a> για να επιλύσουμε αυτό το πρόβλημα.
+            <div style="margin-left:150px;">
+                <br />Το σύστημά μας παρατήρησε ότι πραγματοποιείς ασυνήθιστη χρήση του λογαριασμού σου. Προκειμένου να προστατευθούν οι χρήστες του <?php 
+                echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
+                ?> από την ενδεχομένως επιβλαβή χρήση του <?php
+                echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
+                ?>, αυτός ο λογαριασμό έχει τεθεί εκτός λειτουργίας για μερικές ώρες. Εάν χρησιμοποιείς οποιοδήποτε λογισμικό τρίτων που αλληλεπιδρά με τον λογαριασμό σου στο <?php 
+                echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
+                ?>, παρακαλούμε απενεργοποίησέ το ή ρυθμισέ το έτσι ώστε η λειτουργία του να συμμορφώνεται με τους Όρους Χρήσης των υπηρεσιών του <?php 
+                echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
+                ?>. Εάν θεωρείς ότι έχεις χρησιμοποιήσει τον λογαριασμό σου στο <?php 
+                echo htmlspecialchars( $rabbit_settings[ 'applicationname' ] );
+                ?> σύμφωνα με τους Όρους Χρήσης, παρακαλώ <a href="mailto:webmaster@zino.gr">επικοινώνησε μαζί μας</a> για να επιλύσουμε αυτό το πρόβλημα.
+            </div>
         </div><?php
     }
 ?>
