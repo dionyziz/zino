d87d5a5858616446e666200d8dab95baa0a180a1
Dionysis Zindros <dionyziz@gmail.com>
2008-04-01 11:51:13 +0000

    userlogin

---
+++ phoenix/actions/user/login.php
@@ -0,0 +1,28 @@
+<?php
+    function ActionUserLogon( tString $username, tString $password ) {
+    	global $user;
+        global $rabbit_settings;
+    	
+        $rawpassword = $password->Get();
+    	$s_username = $username->Get();
+    	$s_password = $password->Get();
+    	$s_password = md5( $s_password );
+    	
+    	$_SESSION[ 's_password' ] = $s_password;
+    	$_SESSION[ 's_username' ] = $s_username;
+    	
+        $finder = New UserFinder();
+        $user = $finder->FindByNameAndPassword( $_SESSION[ 's_username' ] , $_SESSION[ 's_password' ] );
+    	
+        if ( !$user->Exists() ) {
+    		return Redirect( "?p=a" );
+    	}
+        // else...
+        $user->UpdateLastLogon();
+        $user->RenewAuthtoken();
+        $user->Save();
+        User_SetCookie( $user->Id, $user->Authtoken );
+
+        return Redirect( $_SERVER[ 'HTTP_REFERER' ]  );
+    }
+?>
--- phoenix/libs/project.php
+++ phoenix/libs/project.php
@@ -10,35 +10,33 @@
         
     	$libs->Load( 'magic' );
     	$libs->Load( 'user/user' );
+        $libs->Load( 'user/cookie' );
     	$libs->Load( 'ban' );
         
         $xc_settings = $rabbit_settings[ '_excalibur' ];
 
         $libs->Load( 'memcache/mc' ); // needs xc_settings
         
-    	$_SESSION[ 'previousuri' ] = ( isset ( $_SESSION[ 'thisuri' ] ) ? $_SESSION[ 'thisuri' ] : "" );
-    	$_SESSION[ 'thisuri' ] = $_SERVER[ 'REQUEST_URI' ];
-    	
         $finder = New UserFinder();
         if ( !empty( $_SESSION[ 's_username' ] ) && !empty( $_SESSION[ 's_password' ] ) ) {
             $user = $finder->FindByNameAndPassword( $_SESSION[ 's_username' ] , $_SESSION[ 's_password' ] );
         }
-        else if ( !empty( $_COOKIE[ $xc_settings[ 'cookiename' ] ] ) ) {
-            $logininfo = $_COOKIE[ $xc_settings['cookiename'] ];
-            $logininfos = explode( ':' , $logininfo );
-            $userid = $logininfos[ 0 ];
-            $userauth = $logininfos[ 1 ];
-            if ( strlen( $userauth ) != 32 ) {
+        else {
+            $cookie = User_GetCookie();
+            if ( $cookie === false ) {
                 $user = new User( array() );
             }
             else {
+                $userid = $cookie[ 'userid' ];
+                $userauth = $cookie[ 'authtoken' ];
                 $user = $finder->FindByIdAndAuthtoken( $userid, $userauth );
+                if ( $user === false ) {
+                    // not found
+                    $user = new User( array() );
+                }
             }
         }
-        else {
-            $user = new User( array() );
-        }
-        
+
         $banned = false;
         if ( !$user->HasPermission( PERMISSION_ACCESS_SITE ) ) {
             $banned = true;
