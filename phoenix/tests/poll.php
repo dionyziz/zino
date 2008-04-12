9be37d65c9c9448a01fe57ae95d27ea3b0bb8263
Aleksis Brezas <abresas@gmail.com>
2008-04-12 03:49:47 +0000

    poll testcase fix

--- phoenix/tests/poll.php
+++ phoenix/tests/poll.php
@@ -96,7 +96,7 @@
 
             $this->Assert( $vote->Exists(), 'Vote does not appear to exist after saving' );
 
-            $vote = New PollVote( $this->mOptionId, 3 );
+            $vote2 = New PollVote( $this->mOptionId, 3 );
             $this->Assert( $vote2->Exists(), 'Vote does not appear to exist after creating a new instance' );
             $this->AssertEquals( $vote->Created, $vote2->Created, 'Vote created changed on new instance' );
             $this->AssertEquals( $vote->Userid, $vote2->Userid, 'Vote userid changed on new instance' );
