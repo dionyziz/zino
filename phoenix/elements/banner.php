eab6208c085eb911c30b1e970938fac5ae3b28c9
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-04-29 12:52:40 +0000

    Logo redirects to main page

--- phoenix/elements/banner.php
+++ phoenix/elements/banner.php
@@ -3,13 +3,16 @@
 	function ElementBanner() {
 		global $page;
 		global $user;
+		global $rabbit_settings;
 		
 		//$page->AttachStylesheet( 'css/banner.css' );
 		//$page->AttachScript( 'js/banner.js' );
 		//$page->AttachScript( 'js/animations.js' );
 		
 		?><div class="header" id="banner">
-		<h1><a href="http://www.zino.gr/" onclick="return false"><img src="images/zino.png" alt="Zino" /></a></h1>
+		<h1><a href="<?php
+		echo $rabbit_settings[ 'webaddress' ];
+		?>" onclick="return false"><img src="images/zino.png" alt="Zino" /></a></h1>
 	    <a href="#content" class="novisual">Πλοήγηση στο περιεχόμενο</a>
 		<ul><?php   
 	        if ( !$user->Exists() ) {
