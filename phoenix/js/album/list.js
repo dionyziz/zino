1c9692d8113a310bc3d281aff8d08a53538fd94f
Chris Pappas <chrispappas12@gmail.com>
2008-05-01 11:33:47 +0000

    album list

--- phoenix/js/album/list.js
+++ phoenix/js/album/list.js
@@ -2,7 +2,7 @@ $( document ).ready( function() {
 	$( 'li.create a.new' ).click( function() {
 		var newalbum = document.createElement( 'li' );
 		$( newalbum ).append(  $( 'div.createalbum' ).clone() ).css( "width" , "0" ).animate( { width: "180px" } , 400 ).find( "div.createalbum" ).removeClass( "createalbum" );
-		$( 'li.create a.new' )[ 0 ].insertBefore( newalbum );		
+		$( 'ul.albums' )[ 0 ].insertBefore( newalbum , $( 'li.create' )[ 0 ] );		
 		$( 'span.desc input' )[ 0 ].select();
 		$( 'span.desc input' )[ 0 ].focus();
 		//make creation link disabled
