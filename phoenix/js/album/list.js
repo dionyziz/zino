c94930ea56824fa6f56c9c681feea36f1a7f3a0f
Chris Pappas <chrispappas12@gmail.com>
2008-05-04 08:11:29 +0000

    album creation

--- phoenix/js/album/list.js
+++ phoenix/js/album/list.js
@@ -1,7 +1,7 @@
 var AlbumList = {
 	Create : function() {
 		var newalbum = document.createElement( 'li' );
-		$( newalbum ).append(  $( 'div.createalbum' ).clone() ).css( "display" , "none" ).show( 400 ).find( "div.createalbum" ).removeClass( "createalbum" );
+		$( newalbum ).append(  $( 'div.createalbum' ).clone() ).show( 400 ).find( "div.createalbum" ).removeClass( "createalbum" );
 		$( 'ul.albums' )[ 0 ].insertBefore( newalbum , $( 'li.create' )[ 0 ] );
 		$( 'span.desc input' ).keydown( function( event ) {
 			if ( event.keyCode == 13 ) {
