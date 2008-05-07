ad178b82eee9154c52764edb4d3dccce012c40db
Dionysis Zindros <dionyziz@gmail.com>
2008-05-07 23:54:45 +0000

    Branch Rabbit 0.7

--- phoenix/etc/rabbit.compile.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define( 'TARGET', 'e:/work/kamibu/rabbit' );
-
-    $files = explode( "\n", file_get_contents( 'rabbit.compile.lst' ) );
-    
-    foreach ( $files as $file ) {
-        echo "Copying $file to " . TARGET . $file . "\n";
-    }
-?>
