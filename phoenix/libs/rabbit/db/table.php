bb9db4836d474106ae2ede642930de86e31300f7
Aleksis Brezas <abresas@gmail.com>
2008-05-10 07:19:51 +0000

    dbtable copy

--- phoenix/libs/rabbit/db/table.php
+++ phoenix/libs/rabbit/db/table.php
@@ -201,9 +201,8 @@
             $this->mIndexes = false;
 		}
         public function Copy( $newtable ) {
-            $query = $this->mDb->Prepare( 'CREATE TABLE :newtable LIKE :' . $this->mAlias . ';' );
+            $query = $this->mDb->Prepare( 'CREATE TABLE ' . $newtable . ' LIKE :' . $this->mAlias . ';' );
             $query->BindTable( $this->mAlias );
-            $query->Bind( 'newtable', $newtable ); // no BindTable as newtable doesn't exist
             return $query->Execute();
         }
 		public function Truncate() {
