4943e06a0d6a28ca950f46c4928caf601a0755fb
Dionysis Zindros <dionyziz@gmail.com>
2008-05-17 18:59:59 +0000

    user counts

---
+++ phoenix/libs/user/count.php
@@ -0,0 +1,10 @@
+<?php
+    class UserCount extends Satori {
+        protected $mDbTableAlias = 'usercounts';
+
+        protected function Relations() {
+            $this->User = $this->HasOne( 'User', 'Userid' );
+        }
+    }
+?>
+
