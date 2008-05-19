cae89b179d0f32761a4aa8e7044ecc524d5672ec
Chris Pappas <chrispappas12@gmail.com>
2008-05-19 18:19:49 +0000

    interesttags frontend

--- phoenix/elements/user/settings/interests.php
+++ phoenix/elements/user/settings/interests.php
@@ -5,7 +5,7 @@
 		?><div class="option">
 			<label>Hobbies:</label>
 			<div class="setting">
-				<ul class="interesttags">
+				<ul class="interesttags hobbies">
 					<li>
 						<div class="aplbubble">
 							<img src="<?php
@@ -170,6 +170,78 @@
 		<div class="option">
 			<label>Αγαπημένες ταινίες:</label>
 			<div class="setting">
+				<ul class="interesttags movies">
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Amelie</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Lord of the Rings</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Pi</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Requiem for a dream</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Butterfly Effect</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div>
 		<div class="barfade">
@@ -179,7 +251,8 @@
 		<div class="option">
 			<label>Αγαπημένα βιβλία:</label>
 			<div class="setting">
-
+				<ul class="interesttags books">
+				</ul>
 			</div>
 		</div>
 		<div class="barfade">
@@ -189,6 +262,78 @@
 		<div class="option">
 			<label>Αγαπημένα τραγούδια:</label>
 			<div class="setting">
+				<ul class="interesttags songs">
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Hanging by a moment</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Bubbly</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Where'd you go</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Fear of the dark</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Wanna Cry</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div>
 		<div class="barfade">
@@ -198,7 +343,78 @@
 		<div class="option">
 			<label>Αγαπημένοι καλλιτέχνες:</label>
 			<div class="setting">
-
+				<ul class="interesttags artists">
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Jim Carrey</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Linkin Park</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Nightwish</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Korgialas</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Avril Lavigne</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div>
 		<div class="barfade">
@@ -208,7 +424,36 @@
 		<div class="option">
 			<label>Αγαπημένα παιχνίδια:</label>
 			<div class="setting">
-
+				<ul class="interesttags games">
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Need For Speed</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Flight Simulator</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div>
 		<div class="barfade">
@@ -218,7 +463,36 @@
 		<div class="option">
 			<label>Αγαπημένες φράσεις:</label>
 			<div class="setting">
-
+				<ul class="interesttags hobbies">
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Knowledge is power</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>It is a paradoxical but profoundly true and important principle of life that the most likely way to reach a goal is to be aiming not at that goal itself but at some more ambitious goal beyond it.</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div>
 		<div class="barfade">
@@ -228,7 +502,22 @@
 		<div class="option">
 			<label>Αγαπημένες σειρές:</label>
 			<div class="setting">
-
+				<ul class="interesttags shows">
+					<li>
+						<div class="aplbubble">
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_left.png" />
+							<span>Friends</span>
+							<a href="" class="delete"><img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>delete.png" /></a>
+							<img src="<?php
+							echo $rabbit_settings[ 'imagesurl' ];
+							?>aplbubble_right.png" />
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div><?php
 	}
