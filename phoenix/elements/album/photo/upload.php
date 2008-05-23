f1aea3f46bff5f4be3a1ed01d0bee1c19dd46057
Chris Pappas <chrispappas12@gmail.com>
2008-05-23 08:33:25 +0000

    upload photos

--- phoenix/elements/album/photo/upload.php
+++ phoenix/elements/album/photo/upload.php
@@ -7,7 +7,7 @@
 		//$water->Disable();
 		//$albumid = $albumid->Get();
 		$album = New Album( $albumid->Get() );
-		?><script type="text/javascript">alert( PhotoList );</script><?php
+
 		if ( $album->User->Id == $user->Id && $user->HasPermission( PERMISSION_IMAGE_CREATE ) ) {
 			?><form method="post" enctype="multipart/form-data" action="do/image/upload2" id="uploadform">
 				<div class="colorlink">Νέα φωτογραφία</div>
