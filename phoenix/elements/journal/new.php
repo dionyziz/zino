38c70e9af39e1574d43980db0d3bc41d3f54ebdd
Dionysis Zindros <dionyziz@gmail.com>
2008-05-23 14:51:16 +0000

    wysiwyg

--- phoenix/elements/journal/new.php
+++ phoenix/elements/journal/new.php
@@ -37,31 +37,33 @@
                     }
                     ?></div>
                     <script type="text/javascript">
-                        WYSIWYG.Create( document.getElementById( 'wysiwyg' ), 'text', [
-                            {
-                                'tooltip': 'Έντονη Γραφή',
-                                'image': 'http://static.zino.gr/phoenix/text_bold.png',
-                                'command': 'bold'
-                            },
-                            {
-                                'tooltip': 'Πλάγια Γραφή',
-                                'image': 'http://static.zino.gr/phoenix/text_italic.png',
-                                'command': 'italic'
-                            },
-                            {
-                                'tooltip': 'Υπογράμμιση',
-                                'image': 'http://static.zino.gr/phoenix/text_underline.png',
-                                'command': 'underline'
-                            },
-                            {
-                                'tooltip': 'Εισαγωγή Εικόνας',
-                                'image': 'http://static.zino.gr/phoenix/picture.png'
-                            },
-                            {
-                                'tooltip': 'Εισαγωγή Video',
-                                'image': 'http://static.zino.gr/phoenix/television.png'
-                            }
-                        ] );
+                        $( document ).ready( function() {
+                            WYSIWYG.Create( document.getElementById( 'wysiwyg' ), 'text', [
+                                {
+                                    'tooltip': 'Έντονη Γραφή',
+                                    'image': 'http://static.zino.gr/phoenix/text_bold.png',
+                                    'command': 'bold'
+                                },
+                                {
+                                    'tooltip': 'Πλάγια Γραφή',
+                                    'image': 'http://static.zino.gr/phoenix/text_italic.png',
+                                    'command': 'italic'
+                                },
+                                {
+                                    'tooltip': 'Υπογράμμιση',
+                                    'image': 'http://static.zino.gr/phoenix/text_underline.png',
+                                    'command': 'underline'
+                                },
+                                {
+                                    'tooltip': 'Εισαγωγή Εικόνας',
+                                    'image': 'http://static.zino.gr/phoenix/picture.png'
+                                },
+                                {
+                                    'tooltip': 'Εισαγωγή Video',
+                                    'image': 'http://static.zino.gr/phoenix/television.png'
+                                }
+                            ] );
+                        } );
                     </script>
 					<div class="submit">
 						<input type="submit" value="Δημοσίευση" onclick="JournalView.Create( '<?php
