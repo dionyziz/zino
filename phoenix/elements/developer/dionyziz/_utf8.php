6b86ea6432b29e7f8017bd54e99fde32dd4bf1b9
Dionysis Zindros <dionyziz@gmail.com>
2008-05-24 13:59:16 +0000

    utf-8

--- phoenix/elements/developer/dionyziz/_utf8.php
+++ phoenix/elements/developer/dionyziz/_utf8.php
@@ -1,6 +1,6 @@
 <?php
     function ElementDeveloperDionyzizUTF8() {
-        ?><br /><br /><form action="do/album/rename" accept-charset="utf8">
+        ?><br /><br /><form action="do/album/rename" accept-charset="utf8" method="post">
             Album Id: <input type="text" value="" name="albumid" /><br />
             Album Name: <input type="text" value="" name="albumname" />
             <br /><input type="submit" value="Rename" /><?php
