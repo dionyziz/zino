096d89ea92fa392d4a015d949c10896f84b66f41
Aleksis Brezas <abresas@gmail.com>
2008-05-24 19:00:29 +0000

    journal testcase

---
+++ phoenix/tests/journal.php
@@ -0,0 +1,33 @@
+<?php
+
+    class TestJournal extends Testcase {
+        protected $mAppliesTo = 'libs/journal';
+        private $mUser;
+
+        public function TestClassesExist() {
+            $this->Assert( class_exists( 'Journal' ), 'Class Journal does not exist' );
+            $this->Assert( class_exists( 'JournalFinder' ), 'Class JournalFinder does not exist' );
+        }
+        public function SetUp() {
+            $user = New User();
+            $user->Name = 'testjournals';
+            $user->Subdomain = 'testjournals';
+            $user->Save();
+
+            $this->mUser = $user;
+        }
+        public function TestCreateJournal() {
+            $journal = New Journal();
+            $journal->Text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cras rhoncus. Nam aliquam. Donec augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur pulvinar mollis nisi. Vivamus euismod vehicula turpis. Morbi turpis sapien, vestibulum sit amet, luctus ac, tincidunt imperdiet, lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec scelerisque est fermentum ante. In nec enim sit amet dui consectetuer auctor. Quisque sagittis nunc vel lorem. Curabitur id leo.";
+            $journal->Userid = $this->mUser->Id;
+            $journal->Save();
+
+            $journal2 = New Journal( $journal->Id );
+            $this->AssertEquals( "Lorem ipsum dolor sit amet", $journal2->GetText( 26 ) );
+        }
+        public function TearDown() {
+            $this->mUser->Delete();
+        }
+    }
+
+?>
