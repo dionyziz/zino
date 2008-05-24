013cead42277f7d0c4e905639ffa4c25a29e177f
Chris Pappas <chrispappas12@gmail.com>
2008-05-24 11:49:55 +0000

    album mainimage

--- phoenix/units/album/photo/delete.do.php
+++ phoenix/units/album/photo/delete.do.php
@@ -6,6 +6,10 @@
 		$image = New Image( $photoid->Get() );
 		if ( $image->User->Id == $user->Id ) {
 			$albumid = $image->Album->Id;
+			if ( $image->Album->Mainimage == $image->Id ) {
+				$image->Album->Mainimage = 0;
+				$image->Album->Save();
+			}
 			$image->Delete();
 			if ( $albumid != 0 ) {
 				?>window.location.href = '<?php
