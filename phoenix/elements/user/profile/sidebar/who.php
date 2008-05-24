d364d4283c162daa57150b5f9b1fbb1cad42e766
Chris Pappas <chrispappas12@gmail.com>
2008-05-24 09:46:20 +0000

    avatars

--- phoenix/elements/user/profile/sidebar/who.php
+++ phoenix/elements/user/profile/sidebar/who.php
@@ -1,7 +1,8 @@
 <?php
 	function ElementUserProfileSidebarWho( $theuser ) {
-		?><h2>
-			<img src="http://static.zino.gr/phoenix/mockups/dionyziz.200.jpg" style="width:150px;height:150px;" alt="dionyziz" title="dionyziz" /><span class="name"><?php
+		?><h2><?php
+			Element( 'user/avatar' , $theuser , 150 );
+			?><span class="name"><?php
 			Element( 'user/name' , $theuser , false );
 			?></span>
 		</h2><?php
