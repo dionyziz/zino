40e7d579ff4af0088e11e4033a829571f57e496e
Dionysis Zindros <dionyziz@gmail.com>
2008-05-25 18:18:45 +0000

    unrv

--- reloaded/elements/comment/view.php
+++ reloaded/elements/comment/view.php
@@ -56,34 +56,39 @@
 				<div class="leftcorner">&nbsp;</div>
 				<div class="rightcorner">&nbsp;</div>
 				<div class="middle">&nbsp;</div>
-				<div class="toolbar">
-					<ul id="comment_<?php
-					echo $comment->Id(); 
-					?>_toolbar"><?php
-                        if ( $allowreply && !( $user->IsAnonymous() && !$xc_settings[ 'anonymouscomments' ] ) && $user->Rights() >= $xc_settings[ 'readonly' ] ) {
-    						?><li><a onclick="Comments.Reply( <?php 
+				<div class="toolbar"><?php
+                    ob_start();
+                    if ( $allowreply && !( $user->IsAnonymous() && !$xc_settings[ 'anonymouscomments' ] ) && $user->Rights() >= $xc_settings[ 'readonly' ] ) {
+                        ?><li><a onclick="Comments.Reply( <?php 
+                        echo $comment->Id(); 
+                        ?>, <?php 
+                        echo $indent; 
+                        ?> ); return false;">Απάντηση</a></li><?php
+                    }
+                    if ( $user->CanModifyCategories() || ( $user->Exists() && $user->Id() == $theuser->Id() && daysDistance($comment->SQLDate() ) < 1 ) && $user->Rights() >= $xc_settings[ 'readonly' ] ) { 
+                        ?><li><a style="cursor: pointer;" onclick="Comments.Edit( <?php 
+                        echo $comment->Id(); 
+                        ?> ); return false;">Επεξεργασία</a></li><?php
+                        if ( !$numchildren ) {
+                            ?><li><a style="cursor:pointer" onclick="Comments.Delete( <?php 
                             echo $comment->Id(); 
-                            ?>, <?php 
-                            echo $indent; 
-                            ?> ); return false;">Απάντηση</a></li><?php
+                            ?> ); return false;">Διαγραφή</a></li><?php
                         }
-						if ( $user->CanModifyCategories() || ( $user->Exists() && $user->Id() == $theuser->Id() && daysDistance($comment->SQLDate() ) < 1 ) && $user->Rights() >= $xc_settings[ 'readonly' ] ) { 
-							?><li><a style="cursor: pointer;" onclick="Comments.Edit( <?php 
-                            echo $comment->Id(); 
-                            ?> ); return false;">Επεξεργασία</a></li><?php
-							if ( !$numchildren ) {
-                                ?><li><a style="cursor:pointer" onclick="Comments.Delete( <?php 
-                                echo $comment->Id(); 
-                                ?> ); return false;">Διαγραφή</a></li><?php
-							}
-						}
-						if ( $theuser->IsAnonymous() && !$numchildren && $user->CanModifyCategories() && $user->Rights() >= $xc_settings[ 'readonly' ] ) {
-							?><li><a style="cursor:pointer" onclick="Comments.MarkAsSpam( <?php
-							echo $comment->Id();
-							?> ); return false;">Spam</a></li><?php
-						}
-						
-					?></ul>
+                    }
+                    if ( $theuser->IsAnonymous() && !$numchildren && $user->CanModifyCategories() && $user->Rights() >= $xc_settings[ 'readonly' ] ) {
+                        ?><li><a style="cursor:pointer" onclick="Comments.MarkAsSpam( <?php
+                        echo $comment->Id();
+                        ?> ); return false;">Spam</a></li><?php
+                    }
+                    $lis = ob_get_clean();
+                    if ( !empty( $lis ) ) {
+                        ?><ul id="comment_<?php
+                        echo $comment->Id(); 
+                        ?>_toolbar"><?php
+                        echo $lis;
+                        ?></ul><?php
+                    }
+                    ?>
 					<ul id="comment_edit_<?php 
                         echo $comment->Id(); 
                         ?>_toolbar" style="display: none">
@@ -92,8 +97,8 @@
 						?> );">Επεξεργασία!</a></li>
 						<li><a style="cursor: pointer;" onclick="Comments.cancelEdit( <?php 
                         echo $comment->Id(); 
-                        ?> ); return false;">Ακύρωση</a></li>
-					</ul>
+                        ?> ); return false;">Ακύρωση</a></li><?php
+                    ?></ul>
 				</div>
 				<div id="<?php
 				echo $comment->Id();
