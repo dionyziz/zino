6abf709c82a9901a7b449618e1200a2d8177d4d8
Chris Pappas <chrispappas12@gmail.com>
2008-05-25 15:50:44 +0000

    jounal settings avatar

--- phoenix/elements/user/settings/personal/avatar.php
+++ phoenix/elements/user/settings/personal/avatar.php
@@ -17,16 +17,15 @@
 			$images = $finder->FindByAlbum( $egoalbum , 0 , $egoalbum->Numphotos );
 			foreach ( $images as $image ) {	
 				$size = $image->ProportionalSize( 100 , 100 );
-				?><li><?php
+				?><li><a href="" onclick="alert( 'test' );return false;"<?php
 				Element( 'image' , $image , $size[ 0 ] , $size[ 1 ] , 'photosmall' , $image->Name , $image->Name , '' );
-				?></li><?php
+				?></li>
+				</a><?php
 			}
 			?></ul>
-			<div class="cancel">
-				<a href="" onclick="Modals.Destroy();return false;" class="button">
+			<a href="" onclick="Modals.Destroy();return false;" class="button">
 				Ακύρωση
-				</a>
-			</div>
+			</a>
 		</div><?php
 	}
 ?>
