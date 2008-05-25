2475826b11443a52578aa0aff6d930f21006ca30
Aleksis Brezas <abresas@gmail.com>
2008-05-25 19:10:13 +0000

    comments testcase

--- phoenix/tests/comment.php
+++ phoenix/tests/comment.php
@@ -179,6 +179,8 @@ West of the Moon, East of the Sun.";
             
             $this->Assert( is_array( $comments ), 'CommentFinder::FindByPage did not return an array' );
             $this->AssertEquals( 4, count( $comments ), 'CommentFinder::FindByPage did not return the right number of comments' );
+            
+            print_r( $comments );
 
             $user1->Delete();
             $user2->Delete();
