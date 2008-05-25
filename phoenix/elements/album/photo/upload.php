dbf7f09b3fd6d8ab80f00d6d01c45d01a62fea7c
Chris Pappas <chrispappas12@gmail.com>
2008-05-25 17:53:36 +0000

    settings avatar

--- phoenix/elements/album/photo/upload.php
+++ phoenix/elements/album/photo/upload.php
@@ -1,6 +1,6 @@
 <?php
 	
-	function ElementAlbumPhotoUpload( tInteger $albumid ) {
+	function ElementAlbumPhotoUpload( tInteger $albumid , tInteger $typeid ) {
 		global $water;
 		global $user;
 		global $rabbit_settings;
@@ -11,12 +11,15 @@
 		$page->AttachScript( 'js/album/photo/list.js' );
 		$water->Disable();
 		$page->SetWaterDump( false );
-		
+		//typeid is 0 for album photo uploads and 1 for avatar uploads at settings
 		$album = New Album( $albumid->Get() );
 		if ( $album->User->Id == $user->Id && $user->HasPermission( PERMISSION_IMAGE_CREATE ) ) {
 			?><form method="post" enctype="multipart/form-data" action="do/image/upload2" id="uploadform">
 					<input type="hidden" name="albumid" value="<?php
 					echo $album->Id;
+					?>" />
+					<input type="hidden" name="typeid" value="<?php
+					echo $typeid->Get();
 					?>" />
 					<div class="colorlink">
 						Νέα φωτογραφία
