c31e277fe4375d166e2883e39e02aeeef7c2836d
Chris Pappas <chrispappas12@gmail.com>
2008-05-25 22:01:34 +0000

    photo list

--- phoenix/elements/album/photo/view.php
+++ phoenix/elements/album/photo/view.php
@@ -120,7 +120,7 @@
 								Element( 'image' , $photos[ $pivot ] , $size[ 0 ] , $size[ 1 ] , '' , $photos[ $pivot ]->Name , $photos[ $pivot ]->Name , '' );
 							?></li><?php
 							if ( $pivot < 7 && $pivot != 0 ) {						
-								for ( $i = $pivot + 1; $i < 7; ++$i ) {
+								for ( $i = $pivot + 1; $i < count( $photos ); ++$i ) {
 									$size = $photos[ $i ]->ProportionalSize( 150 , 150 );
 									?><li><span><a href="?p=photo&amp;id=<?php
 									echo $photos[ $i ]->Id;
