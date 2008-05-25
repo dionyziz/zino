8c584b8ce73013fa48462dbb2f6a03b199a47c62
Chris Pappas <chrispappas12@gmail.com>
2008-05-25 16:22:05 +0000

    settings avatar

--- phoenix/units/user/settings/avatar.do.php
+++ phoenix/units/user/settings/avatar.do.php
@@ -4,13 +4,14 @@
 		global $user;
 		
 		$image = New Image( $imageid->Get() );
-		
+		?>alert( 'brkpnt1' );<?php
 		if ( !$image->IsDeleted() ) {
 			if ( $image->User->Id == $user->Id ) {
 				$egoalbum = New Album( $user->Egoalbumid );
 				$egoalbum->Mainimage = $image->Id;
 				$egoalbum->Save();
-				?>$( 'div.settings div.tabs form#personalinfo div.option div.setting img.avie' ).attr( {
+				?>alert( 'brkpnt2' );
+				$( 'div.settings div.tabs form#personalinfo div.option div.setting img.avie' ).attr( {
 					src : ExcaliburSettings.photosurl + '<?php
 					echo $user->Id;
 					?>/<?php
