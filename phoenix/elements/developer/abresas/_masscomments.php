7d7bc998db998c8204390692d8de4a0f4666327d
Aleksis Brezas <abresas@gmail.com>
2008-05-29 18:02:40 +0000

    mass comments

--- phoenix/elements/developer/abresas/_masscomments.php
+++ phoenix/elements/developer/abresas/_masscomments.php
@@ -2,7 +2,7 @@
 
     function ElementDeveloperAbresasMassComments() {
         ?><h2>Mass Comments</h2>
-        <form method="post" action="/do/comment/fill">
+        <form method="post" action="/phoenix/do/comment/fill">
             Typeid: <input type="text" name="typeid" /><br />
             Itemid: <input type="text" name="itemid" /><br />
             <input type="submit" value="Fill with comments!" />
