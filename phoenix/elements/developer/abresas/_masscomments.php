f16f067ee35faee1d7771e2dbd6fe54bd8a77e90
Aleksis Brezas <abresas@gmail.com>
2008-05-29 18:09:53 +0000

    mass comments

--- phoenix/elements/developer/abresas/_masscomments.php
+++ phoenix/elements/developer/abresas/_masscomments.php
@@ -4,10 +4,10 @@
         ?><h2>Mass Comments</h2>
         <form method="post" action="/phoenix/do/comment/fill">
             Typeid: <select name="typeid">
-            <option name="Journal" value="4" />
-            <option name="Poll" value="1" />
-            <option name="Image" value="2" />
-            <option name="User Profile" value="3" />
+            <option value="4">Journal</option>
+            <option value="1">Poll</option>
+            <option value="2">Image</option>
+            <option value="3">User Profile</option>
             </select><br />
             Itemid: <input type="text" name="itemid" /><br /><br />
             <input type="submit" value="Fill with comments!" />
