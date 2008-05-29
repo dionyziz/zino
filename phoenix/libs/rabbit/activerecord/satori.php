4bc18133461c0f304c5fcbd63376aee00f12de2a
Dionysis Zindros <dionyziz@gmail.com>
2008-05-29 12:35:52 +0000

    satori optimization

--- phoenix/libs/rabbit/activerecord/satori.php
+++ phoenix/libs/rabbit/activerecord/satori.php
@@ -19,7 +19,11 @@
         protected abstract function MakeObj();
         protected abstract function Modified();
         public function Rebuild() {
-            if ( ( !is_object( $this->mRetrieved ) && $this->mRetrieved == 0 ) || $this->Modified() ) {
+            if ( ( !is_object( $this->mRetrieved ) && $this->mRetrieved == 0 ) ) {
+                continue;
+            }
+            /* else */
+            if ( $this->Modified() ) {
                 $this->mRetrieved = $this->MakeObj();
             }
         }
