cd0e54463b10be31aec82b4ce4eb5ac9199179e3
Chris Pappas <chrispappas12@gmail.com>
2008-05-30 18:14:47 +0000

    frontend imagesserver

--- phoenix/elements/user/avatar.php
+++ phoenix/elements/user/avatar.php
@@ -6,10 +6,15 @@
 		// size can either be 150 or 50, which means avatars of size 150x150 or 50x50 respectively
 		if ( $theuser->Icon > 0 ) {
 			$avatar = New Image( $theuser->Icon );
-			Element( 'image' , $avatar , IMAGE_CROPPED_150x150, $class , $theuser->Name , $theuser->Name , $style );
+			if ( $size == 150 ) {
+				Element( 'image' , $avatar , IMAGE_CROPPED_150x150, $class , $theuser->Name , $theuser->Name , $style );
+			}
+			else if ( $size == 100 ) {
+				Element( 'image' , $avatar , IMAGE_CROPPED_100x100, $class , $theuser->Name , $theuser->Name , $style );
+			}
 		}
 		else {
-			Element( 'image' , 'anonymous' . $size . '.jpg' , '150x150', $class , $theuser->Name , $theuser->Name , $style );
+			Element( 'image' , 'anonymous' . $size . '.jpg' , $size . 'x' . $size , $class , $theuser->Name , $theuser->Name , $style );
 		}
 	}
 ?>
--- phoenix/elements/user/settings/personal/photosmall.php
+++ phoenix/elements/user/settings/personal/photosmall.php
@@ -1,11 +1,10 @@
 <?php
 	
 	function ElementUserSettingsPersonalPhotosmall( $image ) {
-		$size = $image->ProportionalSize( 100 , 100 );
 		?><a href="" onclick="Settings.SelectAvatar( '<?php
 		echo $image->Id;
 		?>' );return false;"><?php
-		Element( 'image' , $image , $size[ 0 ] , $size[ 1 ] , 'photosmall' , $image->Name , $image->Name , '' );
+		Element( 'image' , $image , $IMAGE_CROPPED_100x100 , 'photosmall' , $image->Name , $image->Name , '' );
 		?></a><?php
 	}
 ?>
