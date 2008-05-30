dc7a179e1ceda26a36bf646902190746b1b4ae94
Makis Kanavidis <makis.k@gmail.com>
2008-05-30 08:47:37 +0000

    nodebug

--- phoenix/libs/contacts/gmail.php
+++ phoenix/libs/contacts/gmail.php
@@ -62,7 +62,7 @@ else {
 			}
 			
 			if ( $return_value != 0 ) { // unknown error
-				die ( "Error($user:$pass): $allcontacts \n$errors" );
+				die ( "Error: $allcontacts \n$errors" );
 				return false;
 			}
 
