054dc7f52abff3f7caa11415123f68983dcbfbc0
Aleksis Brezas <abresas@gmail.com>
2008-06-02 20:31:41 +0000

    shoutbox testcase

---
+++ phoenix/tests/shoutbox.php
@@ -0,0 +1,51 @@
+<?php
+
+    class TestShoutbox extends Testcase {
+        protected $mAppliesTo = 'libs/shoutbox';
+        private $mUsers;
+
+        public function SetUp() {
+            global $libs;
+            $libs->Load( 'shoutbox' );
+
+            $ufinder = New UserFinder();
+            $user = $ufinder->FindByName( 'testshoutbox' );
+            if ( is_object( $user ) ) {
+                $user->Delete();
+            }
+
+            $user = New User();
+            $user->Name = 'testshoutbox';
+            $user->Subdomain = 'testshoutbox';
+            $user->Save();
+
+            $this->mUsers[] = $user;
+        }
+        public function TestCreation() {
+            $shout = New Shout();
+            $shout->Userid = $this->mUsers[ 0 ]->Id;
+            $shout->Text = "foo bar blah";
+            $shout->Save();
+
+            $s = New Shout( $shout->Id );
+            $this->AssertEquals( $this->mUsers[ 0 ]->Id, $s->Userid, 'Wrong userid' );
+            $this->AssertEquals( "foo bar blah", $s->Text, 'Wrong text' );
+
+            $shout->Delete();
+        }
+        public function TestCount() {
+        }
+        public function TestFindLatest() {
+        }
+        public function TearDown() {
+            if ( is_array( $this->mUsers ) ) {
+                foreach ( $this->mUsers as $user ) {
+                    if ( is_object( $user ) ) {
+                        $user->Delete();
+                    }
+                }
+            }
+        }
+    }
+
+?>
