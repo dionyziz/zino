9bdf74554bb62c05c6e4b3a56aa7fae39e6e0407
Chris Pappas <chrispappas12@gmail.com>
2008-06-03 16:12:46 +0000

    shoutbox

--- phoenix/elements/frontpage/shoutbox/list.php
+++ phoenix/elements/frontpage/shoutbox/list.php
@@ -4,12 +4,12 @@
 		?><div class="shoutbox">
 			<h2>Συζήτηση</h2>
 			<div class="comments"><?php
-			Element( 'frontpage/shoutbox/reply' );
-			Element( 'frontpage/shoutbox/view' );
-			Element( 'frontpage/shoutbox/view' );
-			Element( 'frontpage/shoutbox/view' );
-			Element( 'frontpage/shoutbox/view' );
-			Element( 'frontpage/shoutbox/view' );
+				Element( 'frontpage/shoutbox/reply' );
+				Element( 'frontpage/shoutbox/view' );
+				Element( 'frontpage/shoutbox/view' );
+				Element( 'frontpage/shoutbox/view' );
+				Element( 'frontpage/shoutbox/view' );
+				Element( 'frontpage/shoutbox/view' );
 			?></div>
 		</div><?php
 	}
