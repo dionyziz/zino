db8c3a42a475a5f8665a8d0d9a478103ff25a16b
Dionysis Zindros <dionyziz@gmail.com>
2008-06-04 19:57:16 +0000

    dob

--- phoenix/elements/developer/dionyziz/_user.php
+++ phoenix/elements/developer/dionyziz/_user.php
@@ -3,5 +3,11 @@
         global $user;
 
         echo $user->Profile->Dob;
+        ?><br /><?php
+        echo $user->BirthDay;
+        ?><br /><?php
+        echo $user->BirthMonth;
+        ?><br /><?php
+        echo $user->BirthYear;
     }
 ?>
