e864a82444ccd130d83b8296a3d489e44b3aa279
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-04 21:21:55 +0000

    Fixing comments.js

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -8,7 +8,6 @@ var Comments = {
 		var showcomment = $("div.newcomment").clone( true );
 		var a = document.createElement( 'a' );
 		a.onclick = false;
-		$( showcomment ).removeClass( "newcomment" ).find( "span.time" ).text( "πριν λίγο" ).end().find( "div.text textarea" ).remove().end().find( "div.text" ).append( document.createTextNode( texter ) ).end().find( "div.bottom" ).remove().append( a ).append( document.createTextNode( "Απάντα" ) ).end().find( "div.who:last" ).replaceWith( document.createTextNode( " είπε:" ) );
-		$( showcomment ).insertAfter( "div.newcomment" );
+		$( showcomment ).removeClass( "newcomment" ).find( "span.time" ).text( "πριν λίγο" ).end().find( "div.text textarea" ).remove().end().find( "div.text" ).append( document.createTextNode( texter ) ).end().find( "div.bottom" ).remove().append( a ).append( document.createTextNode( "Απάντα" ) ).end().find( "div.who" ).find( ":last" ).replaceWith( document.createTextNode( " είπε:" ) ).insertAfter( "div.newcomment" );
 	}
 };
