eeb51c6330c35ff0d6640d98bdb54f89f6d02027
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-04 20:22:23 +0000

    Fixing comments.js

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -1,14 +1,14 @@
 var Comments = {
 	Create : function() {
 		var texter = $("div.newcomment div.text textarea").value;
-		if ( texter == "" ) {
+		if ( texter === "" ) {
 			alert( "Δε μπορείς να δημοσιεύσεις κενό μήνυμα" );
 			return;
 		}
-		var showcomment = $("div.newcomment").eq[0].cloneNode( true );
+		var showcomment = $("div.newcomment").cloneNode( true );
 		var a = document.createElement( 'a' );
 		a.onclick = false;
 		//TODO: who
 		showcomment.removeClass( "newcomment" ).find( "span.time" )[0].text( "πριν λίγο" ).end().find( "div.text textarea" ).remove().text( texter ).end().find( "div.bottom" )[0].remove().append( a ).append( document.createTextNode( "Απάντα" ) ).end().insertAfter( "div.newcomment" );
 	}
-}
+};
