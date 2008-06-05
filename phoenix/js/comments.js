7b51974af99d2197f29df0fc09192e3a651c5e5f
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-05 13:38:19 +0000

    Delete Button

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -19,8 +19,7 @@ var Comments = {
 /*		del.style.opacity = 0;
 		del.style.backgroundImage = "url( 'http://static.zino.gr/phoenix/delete.png' )";*/
 		
-		var temp = $("div.newcomment").clone( true ).removeClass( "newcomment" ).find( "span.time" ).text( "πριν λίγο" ).end().find( "div.text" ).empty().append( document.createTextNode( texter ) ).end().find( "div.bottom" ).empty().append( a ).append( document.createTextNode( " σε αυτό το σχόλιο" ) ).end().find( "div.toolbox" ).append( del ).end();
-		//.find( "div.toolbox a" ).fadeIn( 450 ).end();
+		var temp = $("div.newcomment").clone( true ).removeClass( "newcomment" ).find( "span.time" ).text( "πριν λίγο" ).end().find( "div.text" ).empty().append( document.createTextNode( texter ) ).end().find( "div.bottom" ).empty().append( a ).append( document.createTextNode( " σε αυτό το σχόλιο" ) ).end().find( "div.toolbox" ).append( del ).end().find( "div.toolbox a" ).fadeOut( 450 ).end();
 		var useros = temp.find( "div.who" ).get(0);
 		useros.removeChild( useros.lastChild );
 		useros.appendChild( document.createTextNode( " είπε:" ) );
