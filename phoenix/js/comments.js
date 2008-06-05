96ef17eb7a6c5f7284c162b6f97e0926b706078e
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-05 13:40:49 +0000

    Delete Button

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -16,6 +16,7 @@ var Comments = {
 				return false;
 			};
 		del.title = "Διαγραφή";
+		del.style.opacity=0;
 		
 		var temp = $("div.newcomment").clone( true ).removeClass( "newcomment" ).find( "span.time" ).text( "πριν λίγο" ).end().find( "div.text" ).empty().append( document.createTextNode( texter ) ).end().find( "div.bottom" ).empty().append( a ).append( document.createTextNode( " σε αυτό το σχόλιο" ) ).end().find( "div.toolbox" ).append( del ).end().find( "div.toolbox a" ).fadeIn( 450 ).end();
 		var useros = temp.find( "div.who" ).get(0);
