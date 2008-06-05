e33423ab12530108f805687893305c446f8df9e1
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-05 05:31:30 +0000

    Fixes

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -11,7 +11,7 @@ var Comments = {
 		var temp = $("div.newcomment").clone( true ).removeClass( "newcomment" ).find( "span.time" ).text( "πριν λίγο" ).end().find( "div.text" ).empty().append( document.createTextNode( texter ) ).end().find( "div.bottom" ).empty().end().find( "div.bottom" ).append( a ).append( document.createTextNode( " σε αυτό το σχόλιο" ) ).end();
 		var useros = temp.find( "div.who" ).get();
 		useros.removeChild( useros.lastChild );
-		useros.appendChild( d.createTextNode( " είπε:" );
+		useros.appendChild( d.createTextNode( " είπε:" ) );
 		temp.insertAfter( "div.newcomment" );
 		/*end().find( "div.text" ).append( document.createTextNode( texter ) ).end().find( "div.bottom" ).empty().end().find( "div.bottom" ).append( a ).append( document.createTextNode( "Απάντα" ) ).end().find( "div.who" ).text( " είπε:" ).end().insertAfter( "div.newcomment" );*/
 	}
