5014fa651d3c477dade36fd4563aeda56e8048dc
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-05 05:16:49 +0000

    Fixes

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -7,7 +7,8 @@ var Comments = {
 		}
 		var a = document.createElement( 'a' );
 		a.onclick = false;
-		$("div.newcomment").clone( true ).removeClass( "newcomment" ).find( "span.time" ).text( "πριν λίγο" ).end().find( "div.text" ).empty().append( document.createTextNode( texter ) ).end().find( "div.bottom" ).empty().end().find( "div.bottom" ).append( a ).append( document.createTextNode( "Απάντα" ) ).end().find( "div.who" ).text( " είπε:" ).end().insertAfter( "div.newcomment" );
+		a.appendChild( document.createTextNode( "Απάντα" ) );
+		$("div.newcomment").clone( true ).removeClass( "newcomment" ).find( "span.time" ).text( "πριν λίγο" ).end().find( "div.text" ).empty().append( document.createTextNode( texter ) ).end().find( "div.bottom" ).empty().end().find( "div.bottom" ).append( a ).append( document.createTextNode( " σε αυτό το σχόλιο" ) ).end().find( "div.who" ).text( " είπε:" ).end().insertAfter( "div.newcomment" );
 		/*end().find( "div.text" ).append( document.createTextNode( texter ) ).end().find( "div.bottom" ).empty().end().find( "div.bottom" ).append( a ).append( document.createTextNode( "Απάντα" ) ).end().find( "div.who" ).text( " είπε:" ).end().insertAfter( "div.newcomment" );*/
 	}
 };
