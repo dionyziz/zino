6e945c99d17a56cc71e0d7f5b0eb8d5334ac3e0c
Dionysis Zindros <dionyziz@gmail.com>
2008-06-06 18:38:11 +0000

    moods

--- phoenix/libs/mood.php
+++ phoenix/libs/mood.php
@@ -1,5 +1,15 @@
 <?php
+    class MoodException extends Exception {
+    }
+
     class Mood extends Satori {
         protected $mDbTableAlias = 'moods';    
+
+        protected function OnBeforeCreate() {
+            throw New MoodException( 'Mood list is immutable' );
+        }
+        protected function OnBeforeUpdate() {
+            throw New MoodException( 'Mood list is immutable' );
+        }
     }
 ?>
