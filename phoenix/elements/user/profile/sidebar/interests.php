dd8ef65d5305ea6b729b63298e4b7668fe3d4793
Chris Pappas <chrispappas12@gmail.com>
2008-06-06 22:48:00 +0000

    tags frontend

--- phoenix/elements/user/profile/sidebar/interests.php
+++ phoenix/elements/user/profile/sidebar/interests.php
@@ -1,68 +1,80 @@
 <?php 
-	function ElementUserProfileSidebarInterests( $theuser ) {
-		?><dl>
-			<dt><strong>Hobbies</strong></dt>
-			<dd>
-				<a href="">Web Development</a>, 
-				<a href="">PHP</a>, 
-				<a href="">HTML</a>, 
-				<a href="">Javascript</a>, 
-				<a href="">Underage</a>, 
-				<a href="">Πλέξιμο</a>, 
-				<a href="">Μπαλέτο</a>, 
-				<a href="">Maths</a>
-			</dd>
-			
-			<dt><strong>Αγαπημένα τραγούδια</strong></dt>
-			<dd>
-				<a href="">Papercut</a>, 
-				<a href="">Perfection is my direction</a>, 
-				<a href="">Over my head</a>, 
-				<a href="">Teenagers</a>, 
-				<a href="">Black parade</a>
-			</dd>
+	function ElementUserProfileSidebarInterests( $theuser ) {	
+		global $libs;
 		
-			<dt><strong>Αγαπημένες ταινίες</strong></dt>
-			<dd>
-				<a href="">Amelie</a>, 
-				<a href="">Elephant</a>, 
-				<a href="">Sommersturm</a>, 
-				<a href="">Lord of the Rings</a>
-			</dd>
-			
-			<dt><strong>Αγαπημένες σειρές</strong></dt>
-			<dd>
-				<a href="">Pixelperfect</a>, 
-				<a href="">Friends</a>
-			</dd>
-			
-			<dt><strong>Αγαπημένα βιβλία</strong></dt>
-			<dd>
-				<a href="">PHP cookbook</a>, 
-				<a href="">MySQL cookbook</a>, 
-				<a href="">Code complete</a>
-			</dd>
-			
-			<dt><strong>Αγαπημένοι καλλιτέχνες</strong></dt>
-			<dd>
-				<a href="">Angelina Jolie</a>, 
-				<a href="">Rowan Atkinson</a>, 
-				<a href="">Harisson Ford</a>
-			</dd>
-			
-			<dt><strong>Αγαπημένα videogames</strong></dt>
-			<dd>
-				<a href="">WoW</a>, 
-				<a href="">Baldrus gate</a>, 
-				<a href="">Empire earth</a>, 
-				<a href="">Age of empires</a>
-			</dd><?php
-			if ( $theuser->Profile->Favquote != '' ) {
-				?><dt><strong>Αγαπημένα ρητό</strong></dt>
-				<dd><?php
-				echo htmlspecialchars( $theuser->Profile->Favquote );
-				?></dd><?php
+		$libs->Load( 'tag' );
+		$finder = New TagFinder();
+		$tags = $finder->FindByUser( $theuser );
+		$bytype = array(
+		    TAG_HOBBIE => array(),
+		    TAG_MOVIE => array(),
+		    TAG_BOOK => array(),
+			TAG_SONG => array(),
+			TAG_ARTIST => array(),
+			TAG_GAME => array(),
+			TAG_SHOW => array()
+		);
+		if ( !empty( $tags ) ) {
+			foreach ( $tags as $tag ) {
+				$bytype[ $tag->Typeid ][] = $tag;
 			}
-		?></dl><?php
+			?><dl><?php
+				$hobbies = $bytype[ TAG_HOBBIE ];
+				if ( !empty( $hobbies ) ) {
+					?><dt><strong>Hobbies</strong></dt>
+					<dd><?php
+						echo htmlspecialchars( implode( ", " , $hobbies ) );
+					?></dd><?php
+				}
+				$songs = $bytype[ TAG_SONG ];
+				if ( !empty( $songs ) ) {
+					?><dt><strong>Αγαπημένα τραγούδια</strong></dt>
+					<dd><?php
+						echo htmlspecialchars( implode( ", " , $songs ) );
+					?></dd><?php
+				}
+				$movies = $bytype[ TAG_MOVIE ];
+				if ( !empty( $movies ) ) {
+					?><dt><strong>Αγαπημένες ταινίες</strong></dt>
+					<dd><?php
+						echo htmlspecialchars( implode( ", " , $movies ) );
+					?></dd><?php
+				}
+				$shows = $bytype[ TAG_SHOW ];
+				if ( !empty( $shows ) ) {
+					?><dt><strong>Αγαπημένες σειρές</strong></dt>
+					<dd><?php
+						echo htmlspecialchars( implode( ", " , $shows ) );
+					?></dd><?php
+				}
+				$books = $bytype[ TAG_BOOK ];
+				if ( !empty( $books ) ) {
+					?><dt><strong>Αγαπημένα βιβλία</strong></dt>
+					<dd><?php
+						echo htmlspecialchars( implode( ", " , $books ) ) ;
+					?></dd><?php
+				}
+				$artists = $bytype[ TAG_ARTIST ];
+				if ( !empty( $artists ) ) {
+					?><dt><strong>Αγαπημένοι καλλιτέχνες</strong></dt>
+					<dd><?php
+						echo htmlspecialchars( implode( ", " , $artists ) );
+					?></dd><?php
+				}
+				$games = $bytype[ TAG_GAME ];
+				if ( !empty( $games ) ) {
+					?><dt><strong>Αγαπημένα videogames</strong></dt>
+					<dd><?php
+						echo htmlspecialchars( implode( ", " , $games ) );
+					?></dd><?php
+				}
+				if ( $theuser->Profile->Favquote != '' ) {
+					?><dt><strong>Αγαπημένα ρητό</strong></dt>
+					<dd><?php
+					echo htmlspecialchars( $theuser->Profile->Favquote );
+					?></dd><?php
+				}
+			?></dl><?php
+		}
 	}
 ?>
