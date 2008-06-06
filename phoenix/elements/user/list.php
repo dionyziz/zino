c00475cd33454a1d31d4f7f89069c5baf2544be9
Chris Pappas <chrispappas12@gmail.com>
2008-06-06 17:22:56 +0000

    user relations

--- phoenix/elements/user/list.php
+++ phoenix/elements/user/list.php
@@ -5,11 +5,11 @@
 			<ul><?php
 				foreach ( $relations as $relation ) {
 					?><li><a href="<?php
-					Element( 'user/url' , $relation->User );
+					Element( 'user/url' , $relation->Friend );
 					?>"><?php
-					Element( 'user/avatar' , $relation->User , 100 , '' , '' , false , 0 , 0 );
+					Element( 'user/avatar' , $relation->Friend , 100 , '' , '' , false , 0 , 0 );
 					?><strong><?php
-					echo Element( 'user/name' , $relation->User , false );
+					echo Element( 'user/name' , $relation->Friend , false );
 					?></strong><span>προβολή προφίλ &raquo;</span></a></li><?php
 				}			
 			?></ul>
