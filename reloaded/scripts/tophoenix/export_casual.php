324014c7d9cdfdc7125b2e364a1b23bbb123db3c
Dionysis Zindros <dionyziz@gmail.com>
2008-06-06 11:53:43 +0000

    reloaded2phoenix

--- reloaded/scripts/tophoenix/export_casual.php
+++ reloaded/scripts/tophoenix/export_casual.php
@@ -7,14 +7,73 @@
 		
 	$water->Enable(); // on for all
 
-    global $db, $shoutbox, $user;
+    global $db, $shoutbox, $user, $images, $albums, $polls, $polloptions, $votes, $universities;
 
     if ( !$user->IsSysOp() ) {
         die( 'Permission denied' );
     }
 
+    function MigrateAsIs( $from, $to, $fields = false ) {
+        global $db;
+
+        if ( $fields === false ) {
+            $selectfields = '*';
+        }
+        else {
+            $selectfields = implode( ',', array_keys( $fields ) );
+            $out = array();
+            foreach ( $fields as $fromfield => $tofield ) {
+                if ( is_numeric( $fromfield ) ) {
+                    $out[ $tofield ] = $tofield;
+                }
+                else {
+                    $out[ $fromfield ] = $tofield;
+                }
+            }
+            $fields = $out;
+        }
+
+        $res = $db->Query(
+            "SELECT
+                $selectfields
+            FROM
+                $from;"
+        );
+        ?>TRUNCATE TABLE `<?php
+        echo $to;
+        ?>`;<?php
+        while ( $row = $res->FetchArray() ) {
+            ?>INSERT INTO `<?php
+            echo $to;
+            ?>` SET <?php
+            $values = array();
+            foreach ( $row as $field => $value ) {
+                $value = addslashes( $value );
+                if ( $fields === false ) {
+                    $targetfield = $field;
+                }
+                else {
+                    $targetfield = $fields[ $field ];
+                }
+                $values[] = "$targetfield = '$value'";
+            }
+            echo implode( ',', $values );
+            ?>;<?php
+        }
+    }
 
     header( 'Content-type: text/html; charset=utf8' );
+
+    $res = $db->Query(
+        "SELECT
+            `uni_id`, `uni_typeid`
+        FROM
+            `universities`;"
+    );
+    $unitypes = array();
+    while ( $row = $res->FetchArray() ) {
+        $unitypes[ $row[ 'uni_id' ] ] = $row[ 'uni_typeid' ];
+    }
 
     // migrate users
     $res = $db->Query(
@@ -64,18 +123,22 @@
             ?>', `profile_uniid` = '<?php
             echo $row[ 'user_uniid' ];
             ?>', `profile_education` = '<?php
-            if ( empty( $row[ 'user_uniid' ] ) ) {
+            if ( empty( $row[ 'user_uniid' ] ) ) { // no uni set
                 ?>-<?php
             }
             else {
-                ?>-<?php // TODO: find out if their uni is a TEI or a university
+                if ( isset( $unitypes[ $row[ 'user_uniid' ] ] ) && $unitypes[ $row[ 'user_uniid' ] ] != 0 ) {
+                    ?>TEI<?php
+                }
+                else {
+                    ?>university<?php
+                }
             }
             ?>', `profile_sexualorientation` = '-', `profile_religion` = '-', 
             `profile_politics` = '-', `profile_aboutme` = '', `profile_moodid` = '0', 
             `profile_eyecolor`='-', `profile_haircolor`='-', `profile_height`='0', 
             `profile_weight`='0', `profile_smoker`='-', `profile_drinker`='-',
             `profile_favquote`='', `profile_skype`='<?php
-            // TODO: decide about default mood
             // TODO: haircolor/eyecolor/height/weight heuristic
             echo $row[ 'user_skype' ];
             ?>', `profile_msn`='<?php
@@ -94,7 +157,7 @@
             echo $row[ 'user_contribs' ];
             ?>, `count_shouts`=<?php
             echo $row[ 'user_numsmallnews' ];
-            // TODO: count polls, journals, albums
+            // TODO: count journals
             ?>;<?php
         ?>INSERT INTO `usersettings` SET
             `setting_userid`=LAST_INSERT_ID(), `setting_emailprofile`='yes', `setting_emailphotos`='yes', `setting_emailjournals`='yes', `setting_emailpolls`='yes', `setting_emailreplies`='yes', `setting_emailfriends`='yes', `setting_notifyprofile`='yes', `setting_notifyphotos`='yes', `setting_notifyjournals`='yes', `setting_notifypolls`='yes', `setting_notifyreplies`='yes', `setting_notifyfriends`='yes';<?php
@@ -108,6 +171,7 @@
         FROM
             `$albums`;"
     );
+
     ?>TRUNCATE TABLE `albums`;<?php
     while ( $row = $res->FetchArray() ) {
         ?>INSERT INTO `albums` SET
@@ -127,8 +191,93 @@
             echo $row[ 'album_description' ];
             ?>', `album_delid`=<?php
             echo $row[ 'album_delid' ];
-            ?>, `album_pageviews`=0, `album_numcomments`=0, `album_numphotos`=0;<?php // TODO: fill-in those values
+            ?>, `album_numcomments`=<?php
+            echo $row[ 'album_numcomments' ];
+            ?>, `album_numphotos`=0;<?php
     }
+    
+    // migrate images
+    $res = $db->Query(
+        "SELECT
+            `image_id`, `image_userid`, `image_created`, `image_userip`, `image_name`, `image_mime`,
+            `image_width`, `image_height`, `image_size`, `image_delid`, `image_albumid`, `image_description`,
+            `image_numcomments`
+        FROM
+            `$images`"
+    );
+    ?>TRUNCATE TABLE `images`;<?php
+    while ( $row = $res->FetchArray() ) {
+        ?>INSERT INTO `images` SET
+            `image_id`=<?php
+            echo $row[ 'image_id' ];
+            ?>, `image_userid`=<?php
+            echo $row[ 'image_userid' ];
+            ?>, `image_created`=<?php
+            echo $row[ 'image_created' ];
+            ?>, `image_userip`=<?php
+            echo ip2long( $row[ 'image_userip' ] );
+            ?>, `image_name`=<?php
+            echo addslashes( $row[ 'image_name' ] );
+            ?>, `image_mime`='<?php
+            echo addslashes( $row[ 'image_mime' ] );
+            ?>', `image_width`=<?php
+            echo $row[ 'image_width' ];
+            ?>, `image_height`=<?php
+            echo $row[ 'image_height' ];
+            ?>, `image_size`=<?php
+            echo $row[ 'image_size' ];
+            ?>, `image_delid`=<?php
+            echo $row[ 'image_delid' ];
+            ?>, `image_albumid`=<?php
+            echo $row[ 'image_albumid' ];
+            ?>, `image_numcomments`=<?php
+            echo $row[ 'image_numcomments' ];
+            ?>;<?php
+            // TODO: migrate images to serverv2 and recalculate width/height/size
+    }
+
+    // fill in numphotos in albums
+    ?>UPDATE
+        `albums` LEFT JOIN (
+            SELECT
+                COUNT(*) AS numimages, image_albumid AS albumid
+            FROM
+                `images`
+            GROUP BY
+                `image_albumid`
+        ) AS tmp
+    SET
+        `albums`.`album_numphotos`=tmp.numimages
+    WHERE
+        tmp.albumid=`albums`.`album_id`;<?php
+
+    // migrate polls
+    MigrateAsIs( $polls, 'polls' );
+    MigrateAsIs( $votes, 'votes', array( 'vote_userid', 'vote_date' => 'vote_created', 'vote_optionid', 'vote_pollid' ) );
+    MigrateAsIs( $polloptions, 'polloptions' );
+
+    // count polls and albums
+    ?>UPDATE
+        `usercounts` LEFT JOIN (
+            SELECT
+                `poll_userid` AS userid, COUNT(*) AS countpolls
+            FROM
+                `polls`
+            GROUP BY
+                `poll_userid`
+        ) AS tmp ON `count_userid` = tmp.userid
+        LEFT JOIN (
+            SELECT
+                `album_userid` AS userid, COUNT(*) AS countalbums
+            FROM
+                `albums`
+            GROUP BY
+                `album_userid`
+        ) AS tmp1 ON `count_userid` = tmp1.userid
+    SET
+        `usercounts`.`count_polls`=tmp.countpolls,
+        `usercounts`.`count_albums`=tmp1.countalbums;
+    <?php
 
     // migrate shouts
     $res = $db->Query(
