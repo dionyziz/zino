508d6eb277a9fdd946c552ddac27c7af748f5141
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-06 14:55:56 +0000

    Tag Types constants

--- phoenix/libs/tag.php
+++ phoenix/libs/tag.php
@@ -1,6 +1,11 @@
 <?php
-    define( 'TAG_BOOK', 1 );
+    define( 'TAG_HOBBIE', 1 );
     define( 'TAG_MOVIE', 2 );
+    define( 'TAG_BOOK', 3 );
+    define( 'TAG_SONG', 4 );
+    define( 'TAG_ARTIST', 5 );
+    define( 'TAG_GAME', 6 );
+    define( 'TAG_SHOW', 7 );
 
 	function is_tag( $tag ) {
 		if ( ( $tag instanceof Tag ) && $tag->Exists() ) {
