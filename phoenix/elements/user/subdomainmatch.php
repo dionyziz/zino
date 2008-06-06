0e3b0b84186b7d73e30fc16632cc319cf7ec6e6e
Dionysis Zindros <dionyziz@gmail.com>
2008-06-06 16:13:38 +0000

    coala on https VS http and subdomains

--- phoenix/elements/user/subdomainmatch.php
+++ phoenix/elements/user/subdomainmatch.php
@@ -1,10 +1,31 @@
 <?php
     function ElementUserSubdomainmatch() {
         global $page;
+        global $xc_settings;
+
+        $subdomains = explode( '*', $xc_settings[ 'usersubdomains' ], 2 );
+        $subdomains[ 0 ] = preg_quote( $subdomains[ 0 ], '#' );
+        $subdomains[ 1 ] = preg_quote( $subdomains[ 1 ], '#' );
+
+        if ( !empty( $_SERVER[ 'HTTPS' ] ) ) {
+            $currentaddress = 'https://';
+        }
+        else {
+            $currentaddress = 'http://';
+        }
+
+        $currentaddress .= $_SERVER[ 'HTTP_HOST' ] . $_SERVER[ 'REQUEST_URI' ];
+
+        if ( preg_match( '#^' . $subdomains[ 0 ] . '[a-zA-Z0-9-]+' . $subdomains[ 1 ] . '#', $currentaddress, $matches ) ) {
+            $coalabase = $matches[ 0 ];
+        }
+        else {
+            $coalabase = $xc_settings[ 'webaddress' ];
+        }
 
         ob_start();
         ?>alert( <?php
-        echo w_json_encode( $_SERVER[ 'HTTP_HOST' ] . $_SERVER[ 'REQUEST_URI' ] );
+        echo w_json_encode( $coalabase );
         ?> );<?php
         $page->AttachInlineScript( ob_get_clean() );
     }
