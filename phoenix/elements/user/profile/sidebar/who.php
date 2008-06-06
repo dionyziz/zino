6e54aeb606ccc0dd35ff25eaff2a44e40ae2a7fe
Chris Pappas <chrispappas12@gmail.com>
2008-06-06 19:39:24 +0000

    user relations

--- phoenix/elements/user/profile/sidebar/who.php
+++ phoenix/elements/user/profile/sidebar/who.php
@@ -1,7 +1,7 @@
 <?php
 	function ElementUserProfileSidebarWho( $theuser ) {
 		?><h2><?php
-			Element( 'user/avatar' , $theuser , 150 );
+			Element( 'user/avatar' , $theuser , 150 , false , 0 , 0 );
 			?><span class="name"><?php
 			Element( 'user/name' , $theuser , false );
 			?></span>
