b4865e21bb143c098fac850f14942cf7d9b42c8e
Chris Pappas <chrispappas12@gmail.com>
2008-06-07 02:16:09 +0000

    moods profile

--- phoenix/elements/user/profile/sidebar/mood.php
+++ phoenix/elements/user/profile/sidebar/mood.php
@@ -1,8 +1,31 @@
 <?php
 	function ElementUserProfileSidebarMood( $theuser ) {
-		?><div class="mood">
-			<strong>Διάθεση</strong> 
-			<img src="../../../images/avatars/cool.gif" alt="cool" title="cool" />
-		</div><?php
+		global $xc_settings;
+		
+		if ( $theuser->Profile->Mood->Exists() ) {
+			?><div class="mood">
+				<strong>Διάθεση</strong> 
+				<img src="<?php
+				echo $xc_settings[ 'staticimagesurl' ];
+				?>moods/<?php
+				echo $theuser->Profile->Mood->Url;
+				?>"<?php
+				if ( $theuser->Gender == 'f' ) {
+					?> alt="<?php
+					echo htmlspecialchars( $theuser->Profile->Mood->Labelfemale );
+					?>" title="<?php
+					echo htmlspecialchars( $theuser->Profile->Mood->Labelfemale );
+					?>"<?php
+				} 
+				else {
+					?> alt="<?php
+					echo htmlspecialchars( $theuser->Profile->Mood->Labelmale );
+					?>" title="<?php
+					echo htmlspecialchars( $theuser->Profile->Mood->Labelmale );
+					?>"<?php
+				}
+				?>/>
+			</div><?php
+		}
 	}
 ?>
