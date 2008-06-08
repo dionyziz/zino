d79a1d9a4e9dd06fef078a9e5c9cd21ed1dcc920
Chris Pappas <chrispappas12@gmail.com>
2008-06-08 20:47:37 +0000

    settings password

--- phoenix/js/user/settings.js
+++ phoenix/js/user/settings.js
@@ -526,14 +526,10 @@ $( document ).ready( function() {
 					}
 				}
 			}
-			return false;
-		} )
-		.keydown( function( event ) {
 			if ( Settings.newpassworderror && Settings.newpassword.value.length >= 4 ) {
 				Settings.newpassworderror = false;
 				$( '#newpassword div span' ).fadeOut( 400 );
 			}
-			return false;
 		} );
 		$( Settings.renewpassword ).change( function() {
 			if ( Settings.oldpassword.value && Settings.newpassword.value && Settings.renewpassword.value ) {
@@ -561,6 +557,8 @@ $( document ).ready( function() {
 					}
 				}
 			}
+		} )
+		.keydown( function( event ) {
 			if ( Settings.renewpassworderror && Settings.renewpassword.value == Settings.newpassword.value ) {
 				Settings.renewpassworderror = false;
 				$( '#renewpassword div span' ).fadeOut( 400 );
