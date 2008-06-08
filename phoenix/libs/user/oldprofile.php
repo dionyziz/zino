e9b7241ea17c50e3068eb9020dccf12b5b6e1d77
Aleksis Brezas <abresas@gmail.com>
2008-06-08 14:55:54 +0000

    old profile

---
+++ phoenix/libs/user/oldprofile.php
@@ -0,0 +1,25 @@
+<?php
+
+    class OldUserProfile extends Satori {
+        protected $mDbTableAlias = 'olduserprofiles';
+        
+        protected function Relations() {
+            $this->Location = $this->HasOne( 'Place', 'Placeid' );
+            $this->Uni = $this->HasOne( 'Uni', 'Uniid' );
+            $this->Mood = $this->HasOne( 'Mood', 'Moodid' );
+        }
+		protected function LoadDefaults() {
+			$this->Education = '-';
+			$this->Sexualorientation = '-';
+			$this->Religion = '-';
+			$this->Politics = '-';	
+			$this->Eyecolor = '-';
+			$this->Haircolor = '-';
+			$this->Smoker = '-';
+			$this->Drinker = '-';
+			$this->Height = -1;
+			$this->Weight = -1;
+		}
+    }
+
+?>
