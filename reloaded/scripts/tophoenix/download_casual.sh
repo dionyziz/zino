b881ac158aa382438fc7e719a5b6c9c9e74de6f9
Aleksis Brezas <abresas@gmail.com>
2008-06-09 21:57:03 +0000

    download offsets

--- reloaded/scripts/tophoenix/download_casual.sh
+++ reloaded/scripts/tophoenix/download_casual.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-standalonesteps=( 0 1 3 5 6 8 9 10 11 12 13 14 15 )
+
+echo "CONTINUE" > ~/migrate/continue
+
+standalonesteps=( 0 1 6 8 9 10 11 12 13 14 15 )
 for i in "${standalonesteps[@]}"
 do
     wget http://www.zino.gr/scripts/tophoenix/export_casual.php?step=$i -O ~/migrate/$i.sql.gz
 done
 
+offsetsteps=( 2 3 4 5 7 )
+for i in "${offsetsteps[@]}"
+do
+    offset=0
+    while true; do
+        wget http://www.zino.gr/scripts/tophoenix/export_casual.php?step=$i&testoffset=$i -O ~/migrate/$i-$offset-test
+        diff ~/migrate/continue ~/migrate/$i-$offset-test
+
+        if [[ $? -eq 0 ]]; then
+            break;
+        fi
+
+        wget http://www.zino.gr/scripts/tophoenix/export_casual.php?step=$i&offset=$i -O ~/migrate/$i-$offset.sql.gz
+
+        offset=$(expr $offset + 1)
+    done
+done
