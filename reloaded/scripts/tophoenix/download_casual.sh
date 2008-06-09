ff0b39c8231416a1f11a7bc79c31e43080b8dd8c
Dionysis Zindros <dionyziz@gmail.com>
2008-06-09 20:37:01 +0000

    migrate

--- reloaded/scripts/tophoenix/download_casual.sh
+++ reloaded/scripts/tophoenix/download_casual.sh
@@ -2,6 +2,6 @@
 standalonesteps=( 0 1 3 5 6 8 9 10 11 12 13 14 15 )
 for i in "${standalonesteps[@]}"
 do
-    wget http://www.zino.gr/scripts/tophoenix/export_casual.php?step=$i -O >~/migrate/$i.sql.gz
+    wget http://www.zino.gr/scripts/tophoenix/export_casual.php?step=$i -O ~/migrate/$i.sql.gz
 done
 
