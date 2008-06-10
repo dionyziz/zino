a6bea2b8bebfe4745286778552fef1bc8e3e12b6
Dionysis Pantazopoulos <d3nnn1z@gmail.com>
2008-06-10 20:39:43 +0000

    Questions library

--- phoenix/libs/user/user.php
+++ phoenix/libs/user/user.php
@@ -150,7 +150,7 @@
             $this->Count = $this->HasOne( 'UserCount', 'Id' );
             $this->Journals = $this->HasMany( 'JournalFinder', 'FindByUser', $this );
             $this->Albums = $this->HasMany( 'AlbumFinder', 'FindByUser', $this );
-            $this->Answers = $this->HasMany( 'AnswersFinder', 'FindByUser', $this );
+            $this->Answers = $this->HasMany( 'AnswerFinder', 'FindByUser', $this );
             $this->Space = $this->HasOne( 'UserSpace', 'Id' );
             $this->LastActivity = $this->HasOne( 'UserLastActive', 'Id' );
             $this->EgoAlbum = $this->HasOne( 'Album', 'Egoalbumid' );
