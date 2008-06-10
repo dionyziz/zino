afb9f639a773651c402e7fdf5e53c9e81a17fc56
Dionysis Zindros <dionyziz@gmail.com>
2008-06-10 19:39:07 +0000

    WYSIWYG youtube

---
+++ phoenix/libs/wysiwyg.php
@@ -0,0 +1,40 @@
+<?php
+    global $libs;
+
+    $libs->Load( 'sanitizer' );
+
+    function WYSIWYG_PostProcess( $html ) {
+        global $xhtmlsanitizer_goodtags, $rabbit_settings;
+
+        // YouTube support
+        $html = preg_replace( 
+            '#\<img\s*src\=["\']?' 
+            . $rabbit_settings[ 'imagesurl' ] 
+            . 'video-placeholder\.png?v\=([a-zA-Z0-9_-]+)["\']?\s*/?\>#i', 
+            '<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/\1"></param><embed src="http://www.youtube.com/v/\1" type="application/x-shockwave-flash" width="425" height="344"></embed></object>', 
+            $html
+        );
+
+        $sanitizer = New XHTMLSanitizer();
+
+        foreach ( $xhtmlsanitizer_goodtags as $tag => $attributes ) {
+            if ( $tag == '' ) {
+                continue;
+            }
+
+            $goodtag = New XHTMLSaneTag( $tag );
+            if ( is_array( $attributes ) ) {
+                foreach ( $attributes as $attribute => $true ) {
+                    $goodtag->AllowAttribute( New XHTMLSaneAttribute( $attribute ) );
+                }
+            }
+            foreach ( $xhtmlsanitizer_goodtags[ '' ] as $attribute => $true ) {
+                $goodtag->AllowAttribute( New XHTMLSaneAttribute( $attribute ) );
+            }
+            $sanitizer->AllowTag( $goodtag );
+        }
+        $sanitizer->SetSource( $html );
+
+        return $sanitizer->GetXHTML();
+    }
+?>
