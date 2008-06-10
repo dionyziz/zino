27340d0373c2b1d1c09334bec5eaca2695f3601e
Dionysis Zindros <dionyziz@gmail.com>
2008-06-10 13:28:37 +0000

    events strtolower

--- phoenix/libs/rabbit/helpers/string.php
+++ phoenix/libs/rabbit/helpers/string.php
@@ -8,6 +8,15 @@
 	function utf8_substr( $string, $start, $len ) {
 		return mb_substr( $string, $start, $len, 'UTF-8' );
 	}
+    function utf8_strtolower( $string ) {
+        return mb_convert_case( $string, MB_CASE_LOWER, 'UTF-8' );
+    }
+    function utf8_strtoupper( $string ) {
+        return mb_convert_case( $string, MB_CASE_UPPER, 'UTF-8' );
+    }
+    function utf8_ucfirst( $string ) {
+        return mb_convert_case( utf8_substr( $string, 0, 1 ), MB_CASE_UPPER ) . mb_convert_case( utf8_substr( $string, 1, mb_strlen( $string ) - 1 ), MB_CASE_LOWER );
+    }
 	function myescape( $str ) {
 		return mysql_escape_string( $str );
 	}
