67bf656c5b9a6fe4a30c18fcd1377099bfe51e21
Chris Pappas <chrispappas12@gmail.com>
2008-06-11 21:47:41 +0000

    notification settings

--- phoenix/elements/user/profile/main/view.php
+++ phoenix/elements/user/profile/main/view.php
@@ -76,14 +76,16 @@
 					else {
 						Element( 'user/list' , $friends );
 					}
-				?><a href="?p=friends&amp;subdomain=<?php
-				echo $theuser->Subdomain;
-				?>" class="button">Περισσότεροι φίλοι&raquo;</a>
-				</div>
-				<div class="barfade">
-					<div class="leftbar"></div>
-					<div class="rightbar"></div>
-				</div><?php
+				if ( $theuser->Count->Relations > 5 ) {
+					?><a href="?p=friends&amp;subdomain=<?php
+					echo $theuser->Subdomain;
+					?>" class="button">Περισσότεροι φίλοι&raquo;</a>
+					</div>
+					<div class="barfade">
+						<div class="leftbar"></div>
+						<div class="rightbar"></div>
+					</div><?php
+				}
 			}
 			if ( !empty( $polls ) || ( $user->Id == $theuser->Id && $user->Count->Polls == 0 ) ) {
 				?><div class="lastpoll">
