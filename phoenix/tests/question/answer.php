d9e08d655a3fb9a0b7078fe06f8c2d5bfe76d96a
Dionysis Pantazopoulos <d3nnn1z@gmail.com>
2008-06-12 21:47:56 +0000

    Question testcases

--- phoenix/tests/question/answer.php
+++ phoenix/tests/question/answer.php
@@ -7,6 +7,18 @@
             $this->Assert( class_exists( 'Answer' ), 'Class Answer does not exist' );
             $this->Assert( class_exists( 'AnswerFinder' ), 'Class AnswerFinder does not exist' );
         }
+
+		public function TestMethodsExist() {
+            $finder = New AnswerFinder();
+            $this->Assert( method_exists( $finder, 'Count' ), 'AnswerFinder::Count method does not exist' );
+            $this->Assert( method_exists( $finder, 'FindAll' ), 'AnswerFinder::FindAll method does not exist' );
+            $this->Assert( method_exists( $finder, 'FindByUser' ), 'AnswerFinder::FindByUser method does not exist' );
+        }
+
+        public function TestCreate() {
+			// TODO
+		}
+
         
 		public function SetUp() {
         	// TODO
--- phoenix/tests/question/question.php
+++ phoenix/tests/question/question.php
@@ -7,6 +7,20 @@
             $this->Assert( class_exists( 'Question' ), 'Class Question does not exist' );
             $this->Assert( class_exists( 'QuestionFinder' ), 'Class QuestionFinder does not exist' );
         }
+
+		public function TestMethodsExist() {
+            $finder = New QuestionFinder();
+            $this->Assert( method_exists( $finder, 'Count' ), 'QuestionFinder::Count method does not exist' );
+            $this->Assert( method_exists( $finder, 'FindAll' ), 'QuestionFinder::FindAll method does not exist' );
+            $this->Assert( method_exists( $finder, 'FindRandomByUser' ), 'QuestionFinder::FindRandomByUser method does not exist' );
+            $this->Assert( method_exists( $finder, 'FindNewQuestion' ), 'QuestionFinder::FindNewQuestion method does not exist' );
+        }
+
+        public function TestCreate() {
+			// TODO
+		}
+
+
         public function SetUp() {
 			// TODO
         }
