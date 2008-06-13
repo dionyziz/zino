253c1a4b0db59b1f7603b169f38e1c30a7658559
Chris Pappas <chrispappas12@gmail.com>
2008-06-13 15:57:09 +0000

    notifications frontend

--- phoenix/units/notification/delete.do.php
+++ phoenix/units/notification/delete.do.php
@@ -2,9 +2,10 @@
 	
 	function UnitNotificationDelete( tInteger $eventid ) {
 		global $user;
+		global $libs;
 		
+		$libs->Load( 'notification' );
 		$eventid = $eventid->Get();
-		
 		$notif = New Notification( $eventid->Get() );
 		
 		if ( $notif->ToUser->Id == $user->Id ) {
