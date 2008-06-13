2343097d2a9ac91a5fb76e82ef5bd08932a339f6
Chris Pappas <chrispappas12@gmail.com>
2008-06-13 20:55:05 +0000

    notifications deletion at elements

--- phoenix/js/notification/view.js
+++ phoenix/js/notification/view.js
@@ -1,12 +1,12 @@
 var Notification = {
 	Visit : function( url , typeid , eventid , commentid ) {
-		alert( 'typeid is ' + typeid + ' eventid is ' + eventid + ' commentid is ' + commentid );
 		if ( typeid == 3 ) {
 			if ( $( '#comment_' + commentid )[ 0 ] ) {
 				window.location.hash = 'comment_' + commentid;
+				Notification.Delete( eventid );
 			}
 			else {
-				alert( url );
+				//alert( url );
 				document.location.href = url;
 			}
 		} 
