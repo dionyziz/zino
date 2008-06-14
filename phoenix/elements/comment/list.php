b1fc1383b596d2e12dc75f818b024962d18f628f
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-14 12:18:20 +0000

    Comments numchildren array

--- phoenix/elements/comment/list.php
+++ phoenix/elements/comment/list.php
@@ -1,9 +1,10 @@
 <?php
 	function ElementCommentList( $comments ) {
         global $water;
+        global $page;
 
         $water->Trace( 'In comments list: Got ' . count( $comments ) . ' comment parents and ' . count( $comments[ 0 ] ) . ' root comments' );
-
+		$jsarr = "Comments.numchildren = { ";
 		$stack = array();
 		for ( $i = count( $comments[ 0 ] ) - 1; $i >= 0; --$i ) {
 			array_push( $stack, array( $comments[ 0 ][ $i ], 0 ) );
@@ -13,15 +14,19 @@
 			$comment = $item[ 0 ];
 			$root = $comment->Id;
 			$indent = $item[ 1 ];
+			$children = isset( $comments[ $root ] ) ? count( $comments[ $root ] ) : 0;
 
-			Element( 'comment/view', $comment, $indent, isset( $comments[ $comment->Id ] ) ? count( $comments[ $comment->Id ] ) : 0 );
+			Element( 'comment/view', $comment, $indent, $children );
 			
-            if ( isset( $comments[ $root ] ) ) {
-                for ( $i = count( $comments[ $root ] ) - 1; $i >= 0; --$i ) {
-                    array_push( $stack, array( $comments[ $root ][ $i ], $indent + 1 ) );
-                }
+			$jsarr .= $root . " : " . $children . ", ";
+			
+         	for ( $i = $children - 1; $i >= 0; --$i ) {
+                array_push( $stack, array( $comments[ $root ][ $i ], $indent + 1 ) );
             }
 		}
+		$jsarr = substr( $jsarr, 0, -2);
+		$jsarr .= " };";
+		$page->AttachInlineScript( $jsarr );
 	}
 /*
 	function ElementCommentList( $comments , $root , $indent ) {
