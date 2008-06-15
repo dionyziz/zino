8568a4b6a11ccde27edea0299ad1397a71e2f26f
Chris Pappas <chrispappas12@gmail.com>
2008-06-15 11:52:17 +0000

    join page

--- phoenix/js/frontpage.js
+++ phoenix/js/frontpage.js
@@ -27,7 +27,7 @@ $( document ).ready( function() {
 		$( 'div.frontpage div.inshoutbox div.shoutbox div.comments div.newcomment div.bottom input' ).click( function() {
 			var list = $( 'div.frontpage div.inshoutbox div.shoutbox div.comments' );
 			var text = $( list ).find( 'div.newcomment div.text textarea' )[ 0 ].value;
-			if ( text == '' ) {
+			if ( $.trim( text ) == '' ) {
 				alert( 'Δε μπορείς να δημοσιεύσεις κενό μήνυμα' );
 			}
 			else {
