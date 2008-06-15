2628a4d2f6592314596f173e9873f31d1666a717
Chris Pappas <chrispappas12@gmail.com>
2008-06-15 11:01:03 +0000

    profile details

--- phoenix/elements/user/profile/main/view.php
+++ phoenix/elements/user/profile/main/view.php
@@ -85,7 +85,7 @@
 					}
 					Element( 'user/profile/main/photos' , $images , $egoalbum );
 					if ( $theuser->Count->Albums > 1 ) {
-						?><div><a href="" class="button">Προβολή albums&raquo;</a></div><?php
+						?><div class="viewalbums"><a href="" class="button">Προβολή albums&raquo;</a></div><?php
 					}
 				}
 				else {
