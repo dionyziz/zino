52e8a9c77df370b218a2048baac0b7a432d76c35
Chris Pappas <chrispappas12@gmail.com>
2008-06-15 10:15:43 +0000

    notifications newrelation

--- phoenix/elements/user/profile/main/view.php
+++ phoenix/elements/user/profile/main/view.php
@@ -32,8 +32,7 @@
 		$polls = $finder->FindByUser( $theuser , 0 , 1 );
 		$finder = New JournalFinder();
 		$journals = $finder->FindByUser( $theuser , 0 , 1 );
-		//$egoalbum = New Album( $theuser->Egoalbumid );
-		$egoalbum = $theuser->Egoablum;
+		$egoalbum = New Album( $theuser->Egoalbumid );
 		if ( $egoalbum->Numphotos > 0 ) {
 			$finder = New ImageFinder();
 			$images = $finder->FindByAlbum( $egoalbum , 0 , 10 );
