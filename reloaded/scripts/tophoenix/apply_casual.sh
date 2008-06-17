d879c26592667fa13770c52eea6e3444516e392e
Dionysis Zindros <dionyziz@gmail.com>
2008-06-17 21:25:45 +0000

    migrate

--- reloaded/scripts/tophoenix/apply_casual.sh
+++ reloaded/scripts/tophoenix/apply_casual.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for i in `find -iname "*.sql"|awk -F / '{print $2}'|sort -n`
+for i in `find -iname "*.sql"|awk -F / '{print $2}'|sort -n`; do
     echo $i #|xargs cat|mysql --database=zinophoenix -u zinophoenix --password=rm99fixed
+done
 
