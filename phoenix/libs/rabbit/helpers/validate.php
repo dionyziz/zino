ba864d294b1e5e6268eec44a5ea83f6374d69047
Dionysis Zindros <dionyziz@gmail.com>
2008-06-17 19:48:57 +0000

    Email validation and testcases

--- phoenix/libs/rabbit/helpers/validate.php
+++ phoenix/libs/rabbit/helpers/validate.php
@@ -9,6 +9,18 @@
 
 	function ValidURL( $url ) {
 		$pattern = "/^(http|https)\:\/\/[a-zA-Z0-9_\/\?\=\-%\:\.#&]*$/";
-		return preg_match( $pattern, $url );
+		return ( bool )preg_match( $pattern, $url );
 	}
+
+    function ValidEmail( $email ) {
+        // Partially incorrect:
+        // * Will allow some invalid domain names such as domains starting with a -, or
+        //   domains containing same-label siblings.
+        // * Won't allow the ' character in usernames, which can be valid
+        //
+        // If you need absolutely correct validation, use your own manual string manipulation
+        //
+
+        return ( bool )preg_match( '#^[a-z.0-9%_+-]+@([a-z0-9_-]{,63}\.)*([a-z]{2,4}|museum)$#i', $email );
+    }
 ?>
---
+++ phoenix/tests/rabbit/helpers/validate.php
@@ -0,0 +1,33 @@
+<?php
+    class TestValidation extends Testcase {
+        protected $mAppliesTo = 'libs/rabbit/helpers/validation';
+
+        public function TestFunctionsExist() {
+            $this->RequireSuccess(
+                $this->Assert( function_exists( 'ValidEmail' ), 'ValidEmail function does not exist' )
+            );
+            $this->RequireSuccess(
+                $this->Assert( function_exists( 'ValidURL' ), 'ValidURL function does not exist' )
+            );
+            $this->RequireSuccess(
+                $this->Assert( function_exists( 'ValidEmail' ), 'ValidEmail function does not exist' )
+            );
+        }
+        public function TestEmail() {
+            $this->Assert( ValidEmail( 'dionyziz@gmail.com' ), 'ValidEmail failed for "dionyziz@gmail.com"' );
+            $this->AssertFalse( ValidEmail( 'hello' ), 'ValidEmail succeeded for "hello"' );
+            $this->Assert( ValidEmail( 'dionysis.zindros@dionyziz.com' ), 'ValidEmail failed for "dionysis.zindros@dionyziz.com"' );
+            $this->Assert( ValidEmail( 'a@b.gr' ), 'ValidEmail failed for "a@b.gr"' );
+            $this->Assert( ValidEmail( 'admin-foo_bar@baz.aero' ), 'ValidEmail failed for "admin-foo_bar@baz.aero' );
+            $this->Assert( ValidEmail( 'admin@help.a123456789012345678901234567890123456789012345678901234567890b.com' ), 'ValidEmail failed for valid long domain name' );
+            $this->AssertFalse( ValidEmail( 'admin@help.a123456789012345678901234567890123456789012345678901234567890abcdefghijk.com' ), 'ValidEmail succeeded for too long domain name' );
+            $this->Assert( ValidEmail( 'bob@virtual.museum' ), 'ValidEmail doesn\'t allow .museum domains' );
+            $this->AssertFalse( ValidEmail( '@kamibu.com' ), 'ValidEmail succeeded with an empty username' );
+            $this->AssertFalse( ValidEmail( 'bob@' ), 'ValidEmail succeeded with an empty username' );
+            $this->AssertFalse( ValidEmail( 'bob@alice@kamibu.com' ), 'ValidEmail succeeded with a double @' );
+            $this->AssertFalse( ValidEmail( 'bob..brown@kamibu.com' ), 'ValidEmail succeeded with a double .' );
+        }
+    }
+
+    return New TestValidation();
+?>
