a4b7639b2452c2059dbea8876e205792ee78ac47
Dimitris Karagasidis <dkaragasidis@gmail.com>
2008-06-17 20:28:20 +0000

    sendmail action added

---
+++ phoenix/elements/about/advertise/sendmail.php
@@ -0,0 +1,31 @@
+<?php
+	function ActionSendMail( tString $from, tString $text ) {
+		global $libs;
+		$libs->Load( 'rabbit/helpers/validate' );
+		
+		// Hardcoded stuff
+		$to = "abresas@gmail.com, dionyziz@gmail.com, chrispappas12@gmail.com";
+		$subject = "Zino: Διαφημίσεις";
+		$header = "From: admin@zino.gr";
+
+		// Get parameters
+		$from = $from->Get();
+		$text = $text->Get();
+		
+		// Check if e-mail is valid
+		if ( !ValidEmail( $from ) ) {
+			return Redirect( "?advertise.php?mailsent=no" );
+		}
+		
+		// Prepare text messagea
+		$text .= "\n\nEmail: " . $from;
+		
+		// Send message
+		if ( mail( $to, $subject, $text, $headers ) ) {
+			return Redirect( "?advertise.php?mailsent=yes" );
+		}
+		else {
+			return Redirect( "?advertise.php?mailsent=no" );
+		}
+	}
+?>
