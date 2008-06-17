624c5ed319dbcd37d66366c457f4aa6dfcb9fb56
Dionysis Zindros <dionyziz@gmail.com>
2008-06-17 18:11:43 +0000

    migrate

--- reloaded/scripts/tophoenix/download_casual.sh
+++ reloaded/scripts/tophoenix/download_casual.sh
@@ -2,7 +2,7 @@
 
 workpath="~/migrate"
 echo -n "Starting up... "
-rm $workpath/test $workpath/continue $workpath/*.gz $workpath/*.sql 2>/dev/null
+rm $workpath/test $workpath/continue $workpath/*.gz $workpath/*.sql
 echo "CONTINUE" > ~/migrate/continue
 echo "Okay"
 
@@ -11,7 +11,7 @@ for i in "${standalonesteps[@]}"
 do
     URL="http://www.zino.gr/scripts/tophoenix/export_casual.php?step=$i"
     echo -n "Downloading simple step" $i "... "
-    wget $URL -O ~/migrate/$i.sql.gz 2>/dev/null
+    wget $URL -O $workpath/$i.sql.gz 2>/dev/null
     if [[ $? -ne 0 ]]; then
         echo "Error downloading" $URL
         exit 1
@@ -25,13 +25,13 @@ do
     offset=0
     while true; do
         URL="http://www.zino.gr/scripts/tophoenix/export_casual.php?step=$i&testoffset=$offset" 
-        wget $URL -O ~/migrate/test 2>/dev/null
+        wget $URL -O $workpath/test 2>/dev/null
         if [[ $? -ne 0 ]]; then
             echo "Error downloading" $URL
             exit 1
         fi
 
-        diff ~/migrate/continue ~/migrate/test > /dev/null
+        diff $workpath/continue $workpath/test > /dev/null
 
         if [[ $? -ne 0 ]]; then
             sleep 2
@@ -40,7 +40,7 @@ do
 
         echo -n "Downloading step" $i "(part" $offset ")... "
         URL="http://www.zino.gr/scripts/tophoenix/export_casual.php?step=$i&offset=$offset" 
-        wget $URL -O ~/migrate/$i-$offset.sql.gz 2>/dev/null
+        wget $URL -O $workpath/$i-$offset.sql.gz 2>/dev/null
         if [[ $? -ne 0 ]]; then
             echo "Error downloading" $URL
             break;
@@ -55,13 +55,13 @@ do
 done
 
 echo -n "Cleaning up... "
-rm ~/migrate/test ~/migrate/continue
+rm $workpath/test $workpath/continue
 echo "Done"
 
 echo "Downloaded" `du -sh .|awk '{print $1}'`
 
 echo -n "Decompressing..."
-gzip --decrompess ~/migrate/*.gz
+gzip --decrompess $workpath/*.gz
 echo "Done"
 
 echo -n "Decompressed" `du -sh .|awk '{print $1}'`
