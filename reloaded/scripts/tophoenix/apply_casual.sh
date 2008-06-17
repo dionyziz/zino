373b17f091fba210493c7e395401e37c9d069384
Dionysis Zindros <dionyziz@gmail.com>
2008-06-17 21:27:45 +0000

    migrate

--- reloaded/scripts/tophoenix/apply_casual.sh
+++ reloaded/scripts/tophoenix/apply_casual.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 for i in `find -iname "*.sql"|awk -F / '{print $2}'|sort -n`; do
-    echo $i #|xargs cat|mysql --database=zinophoenix -u zinophoenix --password=rm99fixed
+    echo -n "Applying script $i... "
+    mysql --database=zinophoenix -u zinophoenix --password=rm99fixed <$i
+    echo "Done"
 done
 
