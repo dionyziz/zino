8a0b17a4892f4e95a13cad9368a15f3498854e92
Dimitris Karagasidis <dkaragasidis@gmail.com>
2008-06-18 13:20:54 +0000

    "Help" removed from footer. Wrote a more user-friendly, Greek message for invalid username/password combination.

--- phoenix/elements/footer.php
+++ phoenix/elements/footer.php
@@ -10,7 +10,9 @@
 				<li><a href="contact" onclick="return false">Επικοινώνησε</a></li>
 				<li><a href="?p=tos">Όροι χρήσης</a></li>
 				<li><a href="?p=advertise">Διαφημίσου εδώ</a></li>
+				<!-- Not needed for now....
 				<li><a href="help" onclick="return false">Βοήθεια</a></li>
+				-->
 			</ul>
 			<div class="copy">
 				&copy; 2008 <a href="http://www.kamibu.com/">Kamibu</a>
