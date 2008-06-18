1c1290cf604b34320b1fb08febcbd251232fc3a2
Dimitris Karagasidis <dkaragasidis@gmail.com>
2008-06-18 13:25:20 +0000

    Greek chars shown as "?"

--- phoenix/elements/footer.php
+++ phoenix/elements/footer.php
@@ -10,9 +10,6 @@
 				<li><a href="contact" onclick="return false">Επικοινώνησε</a></li>
 				<li><a href="?p=tos">Όροι χρήσης</a></li>
 				<li><a href="?p=advertise">Διαφημίσου εδώ</a></li>
-				<!-- Not needed for now....
-				<li><a href="help" onclick="return false">Βοήθεια</a></li>
-				-->
 			</ul>
 			<div class="copy">
 				&copy; 2008 <a href="http://www.kamibu.com/">Kamibu</a>
