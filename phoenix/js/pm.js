8990189b45770c699f0eb9d358dadf48a84b2dd2
Dionysis Pantazopoulos <d3nnn1z@gmail.com>
2008-06-18 18:22:57 +0000

    Pms

--- phoenix/js/pm.js
+++ phoenix/js/pm.js
@@ -50,7 +50,6 @@ var pms = {
 		if ( notread ) {
 			//remove the unread icon
 			var unreadicon = $( '#pm_' + pmid + ' div.infobar img' )[ 1 ];
-			alert('Pmid: ' + pmid + ', Folderid: ' + folderid);
 			Coala.Warm( 'pm/expandpm' , { pmid : pmid, folderid: folderid } );
 			if ( unreadicon ) {
 				pms.UpdateUnreadPms( - 1 );
--- phoenix/units/pm/expandpm.do.php
+++ phoenix/units/pm/expandpm.do.php
@@ -7,7 +7,6 @@
     	$libs->Load( 'pm/pm' );
     	$pmid = $pmid->Get();
         $folderid = $folderid->Get();
-        $water->Trace( 'pmid,folderid', $pmid, $folderid );
     	$pm = new PM( $pmid, $folderid );
     	if ( !$pm->IsRead() ) {	
             $pm->Read();
