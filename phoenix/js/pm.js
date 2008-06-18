5ec2ac9409df147d0928f9fdd263b64a713b448b
Dionysis Pantazopoulos <d3nnn1z@gmail.com>
2008-06-18 21:36:21 +0000

    Pms folder delete fix

--- phoenix/js/pm.js
+++ phoenix/js/pm.js
@@ -136,7 +136,7 @@ var pms = {
 			$( '#folder_' + folderid ).animate( { opacity : '0' , height : '0' } , function() {
 				$( this ).remove();
 				if ( !pms.writingnewpm ) {
-					pms.ShowFolderPm( $( '#folders div' )[ 0 ], -1 );
+					pms.ShowFolderPm( $( '#folders div' )[ 0 ], 'inbox' );
 				}
 			} );
 			Coala.Warm( 'pm/deletefolder' , { folderid : folderid } );
