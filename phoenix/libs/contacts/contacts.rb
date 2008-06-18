8ff3f36f1c12c0487cf66bf33b981ccdeb5b0ad4
Makis Kanavidis <makis.k@gmail.com>
2008-06-18 23:26:13 +0000

    rescue ArgumentError

--- phoenix/libs/contacts/contacts.rb
+++ phoenix/libs/contacts/contacts.rb
@@ -17,6 +17,9 @@ contacts = Blackbook.get :username => username, :password => passwd
 rescue Blackbook::BadCredentialsError
 	$stderr.print "Wrong username/password."
 	exit 2
+rescue ArgumentError
+	$stderr.print "Invalid email."
+	exit 3
 end
 contacts.each do |contact|
 	print contact[:name] + "\t" + contact[:email] + "\n"
