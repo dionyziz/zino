efb83e2576a7ac7608435d9a4bffee13742990c0
Dionysis Pantazopoulos <d3nnn1z@gmail.com>
2008-06-18 20:18:29 +0000

    Pms

--- phoenix/js/pm.js
+++ phoenix/js/pm.js
@@ -8,7 +8,7 @@ var pms = {
 	writingnewpm : false,
 	ShowFolder : function( folder , folderid ) {
 		if ( pms.activefolder === 0 ) {
-			pms.node = $( '#folders div' )[ 0 ]
+			pms.node = $( '#folders div' )[ 0 ];
 			pms.activefolder = pms.node;
 		}
 		if ( pms.activefolder != pms.node ) {
