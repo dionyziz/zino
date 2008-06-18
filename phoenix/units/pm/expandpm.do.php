60100229b037699c6b2ff04a57671455eb5b989e
Dionysis Pantazopoulos <d3nnn1z@gmail.com>
2008-06-18 18:07:44 +0000

    Pms

--- phoenix/units/pm/expandpm.do.php
+++ phoenix/units/pm/expandpm.do.php
@@ -2,10 +2,12 @@
     function UnitPmExpandpm( tInteger $pmid, tInteger $folderid ) {
     	global $libs;
     	global $user;
+    	global $water;
     	
     	$libs->Load( 'pm/pm' );
     	$pmid = $pmid->Get();
         $folderid = $folderid->Get();
+        $water->Trace( 'pmid,folderid', $pmid, $folderid );
     	$pm = new PM( $pmid, $folderid );
     	if ( !$pm->IsRead() ) {	
             $pm->Read();
