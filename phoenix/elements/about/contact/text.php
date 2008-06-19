6251db7549a8844f559dd130ea4ad970029da0c1
Dimitris Karagasidis <dkaragasidis@gmail.com>
2008-06-19 13:47:45 +0000

    added some stuff to contact form

--- phoenix/elements/about/contact/text.php
+++ phoenix/elements/about/contact/text.php
@@ -1,11 +1,10 @@
 <?php
     function ElementAboutContactText() {
-        ?>
+        ?><p>Έχετε σχόλια, απορίες ή προτάσεις; Μην διστάσετε να έρθετε σε επαφή μαζί μας, μέσω της παρακάτω φόρμας επικοινωνίας:</p>
 		Email:<br />
-		<input type="text" name="from" /><br /><br />
+		<input type="text" name="from" style="width:250px;"/><br /><br />
 		Σχόλια:<br />
-		<textarea name="text"></textarea>
-		<input type="submit" value="Αποστολή" />
-		<?php
+		<textarea name="text" style="width:400px;height:200px;"></textarea><br />
+		<input type="submit" value="Αποστολή" /><?php
     }
 ?>
