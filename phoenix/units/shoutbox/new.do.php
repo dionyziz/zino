ae57d1a17905241b9d46f5aa05b97a92b747b86f
Dionysis Zindros <dionyziz@gmail.com>
2008-06-22 16:32:29 +0000

    shoutbox frontend

---
+++ phoenix/units/shoutbox/new.do.php
@@ -0,0 +1,30 @@
+<?php
+	function UnitShoutboxNew( tText $text , tCoalaPointer $node ) {
+		global $user;
+		global $libs;
+		
+		$libs->Load( 'shoutbox' );
+		
+		$text = $text->Get();
+		if ( $user->Exists() ) {
+			if ( trim ( $text ) != '' ) {
+				$shout = New Shout();
+				$shout->Text = htmlspecialchars( $text ); // TODO: WYSIWYG
+				$shout->Save();
+				?>$( <?php
+				echo $node;
+				?> )
+				.attr( {
+					id : <?php
+					echo $shout->Id;
+					?> } )
+				.find( 'div.toolbox a' ).click( function( shoutid ) {
+					Frontpage.DeleteShout( '<?php
+					echo $shout->Id;
+					?>' );
+					return false;
+				} );<?php
+			}
+		}
+	}
+?>
