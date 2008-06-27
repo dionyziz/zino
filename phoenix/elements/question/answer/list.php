a4f77f66c699d4134fef578d79c5ab6751252ae8
Dionysis Zindros <dionyziz@gmail.com>
2008-06-27 09:18:07 +0000

    questions frontend

--- phoenix/elements/question/answer/list.php
+++ phoenix/elements/question/answer/list.php
@@ -1,5 +1,40 @@
 <?php
-    function ElementQuestionAnswerList( User $theuser ) {
+	function ElementQuestionAnswerList( tText $username , tText $subdomain , tInteger $pageno ) {
+		global $page;
+		global $user;
+		global $rabbit_settings;
+		global $water;
+		
+		$username = $username->Get();
+		$subdomain = $subdomain->Get();
+		$finder = New UserFinder();
+		if ( $username != '' ) {
+			if ( strtolower( $username ) == strtolower( $user->Name ) ) {
+				$theuser = $user;
+			}
+			else {
+				$theuser = $finder->FindByName( $username );
+			}
+		}
+		else if ( $subdomain != '' ) {
+			if ( strtolower( $subdomain ) == strtolower( $user->Subdomain ) ) {
+				$theuser = $user;
+			}
+			else {
+				$theuser = $finder->FindBySubdomain( $subdomain );
+			}
+		}	
+		if ( !isset( $theuser ) || $theuser === false ) {
+			return Element( '404' );
+		}
+		
+		if ( strtoupper( substr( $theuser->Name, 0, 1 ) ) == substr( $theuser->Name, 0, 1 ) ) {
+			$page->SetTitle( $theuser->Name . " Questions" );
+		}
+		else {
+			$page->SetTitle( $theuser->Name . " questions" );
+		}
+
         $answerfinder = New AnswerFinder();
         $answers = $answerfinder->FindByUser( $theuser );
 
