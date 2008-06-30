4acf40f2be019cc09716a65d60b90e7097adb67b
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-06-30 14:00:46 +0000

    Internet Explorer Warning

--- phoenix/js/browsers_allowed.js
+++ phoenix/js/browsers_allowed.js
@@ -1,4 +1,7 @@
 $( document ).ready( function() {
+	if ( window.location.href.substr( 30 ) != "p=ie" ) {
+		return;
+	}
 	if ( $.browser.msie && $.browser.version < 7 ) {
 		window.location.href = "?p=ie";
 	}
