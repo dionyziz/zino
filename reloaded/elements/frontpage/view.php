fe824726835458ba2ad9c061b735152282af9a3b
Aleksis Brezas <abresas@gmail.com>
2008-06-30 11:25:49 +0000

    prepare users for release

--- reloaded/elements/frontpage/view.php
+++ reloaded/elements/frontpage/view.php
@@ -2,10 +2,15 @@
     function ElementFrontpageView() {
         global $page;
         global $rabbit_settings;
+        global $user;
         
         $page->SetTitle( "Είσαι μέσα?" );
         $page->AttachStylesheet( 'css/sidebar.css' );
         $page->AttachScript( 'js/animations.js' );
+
+        if ( $user->Username() == 'finlandos' || $user->Username() == 'd3nnn1z' ) {
+            ?><div id="phoenixrelease">Μεγάλες αλλαγές σήμερα στο Zino... προετοιμάσου! Όταν ξεκινήσει η αναβάθμιση για λίγο το Zino δε θα λειτουργεί.</div><?php
+        }
         
         Element( "photo/latest" );
         Element( "frontpage/leftbar" );
