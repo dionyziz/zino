5b39c3bd6bc9ff98bccb7d360be690684ebb3d60
Aleksis Brezas <abresas@gmail.com>
2008-07-03 01:41:38 +0000

    site down

--- reloaded/index.php
+++ reloaded/index.php
@@ -1,28 +1,20 @@
 <?php
-    /*
-    This source code and all other source code files in
-    this repository, unless otherwise stated, are
+    echo '<?xml version="1.0" encoding="utf-8"?>';
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="el" lang="el">
+    <head>
+        <title>Zino | Προσωρινά μη διαθέσιμο</title>
+        <link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    </head>
+    <body>
+        <div>
+            <img src="http://static.zino.gr/phoenix/zino-150-reflection.jpg" alt="Zino" style="float:left;margin:10px;" />
+            <div style="margin-left:170px;">
+                <br />Το Zino αναβαθμίζεται και είναι προσωρινά μη διαθέσιμο.<br /><br />
 
-    Copyright (c) 2005 - 2007, Kamibu Development Group.
-    
-    More information can be found at /etc/legal.txt.
-    
-    Please leave this notice only in index.php and do not
-    paste it in other files of the source code repository.
-    */
-
-    global $page;
-    
-	require_once 'libs/rabbit/rabbit.php';
-    
-    Rabbit_Construct( 'HTML' );
-
-    $req = $_GET;
-    
-    Rabbit_ClearPostGet();
-    
-    $page->AttachMainElement( 'main' , $req );
-    $page->Output();
-
-    Rabbit_Destruct();
-?>
+                Δοκίμασε πάλι σε λίγα λεπτά.
+            </div>
+        </div>
+    </body>
+</html>
