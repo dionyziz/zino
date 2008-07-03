49a31de041313a241b302b096d6c6ada0c2da051
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-07-03 23:33:38 +0000

    Add reserved usernames

--- phoenix/libs/user/user.php
+++ phoenix/libs/user/user.php
@@ -20,9 +20,9 @@
     $libs->Load( 'mood' );
 	$libs->Load( 'question/answer' );
 	
-	$reserved = Array( 'anonymous', 'www', 'beta' );
-    
+	
     function User_Valid( $username ) {
+    	$reserved = Array( 'anonymous', 'www', 'beta' );
         return ( ( bool )preg_match( '#^[a-zA-Z][a-zA-Z\-_0-9]{3,19}$#', $username ) && !in_array( $username , $reserved ) );
     }
 	function User_DeriveSubdomain( $username ) {
