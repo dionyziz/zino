f623d6fec7aeac8f52e4d09562660b107e74d8a7
Dimitris Karagasidis <dkaragasidis@gmail.com>
2008-07-04 16:50:56 +0000

    fix

--- phoenix/elements/user/profile/sidebar/social/view.php
+++ phoenix/elements/user/profile/sidebar/social/view.php
@@ -2,11 +2,11 @@
 	function ElementUserProfileSidebarSocialView( $theuser ) {
 		?><?php
 		Element( 'user/profile/sidebar/social/sex' , $theuser );
-		Element( 'user/profile/sidebar/social/smoker' , $theuser );
-		Element( 'user/profile/sidebar/social/drinker' , $theuser );
-		Element( 'user/profile/sidebar/social/education' , $theuser );
-		Element( 'user/profile/sidebar/social/religion' , $theuser );
-		Element( 'user/profile/sidebar/social/politics' , $theuser );
+		//Element( 'user/profile/sidebar/social/smoker' , $theuser );
+		//Element( 'user/profile/sidebar/social/drinker' , $theuser );
+		//Element( 'user/profile/sidebar/social/education' , $theuser );
+		//Element( 'user/profile/sidebar/social/religion' , $theuser );
+		//Element( 'user/profile/sidebar/social/politics' , $theuser );
 		?><?php
 	}
 ?>
