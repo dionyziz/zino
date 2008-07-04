568f68aadbc2bcdc9c230a6cc7909cb7a642cc7a
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-07-04 19:57:45 +0000

    Emoticons in shoutbox

--- phoenix/units/shoutbox/new.do.php
+++ phoenix/units/shoutbox/new.do.php
@@ -7,25 +7,36 @@
 		$libs->Load( 'shoutbox' );
 		
 		$text = $text->Get();
-		if ( $user->Exists() ) {
-			if ( trim ( $text ) != '' ) {
-				$shout = New Shout();
-				$shout->Text = WYSIWYG_PostProcess( htmlspecialchars( $text ) ); // TODO: WYSIWYG
-				$shout->Save();
-				?>$( <?php
-				echo $node;
-				?> )
-				.attr( {
-					id : "s_<?php
-					echo $shout->Id;
-					?>" } )
-				.find( 'div.toolbox a' ).click( function( shoutid ) {
-					Frontpage.DeleteShout( '<?php
-					echo $shout->Id;
-					?>' );
-					return false;
-				} );<?php
-			}
+		if ( !$user->Exists() ) {
+			?>alert( "Πρέπει να είσαι συνδεδεμένος για να συμμετέχεις στην συζήτηση" );
+			window.location.reload();<?php
+			return;
 		}
-	}
+		
+		
+		if ( trim ( $text ) == '' ) {
+			?>alert( "Δεν μπορείς να δημοσιεύσεις κενό μήνυμα" );
+			window.location.reload();<?php
+			return;
+		}
+		
+		$shout = New Shout();
+		$shout->Text = WYSIWYG_PostProcess( htmlspecialchars( $text ) ); // TODO: WYSIWYG
+		$shout->Save();
+		?>$( <?php
+		echo $node;
+		?> )
+		.attr( {
+			id : "s_<?php
+			echo $shout->Id;
+			?>" } )
+		.find( 'div.toolbox a' ).click( function( shoutid ) {
+			Frontpage.DeleteShout( '<?php
+			echo $shout->Id;
+			?>' );
+			return false;
+		} ).end()
+		.find( 'div.text' ).html( "<?php
+			echo $shout->Text;
+		?>" );<?php	}
 ?>
