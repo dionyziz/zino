b13b1f03e872263a6f6b80579be400e05067626a
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-07-05 11:30:55 +0000

    Kamibu Style

--- phoenix/units/user/settings/tags/new.do.php
+++ phoenix/units/user/settings/tags/new.do.php
@@ -1,5 +1,4 @@
 <?php
-	
 	function UnitUserSettingsTagsNew( tText $text , tInteger $typeid , tCoalaPointer $node ) {
 		global $libs;
 		global $user;
@@ -7,24 +6,31 @@
 		$libs->Load( 'tag' );
 		$text = $text->Get();
 		
-		if ( $user->Exists() ) {
-			if ( strlen( $text ) <= 32 ) {
-				$tag = New Tag();
-				$tag->Text = $text;
-				$tag->Typeid = $typeid->Get();
-				$tag->Save();
-				?>$( <?php
-				echo $node;
-				?> ).click( function( id , link ) { 
-					Settings.RemoveInterest( '<?php
-					echo $tag->Id;
-					?>' , <?php
-					echo $node;
-					?> );
-					return false;
-				} )
-				.fadeIn( 200 );<?php
-			}
+		if ( !$user->Exists() ) {
+			?>alert( "Πρέπει να είσαι συνδεδεμένος για να αλλάξεις τα ενδιαφέροντά σου" );
+			window.location.reload();<?php
+			return;
 		}
+		if ( strlen( $text ) > 32 ) {
+			?>alert( "Το κείμενο πρέπει να έχει μήκος λιγότερο από 33 χαρακτήρες" );
+			window.location.reload();<?php
+			return;
+		}
+
+		$tag = New Tag();
+		$tag->Text = $text;
+		$tag->Typeid = $typeid->Get();
+		$tag->Save();
+		?>$( <?php
+		echo $node;
+		?> ).click( function( id , link ) { 
+			Settings.RemoveInterest( '<?php
+			echo $tag->Id;
+			?>' , <?php
+			echo $node;
+			?> );
+			return false;
+		} )
+		.fadeIn( 200 );<?php
 	}
 ?>
