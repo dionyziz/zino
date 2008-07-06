a02a0a7deae8886b030a54e2d70bbc684201d982
Aleksis Brezas <abresas@gmail.com>
2008-07-06 12:43:44 +0000

    permission comment view

--- phoenix/libs/user/permission.php
+++ phoenix/libs/user/permission.php
@@ -2,7 +2,7 @@
     define( 'PERMISSION_ACCESS_SITE', 10 );
 
     // comment permissions
-    define( 'PERMISSION_COMMENT_VIEW', 200 );
+    define( 'PERMISSION_COMMENT_VIEW', 10 );
     define( 'PERMISSION_COMMENT_CREATE', 30 );
     define( 'PERMISSION_COMMENT_EDIT_ALL', 50 );
     define( 'PERMISSION_COMMENT_DELETE', 30 );
