92d008221c2e40a44422bb652b8e1fe10e24d9af
Aleksis Brezas <abresas@gmail.com>
2008-07-11 14:15:52 +0000

    events testcase

--- phoenix/tests/event.php
+++ phoenix/tests/event.php
@@ -106,10 +106,10 @@
 			$finder = New EventFinder();
 			$events = $finder->FindByUser( $this->mUser );
 			$this->Assert( is_array( $events ), 'FindByUser did not return an array' );
-			$this->AssertEquals( 3, count( $events ), 'FindByUser did not return right number of events' ); // 3 here
+			$this->AssertEquals( 3, count( $events ), 'FindByUser did not return right number of events' ); // 3 here + user created
 
-			$typeids = array( EVENT_USERPROFILE_VISITED, EVENT_USERPROFILE_UPDATED, EVENT_USERPROFILE_MOOD_UPDATED );
-            $itemids = array( $this->mUser->Id, $this->mUser->Id, $this->mUser->Id, $this->mUser->EgoAlbum->Id );
+			$typeids = array( EVENT_USER_CREATED, EVENT_USERPROFILE_VISITED, EVENT_USERPROFILE_UPDATED, EVENT_USERPROFILE_MOOD_UPDATED );
+            $itemids = array( $this->mUser->Id, $this->mUser->Id, $this->mUser->Id, $this->mUser->Id );
 			foreach ( $events as $key => $e ) {
 				$this->AssertEquals( $this->mUser->Id, $e->Userid, 'Wrong event userid' );
                 $this->AssertEquals( $itemids[ $key ], $e->Itemid, 'Wrong event itemid' );
