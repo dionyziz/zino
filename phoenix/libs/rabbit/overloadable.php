6bd7eed3f9a63f1765ef2f8fb2ee82b83bd99dd0
Dionysis Zindros <dionyziz@gmail.com>
2008-07-16 01:24:45 +0000

    optimizations

--- phoenix/libs/rabbit/overloadable.php
+++ phoenix/libs/rabbit/overloadable.php
@@ -6,15 +6,15 @@
     //          Think before you type, and always benchmark. Thanks! :-)
 
     abstract class Overloadable {
-        private $mCache;
+        private $mMethods = false;
 
         public function __set( $name, $value ) {
             // check if a custom setter is specified
             $methodname = 'Set' . $name;
-            if ( !isset( $this->mCache[ $methodname ] ) ) {
-                $this->mCache[ $methodname ] = method_exists( $this, $methodname );
+            if ( $this->mMethods === false ) {
+                $this->mMethods = array_flip( get_class_methods( $this ) );
             }
-            if ( $this->mCache[ $methodname ] ) {
+            if ( isset( $this->mMethods[ $methodname ] ) ) {
                 return $this->$methodname( $value ) !== false;
             }
             // else fallthru
@@ -23,10 +23,10 @@
         public function __get( $name ) {
             // check if a custom getter is specified
             $methodname = 'Get' . $name;
-            if ( !isset( $this->mCache[ $methodname ] ) ) {
-                $this->mCache[ $methodname ] = method_exists( $this, $methodname );
+            if ( $this->mMethods === false ) {
+                $this->mMethods = array_flip( get_class_methods( $this ) );
             }
-            if ( $this->mCache[ $methodname ] ) {
+            if ( isset( $this->mMethods[ $methodname ] ) ) {
                 return $this->$methodname();
             }
             // else fallthru
