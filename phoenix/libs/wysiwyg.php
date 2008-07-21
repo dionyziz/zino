d1a1e5c73656deab0725d25a81970699ff806067
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-07-21 14:42:07 +0000

    .

--- phoenix/libs/wysiwyg.php
+++ phoenix/libs/wysiwyg.php
@@ -70,7 +70,7 @@
 
     function WYSIWYG_TextProcess( $text ) {
         $text = htmlspecialchars( $text );
-        $text = WYSIWYG_Links( $text );        
+        $text = WYSIWYG_Links( $text );
         $text = WYSIWYG_Smileys( $text );
         return $text;
     }
