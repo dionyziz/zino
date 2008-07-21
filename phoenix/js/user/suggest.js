2e96278856eaf725bf7c9de4175727f043071f4e
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-07-21 18:33:27 +0000

    groumf

--- phoenix/js/user/suggest.js
+++ phoenix/js/user/suggest.js
@@ -57,8 +57,8 @@ var Suggest = {
 			var opt = document.createElement( 'option' );
 			opt.value = suggestions[i];
 			opt.onclick = function() {
-				$( 'div.' + type + ' form' ).hide().find( 'input' ).get(0).value = this.value;
-				$( 'div.' + type + ' option' ).remove();
+				$( 'div.' + type + ' input' ).get( 0 ).value = this.value;
+				$( 'div.' + type + ' form' ).hide().find( 'option' ).remove();
 			}
 			opt.appendChild( document.createTextNode( suggestions[i] ) );
 			sel.appendChild( opt );
