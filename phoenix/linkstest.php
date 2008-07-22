9897d9ecf91067408c129a150a3f082b9e5a3c0c
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-07-22 16:35:45 +0000

    .

--- phoenix/linkstest.php
+++ phoenix/linkstest.php
@@ -81,8 +81,8 @@
 
 	function WYSIWYG_TextProcess($text) {
 		$text = htmlspecialchars($text);
-		/*$text = WYSIWYG_Links($text);
-		$text = WYSIWYG_Smileys($text);*/
+		$text = WYSIWYG_Links($text);
+		/*$text = WYSIWYG_Smileys($text);*/
 		return $text;
 	}
 
