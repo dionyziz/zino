a17e7554c3983ab412f0442d7c9a41d9f77c98d6
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-07-22 11:52:08 +0000

    .

--- phoenix/libs/wysiwyg.php
+++ phoenix/libs/wysiwyg.php
@@ -48,29 +48,29 @@
         $html = $sanitizer->GetXHTML();
         
         // YouTube support
-        $html = preg_replace(
-           '#\<img[^>]*?src\=(["\']?)' 
-            . preg_quote( $rabbit_settings[ 'imagesurl' ], "#i" )
-            . 'video-placeholder\.png\?v\=([a-zA-Z0-9_-]+)\1[^>]*/?\>#i',
-            '<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/\2"></param><embed src="http://www.youtube.com/v/\2" type="application/x-shockwave-flash" width="425" height="344"></embed></object>', 
-            $html
-        );
+        //$html = preg_replace(
+        //   '#\<img[^>]*?src\=(["\']?)' 
+        //    . preg_quote( $rabbit_settings[ 'imagesurl' ], "#i" )
+        //    . 'video-placeholder\.png\?v\=([a-zA-Z0-9_-]+)\1[^>]*/?\>#i',
+        //    '<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/\2"></param><embed src="http://www.youtube.com/v/\2" type="application/x-shockwave-flash" width="425" height="344"></embed></object>', 
+        //    $html
+        //);
         
         // Veoh support
-        $html = preg_replace(
-            '#\<img[^>]*?src\=(["\']?)'
-            . preg_quote( $rabbit_settings[ 'imagesurl' ], '#i' )
-            . 'video-placeholder\.png\?w\=([a-zA-Z0-9_-]+)\1[^>]*/?\>#i',
-            '<embed src="http://www.veoh.com/videodetails2.swf?permalinkId=\2&amp;id=anonymous&amp;player=videodetailsembedded&amp;videoAutoPlay=0" allowFullScreen="true" width="540" height="438" bgcolor="#000000" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed>',
-            $html
-        );
+        //$html = preg_replace(
+        //    '#\<img[^>]*?src\=(["\']?)'
+        //    . preg_quote( $rabbit_settings[ 'imagesurl' ], '#i' )
+        //    . 'video-placeholder\.png\?w\=([a-zA-Z0-9_-]+)\1[^>]*/?\>#i',
+        //    '<embed src="http://www.veoh.com/videodetails2.swf?permalinkId=\2&amp;id=anonymous&amp;player=videodetailsembedded&amp;videoAutoPlay=0" allowFullScreen="true" width="540" height="438" bgcolor="#000000" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed>',
+        //    $html
+        //);
    
         return $html;
     }
 
     function WYSIWYG_TextProcess( $text ) {
         $text = htmlspecialchars( $text );
-        //$text = WYSIWYG_Links( $text );
+        $text = WYSIWYG_Links( $text );
         // $text = WYSIWYG_Smileys( $text );
         return $text;
     }
