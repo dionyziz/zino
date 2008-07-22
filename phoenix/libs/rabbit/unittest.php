8022d309934396490ff90a6f2f3b59f74bad983c
Dionysis Zindros <dionyziz@gmail.com>
2008-07-22 20:07:11 +0000

    unit testing

--- phoenix/libs/rabbit/unittest.php
+++ phoenix/libs/rabbit/unittest.php
@@ -47,7 +47,7 @@
         }
         protected function AssertEquals( $expected, $actual, $message = '' ) {
             if ( $expected == 'hello' ) {
-                 die( $expected );
+                 // die( $expected );
             }
             return $this->InformTester(
                 New AssertResult( $actual === $expected, $message, $actual, $expected )
