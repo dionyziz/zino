79cf6469e8c1ba98e5718feab033e44fbc78afb6
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-07-22 13:51:55 +0000

    .

---
+++ phoenix/_linkstest.php
@@ -0,0 +1,23 @@
+<?php
+	function WYSIWYG_Links($text) {
+		$text = preg_replace(
+			'#\b(https?\://[a-z0-9.-]+(/[a-zA-Z0-9./+?;&=%-]*)?)(\s|$)#',
+			'<a href="\1">\1</a>',
+			$text
+		);
+		return $text;
+	}
+
+	$tests = array(
+		'http://www.google.com/',
+		'Hello https://python.org/ !',
+		'http://localhost/index.php?p=comments&a=show <-- look here',
+		'OK https://foo.bar.gr/wiki.php?a=true&s=false ... htts://mistake.org/ http:/another.net/index.php ...'
+	);
+
+	foreach ( $tests as $t ) {
+		$result = WYSIWYG_Links($text);
+		echo "$result <br />\n";
+	}
+?>
+
