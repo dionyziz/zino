8858c59090ec29d8629a27451f45813d41027cb2
Dionysis Zindros <dionyziz@gmail.com>
2008-07-23 10:56:49 +0000

    xhtmlsanitize

---
+++ phoenix/tests/rabbit/xml.php
@@ -0,0 +1,23 @@
+<?php
+    class TestXML extends Testcase {
+        protected $mAppliesTo = 'libs/rabbit/xml';
+
+        public function TestXML() {
+            $parser = New XMLParser(
+                '<?xml version="1.0"?>
+                 <garden name="flowers">
+                    <flower type="rose" />
+                    <flower type="violet" />
+                    Roses are red &amp;
+                    violets are blue;
+                    You are so sweet,
+                    and I love you too.
+                 </garden>'
+            );
+            $root = $parser->Parse();
+            $this->Assert( $root instanceof XMLNode, 'XML Parser did not return an XML root node' ); 
+        }
+    }
+
+    return New TestXML();
+?>
