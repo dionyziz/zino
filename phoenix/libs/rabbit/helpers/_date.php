0cf75589c6d670e6e4162cf27f53144a9106ccce
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-07-23 15:16:44 +0000

    .

--- phoenix/libs/rabbit/helpers/_date.php
+++ phoenix/libs/rabbit/helpers/_date.php
@@ -21,48 +21,48 @@
             return false;
         }
         
-        $rest= ($diff % 86400);
-        $days  = ($diff - $rest) / 86400; # seconds a day
+        $rest = ($diff % 86400);
+        $days = ($diff - $rest) / 86400; # seconds a day
 
         return $days;
     }
     function dateDistance( $pastDate ) {
         return dateDiff( $pastDate , NowDate() );
     }
-    // TODO: make it non-greek-specific
-    function dateDiff( $dateTimeBegin , $dateTimeEnd , $incseconds = false ) {
-        if( !$dateTimeBegin || !$dateTimeEnd )
+    function dateDiff( $dateTimeBegin, $dateTimeEndS ) {
+        if ( !$dateTimeBegin || !$dateTimeEnd ) {
             return false;
-            
-        $dateTimeBegin = strtotime($dateTimeBegin);
-        $dateTimeEnd   = strtotime($dateTimeEnd);
+        }
+
+        $dateTimeBegin = strtotime( $dateTimeBegin) ;
+        $dateTimeEnd = strtotime( $dateTimeEnd );
         
-        if($dateTimeEnd === -1 || $dateTimeBegin === -1) {
-            # error condition
+        if ( $dateTimeEnd === -1 || $dateTimeBegin === -1 ) {
+            // error condition
             return false;
         }
         
         $diff = $dateTimeEnd - $dateTimeBegin;
         
-        if ($diff < 0) {
-            # error condition
+        if ( $diff < 0 ) {
+            // error condition
             return false;
         }
         
-        $weeks = $days = $hours = $minutes = $seconds = 0; # initialize vars
+        $weeks = $days = $hours = $minutes = $seconds = 0; // initialize vars
         
-        if( $diff % 604800 > 0 ) {
+        if ( $diff % 604800 > 0 ) {
             $rest1  = $diff % 604800;
-            $weeks  = ($diff - $rest1) / 604800; # seconds a week
-            if( $rest1 % 86400 > 0 ) {
-                $rest2 = ($rest1 % 86400);
-                $days  = ($rest1 - $rest2) / 86400; # seconds a day
-                if( $rest2 % 3600 > 0 ) {
-                    $rest3 = ($rest2 % 3600);
-                    $hours = ($rest2 - $rest3) / 3600; # seconds an hour
-                    if( $rest3 % 60 > 0 ) {
-                        $seconds = ($rest3 % 60);
-                        $minutes = ($rest3 - $seconds) / 60;  # seconds a minute
+            $weeks  = ( $diff - $rest1 ) / 604800; // seconds a week
+            if ( $rest1 % 86400 > 0 ) {
+                $rest2 = ( $rest1 % 86400 );
+                $days  = ( $rest1 - $rest2 ) / 86400; // seconds a day
+                if ( $rest2 % 3600 > 0 ) {
+                    $rest3 = ( $rest2 % 3600 );
+                    $hours = ( $rest2 - $rest3 ) / 3600; // seconds an hour
+                    if ( $rest3 % 60 > 0 ) {
+                        $seconds = ( $rest3 % 60 );
+                        $minutes = ( $rest3 - $seconds ) / 60;  // seconds a minute
                     } 
                     else {
                         $minutes = $rest3 / 60;
@@ -73,101 +73,34 @@
                 }
             }
             else {
-                $days = $rest1/ 86400;
+                $days = $rest1 / 86400;
             }
         }
         else {
             $weeks = $diff / 604800;
         }
         // $seconds = $dateTimeEnd - ( ( ( ( $weeks * 7 + $days ) * 24 + $hours ) * 60 ) + $minutes );
-        if( $weeks ) {
+        if ( $weeks ) {
             $hours = 0;
         }
-        if( $days || $weeks ) {
+        if ( $days || $weeks ) {
             $minutes = 0;
         }
         $months = floor( $weeks / 4 );
-        if( $months ) {
+        if ( $months ) {
             $weeks = $weeks - $months * 4;
         }
-        
-        $string = array();
-        if( $months > 1 ) {
-            $string[] = "$months μήνες";
-        }
-        elseif( $months == 1 ) {
-            $string[] = "ένα μήνα";
-        }
-        if( $weeks > 1 ) {
-            $string[]  = "$weeks εβδομάδες";
-        } 
-        elseif ($weeks == 1) {
-            $string[]  = "μία εβδομάδα";
-        }
-        if($days > 1) {
-            $string[] = "$days μέρες";
-        }
-        elseif($days == 1) {
-            $string[] = "μία μέρα";
-        }
-        if($hours > 1) {
-            $string[] = "$hours ώρες";
-        } 
-        elseif ($hours == 1) {
-            $string[] = "μία ώρα";
-        }
 
-        if($minutes > 1) {
-            switch( $minutes ) {
-                case 15:
-                    $string[] = "ένα τέταρτο";
-                    break;
-                case 30:
-                    $string[] = "ένα μισάωρο";
-                    break;
-                case 45:
-                    $string[] = "τρία τέταρτα";
-                    break;
-                default:
-                    $string[] = "$minutes λεπτά";
-            }
-        } 
-        elseif ($minutes == 1) {
-            $string[] = "ένα λεπτό";
-        }
-        if( $seconds && $incseconds ) {
-            $string[] = "$seconds δευτερόλεπτο";
-        }
+        $string = array(
+            'months' => $months,
+            'weeks' => $weeks,
+            'days' => $days,
+            'hours' => $hours,
+            'minutes' => $minutes,
+            'seconds' => $seconds
+        );
         
-        /* if($seconds > 1) {
-            $string[] = "$seconds δευτερόλεπτα";
-        } 
-        elseif($seconds == 1) {
-            $string[] = "δευτερόλεπτο";
-        } */
-        
-        # join together all the strings in the array above except the last element
-        if( sizeof( $string ) == 1 ) {
-            $text = $string[ 0 ];
-        }
-        else {
-            if( sizeof($string) == "" ) {
-                $text = "λίγο";
-            }
-            else {
-                if( sizeof( $string ) > 1 ) {
-                    $ts = 1;
-                }
-                else {
-                    $ts = 0;
-                }
-                $text  = join(', ', array_slice($string,0,$ts)) . " και ";
-                $text .= $string[ $ts ];  # put the last one on after the and
-            }
-        }
-        
-        return $text;
-        //return array($text, $weeks, $days, $hours, $minutes, $seconds);
+        return $string;
     }
     function sqldate2xml( $date ) {
         return str_replace( " " , "T" , $date ) . "Z" ;
