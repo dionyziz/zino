10d9b22f6bcc2931bca63c5f74b610534933a838
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-07-25 16:03:44 +0000

    .

--- phoenix/tests/wysiwyg.php
+++ phoenix/tests/wysiwyg.php
@@ -11,18 +11,22 @@
         public function TestSmileys() {
             global $xc_settings;
 
-            $this->AssertEquals( '<img src=\'' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/smile.png\' alt=\':-)\' title=\':-)\' class=\'emoticon\' width=\'22\' height=\'22\' />', WYSIWYG_Smileys( ':-)' ) );
-            $this->AssertEquals( 'lol <img src=\'' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/tongue.png\' alt=\':P\' title=\':P\' class=\'emoticon\' width=\'22\' height=\'22\' /> at you', WYSIWYG_Smileys( 'lol :P at you' ) );
-            $this->AssertEquals( 'Watch this <img src=\'' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/airplane.png\' alt=\':airplane:\' title=\':airplane:\' class=\'emoticon\' width=\'22\' height=\'22\' />!', WYSIWYG_Smileys( 'Watch this :airplane:!' ) );
-            $this->AssertEquals( '<img src=\'' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/wink.png\' alt=\';-)\' title=\';-)\' class=\'emoticon\' width=\'22\' height=\'22\' />', WYSIWYG_Smileys( ';-)' ) );
+            $this->AssertEquals( '<img src="' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/smile.png" alt=":-)" title=":-)" class="emoticon" width="22" height="22" />', WYSIWYG_Smileys( ':-)' ) );
+            $this->AssertEquals( 'lol <img src="' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/tongue.png" alt=":P" title=":P" class="emoticon" width="22" height="22" /> at you', WYSIWYG_Smileys( 'lol :P at you' ) );
+            $this->AssertEquals( 'Watch this <img src="' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/airplane.png" alt=":airplane:" title=":airplane:" class="emoticon" width="22" height="22" />!', WYSIWYG_Smileys( 'Watch this :airplane:!' ) );
+            $this->AssertEquals( '<img src="' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/wink.png" alt=";-)" title=";-)" class="emoticon" width="22" height="22" />', WYSIWYG_Smileys( ';-)' ) );
         }
         
         public function TestText() {
             global $xc_settings;
 
             $this->AssertEquals(
-                '<a href="http://foo/&amp;bar">http://foo/&amp;bar</a> <img src=\'' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/omg.png\' alt=\':wow:\' title=\':wow:\' class=\'emoticon\' width=\'22\' height=\'22\' />',
+                '<a href="http://foo/&amp;bar">http://foo/&amp;bar</a> <img src="' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/omg.png" alt=":wow:" title=":wow:" class="emoticon" width="22" height="22" />',
                 WYSIWYG_TextProcess( 'http://foo/&bar :wow:' )
+            );
+            $this->AssertEquals(
+                'Look at that: <a href="http://domain/#anchor">http://domain/#anchor</a> <img src="' . $xc_settings[ 'staticimagesurl' ] . 'emoticons/film.png" alt=":film:" title=":film:" class="emoticon" width="22" height="22" />!!',
+                WYSIWYG_TextProcess( 'Look at that: http://domain/#anchor :film:!!' )
             );
         }
     }
