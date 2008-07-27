c46095261478edf047fdac669cf2728744e4ff26
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-07-27 16:00:52 +0000

    .

--- phoenix/tests/school.php
+++ phoenix/tests/school.php
@@ -3,7 +3,18 @@
     class TestSchool extends Testcase {
         protected $mAppliesTo = 'libs/school';
 
-        // TODO
+        public function TestClassesExist() {
+            $this->Assert( class_exists( 'SchoolFinder' ) , 'class SchoolFinder does not exist' );
+            $this->Assert( class_exists( 'School' ) , 'class School does not exist' );
+        }
+
+        public function TestMethodsExist() {
+            $this->Assert( method_exists( 'School' , 'Create' ) , 'method School->Create does not exist' );
+            $this->Assert( method_exists( 'School' , 'Approve' ) , 'method School->Approve does not exist' );
+            $this->Assert( method_exists( 'School' , 'Rename' ) , 'method School->Rename does not exist' );
+            $this->Assert( method_exists( 'School' , 'Replace' ) , 'method School->Replace does not exist' );
+            $this->Assert( method_exists( 'School' , 'Delete' ) , 'method School->Delete does not exist' );
+        }
     }
 
     return New TestSchool();
