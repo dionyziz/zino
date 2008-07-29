707fe1dc7b0a9ae2bdb76deeb339846b5d62d26e
Dionysis Zindros <dionyziz@gmail.com>
2008-07-29 16:33:00 +0000

    image tagging

--- phoenix/tests/image/tag.php
+++ phoenix/tests/image/tag.php
@@ -46,16 +46,20 @@
         }
         public function TestTagCreation() {
             $this->mTag1 = New ImageTag(); 
-            $this->mTag1->X = 20;
-            $this->mTag1->Y = 21;
+            $this->mTag1->Left = 20;
+            $this->mTag1->Top = 21;
+            $this->mTag1->Width = 150;
+            $this->mTag1->Height = 150;
             $this->mTag1->Imageid = $this->mImage->Id;
             $this->mTag1->Ownerid = $this->mUser->Id;
             $this->mTag1->Personid = $this->mUser->Id;
             $this->mTag1->Save();
 
             $this->mTag2 = New ImageTag(); 
-            $this->mTag2->X = 50;
-            $this->mTag2->Y = 60;
+            $this->mTag2->Left = 50;
+            $this->mTag2->Top = 60;
+            $this->mTag2->Width = 200;
+            $this->mTag2->Height = 100;
             $this->mTag2->Imageid = $this->mImage->Id;
             $this->mTag2->Ownerid = $this->mUser->Id;
             $this->mTag2->Personid = $this->mUser2->Id;
@@ -76,16 +80,20 @@
                         $this->AssertEquals( $this->mUser->Id, $tag->Personid, 'Tag1 has wrong personid' );   
                         $this->AssertEquals( $this->mUser->Id, $tag->Ownerid, 'Tag1 has wrong ownerid' );   
                         $this->AssertEquals( $this->mImage->Id, $tag->Imageid, 'Tag1 has wrong imageid' );
-                        $this->AssertEquals( 20, $tag->X, 'Tag1 X is wrong' );
-                        $this->AssertEquals( 21, $tag->Y, 'Tag1 Y is wrong' );
+                        $this->AssertEquals( 20, $tag->Left, 'Tag1 Left is wrong' );
+                        $this->AssertEquals( 21, $tag->Top, 'Tag1 Top is wrong' );
+                        $this->AssertEquals( 150, $tag->Width, 'Tag1 Width is wrong' );
+                        $this->AssertEquals( 150, $tag->Height, 'Tag1 Height is wrong' );
                         break;
                     case 1:
                         $this->Assert( $tag->Exists(), 'Tag2 found, but does not exist' );
                         $this->AssertEquals( $this->mUser2->Id, $tag->Personid, 'Tag2 has wrong personid' );   
                         $this->AssertEquals( $this->mUser->Id, $tag->Ownerid, 'Tag2 has wrong ownerid' );   
                         $this->AssertEquals( $this->mImage->Id, $tag->Imageid, 'Tag2 has wrong imageid' );
-                        $this->AssertEquals( 50, $tag->X, 'Tag1 X is wrong' );
-                        $this->AssertEquals( 60, $tag->Y, 'Tag1 Y is wrong' );
+                        $this->AssertEquals( 50, $tag->Left, 'Tag1 X is wrong' );
+                        $this->AssertEquals( 60, $tag->Top, 'Tag1 Y is wrong' );
+                        $this->AssertEquals( 200, $tag->Width, 'Tag2 Width is wrong' );
+                        $this->AssertEquals( 100, $tag->Height, 'Tag2 Height is wrong' );
                         break;
                 }
                 ++$i;
