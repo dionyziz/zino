806c0148ad18a701b5459cd62fd34cebe028a5fd
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-07-29 14:42:43 +0000

    Fixed logivally

--- phoenix/js/album/photo/tag.js
+++ phoenix/js/album/photo/tag.js
@@ -1,9 +1,7 @@
 var Tag = {
     focus : function( event ) {
-        alert( event.pageX );
-        alert( event.pageY );
-        var x = event.offsetX?(event.offsetX):event.pageX-$( "div.thephoto" ).attr( 'offsetLeft' );
-        var y = event.offsetY?(event.offsetY):event.pageY-$( "div.thephoto" ).attr( 'offsetTop' );
+        var x = event.offsetX?(event.offsetX):event.pageX-$( "div.thephoto" ).get( 0 ).offsetLeft;
+        var y = event.offsetY?(event.offsetY):event.pageY-$( "div.thephoto" ).get( 0 ).offsetTop;
         var tag_width = parseInt( $( 'div.tagme' ).css( 'width' ), 10 );
         var tag_height = parseInt( $( 'div.tagme' ).css( 'height' ), 10 );
         var image_width = parseInt( $( 'div.thephoto' ).css( 'width' ), 10 );
@@ -20,8 +18,6 @@ var Tag = {
         if ( y > image_height-tag_height/2 ) {
             y = image_height-tag_height/2;
         }
-        alert( x );
-        alert( y );
         $( 'div.tagme' ).css( { left : x + 'px', top : y + 'px' } );
     }
 };
