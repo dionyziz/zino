796e1b996699af388b76a47e295c902a01a1e655
Chris Pappas <chrispappas12@gmail.com>
2008-08-04 15:10:01 +0000

    pms spriting

--- phoenix/elements/pm/list.php
+++ phoenix/elements/pm/list.php
@@ -45,7 +45,7 @@
                             Element( 'pm/folder/link', $folder );
                         }
 
-                        ?><div class="newfolder top folder" id="newfolderlink" alt="Δημιούργησε έναν νέο φάκελο" title="Δημιούργησε έναν νέο φάκελο" onclick="pms.NewFolder();return false;"><a href="" class="folderlinksnew"><span></span>Νέος Φάκελος</a></div>
+                        ?><div class="newfolder top folder" id="newfolderlink" alt="Δημιούργησε έναν νέο φάκελο" title="Δημιούργησε έναν νέο φάκελο" onclick="pms.NewFolder();return false;"><a href="" class="folderlinksnew"><span>&nbsp;</span>Νέος Φάκελος</a></div>
                     </div><br />
                     <a href="" class="folder_links newpm" onclick="pms.NewMessage( '' , '' );return false;"><span>&nbsp;</span>Νέο μήνυμα</a><br />
                     <a href="" id="deletefolderlink" class="folder_links deletefolder" onclick="return false;" style="display:none;"><span>&nbsp;</span>Διαγραφή φακέλου</a>
