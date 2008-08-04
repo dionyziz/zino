f0444bdf5ea509735f268b23ec81708f6ea7c8e9
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-04 23:27:25 +0000

    The statistic element

---
+++ phoenix/elements/statistics/view.php
@@ -0,0 +1,16 @@
+<?php
+	class ElementStatisticsView extends Element
+	{
+		public function Render()
+		{
+			//initalize chart
+			while(!$row=mysql_fetch_array($sql_resuts))
+			{
+				//add point to the chart
+				echo '<h1>hi</hi>';
+
+			}
+			//output chart
+		}
+	}
+?>
