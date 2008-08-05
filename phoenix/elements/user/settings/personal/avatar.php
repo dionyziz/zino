92203347d6eec15e492e7a08d324bdb87fe2680e
Dimitris Karagasidis <dkaragasidis@gmail.com>
2008-08-05 16:54:06 +0000

    aarrgghhh

--- phoenix/elements/user/settings/personal/avatar.php
+++ phoenix/elements/user/settings/personal/avatar.php
@@ -16,7 +16,7 @@
                     <?php
                     if ( UserBrowser() == 'MSIE' ) {
                         ?>
-                        <iframe border="0" style="height: 50px;" src="?p=upload&amp;albumid=<?php
+                        <iframe frameborder="0" style="height: 50px;" src="?p=upload&amp;albumid=<?php
                         echo $user->Egoalbumid;
                         ?>&amp;typeid=1" class="uploadframe" id="uploadframe">
                         </iframe>
