75f3e0c14be3716fd41367c2c852bb1c65eda98f
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 14:44:27 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -3,20 +3,22 @@
 	{
 			
 		
-		public function Render() {
+		public function Render(tInteger $daysBefore) {
 			global $page;
 
 			$page->setTitle( 'Daily statistics' );
+
+			$daysBefore=$daysBefore->Get();
 		
 			echo '<h2>Daily statistics</h2>';
 
-			echo '<img src="images/statistics/stats.php?name=shouts" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=users" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=images" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=polls" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=comments" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=albums" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=journals" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=shouts?days='.$daysBefore.'" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=users?days='.$daysBefore.'" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=images?days='.$daysBefore.'" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=polls?days='.$daysBefore.'" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=comments?days='.$daysBefore.'" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=albums?days='.$daysBefore.'" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=journals?days='.$daysBefore.'" alt="img"/>';
 		}
 	}
 ?>
