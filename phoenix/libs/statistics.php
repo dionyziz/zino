16b66cda3e203503c3e0e3132687f46533696029
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 23:33:19 +0000

    statistics

--- phoenix/libs/statistics.php
+++ phoenix/libs/statistics.php
@@ -1,12 +1,42 @@
 <?php 
-	function Statistics_Get($dbname,$date_field,$days_passed) {
+	function Statistics_Get($table_name,$days_passed=30) {
 
 		global  $db;
 
-		if($days_passed==0) $days_passed=30;
+		switch($table_name){
+		case "shoutbox":
+			$date_field="shout_created";
+			break;
+		case "users":
+			$date_field="user_created";
+			break;
+		case "polls":
+			$date_field="poll_created";
+			break;
+		case "comments":
+			$date_field="comment_created";
+			break;
+		case "images":
+			$date_field="image_created";
+			break;
+		case "journals":
+			$date_field="journal_created";
+			break;
+		case "albums":
+			$date_field="album_created";
+			break;
+		default:
+			$date_field="error";
+		}
 
-		$query=$db->Prepare( "SELECT DATE(".$date_field.") AS day,COUNT(*) AS count  FROM ".$dbname." WHERE ".$date_field.">NOW()-INTERVAL ".$days_passed." DAY GROUP BY day ORDER BY day ASC" );
-		$query->BindTable( $dbname );
+		if($date_field=="error") {
+		return;
+		}
+
+		
+		$query=$db->Prepare( "SELECT DATE(".$date_field.") AS day,COUNT(*) AS count  FROM ".$table_name." WHERE ".$date_field.">NOW()-INTERVAL :days_before DAY GROUP BY day ORDER BY day ASC" );
+		$query->BindTable( $table_name );
+		$query->Bind('days_before',$days_passed);
 		$res=$query->Execute();
 		
 		$array = $res->MakeArray();		
