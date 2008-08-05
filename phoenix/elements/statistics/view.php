77af1e3c0bcc961d98c84759b88824a7b0bfe612
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 00:08:17 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -7,16 +7,10 @@
 		{
 			global $libs;	
 
-			$libs->Load('statistics');
+			$libs->Load( 'statistics' );
 		
-			//initalize chart
-			/*while(!$row=mysql_fetch_array($sql_resuts))
-			{*/
-				//add point to the chart
 			echo '<h1>Statistics</h1>';
 			$stats=Statistics_Get();
-			//}
-			//output chart
 		}
 	}
 ?>
