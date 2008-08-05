9d6f9fee4eee0793b52562dc84a627d36e70be6a
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 16:05:23 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -3,14 +3,16 @@
 	{
 			
 		
-		public function Render(tInteger $daysBefore) {
+		public function Render(tInteger $daysBefore,tText $graphType) {
 			global $page;
 
 			$page->setTitle( 'Daily statistics' );
 
 			$daysBefore=$daysBefore->Get();
+			$graphType=$graphType->Get();
 
 			if($daysBefore==0) $daysBefore=30;
+			if($graphType=="") $graphType="Shoutbox";
 		
 			echo '<h2>Daily statistics</h2>';
 
@@ -39,14 +41,14 @@
 					echo '</a>';
 				echo '</li>';
 			echo '</ul>';
-
-			echo '<img src="images/statistics/stats.php?name=shouts&amp;days='.$daysBefore.'" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=users&amp;days='.$daysBefore.'" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=images&amp;days='.$daysBefore.'" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=polls&amp;days='.$daysBefore.'" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=comments&amp;days='.$daysBefore.'" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=albums&amp;days='.$daysBefore.'" alt="img"/>';
-			echo '<img src="images/statistics/stats.php?name=journals&amp;days='.$daysBefore.'" alt="img"/>';
+	
+			if ($graphType=="Shoutbox") echo '<img src="images/statistics/stats.php?name=shouts&amp;days='.$daysBefore.'" alt="img"/>';
+			else if ($graphType=="Users") echo '<img src="images/statistics/stats.php?name=users&amp;days='.$daysBefore.'" alt="img"/>';
+			else if ($graphType=="Images") echo '<img src="images/statistics/stats.php?name=images&amp;days='.$daysBefore.'" alt="img"/>';
+			else if ($graphType=="Polls") echo '<img src="images/statistics/stats.php?name=polls&amp;days='.$daysBefore.'" alt="img"/>';
+			else if ($graphType=="Comments") echo '<img src="images/statistics/stats.php?name=comments&amp;days='.$daysBefore.'" alt="img"/>';
+			else if ($graphType=="Albums") echo '<img src="images/statistics/stats.php?name=albums&amp;days='.$daysBefore.'" alt="img"/>';
+			else if ($graphType=="Journals") echo '<img src="images/statistics/stats.php?name=journals&amp;days='.$daysBefore.'" alt="img"/>';
 		}
 	}
 ?>
