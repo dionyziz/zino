8e9c7b451d66ebd49dc68ed9dc7f992ca60037a4
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 15:56:43 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -9,33 +9,35 @@
 			$page->setTitle( 'Daily statistics' );
 
 			$daysBefore=$daysBefore->Get();
+
+			if($daysBefore==0) $daysBefore=30;
 		
 			echo '<h2>Daily statistics</h2>';
 
 			echo '<ul>';
-			echo '<li>';
-			echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=30">';
-			if($daysBefore==30 || $daysBefore==0) echo'<strong>';
-			echo '30 days before'; 
-			if($daysBefore==30 || $daysBefore==0) echo'</strong>';
-			echo '</a>';
-			echo '</li>';
+				echo '<li>';
+					echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=30">';
+					if($daysBefore==30) echo'<strong>';
+					echo '30 days before'; 
+					if($daysBefore==30) echo'</strong>';
+					echo '</a>';
+				echo '</li>';
 
-			echo '<li>';
-			echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=60">';
-			if($daysBefore==60) echo'<strong>';
-			echo '60 days before'; 
-			if($daysBefore==60) echo'</strong>';
-			echo '</a>';
-			echo '</li>';			
-
-			echo '<li>';
-			echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=90">';
-			if($daysBefore==90) echo'<strong>';
-			echo '90 days before'; 
-			if($daysBefore==90) echo'</strong>';
-			echo '</a>';
-			echo '</li>';
+				echo '<li>';
+					echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=60">';
+					if($daysBefore==60) echo'<strong>';
+					echo '60 days before'; 
+					if($daysBefore==60) echo'</strong>';
+					echo '</a>';
+				echo '</li>';			
+	
+				echo '<li>';
+					echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=90">';
+					if($daysBefore==90) echo'<strong>';
+					echo '90 days before'; 
+					if($daysBefore==90) echo'</strong>';
+					echo '</a>';
+				echo '</li>';
 			echo '</ul>';
 
 			echo '<img src="images/statistics/stats.php?name=shouts&amp;days='.$daysBefore.'" alt="img"/>';
