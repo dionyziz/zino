e7ff64eadc3249c21595dc74db1308f9dea4ca48
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 00:03:03 +0000

    statistics

---
+++ phoenix/libs/statistics.php
@@ -0,0 +1,14 @@
+<?php 
+	function Statistics_Get(){
+
+		global  $db;
+
+		$query=$db->Prepare("SELECT DATE(shout_created) AS day,COUNT(*)  FROM `shoutbox` WHERE shout_created>NOW()-INTERVAL 30 DAY GROUP BY day ORDER BY day ASC");
+		$query->BindTable('shoutbox');
+		$res=$query->Execute();
+		
+		$array = $res->MakeArray();		
+
+		return $array;
+	}
+?>
