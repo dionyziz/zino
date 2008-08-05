93b48fef96cff6c816e218cf7312f182d38bb8d6
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 14:06:01 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -10,11 +10,13 @@
 		
 			echo '<h2>Statistics</h2>';
 
-			echo '<img src="images/statistics/stats.php?name=shout" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=shouts" alt="img"/>';
 			echo '<img src="images/statistics/stats.php?name=users" alt="img"/>';
 			echo '<img src="images/statistics/stats.php?name=images" alt="img"/>';
 			echo '<img src="images/statistics/stats.php?name=polls" alt="img"/>';
 			echo '<img src="images/statistics/stats.php?name=comments" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=albums" alt="img"/>';
+			echo '<img src="images/statistics/stats.php?name=journals" alt="img"/>';
 		}
 	}
 ?>
