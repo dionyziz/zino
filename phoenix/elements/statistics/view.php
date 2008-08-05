d1311020049c5659947cc19acb75956bac65346e
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 13:37:01 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -5,31 +5,15 @@
 		
 		public function Render()
 		{
-			global $libs;	
 			global $page;
 
-			$libs->Load( 'statistics' );
-			//$libs->Load( 'libchart/classes/libchart' );
 			$page->setTitle( 'Statistics' );
 		
 			echo '<h1>Statistics</h1>';
 
 			echo '<img src="images/statistics/stats.php" alt="img"/>';
-		
-			/*$chart=new LineChart(500,250);
-			
-			$dataSet=new XYDataSet();
-			$dataSet->addPoint(new Point("2008-06-24",5));
-			$dataSet->addPoint(new Point("2008-07-7",0));	
-			$dataSet->addPoint(new Point("2008-07-8",22));
-			$dataSet->addPoint(new Point("2008-07-9",0));
-			$dataSet->addPoint(new Point("2008-07-10",20));
-			$dataSet->addPoint(new Point("2008-07-11",1));	
-		
-			$chart->setDataSet($dataSet);
-		
-			$chart->SetTitle("new Shouts per day");
-			$chart->render();*/
+			echo '<img src="images/statistics/stats.php" alt="img"/>';
+			echo '<img src="images/statistics/stats.php" alt="img"/>';
 		}
 	}
 ?>
