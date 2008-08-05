54076dad452ea0d035c6112a337cbe759c79707b
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 14:43:40 +0000

    statistics

--- phoenix/libs/statistics.php
+++ phoenix/libs/statistics.php
@@ -1,9 +1,11 @@
 <?php 
-	function Statistics_Get($dbname,$date_field) {
+	function Statistics_Get($dbname,$date_field,$days_passed) {
 
 		global  $db;
 
-		$query=$db->Prepare( "SELECT DATE(".$date_field.") AS day,COUNT(*) AS count  FROM ".$dbname." WHERE ".$date_field.">NOW()-INTERVAL 30 DAY GROUP BY day ORDER BY day ASC" );
+		if($days_passed==0) $days_passed=30;
+
+		$query=$db->Prepare( "SELECT DATE(".$date_field.") AS day,COUNT(*) AS count  FROM ".$dbname." WHERE ".$date_field.">NOW()-INTERVAL ".$days_passed." DAY GROUP BY day ORDER BY day ASC" );
 		$query->BindTable( $dbname );
 		$res=$query->Execute();
 		
