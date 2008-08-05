2f25b4f8cc53a12abb694f8195f5560fcf3e3300
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 00:17:16 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -6,11 +6,14 @@
 		public function Render()
 		{
 			global $libs;	
+			global $page;
 
 			$libs->Load( 'statistics' );
+			$page->setTitle( 'Statistics' );
 		
 			echo '<h1>Statistics</h1>';
 			$stats=Statistics_Get();
+			echo '<p>'.$stats['day']." ".$stats['count'].'</p>'; 
 		}
 	}
 ?>
