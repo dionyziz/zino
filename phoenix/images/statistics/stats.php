1ff92bdcf879b6ff4f36af05f8d98292b8b5929c
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 13:10:18 +0000

    statistics

---
+++ phoenix/images/statistics/stats.php
@@ -0,0 +1,28 @@
+<?php
+	set_include_path( 'wwwroot' );
+	require_once 'libs/rabbit/rabbit.php';
+	Rabbit_Construct('plain');
+
+	global $libs;
+
+	$libs->Load( 'libchart/classes/libchart' );
+
+	$chart=new LineChart(500,250);
+			
+	$dataSet=new XYDataSet();
+	$dataSet->addPoint(new Point("2008-06-24",5));
+	$dataSet->addPoint(new Point("2008-07-7",0));	
+	$dataSet->addPoint(new Point("2008-07-8",22));
+	$dataSet->addPoint(new Point("2008-07-9",0));
+	$dataSet->addPoint(new Point("2008-07-10",20));
+	$dataSet->addPoint(new Point("2008-07-11",1));	
+
+	$chart->setDataSet($dataSet);
+
+	$chart->SetTitle("new Shouts per day");
+
+	header( 'Content-type: image/png' );
+	$chart->render();
+			
+	Rabbit_Destruct();
+?>
