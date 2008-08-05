22e2ff156badde522622f4aa9ea6aea16bce2580
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 12:10:26 +0000

    statistics

--- phoenix/libs/statistics.php
+++ phoenix/libs/statistics.php
@@ -11,4 +11,24 @@
 
 		return $array;
 	}
+
+	function Graph_Image_Get(){
+	
+		global $libs;	
+		$libs->Load( 'libchart/classes/libchart' );
+
+		$chart=new LineChart(500,250);
+			
+		$dataSet=new XYDataSet();
+		$dataSet->addPoint(new Point("2008-06-24",5));
+		$dataSet->addPoint(new Point("2008-07-7",0));	
+		$dataSet->addPoint(new Point("2008-07-8",22));
+		$dataSet->addPoint(new Point("2008-07-9",0));
+		$dataSet->addPoint(new Point("2008-07-10",20));
+		$dataSet->addPoint(new Point("2008-07-11",1));	
+
+		$chart->setDataSet($dataSet);
+		$chart->SetTitle("new Shouts per day");
+		$chart->render();
+	}
 ?>
