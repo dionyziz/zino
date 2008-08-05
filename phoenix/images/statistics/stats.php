88d5b5f334caef47fb93f4292e46f766a4a022e5
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 13:49:53 +0000

    statistics

--- phoenix/images/statistics/stats.php
+++ phoenix/images/statistics/stats.php
@@ -8,23 +8,18 @@
 	$libs->Load( 'libchart/classes/libchart' );
 	$libs->Load( 'statistics' );
 
-	$chart=new LineChart(500,250);
-			
-	$dataSet=new XYDataSet();
+	if($_GET['name']=='shout')
+	$stat=Statistics_Get('shoutbox','shout_created');
+
+
+
+	$chart=new LineChart(500,250);			
+	$dataSet=new XYDataSet();	
 	
-	$stat=Statistics_Get(1,1);
 	foreach($stat as $row)
 	$dataSet->addPoint(new Point($row['day'],$row['count'])); 
 
-	/*$dataSet->addPoint(new Point("2008-06-24",5));
-	$dataSet->addPoint(new Point("2008-07-7",0));	
-	$dataSet->addPoint(new Point("2008-07-8",22));
-	$dataSet->addPoint(new Point("2008-07-9",0));
-	$dataSet->addPoint(new Point("2008-07-10",20));
-	$dataSet->addPoint(new Point("2008-07-11",1));	*/
-
 	$chart->setDataSet($dataSet);
-
 	$chart->SetTitle("new Shouts per day");
 
 	header( 'Content-type: image/png' );
