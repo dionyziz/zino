74e6a927363b89f8e1cc4fb7b43fe2974f5f5e3e
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 15:35:58 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -13,23 +13,29 @@
 			echo '<h2>Daily statistics</h2>';
 
 			echo '<ul>';
+			echo '<li>';
 			echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=30">';
 			if($daysBefore==30 || $daysBefore==0) echo'<stong>';
 			echo '30'; 
 			if($daysBefore==30 || $daysBefore==0) echo'</stong>';
 			echo '</a>';
+			echo '</li>';
 
+			echo '<li>';
 			echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=60">';
 			if($daysBefore==60) echo'<stong>';
 			echo '60'; 
 			if($daysBefore==60) echo'</stong>';
 			echo '</a>';
-			
+			echo '</li>';			
+
+			echo '<li>';
 			echo '<a href="https://beta.zino.gr/phoenix/?p=statistics&amp;daysBefore=90">';
 			if($daysBefore==90) echo'<stong>';
 			echo '90'; 
 			if($daysBefore==90) echo'</stong>';
 			echo '</a>';
+			echo '</li>';
 			echo '</ul>';
 
 			echo '<img src="images/statistics/stats.php?name=shouts&amp;days='.$daysBefore.'" alt="img"/>';
