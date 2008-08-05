33659d5b5e347d9dd7880fb7595ea5d1afc9b253
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 13:30:32 +0000

    statistics

--- phoenix/images/statistics/stats.php
+++ phoenix/images/statistics/stats.php
@@ -6,16 +6,21 @@
 	global $libs;
 
 	$libs->Load( 'libchart/classes/libchart' );
+	$libs->Load( 'statistics' );
 
 	$chart=new LineChart(500,250);
 			
 	$dataSet=new XYDataSet();
-	$dataSet->addPoint(new Point("2008-06-24",5));
+	
+	foreach($stat as $row)
+	$dataSet->addPoint(new Point($row['day'],$row['count'])); 
+
+	/*$dataSet->addPoint(new Point("2008-06-24",5));
 	$dataSet->addPoint(new Point("2008-07-7",0));	
 	$dataSet->addPoint(new Point("2008-07-8",22));
 	$dataSet->addPoint(new Point("2008-07-9",0));
 	$dataSet->addPoint(new Point("2008-07-10",20));
-	$dataSet->addPoint(new Point("2008-07-11",1));	
+	$dataSet->addPoint(new Point("2008-07-11",1));	*/
 
 	$chart->setDataSet($dataSet);
 
