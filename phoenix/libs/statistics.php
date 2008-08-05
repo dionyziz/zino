99ee13d8d1025d0d42c4e6bcdcb24884cf243f9e
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 14:47:03 +0000

    statistics

--- phoenix/libs/statistics.php
+++ phoenix/libs/statistics.php
@@ -5,7 +5,7 @@
 
 		if($days_passed==0) $days_passed=30;
 
-		$query=$db->Prepare( "SELECT DATE(".$date_field.") AS day,COUNT(*) AS count  FROM ".$dbname." WHERE ".$date_field.">NOW()-INTERVAL ".$days_passed." DAY GROUP BY day ORDER BY day ASC" );
+		$query=$db->Prepare( "SELECT DATE(".$date_field.") AS day,COUNT(*) AS count  FROM ".$dbname." WHERE ".$date_field.">NOW()-INTERVAL 30 DAY GROUP BY day ORDER BY day ASC" );
 		$query->BindTable( $dbname );
 		$res=$query->Execute();
 		
