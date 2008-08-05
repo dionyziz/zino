06f5650f4825f16fd8e007d52b2bf34ab1cb8433
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 11:15:47 +0000

    statistics

--- phoenix/elements/statistics/view.php
+++ phoenix/elements/statistics/view.php
@@ -16,6 +16,23 @@
 			$stat=Statistics_Get();
 			foreach($stat as $row)
 			echo '<p>'.$row['day']." ".$row['count'].'</p>'; 
+
+			//include "libchart/classes/libchart.php";
+		
+			$chart=new LineChart(500,250);
+			
+			$dataSet=new XYDataSet();
+			$dataSet->addPoint(new Point("2008-06-24",5));
+			$dataSet->addPoint(new Point("2008-07-7",0));	
+			$dataSet->addPoint(new Point("2008-07-8",22));
+			$dataSet->addPoint(new Point("2008-07-9",0));
+			$dataSet->addPoint(new Point("2008-07-10",20));
+			$dataSet->addPoint(new Point("2008-07-11",1));	
+		
+			$chart->setDataSet($dataSet);
+		
+			$chart->SetTitle("new Shouts per day");
+			//$chart->render("graph1.png");
 		}
 	}
 ?>
