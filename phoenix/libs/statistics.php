a470f175db274aa4793b2ad5ebc6df59e745a82d
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-05 00:23:15 +0000

    statistics

--- phoenix/libs/statistics.php
+++ phoenix/libs/statistics.php
@@ -1,10 +1,10 @@
 <?php 
-	function Statistics_Get(){
+	function Statistics_Get() {
 
 		global  $db;
 
-		$query=$db->Prepare("SELECT DATE(shout_created) AS day,COUNT(*)  FROM `shoutbox` WHERE shout_created>NOW()-INTERVAL 30 DAY GROUP BY day ORDER BY day ASC");
-		$query->BindTable('shoutbox');
+		$query=$db->Prepare( "SELECT DATE(shout_created) AS day,COUNT(*) AS count  FROM `shoutbox` WHERE shout_created>NOW()-INTERVAL 30 DAY GROUP BY day ORDER BY day ASC" );
+		$query->BindTable( 'shoutbox' );
 		$res=$query->Execute();
 		
 		$array = $res->MakeArray();		
