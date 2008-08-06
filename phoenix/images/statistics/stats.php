8ccec273ae67242933ebc3bf23aa11f8fe5fb237
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-06 00:07:57 +0000

    statistics

--- phoenix/images/statistics/stats.php
+++ phoenix/images/statistics/stats.php
@@ -1,13 +1,12 @@
 <?php
 	set_include_path( '../../:./' );
 	require_once 'libs/rabbit/rabbit.php';
-	Rabbit_Construct('plain');
+	Rabbit_Construct( 'plain' );
 
 	global $libs;
 	global $user;
 
 	if ( !$user->HasPermission( PERMISSION_STATISTICS_VIEW ) ) {
-	echo 'Permission denied';	
 	return;
 	}
 
@@ -16,53 +15,57 @@
 
 	$ttle="";
 	
-	if((int)$_GET['days']!=60 && (int)$_GET['days']!=90)
-        $_GET['days']=30;
+	switch ( $_GET['days'] ) {
+	case 30: $x=500;
+		  break;
+	case 60: $x=750;
+		  break;
+	case 90: $x=1000;
+		  break;
+	default: $_GET['days']=30;
+		 $x=500;
+	}
 
 	$stat=Statistics_Get($_GET[ 'name' ],$_GET['days']);
-	$title="new ".$_GET[ 'name' ]." per day";
+	$title="new ".$_GET[ 'name' ]." per day";	
 
-	if($_GET['days']==30) $x=500;
-	else if($_GET['days']==60) $x=750;
-	else if($_GET['days']==90) $x=1000;
-	
-
-	$chart=new LineChart($x,$x-250);	//1000 for 90 days,750 for 60 days 500 for 30 days		
+	$chart=new LineChart( $x, $x-250 );	//1000 for 90 days,750 for 60 days 500 for 30 days		
 	$dataSet=new XYDataSet();	
 	
 	$i=0;
 	$lastday="";
 	$lastmonth;
-	foreach ($stat as $row) {	
-		if ($i%10==0) {
-			$date=new DateTime($row['day']);
-			$label=$date->format('m-d');
+	foreach ( $stat as $row ) {	
+		if ( $i % 10 == 0 ) {
+			$date=new DateTime( $row[ 'day' ] );
+			$label=$date->format( 'm-d' );
 		}
-		else 
-		{
+		else {
 			$label="";
-			$date=new DateTime($row['day']);
+			$date=new DateTime( $row[ 'day' ] );
 		}
 
 		if($lastday!="")
 		{
-			for($e=0;$e<((int)$date->format('d')-$lastday+((int)$date->format('m')-$lastmonth)*30)-1;$e++)
-			$dataSet->addPoint(new Point("",0)); 
+			$empty_days=(int)$date->format( 'd' ) - $lastday + ( (int)$date->format( 'm' ) - $lastmonth ) * 30 - 1;
+			for($e=0;$e<$empty_days;$e++)
+			$dataSet->addPoint( new Point( "", 0 ) ); 
 
 		}
 		
-		$dataSet->addPoint(new Point($label,$row['count'])); 
+		$dataSet->addPoint( new Point( $label, $row[ 'count' ] ) ); 
 
-		$i++;
+		++$i;
 	
-		$lastday=(int)$date->format('d');
-		$lastmonth=(int)$date->format('m');
+		$lastday=(int)$date->format( 'd' );
+		$lastmonth=(int)$date->format( 'm' );
 	}
-	for($e=0;$e<((int)date('d')-$lastday+((int)date('m')-$lastmonth)*30)-1;$e++)
-	$dataSet->addPoint(new Point("",0)); 
+	$empty_days=(int)date( 'd' ) - $lastday + ( (int)date( 'm' ) - $lastmonth ) * 30 - 1;
+	for($e=0;$e<$empty_days;$e++)
+	$dataSet->addPoint( new Point( "", 0 ) ); 
 	
 
-	$chart->setDataSet($dataSet);
+	$chart->setDataSet( $dataSet );
 	$chart->SetTitle( $title );
 
 	header( 'Content-type: image/png' );
