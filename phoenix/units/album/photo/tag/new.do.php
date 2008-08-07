060f40f446f8ec43966b16f54d29b21d1b5466df
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-08-07 14:27:56 +0000

    Show some alerts when necessary

--- phoenix/units/album/photo/tag/new.do.php
+++ phoenix/units/album/photo/tag/new.do.php
@@ -33,8 +33,8 @@
         $relationfinder = New FriendRelationFinder();
         // check if user is owner of photo or friend of owner; you can't tag some unknown person's photos
         if ( $photoowner->Id != $user->Id 
-             && $relationfinder->IsFriend( $photoowner, $user ) | FRIENDS_BOTH == FRIENDS_BOTH ) {
-             ?>alert( 'Δεν έχεις καμία σχέση με τον συγκεκριμένο χρήστη' );
+             && $relationfinder->IsFriend( $photouser, $user ) | FRIENDS_BOTH == FRIENDS_BOTH ) {
+             ?>alert( 'Δεν έχεις καμία σχέση με τον κάτοχο της φωτογραφίας' );
              window.location.reload();<?php
             return;
         }
