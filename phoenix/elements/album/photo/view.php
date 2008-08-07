78dc3bf40c1cb43c9b72c076c2f38d411e854ba3
Chris Pappas <chrispappas12@gmail.com>
2008-08-07 23:11:08 +0000

    mad banner

--- phoenix/elements/album/photo/view.php
+++ phoenix/elements/album/photo/view.php
@@ -181,7 +181,7 @@
                 ?><div class="thephoto"><?php
                     Element( 'image/view' , $image , IMAGE_FULLVIEW, '' , $title , $title , '' , false , 0 , 0 );
                 ?></div>
-				<div class="banner 728x90">
+				<div class="banner b728x90">
 					<a href="http://www.mad.tv/madradio/"><img src="http://static.zino.gr/phoenix/banners/madradio.gif" /></a>
 				</div>
                 <div class="comments"><?php
