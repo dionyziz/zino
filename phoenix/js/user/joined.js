676504f6ac3ddd4444ea6d66ee632a60a018074c
Chris Pappas <chrispappas12@gmail.com>
2008-08-09 10:16:39 +0000

    user registration

--- phoenix/js/user/joined.js
+++ phoenix/js/user/joined.js
@@ -12,12 +12,6 @@ $( document ).ready( function() {
 		$( 'div a.button' ).click( function() {
 			if ( Joined.enabled ) {
 				$( this ).addClass( 'button_disabled' );
-				if ( Joined.doby.options[ Joined.doby.selectedIndex ].value != -1 && Joined.dobm.options[ Joined.dobm.selectedIndex ].value != -1 && Joined.dobd.options[ Joined.dobd.selectedIndex ].value != -1 ) {
-					if ( !Dates.ValidDate( Joined.dobd.options[ Joined.dobd.selectedIndex ].value , Joined.dobm.options[ Joined.dobm.selectedIndex ].value , Joined.doby.options[ Joined.doby.selectedIndex ].value ) ) {
-						$( 'div.profinfo form span.invaliddob' ).css( 'opacity' , '0' ).show().animate( { opacity : "1" } , 200 ) ;
-						Joined.invaliddob = true;
-					}
-				}
 				Coala.Warm( 'user/joined' , { 
 					doby : Joined.doby.options[ Joined.doby.selectedIndex ].value,
 					dobm : Joined.dobm.options[ Joined.dobm.selectedIndex ].value,
@@ -31,8 +25,16 @@ $( document ).ready( function() {
 		});
 	}
 	$( 'div.profinfo form div select' ).change( function() {
-		$( 'div.profinfo form span.invaliddob' ).animate( { opacity : "0" } , 200 , function() {
-			$( this ).hide();
-		} );
+		if ( Joined.doby.options[ Joined.doby.selectedIndex ].value != -1 && Joined.dobm.options[ Joined.dobm.selectedIndex ].value != -1 && Joined.dobd.options[ Joined.dobd.selectedIndex ].value != -1 ) {
+			if ( !Dates.ValidDate( Joined.dobd.options[ Joined.dobd.selectedIndex ].value , Joined.dobm.options[ Joined.dobm.selectedIndex ].value , Joined.doby.options[ Joined.doby.selectedIndex ].value ) ) {
+				$( 'div.profinfo form span.invaliddob' ).css( 'opacity' , '0' ).show().animate( { opacity : "1" } , 200 ) ;
+				Joined.invaliddob = true;
+			}
+		}
+		if ( Joined.invaliddob ) {
+			$( 'div.profinfo form span.invaliddob' ).animate( { opacity : "0" } , 200 , function() {
+				$( this ).hide();
+			} );
+		}
 	} );
 });
