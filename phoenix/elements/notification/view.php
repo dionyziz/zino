788a9f0a07ffaaceac2656cb3a3804f9e5aa292a
Chris Pappas <chrispappas12@gmail.com>
2008-08-10 14:36:56 +0000

    user/avatar persistency

--- phoenix/elements/notification/view.php
+++ phoenix/elements/notification/view.php
@@ -40,7 +40,7 @@
                     ?>' );"<?php
                 }
                 ?>><?php
-                    Element( 'user/avatar' , $notif->FromUser->Avatar->Id , $notif->FromUser->Id , $notif->FromUser->Avatar->Width , $notif->FromUser->Height , $notif->FromUser->Name , 100 , 'avatar' , '' , true , 50 , 50 );
+                    Element( 'user/avatar' , $notif->FromUser->Avatar->Id , $notif->FromUser->Id , $notif->FromUser->Avatar->Width , $notif->FromUser->Avatar->Height , $notif->FromUser->Name , 100 , 'avatar' , '' , true , 50 , 50 );
                     Element( 'user/name' , $notif->FromUser->Id , $notif->FromUser->Name , $notif->FromUser->Subdomain , false );
                     if ( $notif->Event->Typeid != EVENT_FRIENDRELATION_CREATED ) {
                         if ( $notif->Item->Parentid == 0 ) {
