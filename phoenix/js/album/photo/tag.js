1d4a874873ed353bb11a688c86e2f32660e8341c
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-08-10 18:10:17 +0000

    Tag message

--- phoenix/js/album/photo/tag.js
+++ phoenix/js/album/photo/tag.js
@@ -206,7 +206,7 @@ $( document ).ready( function() {
                     $( this ).remove(); 
                 } ); 
             } );
-        $( 'div.thephoto div.tanga div' ).mouseover( function() { $( this ).css( "borderWidth", "2px" ); } );
+        $( 'div.thephoto div.tanga div' ).mouseover( function() { $( this ).css( { "borderWidth" : "2px", "cursor" : "pointer" } ); } );
         $( 'div.thephoto div.tanga div' ).mouseout( function() { $( this ).css( "borderWidth", "0px" ); } );
             
         var border_width = 3*2;
