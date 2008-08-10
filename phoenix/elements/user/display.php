151102d6adf8efbb3e3025e72285ced59e22bbfd
Chris Pappas <chrispappas12@gmail.com>
2008-08-10 13:42:45 +0000

    user/avatar persistency

--- phoenix/elements/user/display.php
+++ phoenix/elements/user/display.php
@@ -4,7 +4,7 @@
             ?><a href="<?php
             Element( 'user/url' , $theuser->Id , $theuser->Subdomain );
             ?>"><?php
-            Element( 'user/avatar' , $theuser , 100 , 'avatar' , '' , true , 50 , 50 );
+            Element( 'user/avatar' , $theuser->Avatar->Id , $theuser->Id , $theuser->Avatar->Width , $theuser->Avatar->Height , $theuser->Name , 100 , 'avatar' , '' , true , 50 , 50 );
             Element( 'user/name' , $theuser->Id , $theuser->Name , $theuser->Subdomain , false );
             ?></a><?php
             
