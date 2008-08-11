34f613672177c02045d7f250203414f739cbae40
Dionysis Zindros <dionyziz@gmail.com>
2008-08-11 14:17:54 +0000

    memcache testing

---
+++ phoenix/elements/developer/memcache/view.php
@@ -0,0 +1,38 @@
+<?php
+    class ElementDeveloperMemcacheView extends Element {
+        public function Render( tText $key ) {
+            global $mc;
+            global $user;
+
+            if ( !$user->HasPermission( PERMISSION_MEMCACHE_VIEW ) ) {
+                ?>Access denied<?php
+                return;
+            }
+
+            $key = $key->Get();
+
+            ?><form action="?p=testmc" method="get">
+                Memcache key: <input type="text" name="key" value="<?php
+                echo htmlspecialchars( $key );
+                ?>" />
+                <input type="submit" value="Check" />
+            </form><?php
+
+            $value = $mc->get( $key );
+            ?><br /><br /><br /><?php
+            echo var_dump( $value );
+            ?><br /><br />---------------<br /><br /><?php
+            print_r( $value );
+
+
+            ?><form action="do/memcache/delete" method="post">
+                <input type="hidden" name="key" value="<?php
+                echo htmlspecialchars( $key );
+                ?>" />
+                <input type="submit" value="Delete this key" />
+            </form>
+            
+            <?php
+        }
+    }
+?>
