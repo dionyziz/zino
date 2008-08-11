047cd1e2db787c4f7cb2f732e2ad2a0ee391d0c1
Chris Pappas <chrispappas12@gmail.com>
2008-08-11 23:02:40 +0000

    frontpage mockup

--- phoenix/elements/frontpage/journal/list.php
+++ phoenix/elements/frontpage/journal/list.php
@@ -1,9 +1,26 @@
 <?php
     class ElementFrontpageJournalList extends Element {
         public function Render() {
-            ?><div class="latestjournals">
-                <h2>Ημερολόγια</h2>
-            </div><?php
+            $finder = New JournalFinder();
+            $journals = $finder->FindAll( 0 , 5 );
+            ?><div class="list">
+                <h2>Ημερολόγια</h2><?php
+                foreach ( $journals as $journal ) {
+                    ?><div class="event">
+                        <div class="who"><?php
+                            Element( 'user/display' , $journal->User->Id , $journal->User->Avatar->Id , $journal->User );
+                        ?> καταχώρησε
+                        </div>
+                        <div class="subject">
+                            <a href="?p=journal&amp;id=<?php
+                            echo $journal->Id;
+                            ?>"><?php
+                            echo htmlspecialchars( $journal->Title );
+                            ?></a>
+                        </div>
+                    </div><?php
+                }
+            ?></div><?php
         }
     }
 ?>
