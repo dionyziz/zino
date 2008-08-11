f59d1daf49fcbfc846d232dc656ded49d635e51c
Chris Pappas <chrispappas12@gmail.com>
2008-08-11 21:43:25 +0000

    frontpage mockup

--- phoenix/elements/frontpage/shoutbox/_list.php
+++ phoenix/elements/frontpage/shoutbox/_list.php
@@ -8,20 +8,18 @@
             
             $finder = New ShoutboxFinder();
             $shouts = $finder->FindLatest( 0 , 5 )
-            ?><div class="shoutbox">
-                <h2>Συζήτηση</h2>
-                <div class="comments"><?php
-                    if ( $user->Exists() && $user->HasPermission( PERMISSION_SHOUTBOX_CREATE ) ) {
-                        Element( 'shoutbox/reply' , $user->Id , $user->Avatar->Id , $user );
-                    }
-                    foreach ( $shouts as $shout ) {
-                        Element( 'shoutbox/view' , $shout , false );
-                    }
-                    Element( 'shoutbox/view'  , false , true );
-                ?></div>
+            ?><h2>Συζήτηση</h2>
+            <div class="comments"><?php
+                if ( $user->Exists() && $user->HasPermission( PERMISSION_SHOUTBOX_CREATE ) ) {
+                    Element( 'shoutbox/reply' , $user->Id , $user->Avatar->Id , $user );
+                }
+                foreach ( $shouts as $shout ) {
+                    Element( 'shoutbox/view' , $shout , false );
+                }
+                Element( 'shoutbox/view'  , false , true );
+            ?></div>
             <div class="eof"></div>
-            <div class="more"><a href="shouts" class="button">Όλες οι συζητήσεις&raquo;</a></div>
-            </div><?php
+            <div class="more"><a href="shouts" class="button">Όλες οι συζητήσεις&raquo;</a></div><?php
         }
     }
 ?>
