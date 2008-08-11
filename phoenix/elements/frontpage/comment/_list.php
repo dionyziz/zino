bd6bfa90de3d7c3837a4a7d5733aada38161757f
Chris Pappas <chrispappas12@gmail.com>
2008-08-11 21:05:38 +0000

    frontpage mockup

---
+++ phoenix/elements/frontpage/comment/_list.php
@@ -0,0 +1,23 @@
+<?php
+    class ElementFrontpageCommentList extends Element {
+        public function Render() {
+            global $libs;
+
+            $libs->Load( 'comment' );
+            
+            $finder = New CommentFinder();
+            $comments = $finder->FindLatest( 0 , 5 );
+            ?><div class="latestcomments">
+                <h2>Σχόλια</h2>
+                <div class="list"><?php
+                    foreach ( $comments as $comment ) {
+                        Element( 'frontpage/comment/view' , $comment );
+                    }
+                ?></div>
+            <div class="eof"></div>
+            <div class="more"><a href="comments" class="button">Όλα τα σχόλια&raquo;</a></div>
+            </div><?php
+        
+        }
+    }
+?>
