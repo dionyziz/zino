d7674fda12772c3d315327a744947ad117efece5
Dionysis Zindros <dionyziz@gmail.com>
2008-08-12 23:46:58 +0000

    allpolls/alljournals

--- phoenix/libs/project.php
+++ phoenix/libs/project.php
@@ -113,8 +113,8 @@ Project_Destruct()' );
 	        'statistics'	=> 'statistics/view',
             'mcdelete'          => 'developer/abresas/mcdelete',
 			'favourites'		=> 'favourite/favourite',
-            'polls'             => 'poll/recent/list',
-            'journals'          => 'journal/recent/list'
+            'allpolls'             => 'poll/recent/list',
+            'alljournals'          => 'journal/recent/list'
         );
     }
 ?>
