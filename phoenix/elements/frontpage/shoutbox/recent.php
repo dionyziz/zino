5aed53a3f7b14da7f9851f7cb9a45262567b1183
Chris Pappas <chrispappas12@gmail.com>
2008-08-13 22:00:28 +0000

    frontpage caching

---
+++ phoenix/elements/frontpage/shoutbox/recent.php
@@ -0,0 +1,15 @@
+<?php    
+    class ElementFrontpageShoutboxList extends Element {
+        protected $mPersistent = array( 'shoutboxseq' );
+        public function Render( $shoutboxseq ) {
+            global $libs;
+            $libs->Load( 'shoutbox' );
+
+            $finder = New ShoutboxFinder();
+            $shouts = $finder->FindLatest( 0 , 7 );
+            foreach ( $shouts as $shout ) {
+                Element( 'shoutbox/view' , $shout , false );
+            }
+        }
+    }
+?>
