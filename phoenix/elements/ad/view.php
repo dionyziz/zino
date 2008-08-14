6b762ad09aaa54af6d1cc41320c838706e84ed6e
Dionysis Zindros <dionyziz@gmail.com>
2008-08-14 11:29:51 +0000

    ads

--- phoenix/elements/ad/view.php
+++ phoenix/elements/ad/view.php
@@ -1,8 +1,6 @@
 <?php
     class ElementAdView extends Element {
         public function Render( $type ) {
-            die( 'fools!' );
-
             switch ( $type ) {
                 case AD_JOURNAL:
                     ?><script type="text/javascript"><!--
