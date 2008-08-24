c05e0c6d17cc5455ea9a5500e5f84c48345dc2ea
Dionysis Zindros <dionyziz@gmail.com>
2008-08-24 23:45:59 +0000

    s

--- phoenix/libs/event.php
+++ phoenix/libs/event.php
@@ -311,7 +311,7 @@
             $this->User = $this->HasOne( 'User', 'Userid' );
             if ( $this->Exists() ) {
                 $this->Item = $this->HasOne( $model, 'Itemid' );
-                die( "Trexo " );
+                die( $model . " " . var_dump( $this->Item ) );
             }
         }
         protected function OnCreate() {
