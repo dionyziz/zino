11fa0d3261e52397c923aaabb5545a25f6b422f3
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-08-26 22:06:41 +0000

    Search

--- phoenix/elements/search/view.php
+++ phoenix/elements/search/view.php
@@ -4,6 +4,10 @@
             ?><div id="search"><?php
             Element( 'search/options' );
             ?></div><?php
+                $users = array();
+                $users[] = New User( 1 );
+                $users[] = New User( 791 );
+            Element( 'user/list', $users );
         }
     }
 ?>
