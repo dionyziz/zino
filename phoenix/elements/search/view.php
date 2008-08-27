92a43059f758ae8353d1d8a5a96b1d52084a433f
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-08-27 09:20:46 +0000

    Pagification

--- phoenix/elements/search/view.php
+++ phoenix/elements/search/view.php
@@ -1,13 +1,22 @@
 <?php
     class ElementSearchView extends Element {
-        public function Render() {
+        public function Render( $pageno ) {
+            global $xc_settings;
+
             ?><div id="search"><?php
             Element( 'search/options' );
             ?></div><?php
+                // Get $users by a finder using $users_per_page, $pageno in the LIMIT statement.
                 $users = array();
                 $users[] = New User( 1 );
                 $users[] = New User( 791 );
+                $users_per_page = 2;
             Element( 'user/list', $users );
+            if ( $pageno <= 0 ) {
+                $pageno = 1;
+            }
+            // Change the link
+            Element( 'pagify', $pageno, $xc_settings[ 'webaddress'] . "?p=search&pageno=", $users/$users_per_page );
         }
     }
 ?>
