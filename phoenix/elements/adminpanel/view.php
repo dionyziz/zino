6c3f2b241d59061c5534e7f5bb57f32bbf15dc55
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-08-27 20:24:16 +0000

    Administration panel dev

---
+++ phoenix/elements/adminpanel/view.php
@@ -0,0 +1,30 @@
+<?php
+    
+    class ElementAdminpanelView extends Element
+    {
+    
+        public function Render() {
+        global $page;
+        global $user;
+        
+        if( ! $user->HasPermission( PERMISSION_ADMINPANEL_VIEW ) ) {
+        ?> Permission Denied <?php
+        return;
+        }
+        
+        $page->setTitle( 'Administration Panel' );
+        
+        ?> <h2>Administrtion Panel</h2> <?php
+        
+        ?> <ul> <?php
+        ?> <li><a href="?p=statistics" > Daily Statistics </a></li> <?php
+        ?> <li><a href="?p=ban" > Ban list(not implemented yet) </a></li> <?php
+        ?> <li><a href="?=admin_log" > Admin's log(not implemented yet) </a></li> <?php
+        ?> </ul> <?php
+        
+        }
+    }
+?>
+        
+        
+        
