abddc819751a8e81807f1a263018feced3c2034d
Chris Pappas <chrispappas12@gmail.com>
2008-08-27 20:56:50 +0000

    adminpanel

--- phoenix/elements/adminpanel/view.php
+++ phoenix/elements/adminpanel/view.php
@@ -1,30 +1,23 @@
 <?php    
-    class ElementAdminpanelView extends Element
-    {
-    
+    class ElementAdminpanelView extends Element {
         public function Render() {
-        global $page;
-        global $user;
-        
-        if( ! $user->HasPermission( PERMISSION_ADMINPANEL_VIEW ) ) {
-        ?> Permission Denied <?php
-        return;
-        }
-        
-        $page->setTitle( 'Administration Panel' );
-        
-        ?> <h2>Administration Panel</h2> <?php
-        
-        ?> <ul> <?php
-        ?> <li><a href="?p=statistics" > Daily Statistics </a></li> <?php
-        ?> <li><a href="?p=ban" > Ban list(not implemented yet) </a></li> <?php
-        ?> <li><a href="?=admin_log" > Admin's log(not implemented yet) </a></li> <?php
-        ?> </ul> <?php
-        
-        return;        
+	        global $page;
+	        global $user;
+	        
+	        if( ! $user->HasPermission( PERMISSION_ADMINPANEL_VIEW ) ) {
+	        ?> Permission Denied <?php
+	        return;
+	        }
+	        
+	        $page->setTitle( 'Administration Panel' );
+	        
+	        ?><h2>Administration Panel</h2><?php
+	        
+	        ?><ul><?php
+		        ?><li><a href="?p=statistics" > Daily Statistics </a></li><?php
+		        ?><li><a href="?p=ban" > Ban list(not implemented yet) </a></li><?php
+		        ?><li><a href="?=admin_log" > Admin's log(not implemented yet) </a></li><?php
+	        ?></ul><?php    
         }
     }
 ?>
-        
-        
-        
