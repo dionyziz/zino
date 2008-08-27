071eb3663cbcf936958a41971e17743d955dd4e8
Dionysis Zindros <dionyziz@gmail.com>
2008-08-27 11:57:14 +0000

    zino user search

--- phoenix/elements/search/view.php
+++ phoenix/elements/search/view.php
@@ -30,12 +30,14 @@
             ?></div><?php
             // Get $users by a finder using $users_per_page, $pageno in the LIMIT statement.
             $finder = New UserSearch();
+            /*
             $users = $finder->FindByDetails( $minage, $maxage, $location, $gender, $sexual, '', $offset, $limit );
             Element( 'user/list', $users );
             // Change the link
             $link = $xc_settings[ 'webaddress' ] . "?p=search&pageno=";
             $pages = $finder->FoundRows() / $limit;
             Element( 'pagify', $pageno, $link, $pages );
+            */
         }
     }
 ?>
