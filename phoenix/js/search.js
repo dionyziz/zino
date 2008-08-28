32e1ca6e6aedbdfe4e07792519d3f9b284360133
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-08-28 19:13:12 +0000

    Search Fixes

---
+++ phoenix/js/search.js
@@ -0,0 +1,18 @@
+var Search = {
+    check : function() {
+        var check = false;
+        if ( $( 'div.ybubble div.body form div.search input' )[2].checked != 'checked' ) {
+            return true;
+        }
+        $( 'div.ybubble div.body form div.search select' ).each( function() {
+                if ( this.selectedIndex == 0 ) {
+                    check = true;
+                }
+            } );
+        if ( check ) {
+            alert( "Please fill something" );
+            return false;
+        }
+        return true;
+    }
+};
