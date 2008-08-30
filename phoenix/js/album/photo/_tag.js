bbf5991c9956b32a9cfaaa09437a887bc08308ca
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-08-30 18:46:35 +0000

    Tag Resize Box

--- phoenix/js/album/photo/_tag.js
+++ phoenix/js/album/photo/_tag.js
@@ -374,7 +374,11 @@ var Tag = {
         var y = event.offsetY?(event.offsetY):event.pageY-$( "div.thephoto" ).get( 0 ).offsetTop;
 		var pos_x = parseInt( $( 'div.tagme' ).css( 'left' ), 10 );
 		var pos_y = parseInt( $( 'div.tagme').css( 'top' ), 10 );
-		$( 'div.tagme' ).css( { "width" : ( x - pos_x ) + 'px', "height" : ( y - pos_y ) + 'px' } );
+		var width = x - pos_x;
+		var height = y - pos_y;
+		$( 'div.tagme' ).css( { "width" : width + 'px', "height" : height + 'px' } );
+		var left = parseInt( $( 'div.tagme' ).css( 'left' ), 10 );
+		$( 'div.thephoto div.frienders' ).css( 'left', ( left + width ) + 'px' );
 	}
 };
 $( document ).ready( function() {
