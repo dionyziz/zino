a0857ea00e10c66fa72e6dc3c6a2363e01091417
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-09-05 16:29:32 +0000

    ban satori class

---
+++ phoenix/elements/banlist/view.php
@@ -0,0 +1,27 @@
+<?php
+    class ElementBanlistView extends Element {
+        public function Render() {
+            global $user;
+            global $libs;
+            global $page;
+            
+            if ( !$user->hasPermission( PERMISSION_ADMINPANEL_VIEW ) ) {
+                ?> Permission Denied <?php
+                return;
+            }
+            
+            $libs->Load( 'adminpanel/ban' );
+            
+            $page->setTitle( 'List of banned members' );
+            
+            $ban = new Ban();
+            $res = $ban->BanUser( 'kapios123' );
+            
+            ?><p><?php
+            echo $res;
+            ?></p><?php
+            
+            return;
+        }
+    }
+?>
