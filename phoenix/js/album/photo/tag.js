263e24789a5f86f5756cfd3650dab98c5d0769f5
Kostis Roussopoulos <kostis90gr@gmail.com>
2008-09-06 18:44:15 +0000

    AutoScrolling done

--- phoenix/js/album/photo/tag.js
+++ phoenix/js/album/photo/tag.js
@@ -62,7 +62,7 @@ var Tag = {
         $( 'dd.addtag' ).hide(); // Hide tagging button
         $( 'div.thephoto > div:not(.tanga)' ).show(); // Show tagging windows, but not image tags
         if ( aux === true ) {
-            $( 'div.thephoto' ).get(0).scrollIntoView( true );
+            $( 'html, body' ).animate( { scrollTop: ( $( 'div.thephoto' ).offset().top - 20 ) }, 700 );
         }
         $( 'div.thephoto div.frienders form input' ).focus();
         Tag.run = true; // Tagging is now fully enabled
