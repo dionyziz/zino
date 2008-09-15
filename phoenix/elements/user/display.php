e7d9f1770eedb148e50c00c5e113246a5b5d88e4
Dionysis Zindros <dionyziz@gmail.com>
2008-09-15 11:22:33 +0000

    last username change

--- phoenix/elements/user/display.php
+++ phoenix/elements/user/display.php
@@ -1,6 +1,7 @@
 <?php
     class ElementUserDisplay extends Element {
         protected $mPersistent = array( 'theuserid' , 'avatarid' );
+
         public function Render( $theuserid , $avatarid , $theuser ) {
             ?><a href="<?php
             Element( 'user/url' , $theuserid , $theuser->Subdomain );
