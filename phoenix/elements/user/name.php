024d59978a9cbd5ac87e19308b2a8fe99b4b0117
Dionysis Zindros <dionyziz@gmail.com>
2008-09-15 11:21:42 +0000

    last username change

--- phoenix/elements/user/name.php
+++ phoenix/elements/user/name.php
@@ -1,6 +1,7 @@
 <?php
     class ElementUserName extends Element {
         protected $mPersistent = array( 'theuserid' , 'link' );
+        
 		public function Render( $theuserid , $theusername , $theusersubdomain, $link = true ) {
             if ( !$link ) {
                 echo htmlspecialchars( $theusername );
