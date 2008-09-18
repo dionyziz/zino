bf5a43c1267e8748f3da44efa191a734a2aead9d
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-09-18 17:41:12 +0000

    ban ending credits

--- phoenix/libs/user/user.php
+++ phoenix/libs/user/user.php
@@ -242,7 +242,7 @@
             $libs->Load( 'adminpanel/ban' );
             
             $ban = new Ban();            
-            if ( $ban->isBannedIp( UserIp() )) {// || $ban->isBannedUser( $this->Id ) ) {
+            if ( $ban->isBannedIp( UserIp() ) || $ban->isBannedUser( $this->Id ) ) {
                 return false;
             } 
                 
