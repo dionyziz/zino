8bf62f0ef13804b1d9946e27a6b6fd9ac21b50e2
Dionysis Zindros <dionyziz@gmail.com>
2008-09-25 15:23:52 +0000

    image links

--- phoenix/libs/_wysiwyg.php
+++ phoenix/libs/_wysiwyg.php
@@ -77,7 +77,7 @@
 
     function WYSIWYG_Links( $text ) {
         $text = preg_replace( // LOL is a special testcase -- when it exists within a link it should not be matched as a link, because it will be matched from the smiley code later
-            '#\b(https?\://[a-z0-9.-]+(/([a-zA-Z0-9./+?=&\(\)_;\#~%-](?<!LOL))*+)?+)(?<!\.jpg|\.png|\.gif)#',
+            '#\b(https?+\://[a-z0-9.-]++(/([a-zA-Z0-9./+?=&\(\)_;\#~%-](?<!LOL))*+)?+)(?<!\.jpg|\.png|\.gif)#',
             '<a href="\1">\1</a>',
             $text
         );
