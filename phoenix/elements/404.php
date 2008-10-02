19582ab802f71ed4819ea2f6849a8f080dc57fa4
Dionysis Zindros <dionyziz@gmail.com>
2008-10-02 12:08:28 +0000

    404

--- phoenix/elements/404.php
+++ phoenix/elements/404.php
@@ -1,6 +1,7 @@
 <?php
     class Element404 extends Element {
         public function Render() {
+            header( 'HTTP/1.0 404 File Not Found' );
             ?>Συγγνώμη, αλλά η σελίδα που ζητήσατε δεν είναι διαθέσιμη.<br />
             Προσπαθήστε ξανά σε μερικά λεπτά.<br /><?php
         }
