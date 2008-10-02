8cb5914e1202cdfa2a635bd6b0b33e36b8be6e4e
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-10-02 10:30:24 +0000

    frontend changes

--- phoenix/elements/banlist/view.php
+++ phoenix/elements/banlist/view.php
@@ -36,6 +36,34 @@
                     ?></p><?php
                 ?></form><?php
             }
+            
+            //table
+            ?><table>
+                <tr>
+                    <th>Χρήστης</th>
+                    <th>Άρχισε</th>
+                    <th>Τελειώνει</th>
+                    <th>Αιτία</th>
+                </tr>
+              </table><?php                    
+            
+            foreach ( $bannedUsers as $bannedUser ) {
+                ?><form method="post" action="do/adminpanel/revoke"><?php
+                    ?><p>Ο χρήστης <?php
+                    echo $bannedUser->Name;
+                    ?> αποκλείστηκε στις <?php
+                    echo $bannedUser->Started;
+                    ?> μέχρι τις <?php
+                    echo $bannedUser->Expire;                
+                    ?>.  <?php
+                    ?><input type="submit" value="Επαναφορά" /><?php
+                    ?><input type="hidden" name="userid" value="<?php
+                    echo $bannedUser->Userid; 
+                    ?>" /><?php
+                    ?></p><?php
+                ?></form><?php
+            }
+            //
 
             ?><form method="post" action="do/adminpanel/ban"><?php
             ?><p>Όνομα χρήστη : <input type="text" name="username" /></p><?php
