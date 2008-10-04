0146cac665cac7f2a82da377847338e7c66de1ba
Chris Pappas <chrispappas12@gmail.com>
2008-10-04 07:52:06 +0000

    userprofile caching sidebar

--- phoenix/elements/user/profile/sidebar/details.php
+++ phoenix/elements/user/profile/sidebar/details.php
@@ -6,20 +6,19 @@
             ?><div class="look">
 				<span class="malebody">&nbsp;</span><?php
                 Element( 'user/profile/sidebar/look', $theuser->Profile->Height, $theuser->Profile->Weight,  $theuser->Gender );
-                ?></div>
-                <div class="social"><?php
-                    Element( 'user/profile/sidebar/social/view' , $theuser );
-                ?></div>
-                <div class="aboutme"><?php
-                    Element( 'user/profile/sidebar/aboutme' , $theuser->Profile->Aboutme, $theuser->Id, $theuser->Profile->Updated );
-                ?></div>
-                <div class="interests"><?php
-                    Element( 'user/profile/sidebar/interests' , $theuser );
-                ?></div>
-                <div class="contacts"><?php
-                    Element( 'user/profile/sidebar/contacts' , $theuser, $theuser->Id, $theuser->Profile->Updated );
-                ?></div>
-            </div><?php
+            ?></div>
+            <div class="social"><?php
+                Element( 'user/profile/sidebar/social/view' , $theuser );
+            ?></div>
+            <div class="aboutme"><?php
+                Element( 'user/profile/sidebar/aboutme' , $theuser->Profile->Aboutme, $theuser->Id, $theuser->Profile->Updated );
+            ?></div>
+            <div class="interests"><?php
+                Element( 'user/profile/sidebar/interests' , $theuser );
+            ?></div>
+            <div class="contacts"><?php
+                Element( 'user/profile/sidebar/contacts' , $theuser, $theuser->Id, $theuser->Profile->Updated );
+            ?></div><?php
         }
     }
 ?>
