36577fbf9f183ac573025eaa13efc26fee7a78a5
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-10-04 09:38:48 +0000

    masked login check for bot

---
+++ phoenix/actions/user/_login.php
@@ -0,0 +1,41 @@
+<?php
+    function ActionUserLogin( tText $username, tText $password ) {
+        global $user;
+        global $rabbit_settings;
+        global $water;
+        global $libs;
+        
+        $username = $username->Get();
+        $password = $password->Get();
+        $finder = New UserFinder();
+        $user = $finder->FindByNameAndPassword( $username, $password );
+        
+        $libs->Load( 'loginattempt' );
+        $libs->Load( 'adminpanel/ban' );
+        $loginattempt = New LoginAttempt();
+        $loginattempt->Username = $username;
+        if ( $user === false ) {
+            $loginattempt->Password = $password;
+            $loginattempt->Save();
+            
+            if ( LoginAttempt_checkBot( UserIp() ) ) {
+                $ban = new Ban();
+                $ban->BanIp( UserIp(), 1*60 );
+            }
+
+            return Redirect( '?p=a' );
+        }
+        // don't store the password for security reasons
+        $loginattempt->Success = 'yes';
+        $loginattempt->Save();
+        // else...
+        $user->UpdateLastLogin();
+        $user->RenewAuthtoken();
+        $user->Save();
+        $_SESSION[ 's_userid' ] = $user->Id;
+        $_SESSION[ 's_authtoken' ] = $user->Authtoken;
+        User_SetCookie( $user->Id, $user->Authtoken );
+
+        return Redirect( $_SERVER[ 'HTTP_REFERER' ]  );
+    }
+?>
