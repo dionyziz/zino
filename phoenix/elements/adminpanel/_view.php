c37940ed669fd73f98a23b694145f5eda9a5ab4f
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-10-05 17:55:35 +0000

    bennu experimenting

--- phoenix/elements/adminpanel/_view.php
+++ phoenix/elements/adminpanel/_view.php
@@ -19,7 +19,7 @@
 		        ?><li><a href="?p=adminlog" >Ενέργειες διαχειριστών</a></li><?php
 	        ?></ul><?php    
 	        
-	        global $libs;
+	        /*global $libs;
 	        $libs->Load( 'user/user' );
 	        $libs->Load( 'bennu/bennu' );
 	        
@@ -36,6 +36,7 @@
 	            echo '<p>'.$key.' '.$val.'</p>';
 
             }
+            */
 	        
         }
     }
