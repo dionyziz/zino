0c7958bd674ecd88638b51ee64af703f562ab5bb
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-10-07 15:59:39 +0000

    bennu testing

--- phoenix/libs/bennu/bennu.php
+++ phoenix/libs/bennu/bennu.php
@@ -1,12 +1,10 @@
 <?php
     class BennuRule {
-        protected $mAttribute;
         protected $mSigma;
         protected $mValue;
         protected $mCost;//low medium high 
         
-        public function SetRule( $attribute, $value, $priority, $sigma ) {
-            $this->mAttribute = $attribute;//attribute's name
+        public function SetRule( $value, $priority, $sigma ) {
             $this->mSigma = $sigma;
             $this->mValue = $value;//the ideal value
             
@@ -23,11 +21,11 @@
             }      
         }
         
-        public function Calculate( $sample ) {
+        protected function Calculate( $sample ) {
             $value;
             
             if ( $this->mSigma == 0 ) {// for yes-on type of rules
-                if ( $this->Value( $sample ) === $this->mValue ) {
+                if ( $this->Get( $sample ) === $this->mValue ) {
                     return $this->mCost;
                 }
                 else {
@@ -36,7 +34,7 @@
             }            
             
             if ( $this->mSigma > 0 ) {//for int rules with sigma            
-                $value = abs( ( $this->mCost * ( $this->Value( $sample )  - $this->mValue ) ) / $this->mSigma );
+                $value = abs( ( $this->mCost * ( $this->Get( $sample )  - $this->mValue ) ) / $this->mSigma );
                 if ( $value < $this->mCost ) {
                     return ( $this->mCost - $value );
                 }
@@ -47,18 +45,8 @@
             
             return;
         }
-        
-        protected function Value( $sample ) {        
-            $attributes = explode( '->', $this->mAttribute );                       
-            if ( count( $attributes ) == 2 ) {
-                return $sample->$attributes[ 1 ];
-            }
-            else if ( count( $attributes ) == 3 ) {
-                return $sample->$attributes[ 1 ]->$attributes[ 2 ];
-            }
-            return;
-        }
     }
+    
 
     class Bennu {
         protected $mInput;
@@ -71,9 +59,7 @@
             return;
         }
         
-        public function AddRule( $attribute, $value, $priority = 'medium', $sigma = 0 ) {
-            $rule = new BennuRule;         
-            $rule->SetRule( $attribute, $value, $priority, $sigma );            
+        public function AddRule( $rule ) {            
             $this->mRules[] = $rule;
             return;
         }
@@ -81,61 +67,28 @@
         public function GetResult() {
             $res = array();
             foreach ( $this->mInput as $sample ) {
-                
-                if ( $sampe->Id == $this->mTarget->Id ) {//exclude the target from  evaluating
-                    continue;
-                }
-            
-                $res[ $sample->Name ] = $this->Calculate( $sample );
+                $res[ $sample->Name ] = $this->GetScore( $sample );
             }
             
             return $res;
         }
         
-        protected function Calculate( $sample ) {
-        
-            $total_score = 0;
-            
+        protected function GetScore( $sample ) {        
+            $total_score = 0;            
             foreach ( $this->mRules as $rule ) {
                 $score += $rule->Calculate( $sample );
-            }
-            
+            }            
             return $score;      
+        }
+    }
+    
+    class BennuRuleGender extends BennuRule {
+        protected function Get( $sample ) {
+            return $sample->Gender;
+        }
         
-        
-            $total_score = 0;
-            $score;
-            $value = 10;
-            
-            //date
-            
-            
-            //age sigma = 2
-            $score = abs( ( 2 * ( $sample->Profile->Age - $this->mTarget->Profile->Age ) ) / $value );
-            if ( $score < $value ) {
-                $total_score += $value - $score;
-            }
-            
-            //location
-            if ( $sample->Profile->Placeid === $this->mTarget->Profile->Placeid ) {
-                $total_score += $value;
-            }
-            
-            //friends
-            
-            //sex
-            if ( $sample->Gender === $this->mTarget->Gender ) {
-                $total_score += $value;
-            }
-            
-            //activity sigma = 7*24*60*60
-            $sigma = 7*24*60*60;
-            $score = abs( ( $sigma * ( strtotime( $sample->Lastlogin ) - strtotime( $this->mTarget->Lastlogin ) ) ) / $value );
-            if ( $score < $value ) {
-                $total_score += $value - $score;
-            }
-            
-            return $total_score;
+        protected function Calculate( $sample ) {
+            return 1;
         }
     }
     
