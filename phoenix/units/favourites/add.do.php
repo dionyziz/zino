1afea9e3245de9a50d5094fe096116fab290abaa
Dionysis Zindros <dionyziz@gmail.com>
2008-10-08 22:06:11 +0000

    favourites bug

--- phoenix/units/favourites/add.do.php
+++ phoenix/units/favourites/add.do.php
@@ -3,13 +3,12 @@
     function UnitFavouritesAdd( tInteger $itemid , tInteger $typeid ) {
         global $libs;
         
-        die( 'Breakpoint 104' );
-
         $libs->Load( 'favourite' );
         
         $favourite = New Favourite();
         $favourite->Itemid = $itemid->Get();
         $favourite->Typeid = $typeid->Get();
+        die( 'Breakpoint 104' );
         $favourite->Save();
     }
 ?>
