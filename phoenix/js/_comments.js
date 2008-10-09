348a2b50703d8194e866dfdc73224866d05cca4b
Chris Pappas <chrispappas12@gmail.com>
2008-10-09 19:18:29 +0000

    comments deletion

--- phoenix/js/_comments.js
+++ phoenix/js/_comments.js
@@ -250,7 +250,8 @@ $( document ).ready( function() {
                     if ( username == $( this ).find( 'div.who a img.avatar' ).attr( 'alt' ) ) {
                         var leftpadd = $( this ).css( 'padding-left' );
                         var value = leftpadd.substr( 0 , leftpadd.length - 2 );
-                        if ( !$( this ).next( "div.comment[style='padding-left:" value + 20  "px;']" )[ 0 ] ) {
+                        value += 20;
+                        if ( !$( this ).next( "div.comment[style='padding-left:" + value + "px;']" )[ 0 ] ) {
                             $( this ).css( 'border' , '1px solid red' );
                         }
                     }
