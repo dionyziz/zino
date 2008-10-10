1c80cdd2f90cdd78f251be865114b9fc46f24bfe
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-10-10 16:51:32 +0000

    bennu deployed at beta frontpage

---
+++ phoenix/elements/frontpage/_online.php
@@ -0,0 +1,42 @@
+<?php
+   class ElementFrontpageOnline extends Element {
+        public function Render() {
+            global $libs;
+            global $user;
+            
+            $libs->Load( 'user/user' );
+	        $libs->Load( 'bennu/mybennu' );
+
+            $finder = New UserFinder();
+		    $users = $finder->FindOnline( 0 , 70 );
+            $count = $users[ 1 ];
+            $users = $users[ 0 ];
+            
+            $target = $userFinder->FindById( $user->Id );//bennu            
+            $users = Bennu_OnlineNow( $target, $users );//bennu
+            
+            if ( $count ) {        
+                ?><div class="onlineusers">
+                    <h2<?php
+                        if ( $count > 1 ) {
+                            ?> title="<?php
+                            echo $count;
+                            ?> άτομα είναι online"<?php
+                        }
+                        ?>>Είναι online τώρα (<?php
+                        echo $count;
+                        ?>)</h2>
+                        <div class="list"><?php
+                            foreach( $users as $onuser ) {
+                                ?><a href="<?php
+                                Element( 'user/url', $onuser->Id , $onuser->Subdomain );
+                                ?>"><?php
+                                Element( 'user/avatar' , $onuser->Avatar->Id , $onuser->Id , $onuser->Avatar->Width , $onuser->Avatar->Height , $onuser->Name , 100 , '' , '' , false , 0 , 0 );
+                                ?></a><?php
+                            }    
+                        ?></div><?php
+                ?></div><?php
+            }
+        }
+    }
+?>
