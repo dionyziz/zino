7567d09f7f1bc4af1fa8d4c8f1c686500179d105
Dimitris Karagasidis <dkaragasidis@gmail.com>
2008-10-13 23:00:51 +0000

    removed all changes.

--- phoenix/elements/frontpage/journal/list.php
+++ phoenix/elements/frontpage/journal/list.php
@@ -1,6 +1,6 @@
 <?php
     class ElementFrontpageJournalList extends Element {
-        //protected $mPersistent = array( 'journalseq' );
+        protected $mPersistent = array( 'journalseq' );
         public function Render( $journalseq ) {
             $finder = New JournalFinder();
             $journals = $finder->FindAll( 0 , 4 );
