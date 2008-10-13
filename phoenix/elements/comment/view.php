4628f7adf6aeaeea2e68dbb1e39c6294f9b8447b
Chris Pappas <chrispappas12@gmail.com>
2008-10-13 18:14:24 +0000

    comments caching

--- phoenix/elements/comment/view.php
+++ phoenix/elements/comment/view.php
@@ -17,39 +17,20 @@
                 ?>px;<?php
             }
             ?>"><div class="toolbox">
-                    <span style="margin-right:<?php
-                    if ( !$deletable ) {
+                    <span class="time invisible"><?php
+                        echo $comment->Created;
+                    ?></span>
+                    <a href="" class="invisible" style="margin-right:<?php
                         echo $indent * 20;
-                    }
-                    else {
-                        ?>0<?php
-                    }
-                    ?>px;" class="time"><?php
-                    Element( 'date/diff', $comment->Created );
-                    ?></span><?php
-                    if ( $deletable ) {
-                        ?><a href="" style="margin-right:<?php
-                            echo $indent * 20;
-                        ?>px;" onclick="return Comments.Delete( <?php
-                        echo $comment->Id;
-                        ?> );" title="Διαγραφή">&nbsp;</a><?php
-                    }
+                    ?>px;" title="Διαγραφή">&nbsp;</a><?php
                 ?></div><div class="who"><?php
                     Element( 'user/display', $comment->User->Id , $comment->User->Avatar->Id , $comment->User );
                     ?> είπε:
-                </div><div class="text"<?php
-                if ( ( $user->Id == $comment->User->Id && ( time()-strtotime( $comment->Created ) < 900 ) ) || $user->HasPermission( PERMISSION_COMMENT_EDIT_ALL )  ) {
-                    ?> ondblclick="Comments.Edit( <?php
-                    echo $comment->Id;
-                    ?> );return false"<?php
-                }
-                ?>><?php
+                </div><div class="text"><?php
                     echo $comment->Text; // no htmlspecialchars(); the text is already sanitized
-                ?></div><?php
-                if ( $indent <= 50 && $user->HasPermission( PERMISSION_COMMENT_CREATE ) ) {
-                    ?><div class="bottom"><a href="">Απάντησε</a> σε αυτό το σχόλιο</div><?php
-                }
-            ?></div><?php
+                ?></div>
+                <div class="bottom"><a href="">Απάντησε</a> σε αυτό το σχόλιο</div>
+            </div><?php
         }
     }
 ?>
