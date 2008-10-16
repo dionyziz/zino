22ad65d0aa2c257672224b2a4ee6f398ca3ad17f
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-10-16 17:22:30 +0000

    bennu at images frontpage

---
+++ phoenix/elements/frontpage/image/_list.php
@@ -0,0 +1,31 @@
+<?php
+    class ElementFrontpageImageList extends Element {
+        protected $mPersistent = array( 'imageseq' );
+        public function Render( $imageseq ) {
+            global $libs;
+            
+            $libs->Load( 'bennu/bennu' );
+        
+            $finder = New ImageFinder();
+            $images = $finder->FindFrontpageLatest( 0, 15 );
+            
+            $images = Bennu_Images_Frontpage( $user, $images );//bennu - target is not required yet            
+            
+            $finder = New NotificationFinder();
+            if ( count( $images ) > 0 ) {
+                ?><div class="latestimages">
+                <ul><?php
+                    foreach ( $images as $image ) {
+                        ?><li><a href="?p=photo&amp;id=<?php
+                        echo $image->Id;
+                        ?>"><?php
+                        Element( 'image/view' , $image->Id , $image->User->Id , $image->Width , $image->Height , IMAGE_CROPPED_100x100 , '' , $image->User->Name , '' , false , 0 , 0 , $image->Numcomments );
+                        ?></a></li><?php
+                    }
+                    ?>
+                </ul>
+                </div><?php
+            }
+        }
+    }
+?>
