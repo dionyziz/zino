0f51cf3971ac67874ac505a151e0e12c2c7fad40
Chris Pappas <chrispappas12@gmail.com>
2008-10-25 09:03:33 +0000

    settings saving

--- phoenix/elements/user/settings/sidebar.php
+++ phoenix/elements/user/settings/sidebar.php
@@ -12,11 +12,16 @@
                 <li class="settings"><a href="" onclick="Settings.SwitchSettings( 'settings' );return false"><span>&nbsp;</span>Λογαριασμός</a></li>
             </ol>
             <div>
+                <?php
+                /*
                 <span class="saving"><img src="<?php
                 echo $rabbit_settings[ 'imagesurl' ];
                 ?>ajax-loader.gif" alt="Αποθήκευση" /> Αποθήκευση...
                 </span>
-                <span class="saved"><span>&nbsp;</span>Οι επιλογές σου αποθηκεύτηκαν αυτόματα</span>
+                 */
+                ?>
+                <a class="savebutton button" href="">Αποθήκευση</a>
+                <span class="saved"><span>&nbsp;</span>Οι αλλαγές σου αποθηκεύτηκαν</span>
             </div>
             <a class="backtoprofile button" href="<?php
             Element( 'user/url' , $user->Id , $user->Subdomain );
