a7ec26c7494e47704b688c2b194b5b9d7e171b1e
Dimitris Karagasidis <dkaragasidis@gmail.com>
2008-10-25 13:20:50 +0000

    changed profile element so it shows school and not only univercity

--- phoenix/elements/user/profile/sidebar/info.php
+++ phoenix/elements/user/profile/sidebar/info.php
@@ -15,12 +15,22 @@
                         echo htmlspecialchars( $theuser->Profile->Location->Name );
                         ?></dd><?php
                     }
-                    if ( $theuser->Profile->Schoolid > 0 && $theuser->Profile->Placeid > 0 && $theuser->Profile->Education == $theuser->Profile->School->Typeid ) {
+                    /*if ( $theuser->Profile->Schoolid > 0 && $theuser->Profile->Placeid > 0 && $theuser->Profile->Education == $theuser->Profile->School->Typeid ) {
                         ?><dt><strong>Πανεπιστήμιο</strong></dt>
                         <dd><?php
                         Element( 'user/trivial/school', $theuser->Profile->School );
                         ?></dd><?php
-                    }
+                    }*/
+					if ( $theuser->Profile->Schoolid > 0 && $theuser->Profile->Placeid > 0 ) {
+						if ( $theuser->Profile->Education < 5 ) {
+							?><dt><strong>Σχολείο</strong></dt><dd><?php
+						}
+						else {
+							?><dt><strong>Πανεπιστήμιο</strong></dt><dd><?php
+						}
+						Element( 'user/trivial/school', $theuser->Profile->School );
+						?></dd><?php
+					}
                     if ( $theuser->Profile->Haircolor != '-' ) {
                         ?><dt><strong>Χρώμα μαλλιών</strong></dt>
                         <dd><?php
