466fa75a72e559d5a9d8745d6fcfbd3040abd8c1
Chris Pappas <chrispappas12@gmail.com>
2008-10-25 12:59:30 +0000

    settings saving

--- phoenix/elements/user/settings/personal/view.php
+++ phoenix/elements/user/settings/personal/view.php
@@ -61,10 +61,9 @@
             ?>">
                 <label for="university">Πανεπιστήμιο</label>
                 <div class="setting" id="university"><?php
-                   /* if ( $showschool ) {
+                   if ( $showschool ) {
                         Element( 'user/settings/personal/school', $user->Profile->Placeid, $user->Profile->Education  );
-                    }
-                    */
+                    } 
                 ?></div>
             </div>
             <div id="unibarfade" class="barfade<?php
