8a711394603431ec937f3b134d7a027f6abf1fb2
Chris Pappas <chrispappas12@gmail.com>
2008-10-25 08:57:07 +0000

    settings changes

--- phoenix/elements/user/settings/sidebar.php
+++ phoenix/elements/user/settings/sidebar.php
@@ -9,7 +9,7 @@
                 <li class="characteristics"><a href="" onclick="Settings.SwitchSettings( 'characteristics' );return false"><span>&nbsp;</span>Χαρακτηριστικά</a></li>
                 <li class="interests"><a href="" onclick="Settings.SwitchSettings( 'interests' );return false"><span>&nbsp;</span>Ενδιαφέροντα</a></li>
                 <li class="contact"><a href="" onclick="Settings.SwitchSettings( 'contact' );return false"><span>&nbsp;</span>Επικοινωνία</a></li>
-                <li class="settings"><a href="" onclick="Settings.SwitchSettings( 'settings' );return false"><span>&nbsp;</span>Ρυθμίσεις</a></li>
+                <li class="settings"><a href="" onclick="Settings.SwitchSettings( 'settings' );return false"><span>&nbsp;</span>Λογαριασμός</a></li>
             </ol>
             <div>
                 <span class="saving"><img src="<?php
