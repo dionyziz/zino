a04127c4fd52d61e169d8363a0facd09aba01083
Chris Pappas <chrispappas12@gmail.com>
2008-10-26 14:51:22 +0000

    settings saving

--- phoenix/elements/user/settings/sidebar.php
+++ phoenix/elements/user/settings/sidebar.php
@@ -27,7 +27,7 @@
                 </div>
             </div>
             <div class="backtoprofile">
-                <a href="" class="button">Επιστροφή στο προφίλ<span>&nbsp;</span></a>
+                <a href="" class="button">Επιστροφή στο προφίλ</a>
             </div><?php
         }
     }
