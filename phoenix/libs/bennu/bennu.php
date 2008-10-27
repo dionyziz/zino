606ae0c6d32c671bc020443a8003172309162b03
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-10-27 11:51:28 +0000

    bennu at images

--- phoenix/libs/bennu/bennu.php
+++ phoenix/libs/bennu/bennu.php
@@ -305,7 +305,7 @@
         
         $bennu->AddRuleInArray( 'Image->Userid', $friends, 'IN', 15 );
         $bennu->AddRuleNormalDist( 'Image->Created', NowDate(), 4 * 24 * 60 * 60, 'DATE', 10 );
-        $bennu->AddRuleNormalDist( 'Image->Numcomments', 100, 90, 'INT', 10 );
+        $bennu->AddRuleNormalDist( 'Image->Numcomments', 100, 100, 'INT', 10 );
         //$bennu->AddRuleRandom( 50 );
         
         return $bennu->GetResult();
