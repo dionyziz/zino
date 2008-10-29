77e6a2ec03a1d6fc5ad4635260405b4b0da538bb
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-10-29 19:24:15 +0000

    contacts class

---
+++ phoenix/libs/contacts/contact.php
@@ -0,0 +1,19 @@
+<?php
+    class Contact extends Finder {
+        protected $mModel = 'Contact';
+    }
+    
+    class Contact extends Satori {
+        protected $mDbTableAlias = 'contacts';
+        
+        public function AddContact( $mail, $usermail ) {
+            global $user;
+            $contact = new Contact();
+            $contact->Mail = $mail;
+            $contact->Usermail = $usermail;
+            $contact->Userid = $user->Id;
+            $contact->Created = DateNow();
+            $contact->Save();
+            return;
+        }
+    }
