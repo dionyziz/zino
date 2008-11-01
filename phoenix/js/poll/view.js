0ac76bff0eb800228652e63cf8a625b05bf0076d
Chris Pappas <chrispappas12@gmail.com>
2008-11-01 10:58:04 +0000

    schools

--- phoenix/js/poll/view.js
+++ phoenix/js/poll/view.js
@@ -8,9 +8,7 @@ var PollView = {
 	Vote : function( optionid , pollid , node ) {
 		var parent = $( 'div.posmall div.results' );
 		$( parent ).html( $( 'div.posmall div.voting' ).html() );
-		//$( 'div.lastpoll div.container' ).css( { marginRight : '260px' } );
 		Coala.Warm( 'poll/vote' , { optionid : optionid , pollid : pollid , node : parent } );
-		//$( 'div.lastpoll' ).css( { marginRight : '0px' } );
 	}
 };
 $( function() {
