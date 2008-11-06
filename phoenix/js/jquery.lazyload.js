91b45f81553702c7b4f87b98d6e3105b90c04cc5
Chris Pappas <chrispappas12@gmail.com>
2008-11-06 22:31:56 +0000

    lazyloading

--- phoenix/js/jquery.lazyload.js
+++ phoenix/js/jquery.lazyload.js
@@ -71,7 +71,7 @@
             /* When appear is triggered load original image. */
             $(self).one("appear", function() {
                 if (!this.loaded) {
-                    $(this)
+                    $(self)
                         .attr("src", $(self).attr("id"))
                         .bind("load", function() {
                             $(self)
