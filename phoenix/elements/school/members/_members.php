3032da5c9001480db3edc9af21528c1ad4afc4ef
Chris Pappas <chrispappas12@gmail.com>
2008-11-10 19:44:13 +0000

    schools

--- phoenix/elements/school/members/_members.php
+++ phoenix/elements/school/members/_members.php
@@ -6,44 +6,46 @@
 
 			?><div class="members">
 				<h4>Μέλη</h4>
-				<ul>
-					<li>
-						<a href="http://dionyziz.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/1/110751/110751_100.jpg" style="width:100px;height:100px;" title="dionyziz" alt="dionyziz" /></span></a>
-					</li>
-					<li>
-						<a href="http://cybertwink.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5327/125056/125056_100.jpg" style="width:100px;height:100px;" title="cybertwink" alt="cybertwink" /></span></a>
-					</li>
-					<li>
-						<a href="http://warrior-70.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5358/125769/125769_100.jpg" style="width:100px;height:100px;" title="warrior-70" alt="warrior-70" /></span></a>
-					</li>
-					<li>
-						<a href="http://mp1skotak1.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5392/125805/125805_100.jpg" style="width:100px;height:100px;" title="Mp1skotak1" alt="Mp1skotak1" /></span></a>
-					</li>
-					<li>
-						<a href="http://agnwsti.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5383/125716/125716_100.jpg" style="width:100px;height:100px;" title="agnwsti" alt="agnwsti" /></span></a>
-					</li>
-					<li>
-						<a href="http://antig0nh.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5003/119968/119968_100.jpg" style="width:100px;height:100px;" title="Antig0nh" alt="Antig0nh" /></span></a>
-					</li>
-					<li>
-						<a href="http://lifo.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5339/125061/125061_100.jpg" style="width:100px;height:100px;" title="lifo" alt="lifo" /></span></a>
-					</li>
-					<li>
-						<a href="http://nikol-aka-ang.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/4187/125667/125667_100.jpg" style="width:100px;height:100px;" title="nikol_aka_ang" alt="nikol_aka_ang" /></span></a>
-					</li>
-					<li>
-						<a href="http://europunk.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/1545/123120/123120_100.jpg" style="width:100px;height:100px;" title="europunk" alt="europunk" /></span></a>
-					</li>
-					<li>
-						<a href="http://titina4.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5297/124854/124854_100.jpg" style="width:100px;height:100px;" title="TiTINa4" alt="TiTINa4" /></span></a>
-					</li>
-					<li>
-						<a href="http://vasilhs.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5271/125086/125086_100.jpg" style="width:100px;height:100px;" title="vasilhs" alt="vasilhs" /></span></a>
-					</li>
-					<li>
-						<a href="http://metalo92.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/1522/111676/111676_100.jpg" style="width:100px;height:100px;" title="metalo92" alt="metalo92" /></span></a>
-					</li>
-				</ul> 
+				<div class="people">
+					<ul>
+						<li>
+							<a href="http://dionyziz.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/1/110751/110751_100.jpg" style="width:100px;height:100px;" title="dionyziz" alt="dionyziz" /></span></a>
+						</li>
+						<li>
+							<a href="http://cybertwink.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5327/125056/125056_100.jpg" style="width:100px;height:100px;" title="cybertwink" alt="cybertwink" /></span></a>
+						</li>
+						<li>
+							<a href="http://warrior-70.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5358/125769/125769_100.jpg" style="width:100px;height:100px;" title="warrior-70" alt="warrior-70" /></span></a>
+						</li>
+						<li>
+							<a href="http://mp1skotak1.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5392/125805/125805_100.jpg" style="width:100px;height:100px;" title="Mp1skotak1" alt="Mp1skotak1" /></span></a>
+						</li>
+						<li>
+							<a href="http://agnwsti.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5383/125716/125716_100.jpg" style="width:100px;height:100px;" title="agnwsti" alt="agnwsti" /></span></a>
+						</li>
+						<li>
+							<a href="http://antig0nh.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5003/119968/119968_100.jpg" style="width:100px;height:100px;" title="Antig0nh" alt="Antig0nh" /></span></a>
+						</li>
+						<li>
+							<a href="http://lifo.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5339/125061/125061_100.jpg" style="width:100px;height:100px;" title="lifo" alt="lifo" /></span></a>
+						</li>
+						<li>
+							<a href="http://nikol-aka-ang.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/4187/125667/125667_100.jpg" style="width:100px;height:100px;" title="nikol_aka_ang" alt="nikol_aka_ang" /></span></a>
+						</li>
+						<li>
+							<a href="http://europunk.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/1545/123120/123120_100.jpg" style="width:100px;height:100px;" title="europunk" alt="europunk" /></span></a>
+						</li>
+						<li>
+							<a href="http://titina4.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5297/124854/124854_100.jpg" style="width:100px;height:100px;" title="TiTINa4" alt="TiTINa4" /></span></a>
+						</li>
+						<li>
+							<a href="http://vasilhs.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/5271/125086/125086_100.jpg" style="width:100px;height:100px;" title="vasilhs" alt="vasilhs" /></span></a>
+						</li>
+						<li>
+							<a href="http://metalo92.zino.gr/"><span class="imageview"><img src="http://images.zino.gr/media/1522/111676/111676_100.jpg" style="width:100px;height:100px;" title="metalo92" alt="metalo92" /></span></a>
+						</li>
+					</ul> 
+				</div>
 			</div>
 			<div class="eof"></div><?php
 			
