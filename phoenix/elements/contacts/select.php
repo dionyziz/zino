b910b8945b452a709d28d1d722e85d68d7e953e3
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-11-30 18:11:37 +0000

    finder changes

--- phoenix/elements/contacts/select.php
+++ phoenix/elements/contacts/select.php
@@ -6,7 +6,7 @@
             global $libs;
             
             $libs->Load( 'contacts/contacts' );
-            $libs->Load( 'user/profile' );
+            //$libs->Load( 'user/profile' );
             
             $page->SetTitle( "Επιλογή Επαφών" );
             
@@ -18,8 +18,8 @@
                 return;
             }
             
-            $mailfinder = new UserProfileFinder();
-            $mails = $mailfinder->FindAllUserEmails();
+            //$mailfinder = new UserProfileFinder();
+            //$mails = $mailfinder->FindAllUserEmails();
             
             ?><p>Επέλεξε τους φίλους σου που θες να σταλεί πρόσκληση: </p><?php
             ?><form method="post" action=""><?php
@@ -28,9 +28,9 @@
                 ?><p><?php
                 ?><input type="checkbox" name="approved" /> <?php 
                 $contact = new Contact( $sample->Id );
-                if ( $mails[ $contact->Mail ] == true ) {
+                /*if ( $mails[ $contact->Mail ] == true ) {
                     ?>Already a zino user : <?php
-                }                 
+                } */                
                 echo $contact->Mail;
                 ?></p><?php
             }
