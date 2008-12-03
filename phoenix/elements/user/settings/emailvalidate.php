3c740b34dd8ec254f94ea61f25ca33dbe1ed0caa
Dionysis Zindros <dionyziz@gmail.com>
2008-12-03 18:40:53 +0000

    e-mail validation

---
+++ phoenix/elements/user/settings/emailvalidate.php
@@ -0,0 +1,16 @@
+<?php
+    class ElementUserSettingsEmailValidate extends Element {
+        public function Render( tInteger $userid, tString $hash ) {
+            $userid = $userid->Get();
+            $hash = $hash->Get();
+            
+            if ( !ValidateEmail( $userid, $hash ) ) {
+                ?><p>Η επιβεβαίωση του e-mail σου δεν ήταν δυνατό να πραγματοποιηθεί.<br />
+                Παρακαλούμε ξαναδοκίμασε.</p><?php
+                return;
+            }
+            
+            return Redirect( '?validated=y' );
+        }
+    }
+?>
