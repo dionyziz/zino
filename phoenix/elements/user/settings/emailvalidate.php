a5865bee33af91def01d6a9cfe5153875c94d1b6
Dionysis Zindros <dionyziz@gmail.com>
2008-12-03 18:59:42 +0000

    e-mail validation

--- phoenix/elements/user/settings/emailvalidate.php
+++ phoenix/elements/user/settings/emailvalidate.php
@@ -1,6 +1,10 @@
 <?php
     class ElementUserSettingsEmailValidate extends Element {
         public function Render( tInteger $userid, tString $hash ) {
+            global $libs;
+            
+            $libs->Load( 'user/profile' );
+            
             $userid = $userid->Get();
             $hash = $hash->Get();
             
