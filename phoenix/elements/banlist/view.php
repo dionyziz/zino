cb59064895867b3bc3c4aeff09b11089c5c5105e
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-12-06 11:38:04 +0000

    ban request

--- phoenix/elements/banlist/view.php
+++ phoenix/elements/banlist/view.php
@@ -24,6 +24,7 @@
                 <tr>
                     <th>Χρήστης</th>
                     <th>Πότε</th>
+                    <th>Μέχρι</th>
                     <th>Αιτία</th>
                     <th></th>
                 </tr>
@@ -33,6 +34,8 @@
                     echo $bannedUser->Name;
                     ?></td><td><?php                    
                     Element( 'date/diff', $bannedUser->Started );              
+                    ?></td><td><?php
+                    Element( 'date/diff', $bannedUser->Expire );              
                     ?></td><td><?php
                     echo htmlspecialchars( $bannedUser->Reason );
                     ?></td><td><?php
