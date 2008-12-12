7e02e1fbfce2100d3d085f9642ca34ce062c9f33
Chris Pappas <chrispappas12@gmail.com>
2008-12-12 18:13:36 +0000

    schools

--- phoenix/elements/school/image/_list.php
+++ phoenix/elements/school/image/_list.php
@@ -3,22 +3,20 @@
     class ElementSchoolImageList extends Element {
 
         public function Render( Array $images ) {
-            if ( count( $images ) > 0 ) {
-                ?><div class="plist">
-                    <ul>
-                        <li>
-                            <a href="" class="uploadphoto">&nbsp;</a>
-                        </li><?php
-                        foreach ( $images as $image ) {
-                            ?><li><a href="?p=photo&amp;id=<?php
-                            echo $image->Id;
-                            ?>"><?php
-                            Element( 'image/view' , $image->Id , $image->User->Id , $image->Width , $image->Height , IMAGE_CROPPED_100x100 , '' , $image->User->Name , '' , false , 0 , 0 , $image->Numcomments );
-                            ?></a></li><?php
-                        }
-                    ?></ul>
-                </div><?php
-            }
+            ?><div class="plist">
+                <ul>
+                    <li>
+                        <a href="" class="uploadphoto">&nbsp;</a>
+                    </li><?php
+                    foreach ( $images as $image ) {
+                        ?><li><a href="?p=photo&amp;id=<?php
+                        echo $image->Id;
+                        ?>"><?php
+                        Element( 'image/view' , $image->Id , $image->User->Id , $image->Width , $image->Height , IMAGE_CROPPED_100x100 , '' , $image->User->Name , '' , false , 0 , 0 , $image->Numcomments );
+                        ?></a></li><?php
+                    }
+                ?></ul>
+            </div><?php
         }
     }
 
