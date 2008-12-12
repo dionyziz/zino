03e95f1360b6043f4df0b744a3c2be0195af56a8
Chris Pappas <chrispappas12@gmail.com>
2008-12-12 19:03:37 +0000

    schools

--- phoenix/js/schools.js
+++ phoenix/js/schools.js
@@ -1,12 +1,12 @@
 $( function() { 
     $( 'div#schview div.photos div.plist ul li a.uploadphoto' ).click( function() {
-        var modal = $( '#uploadmodal' )[ 0 ].cloneNode( true );
+        var modal = $( '#schooluploadmodal' )[ 0 ].cloneNode( true );
         $( modal ).show();
         $( modal ).find( 'a.close' ).click( function() {
             Modals.Destroy();
             return false;
         } );
-        Modals.Create( modal , 400 , 250 );
+        Modals.Create( modal , 350 , 250 );
         return false;
     } );
 } );
