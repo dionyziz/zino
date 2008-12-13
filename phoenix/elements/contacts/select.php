6ccff99f8923ab959af4ef6517c0a1e51c68a31e
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-12-13 10:55:45 +0000

    CONTACT FINDER

--- phoenix/elements/contacts/select.php
+++ phoenix/elements/contacts/select.php
@@ -1,6 +1,6 @@
 <?php
     class ElementContactsSelect extends Element {
-        public function Render( tText $email ) {
+        public function Render( tText $email, tInteger $step ) {
             global $page;
             global $user;
             global $libs;
@@ -18,13 +18,28 @@
                 return;
             }
             
-            $emails = array();
+            $all_emails = array();
             foreach ( $res as $sample ) {
-                $emails[] = $sample->Mail;
+                $all_emails[] = $sample->Mail;
             }
             $mailfinder = new UserProfileFinder();
-            $mails = $mailfinder->FindAllUsersByEmails( $emails );
+            $zino_emails = $mailfinder->FindAllUsersByEmails( $all_emails );
             
+            if ( $step == 1 ) { //step 1:send invites to user that are already in zino
+                ?><p>Αυτοί οι φίλοι σου είναι ήδη στο zino.</p><?php
+                ?><form method="post" action=""><?php
+                echo count( $zino_emails );
+                foreach ( $zino_emails as $key=>$val ) {
+                    ?><p><?php
+                    ?><input type="checkbox" name="approved" /> <?php 
+                    $friend = new User( $val );                              
+                    echo $friend->Name . " " . $friend->Profile->Email;                    
+                    ?></p><?php
+                }
+                ?><input type="button" name="Στείλε τις προσκλήσεις!" /><?php
+                ?></form><?php
+            }
+            /*
             ?><p>Επέλεξε τους φίλους σου που θες να σταλεί πρόσκληση: </p><?php
             ?><form method="post" action=""><?php
             echo count( $res );
@@ -33,12 +48,13 @@
                 ?><input type="checkbox" name="approved" /> <?php 
                 $contact = new Contact( $sample->Id );                              
                 echo $contact->Mail;
-                if ( $mails[ $contact->Mail ] !== NULL  ) {
+                if ( $zino_emails[ $contact->Mail ] !== NULL  ) {
                     ?> ---- Ηδη μέλος στο Zino <?php
                 }   
                 ?></p><?php
             }
             ?></form><?php
+            */
         }
     }
 ?>
