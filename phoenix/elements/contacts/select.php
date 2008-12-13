9c492755cf4a04dc9840a48245c6f7231d24e563
Giorgos Panagiotakos <pagio91i@gmail.com>
2008-12-13 17:03:41 +0000

    CONTACT FINDER

--- phoenix/elements/contacts/select.php
+++ phoenix/elements/contacts/select.php
@@ -40,7 +40,7 @@
                 $zino_friends = array();
                 foreach ( $res as $relation ) {
                     $zino_friends[ $relation->Friend->Id ] = true;
-                    echo '<p>' . $relation->Friend->Id . '</p>';
+                    echo '<p>' . $relation->Friend->Name . '</p>';
                 }
             
                 $friendsN = count( $zino_emails );
