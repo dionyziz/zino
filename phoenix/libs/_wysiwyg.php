6a98e303ea521584698ab261b78345f2d2bde8ec
Chris Pappas <chrispappas12@gmail.com>
2008-12-18 12:59:40 +0000

    photoview bug temporary solution

--- phoenix/libs/_wysiwyg.php
+++ phoenix/libs/_wysiwyg.php
@@ -176,14 +176,14 @@
             foreach ( $smileys as $i => $smiley ) {
                 $smileysprocessed[ $i ] = '<span class="emoticon-'
                                         . htmlspecialchars( $smiley ) 
-                                        . '">&nbsp;</span>';
+                                        . '"> </span>';
             }
             $smileysprocessedkeys = array_keys( $smileysprocessed );
         }
         
         $text = str_replace( $smileysprocessedkeys, $smileysprocessed, $text );
         // wink special case
-        $text = preg_replace( '#(^|\s);-?\)(\s|$)#', '\1<span class="emoticon-wink">&nbsp;</span>', $text );
+        $text = preg_replace( '#(^|\s);-?\)(\s|$)#', '\1<span class="emoticon-wink"> </span>', $text );
         return $text;
     }
 ?>
