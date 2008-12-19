44ba8598352d3bddefc9c318f333193c38599e5d
Chris Pappas <chrispappas12@gmail.com>
2008-12-19 13:29:38 +0000

    modals

--- phoenix/js/user/_settings.js
+++ phoenix/js/user/_settings.js
@@ -112,9 +112,11 @@ var Settings = {
 		Coala.Warm( 'user/settings/avatar' , { imageid : imageid } );
 	},
 	AddAvatar : function( imageid ) {
-		var li = document.createElement( 'li' );
+		alert( 'adding avatar' );
+        var li = document.createElement( 'li' );
 		$( li ).hide();
 		$( 'div.modal div.avatarlist ul' ).prepend( li );
+        alert( 'calling coala' );
 		Coala.Warm( 'user/settings/upload' , { imageid : imageid } );
 		var li2 = document.createElement( 'li' );
 		$( 'div.settings div.tabs form#personalinfo div.option div.setting div.avatarlist ul' ).prepend( li2 );
