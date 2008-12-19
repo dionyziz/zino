6063f1647a1230b170aa20e7fbff92a60009ecbc
Chris Pappas <chrispappas12@gmail.com>
2008-12-19 13:44:52 +0000

    modals

--- phoenix/units/user/settings/upload.do.php
+++ phoenix/units/user/settings/upload.do.php
@@ -8,9 +8,8 @@
         Element( 'user/settings/personal/photosmall' , $image );
         echo w_json_encode( ob_get_clean() );
         ?>;
-        $( $( 'div.modal div.avatarlist ul li' )[ 0 ] ).html( inner ).show();
-        alert( 'adding innerhtml' );
-        $( $( 'div.settings div.tabs form#personalinfo div.option div.setting div.avatarlist ul li' )[ 0 ] ).html( inner );
+        $( $( 'div#avatarlist ul li' )[ 0 ] ).html( inner ).show();
+        $( $( 'div#avatarlist ul li' )[ 0 ] ).html( inner );
         alert( 'added' );<?php
         if ( $image->Album->Numphotos == 1 ) {
             ?>Coala.Warm( 'user/settings/avatar' , { imageid : <?php
