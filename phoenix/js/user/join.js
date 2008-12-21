1c354ceb20c0ee0e95b3ac9c3f3d4be491d3fdfd
Chris Pappas <chrispappas12@gmail.com>
2008-12-21 14:39:22 +0000

    js optimizing

--- phoenix/js/user/join.js
+++ phoenix/js/user/join.js
@@ -7,10 +7,10 @@ var Join = {
 	repwderror : false, //used to check if password is equal with the retyped password
 	usernameexists : false,
 	emailerror : false,
-	username : $( 'form.joinform div input' )[ 0 ] ? $( 'form.joinform div input' )[ 0 ] : false,
-	password : $( 'form.joinform div input' )[ 1 ] ? $( 'form.joinform div input' )[ 1 ] : false,
-	repassword : $( 'form.joinform div input' )[ 2 ] ? $( 'form.joinform div input' )[ 2 ] : false,
-	email : $( 'form.joinform div input' )[ 3 ] ? $( 'form.joinform div input' ) [ 3 ] : false,
+	username : $( 'form.joinform div input' )[ 0 ],
+	password : $( 'form.joinform div input' )[ 1 ],
+	repassword : $( 'form.joinform div input' )[ 2 ],
+	email : $( 'form.joinform div input' )[ 3 ],
 	ShowTos : function () {
 		var area = $( 'div#join_tos' )[ 0 ].cloneNode( true );
 		$( area ).css( "display" , "block" );
@@ -18,162 +18,160 @@ var Join = {
 	},
 	enabled : true,
     JoinOnLoad : function() {
-        if ( Join.username ) {
-            $( 'form.joinform' ).submit( function() {
-                return false;
-            } );
-            $( 'form.joinform div input' ).focus( function() {
-                $( this ).css( "border" , "1px solid #bdbdff" );
-            }).blur( function() {
-                $( this ).css( "border" , "1px solid #999" );
-            });
-            $( Join.username ).keyup( function( event ) {
-                if ( event.keyCode == 13 && !Join.usernameerror && !Join.usernameexists && !Join.invalidusername ) {
-                    Join.password.focus();
+        $( 'form.joinform' ).submit( function() {
+            return false;
+        } );
+        $( 'form.joinform div input' ).focus( function() {
+            $( this ).css( "border" , "1px solid #bdbdff" );
+        }).blur( function() {
+            $( this ).css( "border" , "1px solid #999" );
+        });
+        $( Join.username ).keyup( function( event ) {
+            if ( event.keyCode == 13 && !Join.usernameerror && !Join.usernameexists && !Join.invalidusername ) {
+                Join.password.focus();
+            }
+        } );
+        $( Join.password ).keyup( function( event ) {
+            if ( event.keyCode == 13 && !Join.pwderror ) {
+                Join.repassword.focus();
+            }
+        } );
+        $( Join.repassword ).keyup( function( event ) {
+            if ( event.keyCode == 13 && !Join.repwderror ) {
+                Join.email.focus();
+            }
+        } );
+        $( Join.email ).keyup( function( event ) {
+            if ( event.keyCode == 13 && !Join.emailerror ) {
+                $( 'div a.button' )[ 0 ].focus();
+            }
+        } );
+        $( Join.username ).keydown( function( event ) {
+            if ( Join.usernameerror ) {
+                if ( Join.username.value.length >= 4 && Join.username.value.length <= 20 ) {
+                    Join.usernameerror = false;
+                    $( $( 'form.joinform div > span' )[ 0 ] ).animate( { opacity: "0" } , 700 , function() {
+                        $( this ).css ( "display" , "none");
+                    });
                 }
-            } );
-            $( Join.password ).keyup( function( event ) {
-                if ( event.keyCode == 13 && !Join.pwderror ) {
-                    Join.repassword.focus();
+            }
+            if ( Join.usernameexists ) {
+                if ( event.keyCode != 13 ) {
+                    Join.usernameexists = false;
+                    $( $( 'form.joinform div > span' )[ 1 ] ).animate( { opacity: "0" } , 700 , function() {
+                        $( this ).css( "display" , "none" );
+                    });
+                    $( 'div a.button' ).removeClass( 'button_disabled' );
+                    Join.enabled = true;
                 }
-            } );
-            $( Join.repassword ).keyup( function( event ) {
-                if ( event.keyCode == 13 && !Join.repwderror ) {
-                    Join.email.focus();
-                }
-            } );
-            $( Join.email ).keyup( function( event ) {
-                if ( event.keyCode == 13 && !Join.emailerror ) {
-                    $( 'div a.button' )[ 0 ].focus();
-                }
-            } );
-            $( Join.username ).keydown( function( event ) {
-                if ( Join.usernameerror ) {
-                    if ( Join.username.value.length >= 4 && Join.username.value.length <= 20 ) {
-                        Join.usernameerror = false;
-                        $( $( 'form.joinform div > span' )[ 0 ] ).animate( { opacity: "0" } , 700 , function() {
-                            $( this ).css ( "display" , "none");
-                        });
-                    }
-                }
-                if ( Join.usernameexists ) {
-                    if ( event.keyCode != 13 ) {
-                        Join.usernameexists = false;
-                        $( $( 'form.joinform div > span' )[ 1 ] ).animate( { opacity: "0" } , 700 , function() {
-                            $( this ).css( "display" , "none" );
-                        });
-                        $( 'div a.button' ).removeClass( 'button_disabled' );
-                        Join.enabled = true;
-                    }
-                }
-                if ( Join.invalidusername ) {
-                    Join.invalidusername = false;
-                    $( $( 'form.joinform div > span' )[ 2 ] ).animate( { opacity: "0" } , 700 , function() {
+            }
+            if ( Join.invalidusername ) {
+                Join.invalidusername = false;
+                $( $( 'form.joinform div > span' )[ 2 ] ).animate( { opacity: "0" } , 700 , function() {
+                    $( this ).css( "display" , "none" );
+                });
+            }
+        });	
+        
+        $( Join.password ).keyup( function() {
+            if ( Join.pwderror ) {
+                if ( Join.password.value.length >= 4 ) {
+                    Join.pwderror = false;
+                    $( $( 'form.joinform div > span' )[ 3 ] ).animate( { opacity: "0" } , 700 , function() {
                         $( this ).css( "display" , "none" );
                     });
                 }
-            });	
-            
-            $( Join.password ).keyup( function() {
-                if ( Join.pwderror ) {
-                    if ( Join.password.value.length >= 4 ) {
-                        Join.pwderror = false;
-                        $( $( 'form.joinform div > span' )[ 3 ] ).animate( { opacity: "0" } , 700 , function() {
-                            $( this ).css( "display" , "none" );
-                        });
-                    }
-                }
-            });
-            
-            $( Join.repassword ).keyup( function() {
-                if ( Join.repwderror ) {
-                    if ( Join.repassword.value == Join.password.value ) {
-                        Join.repwderror = false;
-                        $( $( 'form.joinform div > span' )[ 4 ] ).animate( { opacity: "0" } , 700 , function() {
-                            $( this ).css( "display" , "none" );
-                        });
-                    }
-                }
-            });
-            
-            $( Join.email ).keyup( function() {
-                if ( Join.emailerror ) {
-                    if ( Join.email.value === '' || /^[a-zA-Z0-9.\-_]+@[a-zA-Z0-9.\-_]+$/.test( Join.email.value ) ) {
-                        Join.emailerror = false;
-                        $( $( 'form.joinform div > span' )[ 5 ] ).animate( { opacity: "0" } , 700 , function() {
-                            $( this ).css( "display" , "none" );
-                        });
-                    }
-                }
-            });
-            
-            if ( Join.username ) {
-                Join.username.focus();
             }
-            
-            $( 'form.joinform p a' ).click( function () {
-                Join.ShowTos();
-                return false;
-            });
-            
-            $( 'div a.button' ).click( function() {
-                var create = true;
-                if ( Join.username.value.length < 4 || Join.username.value.length > 20 ) {
-                    if ( !Join.usernameerror ) {
-                        Join.usernameerror = true;
-                        $( $( 'form.joinform div > span' )[ 0 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity : "1" } , 400 );
-                    }
-                    Join.username.focus();
-                    create = false;
+        });
+        
+        $( Join.repassword ).keyup( function() {
+            if ( Join.repwderror ) {
+                if ( Join.repassword.value == Join.password.value ) {
+                    Join.repwderror = false;
+                    $( $( 'form.joinform div > span' )[ 4 ] ).animate( { opacity: "0" } , 700 , function() {
+                        $( this ).css( "display" , "none" );
+                    });
                 }
-                if ( Join.username.value.length >= 4 && !/^[a-zA-Z][a-zA-Z\-_0-9]{3,49}$/.test( Join.username.value ) ) {
-                    if ( !Join.invalidusername ) {
-                        Join.invalidusername = true;
-                        $( $( 'form.joinform div > span' )[ 2 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity : "1" } , 400 );
-                    }
-                    Join.username.focus();
-                    create = false;
+            }
+        });
+        
+        $( Join.email ).keyup( function() {
+            if ( Join.emailerror ) {
+                if ( Join.email.value === '' || /^[a-zA-Z0-9.\-_]+@[a-zA-Z0-9.\-_]+$/.test( Join.email.value ) ) {
+                    Join.emailerror = false;
+                    $( $( 'form.joinform div > span' )[ 5 ] ).animate( { opacity: "0" } , 700 , function() {
+                        $( this ).css( "display" , "none" );
+                    });
                 }
-                if ( Join.password.value.length < 4 ) {
-                    if ( !Join.pwderror ) {
-                        Join.pwderror = true;
-                        $( $( 'form.joinform div > span' )[ 3 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
-                    }
-                    if ( !Join.usernamerror && !Join.invalidusername && !Join.usernameexists ) {
-                        //if the username and password are empty then focus the username inputbox
-                        Join.password.focus();
-                    }
-                    create = false;
-                }
-                if ( Join.password.value != Join.repassword.value && !Join.pwderror ) {
-                    if ( !Join.repwderror ) {
-                        Join.repwderror = true;
-                        $( $( 'form.joinform div div > span' )[ 0 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
-                    }
-                    if ( !Join.usernameerror && !Join.invalidusername && !Join.usernameexists ) {
-                        Join.repassword.focus();
-                    }
-                    create = false;
-                }
-                if ( Join.email.value !== '' && !/^[a-zA-Z0-9.\-_]+@[a-zA-Z0-9.\-_]+$/.test( Join.email.value ) ) {
-                    if ( !Join.emailerror ) {
-                        Join.emailerror = true;
-                        $( $( 'form.joinform div > span' )[ 5 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
-                    }
-                    if ( !Join.usernameerror && !Join.invalidusername && !Join.usernameexists && !Join.pwderror && !Join.repwderror ) {
-                        Join.email.focus();
-                    }
-                    create = false;
-                }
-                if ( create ) {
-                    if ( Join.enabled ) {
-                        document.body.style.cursor = 'wait';
-                        $( this ).addClass( 'button_disabled' );
-                        Coala.Warm( 'user/join' , { username : Join.username.value , password : Join.password.value , email : Join.email.value } );
-                    }
-                }
-                return false;
-            } );
+            }
+        });
+        
+        if ( Join.username ) {
+            Join.username.focus();
         }
+        
+        $( 'form.joinform p a' ).click( function () {
+            Join.ShowTos();
+            return false;
+        });
+        
+        $( 'div a.button' ).click( function() {
+            var create = true;
+            if ( Join.username.value.length < 4 || Join.username.value.length > 20 ) {
+                if ( !Join.usernameerror ) {
+                    Join.usernameerror = true;
+                    $( $( 'form.joinform div > span' )[ 0 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity : "1" } , 400 );
+                }
+                Join.username.focus();
+                create = false;
+            }
+            if ( Join.username.value.length >= 4 && !/^[a-zA-Z][a-zA-Z\-_0-9]{3,49}$/.test( Join.username.value ) ) {
+                if ( !Join.invalidusername ) {
+                    Join.invalidusername = true;
+                    $( $( 'form.joinform div > span' )[ 2 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity : "1" } , 400 );
+                }
+                Join.username.focus();
+                create = false;
+            }
+            if ( Join.password.value.length < 4 ) {
+                if ( !Join.pwderror ) {
+                    Join.pwderror = true;
+                    $( $( 'form.joinform div > span' )[ 3 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
+                }
+                if ( !Join.usernamerror && !Join.invalidusername && !Join.usernameexists ) {
+                    //if the username and password are empty then focus the username inputbox
+                    Join.password.focus();
+                }
+                create = false;
+            }
+            if ( Join.password.value != Join.repassword.value && !Join.pwderror ) {
+                if ( !Join.repwderror ) {
+                    Join.repwderror = true;
+                    $( $( 'form.joinform div div > span' )[ 0 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
+                }
+                if ( !Join.usernameerror && !Join.invalidusername && !Join.usernameexists ) {
+                    Join.repassword.focus();
+                }
+                create = false;
+            }
+            if ( Join.email.value !== '' && !/^[a-zA-Z0-9.\-_]+@[a-zA-Z0-9.\-_]+$/.test( Join.email.value ) ) {
+                if ( !Join.emailerror ) {
+                    Join.emailerror = true;
+                    $( $( 'form.joinform div > span' )[ 5 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
+                }
+                if ( !Join.usernameerror && !Join.invalidusername && !Join.usernameexists && !Join.pwderror && !Join.repwderror ) {
+                    Join.email.focus();
+                }
+                create = false;
+            }
+            if ( create ) {
+                if ( Join.enabled ) {
+                    document.body.style.cursor = 'wait';
+                    $( this ).addClass( 'button_disabled' );
+                    Coala.Warm( 'user/join' , { username : Join.username.value , password : Join.password.value , email : Join.email.value } );
+                }
+            }
+            return false;
+        } );
     }
 };
