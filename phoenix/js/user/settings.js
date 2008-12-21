e2fe0550731d78ad884229af8fccd17003ebd4e6
Chris Pappas <chrispappas12@gmail.com>
2008-12-21 14:05:00 +0000

    user settings js optimization

--- phoenix/js/user/settings.js
+++ phoenix/js/user/settings.js
@@ -126,7 +126,8 @@ var Settings = {
 		Modals.Create(  area , 440 , 330 );
 		Settings.oldpassworddiv = $( 'div#pwdmodal div.oldpassword' );
 		Settings.newpassworddiv = $( 'div#pwdmodal div.newpassword' );
-		Settings.renewpassworddiv = $( 'div#pwdmodal div.renewpassword' ); Settings.oldpassword = $( 'div#pwdmodal div.oldpassword div input' )[ 0 ];
+		Settings.renewpassworddiv = $( 'div#pwdmodal div.renewpassword' );
+		Settings.oldpassword = $( 'div#pwdmodal div.oldpassword div input' )[ 0 ];
 		Settings.newpassword = $( 'div#pwdmodal div.newpassword div input' )[ 0 ];
 		Settings.renewpassword = $( 'div#pwdmodal div.renewpassword div input' )[ 0 ];
 
