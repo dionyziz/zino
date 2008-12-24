884477a037a21e894b3d0c0fcdc6b779f18a970f
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-12-24 09:24:04 +0000

    pretty polls urls

--- phoenix/scripts/prettypollsurls.php
+++ phoenix/scripts/prettypollsurls.php
@@ -46,9 +46,15 @@
             $length = strlen( $candidate );
             while ( isset( $urls[ strtolower( $candidate ) ] ) ) {
                 if ( $length < 255 ) {
+                    if ( $pollInfo[ 'id' ] == 10251 ) {
+		        echo 'pros8esa<br />';
+		    }
                     $candidate .= '_';
                 }
-                else {
+		else {
+                    if ( $pollInfo[ 'id' ] == 10251 ) {
+		        echo 'antikatesthsa<br />';
+		    }
                     $candidate[ rand( 0, $length - 1 ) ] = '_';
                 }
             }
