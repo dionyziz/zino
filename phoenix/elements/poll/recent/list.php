659afbd4a41a5158dee9933132a36dba73db7e97
Dionysis Zindros <dionyziz@gmail.com>
2008-12-24 13:49:08 +0000

    poll/journal URLs

--- phoenix/elements/poll/recent/list.php
+++ phoenix/elements/poll/recent/list.php
@@ -2,7 +2,6 @@
 class ElementPollRecentList extends Element {
         public function Render( tInteger $pageno ) {
         global $libs;
-        global $xc_settings;
 
         $pageno = $pageno->Get();
 
@@ -16,8 +15,6 @@ class ElementPollRecentList extends Element {
             <h2>Δημοσκοπήσεις</h2>
             <div class="list"><?php
                 foreach ( $polls as $poll ) {
-                    $domain = str_replace( '*', urlencode( $poll->User->Subdomain ), $xc_settings[ 'usersubdomains' ] );
-                    $url = $domain . 'polls/' . $poll->Url;
                     ?><div class="event">
                         <div class="who"><?php
                             Element( 'user/display' , $poll->User->Id , $poll->User->Avatar->Id , $poll->User );
@@ -25,7 +22,7 @@ class ElementPollRecentList extends Element {
                         </div>
                         <div class="subject">
                             <a href="<?php
-                            echo $url;
+                            Element( 'url', $poll );
                             ?>"><?php
                             echo htmlspecialchars( $poll->Question );
                             ?></a>
