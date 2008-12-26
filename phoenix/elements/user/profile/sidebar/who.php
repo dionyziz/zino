59f075d576e8b44c220c425f5ff00d024415385d
Chris Pappas <chrispappas12@gmail.com>
2008-12-26 23:55:27 +0000

    userprofile, avatar is link

--- phoenix/elements/user/profile/sidebar/who.php
+++ phoenix/elements/user/profile/sidebar/who.php
@@ -2,9 +2,11 @@
     class ElementUserProfileSidebarWho extends Element {
         protected $mPersistent = array( 'theuserid' , 'avatarid' );
         public function Render( $theuser , $theuserid , $avatarid ) {
-            ?><h2><?php
+            ?><h2><a href="?p=photo&amp;id=<?php
+                echo $avatarid;
+                ?>"><?php
                 Element( 'user/avatar' , $avatarid , $theuserid , $theuser->Avatar->Width , $theuser->Avatar->Height , $theuser->Name , 150 , '' , 'margin-bottom:5px' , false , 0 , 0 );
-                ?><span class="name"><?php
+                ?></a><span class="name"><?php
                 Element( 'user/name' , $theuserid , $theuser->Name , $theuser->Subdomain , false );
                 ?></span>
             </h2><?php
