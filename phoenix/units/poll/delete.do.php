1726c2c439a4e56eb50d42a5485f754747f290fe
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-12-26 16:33:45 +0000

    pretty polls urls

--- phoenix/units/poll/delete.do.php
+++ phoenix/units/poll/delete.do.php
@@ -3,14 +3,14 @@
     function UnitPollDelete( tInteger $pollid ) {
         global $libs;
         global $user;
-        global $rabbit_settings;
+        global $xc_settings;
         
         $libs->Load( 'poll/poll' );
         $poll = New Poll( $pollid->Get() );
         if ( $poll->Userid == $user->Id || $user->HasPermission( PERMISSION_POLL_DELETE_ALL ) ) {
             $poll->Delete();
             ?>window.location.href = '<?php
-            echo $rabbit_settings[ 'webaddress' ];
+            echo str_replace( '*', urlencode( $user->Subdomain ), $xc_settings[ 'usersubdomains' ] );
             ?>/polls';<?php
         }
     }
