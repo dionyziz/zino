0078beca4553b453481435b027effd30bc61bd3f
Dionysis Zindros <dionyziz@gmail.com>
2008-12-29 21:02:27 +0000

    autofocus

---
+++ phoenix/elements/comment/focus.php
@@ -0,0 +1,24 @@
+<?php
+    class ElementCommentFocus extends Element {
+        public function Render( $commentid, $indentation ) {
+            global $user;
+            global $page;
+            
+            if ( $commentid !== 0 ) {
+                ob_start();
+                ?>Comments.Focus( <?php
+                echo $commentid;
+                ?>, <?php
+                echo $indentation;
+                ?>, <?php
+                if ( $user->Exists() ) {
+                ?>1<?php
+                }
+                else {
+                ?>0<?php
+                }
+                $page->AttachInlineScript( ob_get_clean() );
+            }
+        }
+    }
+?>
