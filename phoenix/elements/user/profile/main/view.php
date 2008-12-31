8c649ae0c6a80970191d1a3457b2ee12a07fef3c
Chris Pappas <chrispappas12@gmail.com>
2008-12-31 10:49:53 +0000

    userprofile upload photo

--- phoenix/elements/user/profile/main/view.php
+++ phoenix/elements/user/profile/main/view.php
@@ -129,6 +129,24 @@
                     </div>
                     <div id="easyphotoupload">
                         <h3 class="modaltitle">Ανέβασε μια φωτογραφία...</h3> 
+                        <div class="uploaddiv"><?php
+                            if ( UserBrowser() == 'MSIE' ) {
+                                ?>
+                                <iframe frameborder="0" style="height:50px" src="?p=upload&amp;albumid=<?php
+                                echo $user->Egoalbumid;
+                                ?>&amp;typeid=1&amp;color=eef5f9" class="uploadframe" id="uploadframe">
+                                </iframe>
+                                <?php
+                            }
+                            else {
+                                ?>
+                                <object style="height:50px" data="?p=upload&amp;albumid=<?php
+                                echo $user->Egoalbumid;
+                                ?>&amp;typeid=1&amp;color=eef5f9" class="uploadframe" id="uploadframe" type="text/html">
+                                </object>
+                                <?php
+                            }
+                        ?></div>
                     </div><?php
                 }
             }
