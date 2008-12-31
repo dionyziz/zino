13c84e60a5c4247d198c9883c73f73845db52534
Dionysis Zindros <dionyziz@gmail.com>
2008-12-31 17:39:33 +0000

    iPhone

--- phoenix/elements/iphone/user/profile/view.php
+++ phoenix/elements/iphone/user/profile/view.php
@@ -2,12 +2,13 @@
     class ElementiPhoneUserProfileView extends Element {
         public function Render( tText $subdomain ) {
             $subdomain = $subdomain->Get();
+            $finder = New UserFinder();
             $theuser = $finder->FindBySubdomain( $subdomain );
 
             if ( !$theuser->Exists() ) {
                 return;
             }
-
+            echo $theuser->Name;
         }
     }
 ?>
