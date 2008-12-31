13f6817c8643898f8a1ad2e48cb14a7106e473ac
Dionysis Zindros <dionyziz@gmail.com>
2008-12-31 17:52:12 +0000

    iPhone

--- phoenix/elements/iphone/banner.php
+++ phoenix/elements/iphone/banner.php
@@ -2,10 +2,19 @@
     class ElementiPhoneBanner extends Element {
         public function Render() {
             global $rabbit_settings;
+            global $user;
 
-            ?><div class="logo"><img src="<?php
-            echo $rabbit_settings[ 'imagesurl' ];
-            ?>iphone/zino.png" width="70" height="25" /></div><?php
+            ?><div class="banner"><?php
+                if ( $user->Exists() ) {
+                    Element( 'user/avatar', $user->Avatar->Id, $user->Id,
+                             $user->Avatar->Width, $user->Avatar->Height,
+                             $user->Name, 100, 'avatar', '', true, 30, 30 );
+                }
+                ?>
+                <img class="logo" src="<?php
+                echo $rabbit_settings[ 'imagesurl' ];
+                ?>iphone/zino.png" width="70" height="25" />
+            </div><?php
         }
     }
 ?>
