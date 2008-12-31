3cc8dc4e85f841ecab6f8c561e4ba040facde9da
Dionysis Zindros <dionyziz@gmail.com>
2008-12-31 18:07:56 +0000

    iPhone

--- phoenix/elements/iphone/user/profile/view.php
+++ phoenix/elements/iphone/user/profile/view.php
@@ -10,10 +10,14 @@
             if ( !$theuser->Exists() ) {
                 return;
             }
+            ?><div class="profile"><?php
             Element( 'user/avatar', $user->Avatar->Id, $user->Id,
                      $user->Avatar->Width, $user->Avatar->Height,
                      $user->Name, 100, 'avatar', '', true, 30, 30 );
+            ?><h2><?php
             echo $theuser->Name;
+            ?></h2>
+            </div><?php
         }
     }
 ?>
