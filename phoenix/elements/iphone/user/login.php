58c59c11bc3ac230815d169f61ab8751edc4acad
Dionysis Zindros <dionyziz@gmail.com>
2008-12-31 12:32:26 +0000

    iPhone

--- phoenix/elements/iphone/user/login.php
+++ phoenix/elements/iphone/user/login.php
@@ -1,17 +1,22 @@
 <?php
     class ElementiPhoneUserLogin extends Element {
         public function Render() {
+            global $page;
+
+            $page->SetTitle( 'Είσοδος' );
+
             ?><div class="login">
                 <form action="do/user/login" method="post">
-                    <h2>Είσοδος στο zino</h2>
                     <div>
-                        <label>Όνομα:</label> <input type="text" name="username" />
+                        <label>Όνομα:</label>
+                        <input type="text" name="username" />
                     </div>
                     <div>
-                        <label>Κωδικός:</label> <input type="password" name="password" />
+                        <label>Κωδικός:</label>
+                        <input type="password" name="password" />
                     </div>
                     <div>
-                        <input type="submit" value="Είσοδος &raquo;" />
+                        <input type="submit" value="Είσοδος" />
                     </div>
                 </form>
             </div><?php
