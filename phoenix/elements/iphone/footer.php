d8dd831529a3ed9383c4997b7bf9abfaf5c9ad30
Dionysis Zindros <dionyziz@gmail.com>
2008-12-31 13:30:07 +0000

    iPhone

--- phoenix/elements/iphone/footer.php
+++ phoenix/elements/iphone/footer.php
@@ -1,6 +1,8 @@
 <?php
     class ElementiPhoneFooter extends Element {
         public function Render() {
+            global $user;
+
             ?><div class="footer">&copy; 2008 Kamibu<?php
             if ( $user->Exists() ) {
                 ?> | <form method="post" action="do/user/logout"><a href="" onclick="this.parentNode.submit(); return false">Έξοδος</a></form><?php
