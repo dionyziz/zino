c649d03a36a16e5c9176b6261b693c56e8d4bff5
Dionysis Zindros <dionyziz@gmail.com>
2008-12-31 01:57:13 +0000

    iPhone

--- phoenix/elements/iphone/banner.php
+++ phoenix/elements/iphone/banner.php
@@ -1,7 +1,11 @@
 <?php
     class ElementiPhoneBanner extends Element {
         public function Render() {
-            ?><div class="logo">Zino</div><?php
+            global $rabbit_settings;
+
+            ?><div class="logo"><img src="<?php
+            echo $rabbit_settings[ 'imagesurl' ];
+            ?>iphone/zino.png" width="78" height="27" /></div><?php
         }
     }
 ?>
