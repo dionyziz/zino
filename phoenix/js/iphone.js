7a2a7fad0ca0f9ee17962949b316e866f022755c
Dionysis Zindros <dionyziz@gmail.com>
2008-12-31 15:49:32 +0000

    iPhone

--- phoenix/js/iphone.js
+++ phoenix/js/iphone.js
@@ -1,8 +1,25 @@
 var iPhone = {
     Frontpage: {
+        ShoutboxHasValue: false,
         OnLoad: function () {
-            $( 'textarea' ).focus( function() {
-                this.value = '';
+            var tx = $( 'textarea' );
+            tx.focus( function() {
+                if ( !iPhone.Frontpage.ShoutboxHasValue ) {
+                    this.value = '';
+                    this.style.color = 'black';
+                    this.style.textAlign = 'left';
+                }
+            } );
+            tx.blur( function() {
+                if ( this.value != '' ) {
+                    iPhone.Frontpage.ShoutboxHasValue = true;
+                }
+                else {
+                    iPhone.Frontpage.ShoutboxHasValue = false;
+                    this.style.color = '#ccc';
+                    this.style.textAlign = 'center';
+                    this.value = 'Άγγιξε εδώ για να γράψεις...';
+                }
             } );
         }
     }
