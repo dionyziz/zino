1092b3110e4d00b2674153813e11f2b18ae0f556
Dionysis Zindros <dionyziz@gmail.com>
2009-01-01 21:56:44 +0000

    iPhone

--- phoenix/js/iphone.js
+++ phoenix/js/iphone.js
@@ -36,7 +36,7 @@ var iPhone = {
 
 $( function() {
     $( 'a.loadable' ).click( function() {
-        this.style.backgroundColor = '#246887';
+        this.style.backgroundColor = '#047fbb';
         var img = document.createElement( 'img' );
         img.src = ' http://static.zino.gr/phoenix/ajax-loader.gif';
         img.alt = 'Loading...';
