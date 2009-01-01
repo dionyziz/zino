f1453c7484222c0f38e1d1aa6a1448c750fc1e99
Chris Pappas <chrispappas12@gmail.com>
2009-01-01 10:33:46 +0000

    easy photo upload

--- phoenix/units/user/profile/_easyupload.php
+++ phoenix/units/user/profile/_easyupload.php
@@ -6,17 +6,10 @@
         echo w_json_encode( ob_get_clean() );
         ?> );
         $( 'div#easyphotoupload div.modalcontent div ul li' ).click( function() {
-            /*
-            var albumname = $( this ).find( 'span img' ).attr( 'alt' );
-            var username = GetUsername();
-            if ( albumname == username ) {
-                albumname = "Ego";
-            }
-            */
-            $( 'div#easyphotoupload div.modalcontent div b' ).empty().append( document.createTextNode( 'test' ) );
-           // alert( $( this ).attr( 'id' ).substr( 6 ) );
+            alert( $( this ).find( 'span img' ).attr( 'alt' ) );
+            alert( $( this ).id );
         } );
+        
         <?php
     }
 ?>
-
