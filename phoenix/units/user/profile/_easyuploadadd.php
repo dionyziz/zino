8758aea8a739c7b7fc51552bc1fa4995104ed3a5
Chris Pappas <chrispappas12@gmail.com>
2009-01-01 17:27:03 +0000

    easy photo upload

--- phoenix/units/user/profile/_easyuploadadd.php
+++ phoenix/units/user/profile/_easyuploadadd.php
@@ -14,6 +14,7 @@
         Element( 'image/view' , $imageid , $image->User->Id , $image->Width , $image->Height , IMAGE_CROPPED_100x100 , '' , $image->User->Name , '' , false , 0 , 0 , 0 );
         echo w_json_encode( ob_get_clean() );
         ?> );
+        $( newli ).append( newlink );
         $( 'div#profile div.main div.photos ul.plist li.addphoto' ).after( newli );<?php
     }
 ?>
