37c24893805f04866e4e0bc3f0c16cb29a9a5f2f
Chris Pappas <chrispappas12@gmail.com>
2009-01-01 10:43:53 +0000

    easy photo upload

--- phoenix/units/user/profile/_easyupload.php
+++ phoenix/units/user/profile/_easyupload.php
@@ -8,8 +8,17 @@
         $( 'div#easyphotoupload div.modalcontent div ul li' ).click( function() {
 
             $( 'div#easyphotoupload div.modalcontent div b' ).empty().append( document.createTextNode( 'test' ) );
-            alert( $( this ).find( 'span img' ).attr( 'alt' ) );
+            
             alert( $( this ).attr( 'id' ).substr( 6 ) );
+            var albumid = $( this ).attr( 'id' ).substr( 6 );
+            var newurl = '?p=upload&albumid=' + albumid + 'typeid=3&color=eef5f9';
+            alert( newurl );
+            if ( $.browser.msie ) {
+
+            }
+            else {
+
+            }
         } );
         
         <?php
