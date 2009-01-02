e11f07cab4e0c50179e4797f53520681b813ea0a
Chris Pappas <chrispappas12@gmail.com>
2009-01-02 09:16:58 +0000

    easy photo upload

--- phoenix/js/user/_profile.js
+++ phoenix/js/user/_profile.js
@@ -135,7 +135,9 @@ var Profile = {
             overlayClass : 'mdloverlay1'
         } );
         $( 'div#profile div.main div.photos ul li.addphoto a' ).click( function() {
-            Coala.Cold( 'user/profile/easyupload' , {} );
+            if ( !$( '#easyphotoupload div.modalcontent div.uploaddiv' )[ 0 ] ) {
+                Coala.Cold( 'user/profile/easyupload' , {} );
+            }
         } );
         $( 'div.tweetactive div.tweet a' ).click( function () {
             var win = $( '#tweetedit' )[ 0 ];
