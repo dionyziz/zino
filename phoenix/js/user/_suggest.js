36e730c9716ebfe05568931e8e6418e5208b66e3
Kostis Roussopoulos <kostis90gr@gmail.com>
2009-01-02 20:08:43 +0000

    Back to Alerts

--- phoenix/js/user/_suggest.js
+++ phoenix/js/user/_suggest.js
@@ -156,6 +156,7 @@ var Suggest = {
 		if ( !$.browser.mozilla && ul.css( 'display' ) != "none" && parseInt( ul.get( 0 ).clientWidth, 10 ) < 200 ) { // Not firefox and there is a scrollbar
 			var x = event.offsetX?(event.offsetX):event.pageX-ul.get( 0 ).offsetLeft;
             var y = event.offsetY?(event.offsetY):event.pageY-ul.get( 0 ).offsetTop;
+			alert( x + " " + y );
 			if ( y >= 19 && y <= 268 && ( ( $.browser.msie && x >= 185 && x <= 202 ) || ( !$.browser.msie && x >= 183 && x <= 200 ) ) ) {
 				setTimeout( "$( 'div." + type + " input' ).focus()", 5 );
 				return false;
