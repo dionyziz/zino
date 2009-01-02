d0e967186d8520bec7c6f28f448b73d71019389f
Chris Pappas <chrispappas12@gmail.com>
2009-01-02 20:28:25 +0000

    easy photo upload

--- phoenix/elements/user/profile/main/_view.php
+++ phoenix/elements/user/profile/main/_view.php
@@ -132,7 +132,7 @@
                         <div class="modalcontent"> 
                             <img src="<?php
                             echo $rabbit_settings[ 'imagesurl' ];
-                            ?>ajax-loader.gif" alt="Αποθήκευση" /><span>Παρακαλώ περιμένετε...</span>
+                            ?>ajax-loader.gif" /><span>Παρακαλώ περιμένετε...</span>
                         </div>
                     </div><?php
                 }
