5221241c5f8f924dda83fddba619483c0129ed27
Chris Pappas <chrispappas12@gmail.com>
2009-01-08 18:18:38 +0000

    emoticons fix

--- phoenix/libs/wysiwyg.php
+++ phoenix/libs/wysiwyg.php
@@ -207,7 +207,7 @@
             foreach ( $smileys as $i => $smiley ) {
                 $smileysprocessed[ $i ] = '<span class="emoticon-'
                                         . htmlspecialchars( $smiley ) 
-                                        . '">&nbsp;</span>';
+                                        . '" style="color:transparent">.</span>';
             }
             $smileysprocessedkeys = array_keys( $smileysprocessed );
         }
