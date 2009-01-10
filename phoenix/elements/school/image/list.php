8ba618efa568e3e624d55342e095dfab34d6e404
Chris Pappas <chrispappas12@gmail.com>
2009-01-10 19:02:00 +0000

    spriting

--- phoenix/elements/school/image/list.php
+++ phoenix/elements/school/image/list.php
@@ -20,7 +20,7 @@
                     }
                     if ( $user->Profile->Schoolid == $schoolid && $user->HasPermission( PERMISSION_IMAGE_CREATE ) ) {
                         ?><li>
-                            <a href="" class="uploadphoto">&nbsp;</a>
+                            <a href=""><span class="s_bigadd">&nbsp;</span></a>
                         </li><?php
                     }
                     ?>
