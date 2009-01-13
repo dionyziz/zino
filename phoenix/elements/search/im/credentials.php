b1acdbc6112f98cd0198336223e973d84524cc24
Chris Pappas <chrispappas12@gmail.com>
2009-01-13 10:32:09 +0000

    searching users

--- phoenix/elements/search/im/credentials.php
+++ phoenix/elements/search/im/credentials.php
@@ -2,13 +2,33 @@
 	class ElementSearchImCredentials extends Element {
         public function Render( tString $im ) {
 			global $rabbit_settings;
+			global $user;
 			
 			$im = $im->Get();
 			?><div id="im">
-			<h2>Βρες τους φίλους σου στο <?php
-			echo $rabbit_settings[ 'applicationname' ];
-			?></h2>
-			
+				<h2>Βρες τους φίλους σου στο <?php
+				echo $rabbit_settings[ 'applicationname' ];
+				?></h2>
+				<div class="">
+					<input type="text" />
+					<select><?php
+					if ( $im == "msn" ) {
+						?><option value="hotmail.com" selected="selected">hotmail.com</option>
+						<option value="windowslive.com">windowslive.com</option>
+						<option value="msn.com">msn.com</option>
+						<option value="msn.com">live.gr</option>
+						<option value="msn.com">live.com</option><?php
+					}
+					else if ( $im == "gmail" ) {
+						?><option value="gmail.com">gmail.com</option>
+						<option value="googlemail.com">googlemail.com</option><?php
+					}
+					else if ( $im == "yahoo" ) {
+						?><option value="yahoo.gr">yahoo.gr</option>
+						<option value="yahoo.com">yahoo.com</option><?php
+					}
+				?></select>
+				</div>
 			</div><?php
 		}
 	}
