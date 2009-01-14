953f9d4e9908163c64498add796331b627a921b6
Chris Pappas <chrispappas12@gmail.com>
2009-01-14 10:19:39 +0000

    searching users

--- phoenix/js/search/im.js
+++ phoenix/js/search/im.js
@@ -1,9 +1,13 @@
 var Im = {
 	ImOnLoad : function () {
-		$( 'div#im div.cred div.empwd div.mail input' )[ 0 ].focus();
-		$( 'div#im div.cred div.empwd div.mail input' )[ 0 ].select();
+		var email = $( 'div#im div.cred div.empwd div.mail input' )[ 0 ];
+		var pwd = $( 'div#im div.cred div.empwd div.pwd input' )[ 0 ];
+		email.focus();
+		email.select();
 		$( 'div#im div.cred div.next a' ).click( function() {
 			$( 'div#im div.cred div.wrong div.w' ).fadeIn( 400 );
+			alert( 'email is: ' + email.value );
+			alert( 'password is: ' + pwd.value );
 			return false;
 		} );
 	}
