359d9e370e49fc4f6ae9580a0551811ea57bfcc5
Chris Pappas <chrispappas12@gmail.com>
2009-01-14 08:27:13 +0000

    searching users

--- phoenix/elements/search/im/credentials.php
+++ phoenix/elements/search/im/credentials.php
@@ -10,22 +10,28 @@
 				echo $rabbit_settings[ 'applicationname' ];
 				?></h2>
 				<div class="">
-					<input type="text" />@
-					<select><?php
-					if ( $im == "msn" ) {
-						?><option value="hotmail.com" selected="selected">hotmail.com</option>
-						<option value="windowslive.com">windowslive.com</option>
-						<option value="msn.com">msn.com</option>
-						<option value="msn.com">live.com</option><?php
-					}
-					else if ( $im == "gmail" ) {
-						?><option value="gmail.com">gmail.com</option>
-						<option value="googlemail.com">googlemail.com</option><?php
-					}
-					else if ( $im == "yahoo" ) {
-						?><option value="yahoo.com">yahoo.com</option><?php
-					}
-				?></select>
+					<h3>Δώσε το email σου και τον κωδικό πρόσβασης</h3>
+					<div>
+						<span>Εmail</span><input type="text" />@
+						<select><?php
+						if ( $im == "msn" ) {
+							?><option value="hotmail.com" selected="selected">hotmail.com</option>
+							<option value="windowslive.com">windowslive.com</option>
+							<option value="msn.com">msn.com</option>
+							<option value="msn.com">live.com</option><?php
+						}
+						else if ( $im == "gmail" ) {
+							?><option value="gmail.com">gmail.com</option>
+							<option value="googlemail.com">googlemail.com</option><?php
+						}
+						else if ( $im == "yahoo" ) {
+							?><option value="yahoo.com">yahoo.com</option><?php
+						}
+						?></select>
+					</div>
+					<div>
+						<span>Κωδικός</span><input type="text" />
+					</div>
 				</div>
 			</div><?php
 		}
