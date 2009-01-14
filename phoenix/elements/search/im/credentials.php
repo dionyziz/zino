535c4ed648186fe462043f77a54f11b336856bc9
Chris Pappas <chrispappas12@gmail.com>
2009-01-14 09:39:56 +0000

    searching users

--- phoenix/elements/search/im/credentials.php
+++ phoenix/elements/search/im/credentials.php
@@ -4,53 +4,58 @@
 			global $rabbit_settings;
 			global $user;
 			
-			$im = $im->Get();
-			?><div id="im">
-				<h2>Βρες τους φίλους σου στο <?php
-				echo $rabbit_settings[ 'applicationname' ];
-				?></h2>
-				<div class="cred">
-					<h3>Δώσε το email σου και τον κωδικό πρόσβασης</h3>
-					<div class="empwd">	
-						<div class="mail">
-							<span class="credin1">Εmail</span><input type="text" />@
-							<select><?php
-							if ( $im == "msn" ) {
-								?><option value="hotmail.com" selected="selected">hotmail.com</option>
-								<option value="windowslive.com">windowslive.com</option>
-								<option value="msn.com">msn.com</option>
-								<option value="msn.com">live.com</option><?php
-							}
-							else if ( $im == "gmail" ) {
-								?><option value="gmail.com">gmail.com</option>
-								<option value="googlemail.com">googlemail.com</option><?php
-							}
-							else if ( $im == "yahoo" ) {
-								?><option value="yahoo.com">yahoo.com</option><?php
-							}
-							?></select>
+			if ( $user->Exists() ) {
+				$im = $im->Get();
+				?><div id="im">
+					<h2>Βρες τους φίλους σου στο <?php
+					echo $rabbit_settings[ 'applicationname' ];
+					?></h2>
+					<div class="cred">
+						<h3>Δώσε το email σου και τον κωδικό πρόσβασης</h3>
+						<div class="empwd">	
+							<div class="mail">
+								<span class="credin1">Εmail</span><input type="text" />@
+								<select><?php
+								if ( $im == "msn" ) {
+									?><option value="hotmail.com" selected="selected">hotmail.com</option>
+									<option value="windowslive.com">windowslive.com</option>
+									<option value="msn.com">msn.com</option>
+									<option value="msn.com">live.com</option><?php
+								}
+								else if ( $im == "gmail" ) {
+									?><option value="gmail.com">gmail.com</option>
+									<option value="googlemail.com">googlemail.com</option><?php
+								}
+								else if ( $im == "yahoo" ) {
+									?><option value="yahoo.com">yahoo.com</option><?php
+								}
+								?></select>
+							</div>
+							<div class="pwd">
+								<span class="credin2">Κωδικός</span><input type="password" />
+							</div>
 						</div>
-						<div class="pwd">
-							<span class="credin2">Κωδικός</span><input type="text" />
+						<div class="wrong">
+							<div>
+								<span class="s_invalid">&nbsp;</span>Ο συνδυασμός email και κωδικού πρόσβασης είναι λανθασμένος
+							</div>
 						</div>
-					</div>
-					<div class="wrong">
-						<div>
-							<span class="s_invalid">&nbsp;</span>Ο συνδυασμός email και κωδικού πρόσβασης είναι λανθασμένος
+						<div class="next">
+							<a href="" class="button">Επόμενο &raquo;</a>
 						</div>
+						<div class="barfade">
+	                        <div class="leftbar"></div>
+	                        <div class="rightbar"></div>
+	                    </div>
+						<p class="imtos">
+							Δεν αποθηκεύουμε το email και τον κωδικό πρόσβασης σου και δε γίνεται καμία ενέργεια χωρίς την έγκρισή σου
+						</p>
 					</div>
-					<div class="next">
-						<a href="" class="button">Επόμενο &raquo;</a>
-					</div>
-					<div class="barfade">
-                        <div class="leftbar"></div>
-                        <div class="rightbar"></div>
-                    </div>
-					<p class="imtos">
-						Δεν αποθηκεύουμε το email και τον κωδικό πρόσβασης σου και δε γίνεται καμία ενέργεια χωρίς την έγκρισή σου
-					</p>
-				</div>
-			</div><?php
+				</div><?php
+			}
+			else {
+				?>Για να χρησιμοποιήσεις αυτή τη δυνατότητα πρέπει να είσαι εγγεγραμμένος<?php
+			}
 		}
 	}
 ?>
