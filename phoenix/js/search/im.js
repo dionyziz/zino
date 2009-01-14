40488402258a247f09db087fe066b81684533384
Chris Pappas <chrispappas12@gmail.com>
2009-01-14 10:03:28 +0000

    searching users

--- phoenix/js/search/im.js
+++ phoenix/js/search/im.js
@@ -2,7 +2,6 @@ var Im = {
 	ImOnLoad : function () {
 		$( 'div#im div.cred div.empwd div.mail input' )[ 0 ].focus();
 		$( 'div#im div.cred div.empwd div.mail input' )[ 0 ].select();
-		
 		$( 'div#im div.cred div.next a' ).click( function() {
 			$( 'div#im div.cred div.wrong div.w' ).fadeIn( 400 );
 			return false;
