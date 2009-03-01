bc3e97a03e6b828ce7d6e9fc79080778643b08e2
Dionysis Zindros <dionyziz@gmail.com>
2009-03-01 22:53:30 +0000

    SQL optimizations

--- phoenix/libs/image/frontpage.php
+++ phoenix/libs/image/frontpage.php
@@ -4,14 +4,50 @@
 
         public function FindLatest( $offset = 0, $limit = 15, $totalcount = false ) {
             $prototype = New FrontpageImage();
-
-            return $this->FindByPrototype( $prototype, $offset, $limit, array( 'Imageid', 'DESC' ), $totalcount );
+            
+            $latest = $this->FindByPrototype( $prototype, $offset, $limit, array( 'Imageid', 'DESC' ), $totalcount );
+            
+            $imageids = array();
+            $userids = array();
+            
+            foreach ( $latest as $frontpageimage ) {
+                $imageids[] = $frontpageimage->Imageid;
+                $userids[] = $frontpageimage->Userid;
+            }
+            
+            $userfinder = New UserFinder();
+            $users = $userfinder->FindByIds( $userids );
+            
+            $imagefinder = New ImageFinder();
+            $images = $imagefinder->FindByIds( $imageids );
+            
+            $userbyid = array();
+            $imagebyid = array();
+            foreach ( $users as $user ) {
+                $userbyid[ $user->Id ] = $user;
+            }
+            foreach ( $images as $image ) {
+                $imagebyid[ $image->Id ] = $image;
+            }
+            
+            foreach ( $latest as $i => $frontpageimage ) {
+                $latest[ $i ]->CopyImageFrom( $imagebyid[ $frontpageimage->Imageid ] );
+                $latest[ $i ]->CopyUserFrom( $userbyid[ $frontpageimage->Userid ] );
+            }
+            
+            return $latest;
         }
     }
     
     class FrontpageImage extends Satori {
         protected $mDbTableAlias = 'imagesfrontpage';
         
+        public function CopyImageFrom( $value ) {
+            $this->mRelations[ 'Image' ]->CopyFrom( $value );
+        }
+        public function CopyUserFrom( $value ) {
+            $this->mRelations[ 'User' ]->CopyFrom( $value );
+        }
         protected function Relations() {
             $this->User = $this->HasOne( 'User', 'Userid' );
             $this->Image = $this->HasOne( 'Image', 'Imageid' );
