13e3ac8201bdc5accd67018d70d1f43ebafd37cb
Petros Aggelatos <petrosagg@gmail.com>
2009-03-07 00:22:35 +0000

    Fixing XHTML validy of user profile

--- phoenix/elements/poll/result/view.php
+++ phoenix/elements/poll/result/view.php
@@ -5,10 +5,9 @@
                 $finder = New PollOptionFinder();
                 $options = $finder->FindByPoll( $poll );
                 foreach ( $options as $option ) {
-                    ?><li>
-                        <dl><?php
-                            if ( $showresults ) {
-                                ?>
+                    ?><li><?php
+                        if ( $showresults ) {
+                            ?><dl>
                                 <dd><?php //max width will be 220px and minimum 24px
                                 ?><div class="option">
                                     <div class="percentagebar" style="width:<?php
@@ -21,12 +20,14 @@
                                 </div><?php
                                     echo round( $option->Percentage * 100, 0 );
                                 ?>%</dd>
-                                <span class="resultterm"><?php
-                                    echo htmlspecialchars( $option->Text );
-                                ?></span><?php
-                            }
-                            else {
-                                ?><dt class="voteterm"><input type="radio" name="poll_<?php
+                            </dl>
+                            <span class="resultterm"><?php
+                                echo htmlspecialchars( $option->Text );
+                            ?></span><?php
+                        }
+                        else {
+                            ?><dl>
+                                <dt class="voteterm"><input type="radio" name="poll_<?php
                                 echo $poll->Id;
                                 ?>" value="<?php
                                 echo $option->Id;
@@ -37,10 +38,10 @@
                                 ?>' , this );" /></dt>
                                 <dd class="votedefinition"><?php
                                 echo htmlspecialchars( $option->Text );
-                                ?></dd><?php
-                            }
-                        ?></dl>
-                    </li><?php
+                                ?></dd>
+                            </dl><?php
+                        }
+                   ?></li><?php
                 }
             ?></ul><?php
         }
