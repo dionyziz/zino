bd14502a57b0f7a4bbd2fbc99faf8844d2751240
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-07 17:18:12 +0000

    kolaz

--- phoenix/elements/_kolaz.php
+++ phoenix/elements/_kolaz.php
@@ -13,13 +13,13 @@
 	        $tags = $Tagfinder->FindByPersonId( 4005 );
 	        foreach ( $tags as $tag ) {
                 ?><p><?php
-                echo $tag->ImageId;
+                echo $tag->Imageid;
                 ?> <?php
                 echo $tag->Width;                
                 ?> <?php
                 echo $tag->Height;
                 ?> <?php
-                Element( 'image/view', $tag->ImageId , $tag->PersonId , $tag->Width , $tag->Height );/*
+                Element( 'image/view', $tag->Imageid , $tag->PersonId , $tag->Width , $tag->Height );/*
                 public function Render( $imageid, $imageuserid , $imagewidth , $imageheight , $type = IMAGE_PROPORTIONAL_210x210, $class = '', $alttitle = '' , $style = '' , $cssresizable = false , $csswidth = 0 , $cssheight = 0 , $numcom = 0 )*/
                 ?></p><?php
             }
