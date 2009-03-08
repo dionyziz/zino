b6d66f2410e1874bf346c3b2b12071b447d983ab
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-08 20:42:00 +0000

    image php

---
+++ phoenix/images/kolaz/kolaz.php
@@ -0,0 +1,26 @@
+<?php
+	set_include_path( '../../:./' );
+	require_once 'libs/rabbit/rabbit.php';
+	Rabbit_Construct( 'plain' );
+
+	global $libs;
+	global $user;
+	
+	$libs->Load("image/tag");
+	
+	$personid = $_GET[ 'personid' ];
+	        
+    $Tagfinder = new ImageTagFinder();
+    $tags = $Tagfinder->FindByPersonId( $personid );
+    
+    $img = imagecreate(420,510);//Element( 'image/url', $tag->Imageid, $tag->Personid, IMAGE_FULLVIEW );    
+    $src = imagecreatefrompng( Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW ) );
+    imagecopy( $img, $src,0,0,$tags[0]->left,$tags[0]->top,$tags[0]->Width,$tags[0]->Height );
+    imagedestroy($src);
+	
+
+	header( 'Content-type: image/png' );
+	imagepng($dest);
+			
+	Rabbit_Destruct();
+?>
