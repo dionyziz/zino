898c62d5c9e7726dcdec75118e252299c5420de8
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-08 21:01:27 +0000

    image php

--- phoenix/images/kolaz/kolaz.php
+++ phoenix/images/kolaz/kolaz.php
@@ -12,7 +12,7 @@
 	        
     $Tagfinder = new ImageTagFinder();
     $tags = $Tagfinder->FindByPersonId( 4005 );
-    
+    die("shit");
     //$img = imagecreate(420,510);//Element( 'image/url', $tag->Imageid, $tag->Personid, IMAGE_FULLVIEW );   
     $src = imagecreatefrompng( Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW ) );
     //imagecopy( $img, $src,0,0,$tags[0]->left,$tags[0]->top,$tags[0]->Width,$tags[0]->Height );
