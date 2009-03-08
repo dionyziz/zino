48a2111575f4137e5e19ab5d8165e57c4023ac7f
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-08 20:13:46 +0000

    kolaz

--- phoenix/elements/_kolaz.php
+++ phoenix/elements/_kolaz.php
@@ -1,6 +1,6 @@
 <?php    
     class ElementKolaz extends Element {
-        public function Render() {
+        public function Render( $user = 1 ) {
 	        global $page;
 	        global $user;
 	        global $libs;
@@ -10,7 +10,7 @@
 	        $libs->Load("image/tag");
 	        
 	        $Tagfinder = new ImageTagFinder();
-	        $tags = $Tagfinder->FindByPersonId( 4005 );
+	        $tags = $Tagfinder->FindByPersonId( $user );
 	        foreach ( $tags as $tag ) {
                 ?><p><?php
                 echo $tag->Imageid;
