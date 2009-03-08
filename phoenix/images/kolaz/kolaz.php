325a7faa02eca517ba5c54034df3968e64800e4d
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-08 21:00:58 +0000

    image php

--- phoenix/images/kolaz/kolaz.php
+++ phoenix/images/kolaz/kolaz.php
@@ -13,16 +13,17 @@
     $Tagfinder = new ImageTagFinder();
     $tags = $Tagfinder->FindByPersonId( 4005 );
     
-    $img = imagecreate(420,510);//Element( 'image/url', $tag->Imageid, $tag->Personid, IMAGE_FULLVIEW );   
+    //$img = imagecreate(420,510);//Element( 'image/url', $tag->Imageid, $tag->Personid, IMAGE_FULLVIEW );   
     $src = imagecreatefrompng( Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW ) );
     //imagecopy( $img, $src,0,0,$tags[0]->left,$tags[0]->top,$tags[0]->Width,$tags[0]->Height );
-    //imagedestroy($src);
+    
 	
 
 	header( 'Content-type: image/png' );
 	imagepng($src);
 	//imagepng($img);
 	//imagedestroy($img);
+	imagedestroy($src);
 			
 	Rabbit_Destruct();
 ?>
