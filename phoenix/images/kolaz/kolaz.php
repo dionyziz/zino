f07fc9b978886e7b1b68e8c85d21fe1764ecc2ee
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-09 17:31:27 +0000

    kollaz

--- phoenix/images/kolaz/kolaz.php
+++ phoenix/images/kolaz/kolaz.php
@@ -18,16 +18,21 @@
     imagecopy( $img, $src,0,0,$tags[0]->left,$tags[0]->top,$tags[0]->Width,$tags[0]->Height );*/
     /* createfromstring
     imagecreatefromstring*/
+    $img = imagecreate(420,510);
     $url = $xc_settings[ 'imagesurl' ] . $tags[0]->Personid . '/';
     if ( !$rabbit_settings[ 'production' ] ) {
         $url = $url .  '_';
     }
     $url = $url . $tags[0]->Imageid . '/' . $tags[0]->Imageid . '_' . IMAGE_FULLVIEW . '.jpg';
-    /*echo $url;*/
-    /*echo Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW );*/
     $src = imagecreatefromstring(file_get_contents( $url ));
-    $img = imagecreate(420,510);
     imagecopy( $img, $src,0,0,$tags[0]->left,$tags[0]->top,$tags[0]->Width,$tags[0]->Height );
+    $url = $xc_settings[ 'imagesurl' ] . $tags[1]->Personid . '/';
+    if ( !$rabbit_settings[ 'production' ] ) {
+        $url = $url .  '_';
+    }
+    $url = $url . $tags[0]->Imageid . '/' . $tags[1]->Imageid . '_' . IMAGE_FULLVIEW . '.jpg';
+    $src = imagecreatefromstring(file_get_contents( $url ));
+    imagecopy( $img, $src,0,331,$tags[1]->left,$tags[1]->top,$tags[1]->Width,$tags[1]->Height );
     
 	
 
