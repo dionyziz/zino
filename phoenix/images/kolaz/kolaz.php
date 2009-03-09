ed0a3bdbac5fd7fa52912e9c156e0317fa6b8859
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-09 17:12:55 +0000

    kollaz

--- phoenix/images/kolaz/kolaz.php
+++ phoenix/images/kolaz/kolaz.php
@@ -9,24 +9,24 @@
 	$libs->Load("image/tag");
 	
 	/*$personid = $_GET[ 'personid' ];*/
+	
+	echo "hi";
 	        
     $Tagfinder = new ImageTagFinder();
     $tags = $Tagfinder->FindByPersonId( 4005 );
-    //$img = imagecreate(420,510);//Element( 'image/url', $tag->Imageid, $tag->Personid, IMAGE_FULLVIEW );   
-    //$src = imagecreatefrompng( Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW ) );
-    //imagecopy( $img, $src,0,0,$tags[0]->left,$tags[0]->top,$tags[0]->Width,$tags[0]->Height );
+    /*$img = imagecreate(420,510);//Element( 'image/url', $tag->Imageid, $tag->Personid, IMAGE_FULLVIEW );   
+    $src = imagecreatefrompng( Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW ) );
+    imagecopy( $img, $src,0,0,$tags[0]->left,$tags[0]->top,$tags[0]->Width,$tags[0]->Height );*/
     /* createfromstring
     imagecreatefromstring*/
-    echo Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW );
-    $src = imagecreatefromstring( file_get_contents( Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW ) ) );
+    /*echo Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW );
+    $src = imagecreatefromstring( file_get_contents( Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW ) ) );*/
     
 	
 
-	header( 'Content-type: image/png' );
+	/*header( 'Content-type: image/png' );
 	echo $src;
-	//imagepng($img);
-	//imagedestroy($img);
-	imagedestroy($src);
+	imagedestroy($src);*/
 			
 	Rabbit_Destruct();
 ?>
