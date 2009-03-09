914a3cbd3d04ddce9feb901bbbf03e5927abd35b
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-09 17:11:16 +0000

    kollaz

--- phoenix/images/kolaz/kolaz.php
+++ phoenix/images/kolaz/kolaz.php
@@ -17,6 +17,7 @@
     //imagecopy( $img, $src,0,0,$tags[0]->left,$tags[0]->top,$tags[0]->Width,$tags[0]->Height );
     /* createfromstring
     imagecreatefromstring*/
+    echo Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW );
     $src = imagecreatefromstring( file_get_contents( Element( 'image/url', $tags[0]->Imageid, $tags[0]->Personid, IMAGE_FULLVIEW ) ) );
     
 	
