ac2915f1af66f496861386d99a49ca4c9adee2fb
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-11 12:11:26 +0000

    kolaz

--- phoenix/elements/_kolaz.php
+++ phoenix/elements/_kolaz.php
@@ -25,7 +25,7 @@
 	        if ( $kolaz->RetrievePositions( $input ) == true ) {
     	        foreach ( $kolaz->mPositions as $key=>$val ) {
     	            ?><p><?php
-                    echo $key . " " . $one[ 'xpos' ]  . " " . $one[ 'ypos' ];
+                    echo $key . " " . $val[ 'xpos' ]  . " " . $val[ 'ypos' ];
                     ?></p><?php
     	        }
 	        }
