242ebef69365fa6a49925759bdbbcd9ef94c44f0
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-11 11:41:23 +0000

    kolaz

--- phoenix/libs/kolaz/kolaz.php
+++ phoenix/libs/kolaz/kolaz.php
@@ -29,6 +29,7 @@
             echo "n == " . $n;
             fwrite($pipes[0], "$n\n");
             foreach ( $images as $img ) {
+                echo $img["id"] . " " . $img["width"] . " " . $img["height"];
                 fwrite($pipes[0], '$img["id"] $img["width"] $img["height"]\n');    
             }
             fclose($pipes[0]);        // 0 => stdin
