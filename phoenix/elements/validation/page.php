9229775762af13ab893cb98e071c7cb3436d703a
Petros Aggelatos <petrosagg@gmail.com>
2009-03-11 23:08:43 +0000

    Email validation

--- phoenix/elements/validation/page.php
+++ phoenix/elements/validation/page.php
@@ -1,12 +1,9 @@
 <?php
-
     class ElementUserValidation extends Element {
         public function Render() {
             global $user;
-
             ?><p>Ο λογαριασμός σου δεν έχει ενεργοποιηθεί ακόμη. Θα πρέπει να χρησιμοποιήσεις τον σύνδεσμο στο e-mail σου για να τον ενεργοποιήσεις. Δεν έλαβες κάποιο e-mail? Έλεγξε τον φάκελο junk ή <a>ζήτησέ μας να σου το ξαναστείλουμε</a></p><?php
         }
 
     }
 ?>
-
