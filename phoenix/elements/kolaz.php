2bcaa5be365cd59985e611724de4484fc7e6bbc3
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-13 18:12:05 +0000

    kollazcss

--- phoenix/elements/kolaz.php
+++ phoenix/elements/kolaz.php
@@ -45,11 +45,15 @@
 
 	        ?><div class="kolazimage" style="width:<?php echo $kolaz->maxX;?>px;height:<?php echo $kolaz->maxY;?>px;position: relative;"><?php
             foreach ( $kolaz->mPositions as $key=>$val ) {
+                ob_start();
+                Element( 'image/url', $key, $owners[ $key ], IMAGE_FULLVIEW );
+                $url = ob_get_contents();
+                ob_end_clean();/*
                 $url = $xc_settings[ 'imagesurl' ] . $owners[ $key ] . '/';
                 if ( !$rabbit_settings[ 'production' ] ) {
                     $url = $url .  '_';
                 }
-                $url = $url . $key . '/' . $key . '_' . IMAGE_FULLVIEW . '.jpg';
+                $url = $url . $key . '/' . $key . '_' . IMAGE_FULLVIEW . '.jpg';*/
                 
                 ?><div style="width:<?php echo $data[ $key ][ 'width' ];?>px; height:<?php echo $data[ $key ][ 'height' ];?>px; position:absolute; left:<?php echo $val[ 'xpos' ];?>px; top:<?php echo $val[ 'ypos' ];?>px;">
                         <img style="position: absolute;left:-<?php echo $data[ $key ][ 'left' ]?>px;top:-<?php echo $data[ $key ][ 'top' ]?>px" src="<?php echo $url;?>" alt="img" />
