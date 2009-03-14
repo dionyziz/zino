73c6ee549e75fae73fc499d2b177bc9b6749998a
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-14 18:16:58 +0000

    progressbar

--- phoenix/js/kolaz.js
+++ phoenix/js/kolaz.js
@@ -1,9 +1,9 @@
-var maxi = 21;
+var maxi = 3;
 var cur = 0;
 
 function loaded() {
     cur++;
-    document.getElementById( "percentage" ).style.width = (cur/maxi)*500 +  'px';
+    document.getElementById( "percentage" ).style.width = (cur/maxi)*250 +  'px';
     if( cur == maxi ) {
         document.getElementById( "kolaz" ).style.display = "block";
         document.body.removeChild( document.getElementById( "progress" ) );
