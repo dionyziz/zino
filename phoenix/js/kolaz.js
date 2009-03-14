58e0fe36935b725f1169f628cb5bacff25ebc67a
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-03-14 17:49:24 +0000

    progressbar

--- phoenix/js/kolaz.js
+++ phoenix/js/kolaz.js
@@ -1,22 +1,11 @@
-var z = 0;
 var maxi = 0;
-var imgs = {};
-var numloaded = 0;
+var cur = 0;
 
 function loaded() {
-    ++numloaded;
-    /*document.getElementById( 'percentage' ).style.width = ( numloaded / maxi ) * 200 + 'px';*/
-    alert( "one more" );
-    if ( numloaded == maxi ) {
-        /*document.body.removeChild( document.getElementById( "loading" ) );*/
-        return;
+    cur++;
+    document.getElementById( "percentage" ).style.width = (cur/maxi)*500 +  'px';
+    if( cur == maxi ) {
+        document.getElementById( "kolaz" ).style.display = "block";
+        document.body.removeChild( document.getElementById( "progress" ) );
     }
-}
-
-alert("inside");
-maxi = document.getElementsByTagName( 'img' ).length;
-for ( z = 0 ; z < maxi ; z += 1 ) { 
-    alert( "one pic found!" );
-    loaded();
-    document.getElementsByTagName( 'img' )[ z ].onload = function () { loaded(); };
 }
