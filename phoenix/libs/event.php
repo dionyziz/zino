2b7056695279187d2c67b84eaf301293fdb0a79f
Kostis Roussopoulos <kostis90gr@gmail.com>
2009-03-14 17:42:41 +0000

    Works

--- phoenix/libs/event.php
+++ phoenix/libs/event.php
@@ -360,7 +360,7 @@
                     break;
                 case EVENT_USER_BIRTHDAY:
                     $notif = New Notification();
-                    $notif->Touserid = $this->Item->Id;
+                    $notif->Touserid = $this->Itemid;
                     break;
                 default:
                     return; // items that don't create any notifications don't need to be saved
