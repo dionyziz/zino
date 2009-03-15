faa1788878016d0225401f7bdb6f7980d622cbf9
Dionysis Zindros <dionyziz@gmail.com>
2009-03-15 14:18:45 +0000

    ads

--- phoenix/libs/admanager.php
+++ phoenix/libs/admanager.php
@@ -1,4 +1,30 @@
 <?php
+    class AdException extends Exception {
+    }
+    
+    class AdFinder extends Finder {
+        protected $mModel = 'Ad';
+        
+        public function FindToShow() {
+            $query = $this->mDb->Prepare(
+                'SELECT
+                    *
+                FROM
+                    :ads
+                WHERE
+                    
+                ORDER BY
+                    RAND()
+                LIMIT 1;'
+            );
+            $res = $query->Execute();
+            if ( $res->Results() ) {
+                return New Ad( $res->FetchArray() );
+            }
+            return false;
+        }
+    }
+    
     class Ad extends Satori {
         protected $mDbTableAlias = 'ads';
         
@@ -6,6 +32,18 @@
             global $user;
             
             $this->Userid = $user->Id;
+        }
+        public function Relations() {
+            $this->Image = $this->HasOne( 'Image', 'Imageid' );
+        }
+        public function WasShown() {
+            if ( $this->Pageviewsremaining ) {
+                --$this->Pageviewsremaining;
+            }
+            if ( $this->Pageviewsremainingtoday ) {
+                --$this->Pageviewsremainingtoday;
+            }
+            $this->Save();
         }
     }
 ?>
