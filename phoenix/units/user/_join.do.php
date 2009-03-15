638e8a01f4115f1bb0b1ca8b70517bb6f1a51076
Petros Aggelatos <petrosagg@gmail.com>
2009-03-15 20:17:09 +0000

    Email validation

---
+++ phoenix/units/user/_join.do.php
@@ -0,0 +1,50 @@
+<?php
+    function UnitUserJoin( tText $username , tText $password , tText $email ) {
+        global $rabbit_settings;
+        global $libs;
+		
+        $username = $username->Get();
+        $password = $password->Get();
+        $email = $email->Get();
+
+        if ( !User_Valid( $username ) ) {
+            ?>alert( "Το όνομα χρήστη που επιλέξατε δεν είναι έγκυρο" );
+            Join.username.focus();
+            document.body.style.cursor = 'default';
+            $( 'div a.button' ).removeClass( 'button_disabled' );
+            Join.enabled = true;<?php
+            return;
+        }
+        if ( strlen( $password ) < 4 ) {
+            ?>alert( "Ο κωδικός που επιλέξατε δεν είναι αρκετά μεγάλος" );<?php
+            return;
+        }
+        if ( !ValidEmail( $email )  ) {
+            ?> $( $( 'form.joinform div > span' )[ 5 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );<?php
+            return;
+        }
+        $finder = New UserFinder(); 
+        if ( $finder->IsTaken( $username ) ) {
+            ?>if ( !Join.usernameexists ) {
+                Join.usernameexists = true;
+                $( $( 'form.joinform div > span' )[ 1 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity : "1" } , 700 );
+				Join.username.focus();
+				Join.username.select();
+				document.body.style.cursor = 'default';
+            }
+            <?php
+        }
+        else {
+			$libs->Load( 'rabbddit/helpers/validate' );
+            $newuser = New User();
+            $newuser->Name = $username;
+            $newuser->Subdomain = User_DeriveSubdomain( $username );
+            $newuser->Password = $password;
+            $newuser->Profile->Email = $email;
+            $newuser->Save();
+			?>location.href = '<?php
+			echo $rabbit_settings[ 'webaddress' ];
+			?>/?p=notvalidated';<?php
+        }
+    }
+?>
