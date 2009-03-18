28320466c9b872e27e58b84addaf4e0b8ea906cb
Petros Aggelatos <petrosagg@gmail.com>
2009-03-18 19:30:46 +0000

    Email validation (almost there)

--- phoenix/elements/validation/page.php
+++ phoenix/elements/validation/page.php
@@ -2,7 +2,10 @@
     class ElementValidationPage extends Element {
         public function Render() {
             global $user;
-            ?><p>Ο λογαριασμός σου δεν έχει ενεργοποιηθεί ακόμη. Θα πρέπει να χρησιμοποιήσεις τον σύνδεσμο στο e-mail σου για να τον ενεργοποιήσεις. Δεν έλαβες κάποιο e-mail? Έλεγξε τον φάκελο junk ή <a href="http://www.zino.gr/?p=revalidate">ζήτησέ μας να σου το ξαναστείλουμε</a></p><?php
+			$link =  $rabbit_settings[ 'webaddress' ] . '/?p=revalidate'
+            ?><p>Ο λογαριασμός σου δεν έχει ενεργοποιηθεί ακόμη. Θα πρέπει να χρησιμοποιήσεις τον σύνδεσμο στο e-mail σου για να τον ενεργοποιήσεις. Δεν έλαβες κάποιο e-mail? Έλεγξε τον φάκελο junk ή <a href="<?php
+				echo $link;
+			?>">ζήτησέ μας να σου το ξαναστείλουμε</a></p><?php
         }
 
     }
