7127a25f5300ce3184eb4097d8cc0d72a7561f16
Chris Pappas <chrispappas12@gmail.com>
2009-03-19 14:41:42 +0000

    frontpage images info

--- phoenix/elements/frontpage/image/comet.php
+++ phoenix/elements/frontpage/image/comet.php
@@ -4,7 +4,7 @@
             global $page;
             
             ob_start();
-            ?>Comet.Subscribe( 'imagesFrontpage' , Frontpage.Image.OnImageUpload );<?php
+            ?>Comet.Subscribe( 'ImagesFrontpage' , Frontpage.Image.OnImageUpload );<?php
             $page->AttachInlineScript( ob_get_clean() );
         }
     }
