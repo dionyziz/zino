aca953da7993b7d298e38956c76a1f7a32b9a3f8
Chris Pappas <chrispappas12@gmail.com>
2009-03-19 18:01:06 +0000

    frontpage photos cometized

--- phoenix/units/frontpage/image/new.plasma.php
+++ phoenix/units/frontpage/image/new.plasma.php
@@ -1,5 +1,17 @@
 <?php
     function UnitFrontpageImageNew( Image $image ) {
+        ?>var existnode = $( 'div.plist ul li a span img[alt="<?php
+        echo $image->User->Name;
+        ?>"] ).parent().parent().parent()[0];
+        if ( existnode ) {
+            alert( 'removing same user photo' );
+            $( existnode ).remove();
+        }
+        else {
+            alert( 'removing last image' );
+            $( 'div.plist ul li:last-child' ).remove();
+        }
+        <?php
         ?>alert( 'image id: <?php
         echo $image->Id;
         ?>, userid: <?php
