a4c97ce4390bf2f1696acc686404207c8ff93e01
Chris Pappas <chrispappas12@gmail.com>
2009-03-19 14:29:23 +0000

    frontpage images info

--- phoenix/elements/frontpage/image/comet.php
+++ phoenix/elements/frontpage/image/comet.php
@@ -2,11 +2,10 @@
     class ElementFrontpageImageComet extends Element {
         public function Render() {
             global $page;
-            /*
+            
             ob_start();
             ?>Comet.Subscribe( 'images/frontpage' , Frontpage.Image.OnImageUpload );<?php
             $page->AttachInlineScript( ob_get_clean() );
-            */
         }
     }
 ?>
