0d79ba53b4b6c9426128b8420c5fe9528bbb5ca6
Chris Pappas <chrispappas12@gmail.com>
2009-03-19 23:35:31 +0000

    comet debugging

--- phoenix/elements/frontpage/image/comet.php
+++ phoenix/elements/frontpage/image/comet.php
@@ -2,8 +2,9 @@
     class ElementFrontpageImageComet extends Element {
         public function Render() {
             global $page;
-            
+            die( 'calling comet element' );
             ob_start();
+            
             ?>Comet.Subscribe( 'FrontpageImageNew' );<?php
             $page->AttachInlineScript( ob_get_clean() );
         }
