b868bb71fd74010a7a4e8e070a535597a78f7e34
Chris Pappas <chrispappas12@gmail.com>
2009-03-20 14:58:33 +0000

    frontpage comments cometized

--- phoenix/units/frontpage/comment/new.plasma.php
+++ phoenix/units/frontpage/comment/new.plasma.php
@@ -6,6 +6,20 @@
         Element( 'frontpage/comment/view' , $comment );
         echo w_json_encode( ob_get_clean() );
         ?> );
-        $( 'div.latest div.comments div.list' ).prepend( newdiv );<?php
+        $( 'div.latest div.comments div.list' ).prepend( newdiv );
+        var height = newdiv.offsetHeight;
+        $( newdiv ).css( {
+            height: "0",
+            opacity: "0"
+        } ).animate( {
+            height: height,
+            opacity: "1"
+        } , 500 , 'linear' );
+        $( 'div.latest div.comments div.list:last-child' ).animate( {
+            height: "0",
+            opacity: "0"
+        } , 500 , 'linear' );<?php
+        
+        
     }
 ?>
