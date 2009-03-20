be2094bdecd85ac1ce5dc2eb1fb7d0940899cc42
Chris Pappas <chrispappas12@gmail.com>
2009-03-20 15:17:24 +0000

    frontpage comments cometized

--- phoenix/units/frontpage/comment/new.plasma.php
+++ phoenix/units/frontpage/comment/new.plasma.php
@@ -1,15 +1,14 @@
 <?php
     function UnitFrontpageCommentNew( Comment $comment ) {
         ?>var newdiv = document.createElement( 'div' );
-        $( newdiv ).hide().html( <?php
+        $( newdiv ).hide().css( 'opacity' , '0' ).html( <?php
         ob_start();
         Element( 'frontpage/comment/view' , $comment );
         echo w_json_encode( ob_get_clean() );
         ?> );
         $( 'div.latest div.comments div.list' ).prepend( newdiv );
         var targetheight = newdiv.offsetHeight;
-        $( newdiv ).show( 1200 );
-        /*
+        //$( newdiv ).show( 1200 );
         $( newdiv ).hide().css( {
             'height': '0',
             'opacity': '0'
@@ -17,7 +16,6 @@
             height: targetheight,
             opacity: "1"
         } , 500 , 'linear' );
-        */
         $( 'div.latest div.comments div.list>div:last-child' ).animate( {
             height: "0",
             opacity: "0"
