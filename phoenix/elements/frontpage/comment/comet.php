1a947140a5f22a09f4a6e6f710fbf9cfeb1a9a2d
Chris Pappas <chrispappas12@gmail.com>
2009-03-20 14:16:01 +0000

    frontpage comments cometized

--- phoenix/elements/frontpage/comment/comet.php
+++ phoenix/elements/frontpage/comment/comet.php
@@ -2,7 +2,7 @@
     class ElementFrontpageCommentComet extends Element {
         public function Render() {
             global $page;
-            die( 'comet' );
+            
             ob_start();
             ?>Comet.Subscribe( 'FrontpageCommentNew' );<?php
             $page->AttachInlineScript( ob_get_clean() );
