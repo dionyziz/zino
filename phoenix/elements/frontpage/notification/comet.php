873a7c00adab8720ab92e5da7eaa917c18815303
Chris Pappas <chrispappas12@gmail.com>
2009-03-21 00:33:34 +0000

    frontpage notifications cometizing

--- phoenix/elements/frontpage/notification/comet.php
+++ phoenix/elements/frontpage/notification/comet.php
@@ -4,7 +4,6 @@
             global $page;
             global $user;
             
-            die( 'comet' );
             ob_start();
             ?>Comet.Subscribe( 'FrontpageNotificationNew<?php
             echo $user->Id;
