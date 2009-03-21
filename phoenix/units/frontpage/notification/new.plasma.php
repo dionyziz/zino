dde4df359297f4f65a57de4cdd449c494b943d7b
Chris Pappas <chrispappas12@gmail.com>
2009-03-21 00:48:16 +0000

    frontpage notifications cometizing

--- phoenix/units/frontpage/notification/new.plasma.php
+++ phoenix/units/frontpage/notification/new.plasma.php
@@ -1,6 +1,5 @@
 <?php
     function UnitFrontpageNotificationNew( Notification $notif ) {
-        die( 'plasma running' );
         ?>alert( 'notification creation to user <?php
         echo $notif->ToUser->Name;
         ?>' );<?php
