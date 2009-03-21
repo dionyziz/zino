8ae1911f1d23fbff420a47868b549ed09f968def
Petros Aggelatos <petrosagg@gmail.com>
2009-03-21 00:30:13 +0000

    Email validation (almost there)

--- phoenix/elements/validation/page.php
+++ phoenix/elements/validation/page.php
@@ -3,14 +3,17 @@
         public function Render( tInteger $userid ) {
             global $user;
 			$userid = $userid->Get();
-            ?><p>Ο λογαριασμός σου δεν έχει ενεργοποιηθεί ακόμη. Θα πρέπει να χρησιμοποιήσεις τον σύνδεσμο στο e-mail σου για να τον ενεργοποιήσεις. Δεν έλαβες κάποιο e-mail? Έλεγξε τον φάκελο junk ή 
-			<form action="do/user/revalidate" method="post">
-				<input type="submit" value="ζήτησέ μας να σου το ξαναστείλουμε" />
-				<input name="userid" type="hidden" value="<?php
-					echo $userid;
-				?>" />
-			</form>
-			</p><?php
+            ?><div class="error">
+				<h2>Μη επιβεβαιωμένο email</h2>
+				<p>Ο λογαριασμός σου δεν έχει ενεργοποιηθεί ακόμη. Θα πρέπει να χρησιμοποιήσεις τον σύνδεσμο στο e-mail σου για να τον ενεργοποιήσεις. Δεν έλαβες κάποιο e-mail? Έλεγξε τον φάκελο junk ή 
+				<form action="do/user/revalidate" method="post">
+					<input type="submit" value="ζήτησέ μας να σου το ξαναστείλουμε" />
+					<input name="userid" type="hidden" value="<?php
+						echo $userid;
+					?>" />
+				</form>
+				</p>
+			</div><?php
         }
 
     }
