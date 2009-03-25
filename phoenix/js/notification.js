2907f019be5351af2a37de492b7b6d9595e6032a
Chris Pappas <chrispappas12@gmail.com>
2009-03-25 18:21:53 +0000

    jquery upgrading

---
+++ phoenix/js/notification.js
@@ -0,0 +1,124 @@
+var Notification = {
+	VisibleNotifs : $( 'div.frontpage div.notifications div.list div.event' ).length,
+	Visit : function( url , typeid , eventid , commentid ) {
+        Notification.DecrementCount();
+		if ( typeid == 3 ) {
+			document.location.href = url;
+		} 
+		else {
+			Coala.Warm( 'notification/delete' , { eventid : eventid , relationnotif : false } );
+			document.location.href = url;
+		}
+		return false;
+	},
+	Delete : function( eventid ) {
+		$( 'div#event_' + eventid ).animate( { opacity : "0" , height : "0" } , 400 , function() {
+			$( this ).remove();
+			if ( $( 'div.notifications div.list div.event' ).length === 0 ) {
+				$( 'div.notifications' ).remove();
+			}
+		} );
+		Coala.Warm( 'notification/delete' , { eventid : eventid , relationnotif : false } );
+        Notification.DecrementCount(); 
+		return false;
+	},
+    DecrementCount: function () {
+        var count = document.title.split( '(' )[ 1 ].split( ')' )[ 0 ];
+        
+        if ( count == '10+' ) {
+            return;
+        }
+        --count;
+        if ( count === 0 ) {
+            document.title = 'Zino';
+        }
+        else {
+            document.title = 'Zino (' + count + ')';
+        }
+    },
+	AddFriend : function( eventid , theuserid ) {
+		$( 'div#addfriend_' + theuserid  + ' a' )
+		.fadeOut( 400 , function() {
+			$( this )
+			.parent()
+			.empty()
+			.append( document.createTextNode( 'Έγινε προσθήκη' ) );
+		} );
+		Coala.Warm( 'notification/addfriend' , { userid : theuserid } );
+		Coala.Warm( 'notification/delete' , { eventid : eventid , relationnotif : false } );
+        Notification.DecrementCount();
+		return false;
+	},
+	AddNotif : function( node ) {
+		if ( Frontpage.Notif.VisibleNotifs === 0 ) {
+			Frontpage.Notif.VisibleNotifs++;
+			var notifscontainer = document.createElement( 'div' );
+			var list = document.createElement( 'div' );
+			var h3 = document.createElement( 'h3' );
+			var expand = document.createElement( 'div' );
+			var link = document.createElement( 'a' );
+
+			$( expand ).addClass( "expand" ).append( link );
+			$( h3 ).append( document.createTextNode( "Ενημερώσεις" ) );
+			$( list ).addClass( "list" );
+			$( notifscontainer ).addClass( "notifications" )
+			.append( h3 ).append( list ).append( expand );
+			$( 'div.content div.frontpage' ).prepend( notifscontainer );
+			var notiflistheight = $( notiflist )[ 0 ].offsetHeight;
+			$( link ).css( "background-position: 4px -1440px" ).attr( {
+				title : "Απόκρυψη",
+				href : ""
+			} )
+			.click( function() {
+				var notiflist = $( 'div.notifications div.list' )[ 0 ];
+				if ( $( notiflist ).css( 'display' ) == "none" ) {
+					$( link  )
+					.css( "background-position" , "4px -1440px" )
+					.attr( {
+						title : 'Απόκρυψη'
+					} );
+					$( notiflist ).show().animate( { height : notiflistheight } , 400 );
+				}
+				else {
+					$( link )
+					.css( "background-position" , "4px -1252px" )
+					.attr( {
+						title : 'Εμφάνιση'
+					} );
+					$( notiflist ).animate( { height : "0" } , 400 , function() {
+						$( notiflist ).hide();
+					} );
+				}
+				return false;
+			} );
+
+		}
+		else if ( Frontpage.Notif.VisibleNotifs < 5 ) {
+			Frontpage.Notif.VisibleNotifs++;
+		}
+		else {
+			$( 'div.frontpage div.notifications div.list>div:last-child' ).animate( {
+				opacity : "0",
+				height: "0"
+			} , 400 , function() {
+				$( this ).remove();
+			} );
+		}
+		Frontpage.Notif.Show( node );
+	},
+	Show : function( node ) {
+		$( 'div.frontpage div.notifications div.list' ).prepend( node );
+		var targetheight = $( 'div.frontpage div.notifications div.list div div.event' )[ 0 ].offsetHeight;
+		node.style.height = '0';
+		$( node ).css( 'opacity' , '0' ).animate( {
+			height: targetheight,
+			opacity: "1"
+		} , 400 , 'linear' )
+		.find( "div.event" ).mouseover( function() {
+			$( this ).css( "border" , "1px dotted #666" ).css( "padding" , "4px" );
+		} )
+		.mouseout( function() {
+			$( this ).css( "border" , "0" ).css( "padding" , "5px" );
+		} );
+	}
+};
--- phoenix/units/frontpage/notification/new.plasma.php
+++ phoenix/units/frontpage/notification/new.plasma.php
@@ -6,7 +6,7 @@
 	Element( 'notification/view' , $notif ); 
 	echo w_json_encode( ob_get_clean() );
 	?> );
-	Frontpage.Notif.AddNotif( notifcontent );<?php
+	Notification.AddNotif( notifcontent );<?php
         return $notif->ToUser->Id; 
     }
 ?>
