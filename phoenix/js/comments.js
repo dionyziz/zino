7413703a33cc2c5e481d443ba04019bc6d5baf93
Chris Pappas <chrispappas12@gmail.com>
2009-03-26 07:15:30 +0000

    jquery upgrading

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -233,10 +233,33 @@ var Comments = {
     }
 };
 $( function() {
+        if ( $.browser.msie ) {
+            $( "div.comments div[class='comment'] div.bottom a" ).click( function() {
+                var parent = $( this ).parent().parent();
+                var indent = parseInt( $( parent ).css( 'paddingLeft' ), 10 )/20;
+                var id = $( parent ).attr( 'id' ).substring( 8 );
+                var kimeno = $( parent ).find( "div.text" );
+                var wid = kimeno.get( 0 ).offsetWidth-20;
+                kimeno.css( "width", wid-indent*20+'px' );
+                Comments.ToggleReply( id , indent );
+                return false;
+            } );
+        }
+        else {
+            $( "div.comments div[class='comment'] div.bottom a" ).click( function() {
+                var parent = $( this ).parent().parent();
+                var indent = parseInt( $( parent ).css( 'paddingLeft' ), 10 )/20;
+                var id = $( parent ).attr( 'id' ).substring( 8 );
+                var kimeno = $( parent ).find( "div.text" );
+                var wid = parseInt( kimeno.css( "width" ), 10 );
+                kimeno.css( "width", wid-indent*20+'px' );
+                Comments.ToggleReply( id , indent );
+                return false;
+            } );
+        }
+/*
 		$( "div.comments div[class='comment'] div.bottom a" ).click( function() {
             var parent = $( this ).parent().parent();
-            
-            var id = $( parent ).attr( 'id' ).substring( 8 );
 			var indent = parseInt( $( parent ).css( 'paddingLeft' ), 10 )/20;
             var id = $( parent ).attr( 'id' ).substring( 8 );
             var kimeno = $( parent ).find( "div.text" );
