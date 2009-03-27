783e502211c149ab08cd51d1e02b4228c9330418
Chris Pappas <chrispappas12@gmail.com>
2009-03-27 19:19:40 +0000

    jquery upgrading

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -238,9 +238,10 @@ $( function() {
                 var parent = $( this ).parent().parent();
                 var indent = parseInt( $( parent ).css( 'paddingLeft' ), 10 )/20;
                 var id = $( parent ).attr( 'id' ).substring( 8 );
-                var kimeno = $( parent ).find( "div.text" );
+                /*var kimeno = $( parent ).find( "div.text" );
                 var wid = kimeno.get( 0 ).offsetWidth-20;
                 kimeno.css( "width", wid-indent*20+'px' );
+                */
                 Comments.ToggleReply( id , indent );
                 return false;
             } );
@@ -250,9 +251,11 @@ $( function() {
                 var parent = $( this ).parent().parent();
                 var indent = parseInt( $( parent ).css( 'paddingLeft' ), 10 )/20;
                 var id = $( parent ).attr( 'id' ).substring( 8 );
+                /*
                 var kimeno = $( parent ).find( "div.text" );
                 var wid = parseInt( kimeno.css( "width" ), 10 );
                 kimeno.css( "width", wid-indent*20+'px' );
+                */
                 Comments.ToggleReply( id , indent );
                 return false;
             } );
