4001d2e6444930810cae97204851f6f79bf52517
Chris Pappas <chrispappas12@gmail.com>
2009-03-29 11:21:29 +0000

    jquery upgrading

--- phoenix/js/comments.js
+++ phoenix/js/comments.js
@@ -264,11 +264,6 @@ var Comments = {
             $( "div.comments div[id^='comment_'] span.time" ).each( function() {
                 var commdate = $( this ).text();
                 var parent = $( this ).parent().parent();
-                var lmargin = Comments.FindLeftPadding( parent );
-                var nextlmargin = Comments.FindLeftPadding( $( parent ).next( "div[id^='comment_']" )[ 0 ] );
-                if ( lmargin != nextlmargin ) {
-                    lmargin = 0;
-                }
                 $( this ).empty()
                 .css( 'margin-right' , lmargin + 'px' )
                 .text( greekDateDiff( dateDiff( commdate , nowdate ) ) )
