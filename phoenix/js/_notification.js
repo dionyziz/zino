a3b63e13e3e39f6acfc7ab592617da18b0b49290
Chris Pappas <chrispappas12@gmail.com>
2009-03-30 19:20:35 +0000

    commet notifications

--- phoenix/js/_notification.js
+++ phoenix/js/_notification.js
@@ -40,7 +40,10 @@ var Notification = {
             var lastnodeid = $( '#inotifs div.event:last-child' ).attr( "id" );
             var id = lastnodeid.substr( 6 );
             alert( id );
-            
+            Coala.Warm( "notification/find" , {
+                notifid : id,
+                limit : 3
+            } );
 
         }
         
