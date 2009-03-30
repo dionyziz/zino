526b22291af31e68db0516cd6966d0fdc4dc8b30
Chris Pappas <chrispappas12@gmail.com>
2009-03-30 21:46:52 +0000

    commet notifications

--- phoenix/units/notification/find.do.php
+++ phoenix/units/notification/find.do.php
@@ -10,12 +10,14 @@
         $notifid = $notifid->Get();
         $limit = $limit->Get();
         $finder = New NotificationFinder();
-        $notifs = $finder->FindByUserAfterId( $user , $notifid , $limit );
-        ?>
+        /*$notifs = $finder->FindByUserAfterId( $user , $notifid , $limit );
+        
+
         alert( $( <?php
         ob_start();
         Element( "notification/list" , $notifs );
         echo w_json_encode( ob_get_clean() );
         ?> ) );<?php
+        */
     }
 ?>
