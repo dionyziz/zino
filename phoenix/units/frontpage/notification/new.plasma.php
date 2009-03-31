62a16b314dd294f04c25c4d51350cf812a41f11e
Chris Pappas <chrispappas12@gmail.com>
2009-03-31 22:33:42 +0000

    commet notifications

--- phoenix/units/frontpage/notification/new.plasma.php
+++ phoenix/units/frontpage/notification/new.plasma.php
@@ -5,7 +5,6 @@
         Element( 'notification/view' , $notif ); 
         echo w_json_encode( ob_get_clean() );
         ?> );
-        alert( notifcontent );
         Notification.AddNotif( notifcontent );<?php
         
         return $notif->ToUser->Id; 
