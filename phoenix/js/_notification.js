08ed8a79991e79b8f0db251e1a919a8db747e03a
Chris Pappas <chrispappas12@gmail.com>
2009-03-31 21:42:02 +0000

    commet notifications

--- phoenix/js/_notification.js
+++ phoenix/js/_notification.js
@@ -138,7 +138,7 @@ var Notification = {
 		Notification.Show( node );
 	},
 	Show : function( node ) {
-        alert( node.id );
+        alert( $( node ).attr( "id" ) );
 		$( 'div.notifications div.list' ).prepend( node );
 		var targetheight = $( 'div.frontpage div.notifications div.list div.event' )[ 0 ].offsetHeight;
 		node.style.height = '0';
