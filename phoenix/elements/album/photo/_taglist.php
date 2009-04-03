d058e30bf1272e135d1dc7a217c71324e7fa4d68
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-04-03 18:19:54 +0000

    tag album aka taglist

--- phoenix/elements/album/photo/_taglist.php
+++ phoenix/elements/album/photo/_taglist.php
@@ -6,6 +6,10 @@
             global $rabbit_settings; 
             global $water;
             
+            $libs->Load("image/tag");
+	        $libs->Load("kolaz/kolaz");	        
+	        $libs->Load("user/user");
+            
             $newuser = new User( $id->Get() );
             $pageno = $pageno->Get();
             if ( $pageno <= 0 ) {
@@ -18,6 +22,48 @@
             }
             
             Element( 'user/sections', 'album' , $newuser );
+            
+            ?><div id="photolist"<?php
+            
+            $Tagfinder = new ImageTagFinder();
+            $tags = $Tagfinder->FindByPersonId( $newuser->Id ); 
+                        
+            $ids = array();
+            foreach ( $tags as $tag ) { 
+                $ids[] = $tag->Imageid;
+            }             
+            
+            $imageFinder = new ImageFinder();//Find image owners ids
+            $images = $imageFinder->FindByIds( $ids );
+                   
+            ?><ul><?php
+                foreach( $images as $image ) {
+                    ?><li><?php
+                    Element( 'album/photo/small' , $image , false , true );
+                    ?></li><?php
+                }
+            ?></ul><?php
+            
+            ?></div><?php
+            
+            /*<div class="eof"></div>
+            <div class="pagifyimages"><?php
+
+            $link = '?p=album&id=' . $album->Id . '&pageno=';
+            $total_pages = ceil( $album->Numphotos / 20 );
+            $text = '( ' . $album->Numphotos . ' Φωτογραφί' ;
+            if ( $album->Numphotos == 1 ) {
+                $text .= 'α';
+            }
+            else {
+                $text .= 'ες';
+            }
+            $text .= ' )';
+            Element( 'pagify', $pageno, $link, $total_pages, $text );
+            ?></div>
+            </div><div class="eof"></div><?php*/
+            
+            
         }
     }
 ?>
