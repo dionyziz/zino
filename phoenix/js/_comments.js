611f29deca9efc4e2a9040e7b194706028a0eb40
Chris Pappas <chrispappas12@gmail.com>
2009-04-05 07:14:55 +0000

    comments changes

--- phoenix/js/_comments.js
+++ phoenix/js/_comments.js
@@ -116,7 +116,6 @@ var Comments = {
 	},
 	Reply : function( nodeid, indent ) {
 		// Atm prefer marginLeft. When the comment is created it will be converted to paddingLeft. Looks better
-        $( "#comment_" + nodeid + " div.text" ).css( "font-weight" , "400" );
 		var temp = $( "div.newcomment:first" ).clone( true ).css( { marginLeft : (indent+1)*20 + 'px', opacity : 0 } ).attr( 'id', 'comment_reply_' + nodeid );
 		temp.find( "div.toolbox span.time" ).css( { marginRight : (indent+1)*20 + 'px' } );
 		$( temp ).find( "div.toolbox" ).show().end()
@@ -138,6 +137,7 @@ var Comments = {
         
         } ) 
         .blur( function() {
+            $( "#comment_" + nodeid + " div.text" ).css( "font-weight" , "400" );
             if ( this.value  === '' ) {
                 this.value = "Πρόσθεσε ένα σχόλιο..."; 
                 $( this ).css( "color" , "#666" );
