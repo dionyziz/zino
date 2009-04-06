33951db9d80b4cfac0aba4c0b84b08b60e69b9f9
Chris Pappas <chrispappas12@gmail.com>
2009-04-06 23:49:53 +0000

    comments optimizing

--- phoenix/js/default.js
+++ phoenix/js/default.js
@@ -2,20 +2,21 @@ function GetUsername() {
 	var oldtime = new Date().getTime();
     var username = false;
 	if ( $( 'a.profile' )[ 0 ] ) {
-		if ( $( 'a.profile span.imageview img' )[ 0 ] ) {
-			username = $( 'a.profile span.imageview img' ).attr( 'alt' ); // get the username of the logged in user from the banner
-		}
-		else {
+		//if ( $( 'a.profile span.imageview img' )[ 0 ] ) {
+			//username = $( 'a.profile span.imageview img' ).attr( 'alt' ); // get the username of the logged in user from the banner
+		//}
+		//else {
 			// for users without avatar
 			username = $( 'a.profile' ).text();
-		}
+		//}
 	}
 	else {
 		username = false;
 	}
     var newtime = new Date().getTime();
     alert( newtime - oldtime );
-	return username;
+	alert( username );
+    return username;
 }
 $( function() {
     /*if ( $.browser.mozilla ) {
