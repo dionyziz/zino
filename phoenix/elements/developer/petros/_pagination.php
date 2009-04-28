234f5a6581296457c45d3b523414554702f381ef
Petros Aggelatos <petrosagg@gmail.com>
2009-04-28 18:34:05 +0000

    pagination testing

--- phoenix/elements/developer/petros/_pagination.php
+++ phoenix/elements/developer/petros/_pagination.php
@@ -1,6 +1,8 @@
 <?php
     class ElementDeveloperPetrosPagination extends Element {
         public function Render() {
+			global $libs;
+			$libs->Load( 'comment' );
 			$finder = New CommentFinder();
 			$speccomment = New Comment( 1149616 );
 			$entity = New Image( 100589 );
