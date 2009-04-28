5e73523e2a1285112b5b21e8f1b387b5ac92ddb2
Petros Aggelatos <petrosagg@gmail.com>
2009-04-28 18:35:46 +0000

    pagination testing

--- phoenix/elements/developer/petros/_pagination.php
+++ phoenix/elements/developer/petros/_pagination.php
@@ -3,12 +3,12 @@
         public function Render() {
 			global $libs;
 			$libs->Load( 'comment' );
+			die( 'test' );
 			$finder = New CommentFinder();
 			$speccomment = New Comment( 1149616 );
 			$entity = New Image( 100589 );
 			$findnear = $finder->FindNear( $entity, $speccomment );
 			var_dump( $findnear );
-			die( 'test' );
         }
     }
 ?>
