52469bb4dbf3c6d3974c58af15bab1c182563065
Dionysis Zindros <dionyziz@gmail.com>
2009-05-15 07:58:08 +0000

    textual changes

--- phoenix/elements/validation/page.php
+++ phoenix/elements/validation/page.php
@@ -5,11 +5,17 @@
 			$userid = $userid->Get();
             $firsttime = $firsttime->Get();
             ?><div class="error">
-				<h2>Μη επιβεβαιωμένο email</h2>
-				<p>Ο λογαριασμός σου δεν έχει ενεργοποιηθεί ακόμη. Θα πρέπει να χρησιμοποιήσεις τον σύνδεσμο στο e-mail σου για να τον ενεργοποιήσεις. Δεν έλαβες κάποιο e-mail? Έλεγξε τον φάκελο junk ή ζήτησέ μας να σου το ξαναστείλουμε.<?php
+				<h2>Καλώς ήρθες στο Zino! Έλεγξε τα e-mail σου</h2>
+				<p>Ο λογαριασμός σου είναι σχεδόν έτοιμος. Ένας σύνδεσμος για ενεργοποίηση έχει 
+                   σταλεί στην διεύθυνση e-mail που δήλωσες μαζί με οδηγίες για να ενεργοποιήσεις 
+                   τον λογαριασμό σου.<?php
                 if ( !$firsttime ) {
-                    ?><form action="do/user/revalidate" method="post">
-					<input type="submit" value="Αποστολή email" />
+                    ?>
+                    <br /><br />
+                    Αν δεν έλαβες κάποιο μήνυμα, έλεγξε τον φάκελο "junk" ή "ανεπιθύμητη 
+                    αλληλογραφία", ή ζήτησέ μας να σου το ξαναστείλουμε.
+                    <form action="do/user/revalidate" method="post">
+					<input type="submit" value="Ξαναστείλτε μου το e-mail" />
 					<input name="userid" type="hidden" value="<?php
 						echo $userid;
 					?>" />
