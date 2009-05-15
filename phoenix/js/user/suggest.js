4642b48bce4a2dc87c71b8bde44f049fd2e602ac
Dimitris Karagasidis <dkaragasidis@gmail.com>
2009-05-15 16:13:45 +0000

    suggestions fix

--- phoenix/js/user/suggest.js
+++ phoenix/js/user/suggest.js
@@ -80,7 +80,7 @@ var Suggest = {
             lis.removeClass( 'selected' ).next().addClass( 'selected' ).get( 0 ).scrollIntoView( false );
 			Suggest.allowHover = false;
 			setTimeout( function() { Suggest.allowHover = true; }, 15 );
-			alert( $("li.selected").html() );
+			alert( $("div.add.hobbies ul li.selected").html() );
         }
         else if ( event.keyCode == 38 ) { // up
             if ( lis.length === 0 ) {
