d094d7a7dad6ce2bd81a3113c77ee1f87eeedc66
Chris Pappas <chrispappas12@gmail.com>
2009-05-15 11:33:06 +0000

    comet comments

--- phoenix/js/_frontpage.js
+++ phoenix/js/_frontpage.js
@@ -357,7 +357,7 @@ var Frontpage = {
             .animate( {
                 //height: targetheight,
                 opacity: "1"
-            } , 1000 , 'linear' );
+            } , 700 , 'linear' );
             $( 'div.latest div.comments div.list>div:last-child' ).animate( {
                 height: "0",
                 opacity: "0"
