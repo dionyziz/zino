e1731dd4d63cdbc408c54e31147801ec6d651802
Chris Pappas <chrispappas12@gmail.com>
2009-05-15 10:57:27 +0000

    comet comments

--- phoenix/js/_frontpage.js
+++ phoenix/js/_frontpage.js
@@ -353,9 +353,9 @@ var Frontpage = {
             setTimeout( "Frontpage.Comment.Animating = false;Frontpage.Comment.NextComment()" , timerint );
             $( 'div.latest div.comments div.list' ).prepend( node );
             var targetheight = $( 'div.latest div.comments div.list div.event' )[ 0 ].offsetHeight;
-            $( node ).css( "height" , "0" )
+            $( node )/*.css( "height" , "0" )*/
             .animate( {
-                height: targetheight,
+                //height: targetheight,
                 opacity: "1"
             } , 50 , 'linear' );
             $( 'div.latest div.comments div.list>div:last-child' ).animate( {
