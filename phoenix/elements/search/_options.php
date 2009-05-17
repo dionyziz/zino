d27c2db073e8c00c231c8df015332dae134e5a4f
Dimitris Karagasidis <dkaragasidis@gmail.com>
2009-05-17 16:59:22 +0000

    search css fix for IE8 and FF3 on linux

--- phoenix/elements/search/_options.php
+++ phoenix/elements/search/_options.php
@@ -132,7 +132,7 @@
                         }
                     ?></select>
                 </div>
-                <div style="width:120px;margin:auto;"><input type="submit" value="Ψάξε!" class="submit" /></div>
+                <div style="width:120px;margin:auto;height:110px;"><input type="submit" value="Ψάξε!" class="submit" /></div>
             </form>
             </div>
             <i class="bl"></i>
