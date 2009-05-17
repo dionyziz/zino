8e885cc1642f7cd29fb957211095661243530622
Dimitris Karagasidis <dkaragasidis@gmail.com>
2009-05-17 17:29:49 +0000

    search css fix for IE8 and FF3 on linux

--- phoenix/elements/search/_options.php
+++ phoenix/elements/search/_options.php
@@ -7,7 +7,7 @@
             global $page;
 ?>
         <h2>Αναζήτησε άτομα</h2>
-        <div class="ybubble">
+        <div class="ybubble" style="margin:0 auto 0;">
             <i class="tl"></i>
             <i class="tr"></i>
             <div class="body">
