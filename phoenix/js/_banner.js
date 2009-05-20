6cd6a6da63e6eaaeddcd9757e724d355c2a80183
Chris Pappas <chrispappas12@gmail.com>
2009-05-20 23:08:00 +0000

    new banner/footer

--- phoenix/js/_banner.js
+++ phoenix/js/_banner.js
@@ -4,6 +4,10 @@ var Banner = {
             if ( $( this ).attr( "value" ) == 'ψευδώνυμο' ) {
                 $( this ).css( 'color' , '#000' ).attr( 'value' , '' );
             }
+        } ).blur( function() {
+            if ( $( this ).attr( "value" ) == '' ) {
+                $( this ).css( 'color' , '#aaa' ).attr( 'value' , 'κωδικός' );
+            }
         } );
     }
 };
