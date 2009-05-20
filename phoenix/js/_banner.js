ec63341aa0186de40c3f9d0d5e8639431f7785c8
Chris Pappas <chrispappas12@gmail.com>
2009-05-20 22:53:49 +0000

    new banner/footer

--- phoenix/js/_banner.js
+++ phoenix/js/_banner.js
@@ -1,6 +1,8 @@
 var Banner = {
     OnLoad : function() {
+     
         $( "#lusername" ).focus( function() {
+            alert( 'focus' );
             if ( $( this ).attr( "value" ) == 'ψευδώνυμο' ) {
                 $( this ).css( 'color' , '#000' ).attr( 'value' , '' );
             }
