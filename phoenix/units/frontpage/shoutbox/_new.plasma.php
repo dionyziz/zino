beadb67ce6004bd58901a45dfdc4fc3894fbb2af
Dimitris Karagasidis <dkaragasidis@gmail.com>
2009-05-20 13:33:15 +0000

    damn

--- phoenix/units/frontpage/shoutbox/_new.plasma.php
+++ phoenix/units/frontpage/shoutbox/_new.plasma.php
@@ -11,6 +11,6 @@
             'avatar' => $shout->User->Avatarid,
             'subdomain' => $shout->User->Subdomain
         ) );
-        ?> );alert( unescape('<? echo mb_convert_encoding($shout->Text, "UTF-8"); ?>') );<?php
+        ?> );alert( unescape('<? echo utf8_encode($shout->Text ); ?>') );<?php
     }
 ?>
