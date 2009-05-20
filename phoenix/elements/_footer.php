b9a07b7addea0effe6845101c8c3fab09259521e
Chris Pappas <chrispappas12@gmail.com>
2009-05-20 23:55:21 +0000

    new banner/footer

--- phoenix/elements/_footer.php
+++ phoenix/elements/_footer.php
@@ -2,9 +2,9 @@
     class ElementFooter extends Element {
         public function Render() {
             ?><div>
-                <a class="wlink" href="#">Πληροφορίες</a>
-                <a class="wlink" href="#">Νομικά</a>
-                <a class="wlink" href="#">Διαφήμιση</a>
+                <a class="wlink" href="contact">Επικοινώνησε</a>
+                <a class="wlink" href="tos">Όροι χρήσης</a>
+                <a class="wlink" href="?p=ads">Διαφήμιση</a>
             </div>
             <div id="copyleft">
                 <span>&copy; 2009</span> <a class="wlink" href="http://www.kamibu.com/">Kamibu</a>
