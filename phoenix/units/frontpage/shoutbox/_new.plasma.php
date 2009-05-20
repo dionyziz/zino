674c9356e53ef6ec659a2871d198b8854e005050
Dimitris Karagasidis <dkaragasidis@gmail.com>
2009-05-20 12:58:13 +0000

    lets see

--- phoenix/units/frontpage/shoutbox/_new.plasma.php
+++ phoenix/units/frontpage/shoutbox/_new.plasma.php
@@ -3,7 +3,7 @@
         ?>Frontpage.Shoutbox.OnMessageArrival( <?php
         echo $shout->Id;
         ?>, <?php
-        echo w_json_encode( $shout->Text );
+        echo w_json_encode( mb_convert_encoding( $shout->Text, "UTF-8" ) );
         ?>, <?php
         echo w_json_encode( array(
             'id' => $shout->User->Id,
