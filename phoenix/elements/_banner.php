20d576bc726e5af8aa466379199c4effa3e83c56
Chris Pappas <chrispappas12@gmail.com>
2009-05-20 18:02:25 +0000

    new banner/footer

--- phoenix/elements/_banner.php
+++ phoenix/elements/_banner.php
@@ -9,7 +9,9 @@
             ?>
            <div id="lbanner">
                 <h1>
-                    <a href="http://www.zino.gr">
+                    <a href="<?php
+                    echo $rabbit_settings[ 'webaddress' ];
+                    ?>">
                         <img src="http://static.zino.gr/phoenix/logo.png" />
                     </a>
                 </h1>
@@ -18,14 +20,33 @@
            </div>
            <div id="mbanner">
                 <div id="loggedinmenu">
-                    <span class="avatar50">
-                        <img src="http://static.zino.gr/phoenix/dionyziz.png" alt="dionyziz" title="dionyziz" />
-                        <span class="rounded50"></span>
-                    </span>
-                    <a href="#" class="bannerinlink">Προφίλ</a>
-                    <a href="#" class="bannerinlink">Ρυθμίσεις</a>
+                    <span class="avatar50"><?php
+                        if ( $user->Avatar->Id > 0 ) {
+                            Element( 'image/view' , $user->Avatar->Id , $user->Id , $user->Avatar->Width , $user->Avatar->Height , IMAGE_CROPPED_100x100 , '' , $user->Name , '' , true , 50 , 50 , 0 );
+
+                        }
+                        else {
+                            ?><img src="http://static.zino.gr/phoenix/anonymous100.jpg" style="width:50px;height:50px;" alt="<?php
+                            echo htmlspecialchars( $user->Name );
+                            ?>" title="<?php
+                            echo htmlspecialchars( $user->Name );
+                            ?>" /><?php
+                        }
+                    ?></span>
+                    
+                    <a href="<?php
+                    ob_start();
+                    Element( 'user/url' , $user->Id , $user->Subdomain );
+                    echo htmlspecialchars( ob_get_clean() );
+                    ?>" class="bannerinlink">Προφίλ</a>
+                    
+                    <a href="settings" class="bannerinlink">Ρυθμίσεις</a>
+                    
                     <a href="#" class="bannerinlink">Βρες φίλους</a>
-                    <a href="#" class="bannerinlink">Έξοδος</a>
+                    
+                    <form method="post" action="do/user/logout">
+                        <a href="#" class="bannerinlink" onclick="this.parentNode.submit();return false;">Έξοδος</a>
+                    </form>
                 </div>
            </div><?php
        }
