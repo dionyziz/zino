6038f32bfec87020825c1debec153c2ec4048482
Dimitris Karagasidis <dkaragasidis@gmail.com>
2009-05-20 12:44:44 +0000

    masked new.plasma.php

---
+++ phoenix/units/frontpage/shoutbox/_new.plasma.php
@@ -0,0 +1,16 @@
+<?php
+    function UnitFrontpageShoutboxNew( Shout $shout ) {
+        ?>Frontpage.Shoutbox.OnMessageArrival( <?php
+        echo $shout->Id;
+        ?>, <?php
+        echo w_json_encode( $shout->Text );
+        ?>, <?php
+        echo w_json_encode( array(
+            'id' => $shout->User->Id,
+            'name' => $shout->User->Name,
+            'avatar' => $shout->User->Avatarid,
+            'subdomain' => $shout->User->Subdomain
+        ) );
+        ?> );<?php
+    }
+?>
