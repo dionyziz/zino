3c8898235d5f05672f3bd8df0e80b217836ff13b
Chris Pappas <chrispappas12@gmail.com>
2009-05-23 23:16:25 +0000

    zino ads

--- phoenix/js/album/photo/view.js
+++ phoenix/js/album/photo/view.js
@@ -106,5 +106,10 @@ var PhotoView = {
                 return;
             }
         });
+    },
+    OnLoad : function() {
+        Coala.Cold( 'admanager/showad', { f: function ( html ) {
+            $( 'div.ads' )[ 0 ].innerHTML = html;
+        } } );
     }
 };
