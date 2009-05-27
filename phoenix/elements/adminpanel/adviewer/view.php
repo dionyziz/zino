aa455bd45ceb4464e9d194bb32b6fe1d10c205d5
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-05-27 13:08:40 +0000

    adviewer element

---
+++ phoenix/elements/adminpanel/adviewer/view.php
@@ -0,0 +1,55 @@
+<?php    
+    class ElementAdminpanelAdviewerssView extends Element {
+        public function Render() {
+	        global $page;
+	        global $user;
+	        global $libs;
+	        
+	        if( ! $user->HasPermission( PERMISSION_ADMINPANEL_VIEW ) ) {
+	            ?> Permission Denied <?php
+	            return;
+	        }
+	        
+	        $libs->Load( "admanager" );
+	        
+	        $page->setTitle( 'Ενεργές Διαφημίσεις' );
+	        
+	        ?><h2>Ενεργές Διαφημίσεις</h2><?php
+            
+            $adfinder = new AdFinder();
+            $ads = $adfinder->FindAllActive();
+            
+            if ( count ( $ads ) == 0 )  {
+                ?><p>Δεν υπάρχει καμία ενεργή διαφήμιση</p><?php
+                return;
+            }
+            
+            ?><table class="stats">
+                <tr>
+                    <th>Χρήστης</th>
+                    <th>Τίτλος</th>
+                    <th>Κείμενο</th>
+                    <th>Link</th>
+                    <th>Εμφανίσεις ακόμα</th>
+                </tr>
+            <?php  
+            
+            foreach ( $ads as $ad ) {
+                    ?><tr><td><?php
+                    echo $ad->Userid;
+                    ?></td><td><?php                    
+                    echo $ad->Title;
+                    ?></td><td><?php                    
+                    echo $ad->Body;
+                    ?></td><td><?php
+                    echo $ad->Url
+                    ?></td><td><?php
+                    echo $ad->pageviewsremaining;
+                    ?></td></tr><?php
+            }
+            ?></table><?php          
+            
+            return;
+        }
+    }
+?>
