f9d00c346fff8ea18844e1200eecaa226dfd15ba
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-05-28 06:11:50 +0000

    ban upgrade-super ban

--- phoenix/actions/adminpanel/ban.php
+++ phoenix/actions/adminpanel/ban.php
@@ -5,17 +5,22 @@
         $username = $username->Get();
         $reason = $reason->Get();
         $time_banned = $time_banned->Get();
+        $delete_journals = $delete_journals->Get();
         
         if ( $reason == "" ) {
             $reason = "Δεν αναφέρθηκε";
         }
         
-        $time_banned = $time_banned*24*60*60;
+        $time_banned = $time_banned*24*60*60;//<--make days to secs
         
         $libs->Load( 'adminpanel/ban' );
         
         $ban = new Ban();
         $res = $ban->BanUser( $username, $reason, $time_banned );
+        
+        if ( $delete_journals == "yes" ) {
+            return Redirect( '?p=banlist&deletejournals' );;//deletejournals
+        }
            
         return Redirect( '?p=banlist' );
     }
