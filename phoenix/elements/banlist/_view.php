5d01214b05d88b6738667b45a079e83eb61a3f0c
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-05-28 06:04:13 +0000

    ban upgrade-super ban

--- phoenix/elements/banlist/_view.php
+++ phoenix/elements/banlist/_view.php
@@ -50,9 +50,16 @@
             ?></table><?php   
 
             ?><form method="post" action="do/adminpanel/ban"><?php
-            ?><p>Όνομα χρήστη : <input type="text" maxlength="30" name="username" /> &nbsp;&nbsp;  
-                        Λόγος Αποκλεισμού : <input type="text" maxlength="30" name="reason" /> &nbsp;&nbsp;
-                        Διάρκεια : <input type="text" maxlength="5" value="20" name="time_banned" /> μέρες</p><?php
+            ?><p>
+                        Όνομα χρήστη : <input type="text" maxlength="30" name="username" /> &nbsp;&nbsp;&nbsp;  
+                        Λόγος Αποκλεισμού : <input type="text" maxlength="30" name="reason" /> &nbsp;&nbsp;&nbsp;
+                        Διάρκεια : <input type="text" maxlength="5" value="20" name="time_banned" /> μέρες          
+              </p>
+              <p>
+                        Εικόνες <input type="checkbox" value="no" name="delete_images">&nbsp;&nbsp;
+                        Δημοσκοπήσεις <input type="checkbox" value="no" name="delete_polls">&nbsp;&nbsp;
+                        Ημερολόγια <input type="checkbox" value="no" name="delete_journals">&nbsp;&nbsp;
+              </p>              
             ?><p><input type="submit" value="Ban" /></p><?php
             ?></form><?php
    
