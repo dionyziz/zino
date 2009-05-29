c09275ccde3586a7497957e602064319117964c4
Chris Pappas <chrispappas12@gmail.com>
2009-05-29 18:50:25 +0000

    css optimizing

--- phoenix/elements/album/photo/view.php
+++ phoenix/elements/album/photo/view.php
@@ -221,7 +221,7 @@
                 if ( $image->Album->Numphotos > 1 ) {
                     ?><div class="pthumbs plist"><?php
                         $finder = New ImageFinder();
-                        $photos = $finder->FindAround( $image , 10 );
+                        $photos = $finder->FindAround( $image , 5 );
                         $pivot = $i = 0;
                         foreach ( $photos as $photo ) {
                             if ( $photo->Id == $image->Id ) {
