bdd7ed7145919605225dbb2312472c64386ea2e3
Chris Pappas <chrispappas12@gmail.com>
2009-06-01 18:26:34 +0000

    notifications effect

--- phoenix/js/_notification.js
+++ phoenix/js/_notification.js
@@ -12,10 +12,10 @@ var Notification = {
         if ( Notification.INotifs === 0 && Notification.VNotifs <= 5 ) {
             --Notification.VNotifs;
         }
-		$( '#event_' + eventid ).animate( { opacity : "0" , height : "0" } , 400 , "linear" , function() {
+		$( '#event_' + eventid ).animate( { opacity : "0" , height : "0" } , 100 , "linear" , function() {
 			$( this ).remove();
             if ( Notification.VNotifs === 0 ) {
-                $( "div.notifications" ).animate( { opacity : "0" , height : "0" } , 400 , "linear"  , function() {
+                $( "div.notifications" ).animate( { opacity : "0" , height : "0" } , 100 , "linear"  , function() {
                     $( this ).remove();  
                 } );
             }
