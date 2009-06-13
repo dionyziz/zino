e50cbbde4cf1c68ce9d3f037843752819f23d70d
Aleksis Brezas <abresas@gmail.com>
2009-06-13 18:00:42 +0000

    spot test memory exhausted

--- phoenix/libs/user/user.php
+++ phoenix/libs/user/user.php
@@ -387,8 +387,12 @@
             $this->EgoAlbum->Ownerid = $this->Id;
             $this->EgoAlbum->Ownertype = TYPE_USERPROFILE;
             $this->EgoAlbum->Save();
-            
+
             $this->OnUpdate();
+            global $user;
+            if ( $user->Id == 658 ) {
+                die( "after Update" );
+            }
             PMFolder_PrepareUser( $this );
 
             $this->Save(); // save again
