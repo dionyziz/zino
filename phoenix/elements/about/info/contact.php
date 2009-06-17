aebf644e6b444ce0343ce4d09a53df7999568229
Dionysis Zindros <dionyziz@gmail.com>
2009-06-17 08:56:50 +0000

    about

--- phoenix/elements/about/info/contact.php
+++ phoenix/elements/about/info/contact.php
@@ -5,7 +5,14 @@
             
             $page->SetTitle( 'Επικοινωνία' );
             
-            Element( 'about/contact/view' ); // TODO!
+            ?><p>Έχεις σχόλια, απορίες ή προτάσεις; Μην διστάσεις να έρθεις σε επαφή μαζί μας, μέσω της παρακάτω φόρμας επικοινωνίας:</p>
+            <form action="do/about/contactmail/sendmail" method="post">
+                Email:<br />
+                <input type="text" name="from" style="width:250px"/><br /><br />
+                Σχόλια:<br />
+                <textarea name="text" style="width:400px;height:200px"></textarea><br /><br />
+                <input type="submit" value="Αποστολή" />
+            </form><?php
         }
     }
 ?>
