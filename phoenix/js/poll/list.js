44434710534da8383dfe388230be96d959c3432c
Alex Tzo <chorvus@gmail.com>
2009-06-23 19:25:31 +0000

    poll list min-height

--- phoenix/js/poll/list.js
+++ phoenix/js/poll/list.js
@@ -126,9 +126,6 @@ var PollList = {
         Coala.Cold( 'admanager/showad', { f: function ( html ) {
             var ads = $( 'div.ads' )[ 0 ];
             ads.innerHTML = html;
-            if ( ads.offsetHeight >= ads.parentNode.offsetHeight ) {
-                $( ads.parentNode ).css( 'height' , ads.offsetHeight );
-            }
         } } );
     }
 };
