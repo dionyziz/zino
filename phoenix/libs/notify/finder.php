edeb5abc0af39eba5aac4cced07c0866daf49ab9
Aleksis Brezas <abresas@gmail.com>
2009-06-26 08:45:29 +0000

    frontpage notify optimized

--- phoenix/libs/notify/finder.php
+++ phoenix/libs/notify/finder.php
@@ -73,7 +73,7 @@
             $prototype->Touserid = $user->Id;
             $notifications = $this->FindByPrototype( $prototype, $offset, $limit + 6, array( 'Eventid', 'DESC' ), true );
             $notifications->PreloadRelation( 'FromUser', 'User', 'Fromuserid' );
-            $notifications->PreloadUserAvatars();
+            $notifications->PreloadFromUserAvatars();
             $notifications->PreloadItems();
             $notifications->PreloadItemRelations();
             return $notifications;
