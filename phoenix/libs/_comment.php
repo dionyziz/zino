b1b55f21fba6d9dea09ae3ec3eda93507867ba45
Dionysis Zindros <dionyziz@gmail.com>
2009-07-01 12:17:36 +0000

    fix frontpage error

--- phoenix/libs/_comment.php
+++ phoenix/libs/_comment.php
@@ -100,7 +100,7 @@
             $avatarids = array();
             foreach ( $this as $comment ) {
                 w_assert( is_object( $comment->User ) );
-                w_assert( is_int( $comment->User->Avatarid ), 'Avatarid is not an integer, ' . get_type( $comment->User->Avatarid ) . ' given.' );
+                w_assert( is_int( $comment->User->Avatarid ), 'Avatarid is not an integer, ' . gettype( $comment->User->Avatarid ) . ' given.' );
                 $avatarids[] = $comment->User->Avatarid;
             }
             $finder = New ImageFinder();
