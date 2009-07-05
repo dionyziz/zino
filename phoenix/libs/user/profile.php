d4a60d313da1f9580bc65e3de88a7e1998c2366e
Dionysis Zindros <dionyziz@gmail.com>
2009-07-05 12:13:30 +0000

    Change my profile song

--- phoenix/libs/user/profile.php
+++ phoenix/libs/user/profile.php
@@ -134,7 +134,7 @@
                 case 'Song':
                     switch ( $this->Userid ) {
                         case 1:
-							return array( "name" => "Coldplay - Viva La Vida", "url" => "http://cache11-music01.myspacecdn.com/87/std_a3e66d536b1307a5c2b813d2114422f7.mp3" );		
+							return array( "name" => "The nutcracker", "url" => "http://www3.hi.is/~lobbi/hljod/Tonlist/Bl%F3mavalsinn%20-%20Hnotubrj%F3turinn.mp3" );		
 						case 58:
 							return array( "name" => "Blank & Jones ft Elles -  Mind of the wonderful" , "url" => "http://izual.kamibu.com/song.mp3" );
 						case 872:
