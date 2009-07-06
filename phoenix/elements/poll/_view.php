4ee8120b35efa67286a8c80569b518a6a41d5375
Dionysis Zindros <dionyziz@gmail.com>
2009-07-06 17:39:02 +0000

    poll voting bug

--- phoenix/elements/poll/_view.php
+++ phoenix/elements/poll/_view.php
@@ -35,8 +35,6 @@
 
             $finder = New PollVoteFinder();
             $showresults = $finder->FindByPollAndUser( $poll, $user );
-            var_dump( $showresults );
-            die();
             $theuser = $poll->User;
 
             if ( $pageno <= 0 ) {
