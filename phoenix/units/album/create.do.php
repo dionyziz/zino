c083d7ea76deed60b92de37de4f3eb23517eda04
Alex Tzo <chorvus@gmail.com>
2009-07-07 00:12:11 +0000

    album/create.do breakpoints

--- phoenix/units/album/create.do.php
+++ phoenix/units/album/create.do.php
@@ -8,19 +8,13 @@
         if ( !$user->Exists() ) {
             return;
         }
-        echo 1;
+
         $libs->Load( 'album' );
-        echo 2;
         $albumname = $albumname->Get();
-        echo 3;
         if ( $albumname !== '' ) {
-        echo 4;
             $album = New Album();
-        echo 5;
             $album->Name = $albumname;
-        echo 6;
             $album->Save();
-        echo 7;
             ?>window.location.href = '<?php
             echo $rabbit_settings[ 'webaddress' ];
             ?>?p=album&id=<?php
