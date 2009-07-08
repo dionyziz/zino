094f6f2977adc4f2bacd3cf2adc965855ed187e0
Alex Tzo <chorvus@gmail.com>
2009-07-08 12:13:06 +0000

    answers bug

--- phoenix/elements/question/answer/_list.php
+++ phoenix/elements/question/answer/_list.php
@@ -79,9 +79,8 @@
             }
 
             ?><ul class="questions"><?php
-            if ( $answers !== true && $theuser->Id != $user->Id ) {
+            if ( $answers == false && $theuser->Id != $user->Id ) {
                 ?>Ο χρήστης δεν έχει απαντήσει σε καμία ερώτηση.<?php
-                var_dump( $answers !== true );
             }
             else {
                 foreach ( $answers as $answer ) {
