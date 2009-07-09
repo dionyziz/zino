8abb8c757f46964d5259fa8eb871c199f3af771c
Dionysis Zindros <dionyziz@gmail.com>
2009-07-09 13:04:18 +0000

    About pages

---
+++ phoenix/elements/about/info/faq.php
@@ -0,0 +1,9 @@
+<?php
+    class ElementAboutInfoFAQ extends Element {
+        public function Render() {
+            ?><h2 class="sweet">Συχνές ερωτήσεις</h2>
+                
+            <?php
+        }
+    }
+?>
