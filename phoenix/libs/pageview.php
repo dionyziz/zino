a5602c84e724a190928c7a50489fd33500847a2f
Aleksis Brezas <abresas@gmail.com>
2009-07-09 09:20:52 +0000

    pageview class for statistics

--- phoenix/libs/pageview.php
+++ phoenix/libs/pageview.php
@@ -7,8 +7,54 @@
 
     Developer: abresas
     */
-     
 
+    class PageviewFinder extends Satori {
+        protected $mModel = 'Pageview';
+
+        // returns number of bounces by page and
+        // sets $totalbounces parameter to total number of bounces
+        public function FindTopBounces( &$totalbounces, $maxurls = false ) {
+            $query = $this->mDb->Prepare( 
+                'SELECT
+                    pageview_url, COUNT(*) AS bounces
+                FROM
+                    ( SELECT
+                        pageview_url, pageview_sessionid, COUNT(*) AS sessioncount
+                    FROM
+                        :pageviews
+                    GROUP BY
+                        pageview_url, pageview_sessionid ) AS sessioned
+                WHERE
+                    sessioned.sessioncount = 1
+                GROUP BY
+                    pageview_url
+                ORDER BY
+                    bounces DESC;'
+            );
+
+            $query->BindTable( 'pageviews' );
+            $res = $query->Execute();
+            $totalbounces = 0;
+            $bouncesByUrl = array();
+            while ( $row = $res->FetchArray() ) {
+                $bounces = $row[ 'bounces' ];
+                $totalbounces += $bounces;
+                if ( $maxurls !== false ) {
+                    if ( $maxurls == 0 ) {
+                        continue; // do not return any other url
+                    }
+                    --$maxurls;
+                }
+                else {
+                    // maxurls is false (default), return all urls
+                }
+                $bouncesByUrl[ $row[ 'pageview_url' ] ] = $bounces;
+            }
+
+            return $bouncesByUrl;
+        }
+    }
+    
     class Pageview extends Satori {
         protected $mTableAlias = 'pageview';
         protected $mLog = false;
@@ -35,6 +81,7 @@
             return $this->mLog;
         }
         public function LoadDefaults() {
+            $this->Url = $_SERVER[ 'SCRIPT_URL' ];
             $this->Created = NowDate();
             $this->Userip = UserIp(); 
             $this->Sessionid = session_id();
