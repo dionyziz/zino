cb0fc7d53145dfdc157aa16b2f6b11f0434491e0
Aleksis Brezas <abresas@gmail.com>
2009-07-11 10:53:45 +0000

    bounce rates and langings

--- phoenix/elements/statistics/bounces.php
+++ phoenix/elements/statistics/bounces.php
@@ -7,21 +7,25 @@
 
             $finder = New PageviewFinder();
             $bounces = $finder->FindTopBounces( 20 );
-            $bouncesByElement = $bounces[ 0 ];
-            $totalbounces = $bounces[ 1 ];
+            $bounceRates = $bounces[ 0 ];
+            $bouncesByElement = $bounces[ 1 ];
+            $landingsByElement = $bounces[ 2 ];
             ?><table>
             <thead><tr>
             <th>Element</th>
             <th>Bounce Rate</th>
             <th>Bounces</th>
+            <th>Landings</th>
             </tr></thead><tbody><?php
             foreach ( $bouncesByElement as $element => $bounces ) {
                 ?><tr><td><?php
                 echo $element;
                 ?></td><td><?php
-                echo $bounces / $totalbounces * 100;
+                echo $bounceRates[ $element ];
                 ?>%</td><td><?php
                 echo $bounces;
+                ?></td><td><?php
+                echo $landingsByelement[ $element ];
                 ?></td></tr><?php
             }
             ?></tbody></table><?php
