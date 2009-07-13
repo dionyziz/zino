71ca659ad13c69a085c448a1149b6a7379dd664d
Alex Tzo <chorvus@gmail.com>
2009-07-13 21:05:40 +0000

    applications front-end

--- phoenix/elements/application/list.php
+++ phoenix/elements/application/list.php
@@ -16,27 +16,20 @@
                 ?>Δεν έχεις πρόσβαση σε αυτήν την σελίδα.<?
             }
             else {
-            ?><div id="appmanager">
-                <ul id="applist"><?php
-                $finder = New ApplicationFinder();
-                $apps = $finder->FindByUser( $user );
-                if ( $apps ) {
-                    foreach ( $apps as $app ) {
-                        ?><li><div class="appbubble">
-                            <span class="banner"><img src="<?php echo htmlspecialchars( $app->Logo ); ?>" /></span>
-                            <span class="name"><h2><?php echo htmlspecialchars( $app->Name ); ?></h2></span><br />
-                            <span class="description"><h3>Περιγραφή: </h3><?php echo htmlspecialchars( $app->Description ); ?></span><br />
-                            <span class="links">
-                                <h3>Διεύθυνση Εφαρμογής:</h3> <a href="<?php echo htmlspecialchars( $app->Url ); ?>"><?php echo htmlspecialchars( $app->Url ); ?></a>
-                            </span>
-                            <span class="key"><h3>Κλειδί Εφαρμογής: </h3><?php echo $app->GetToken(); ?></span>
-                        </div></li><?php
+                ?><div id="appmanager">
+                    <ul id="applist"><?php
+                    $finder = New ApplicationFinder();
+                    $apps = $finder->FindByUser( $user );
+                    if ( $apps ) {
+                        foreach ( $apps as $app ) {
+                            Element( 'application/view', $app );
+                        }
                     }
-                    ?></ul></div><?php
-                }
-                else {
-                    ?>Δεν έχεις καταχωρήσει καμία εφαρμογή. Μπορείς να το κάνεις <a href="/?p=newapp">εδώ</a><?php
-                }
+                    else {
+                        ?>Δεν έχεις καταχωρήσει καμία εφαρμογή. Μπορείς να το κάνεις <a href="/?p=newapp">εδώ</a><?php
+                    }
+                    ?></ul>
+                </div><?php
             }
         }
     }
