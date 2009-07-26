47c86bb2244e57b09d9069d728972651d70e4624
Alex Tzo <chorvus@gmail.com>
2009-07-26 05:53:57 +0000

    album manager

--- phoenix/units/album/manager/move.do.php
+++ phoenix/units/album/manager/move.do.php
@@ -9,11 +9,15 @@
         $photo = New Image( $photoid->Get() );
         
         if ( !$album->Exists() || !$photo->Exists() ) {
+            ?>alert( "Album or image does not exist" );<?php
             return;
         }
         
         if ( $album->Ownerid == $user->Id && $photo->Ownerid == $user->Id ) {
             $photo->MoveTo( $album );
+        }
+        else {
+            ?>alert( "You do not own either the Album or the Photo" );<?php
         }
     }
 ?>
