981df70b9ddf0ab0137fe4348db8f4d7f42e60b4
Alex Tzo <chorvus@gmail.com>
2009-07-26 04:38:45 +0000

    album manager

--- phoenix/elements/album/_row.php
+++ phoenix/elements/album/_row.php
@@ -13,7 +13,7 @@
             ?><li id="<?php
             echo $album->Id;
             ?>"><?php
-                    if ( $album->Mainimage->Exists() && $album->Mainimageid !== false) {    
+                    if ( $album->Mainimage->Exists() && $album->Mainimageid !== 0) {
                         Element( 'image/view', $album->Mainimageid , $album->Mainimage->Userid , $album->Mainimage->Width , $album->Mainimage->Height , IMAGE_CROPPED_100x100 , '' , $albumname , '' , true , 50 , 50 , 0 ); 
                     }
                     else {
