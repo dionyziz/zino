82a1f2f444d8b8f5819e8ed713f2a665d5a5bbcb
Dionysis Zindros <dionyziz@gmail.com>
2009-07-27 15:46:23 +0000

    contact form

--- phoenix/elements/about/info/contact.php
+++ phoenix/elements/about/info/contact.php
@@ -5,7 +5,23 @@
             
             $page->SetTitle( 'Επικοινωνία' );
             
-            ?><p></p><?php
+            ?><form>
+                <div>
+                   <label>Το e-mail σου:</label>
+                   <input type="text" name="email" /> 
+                </div>
+                <div>
+                   <label>Επικοινωνώ επειδή:</label>
+                   <select name="reason">
+                    <option value="support">Έχω τεχνικό πρόβλημα στο Zino</option>
+                    <option value="feature">Έχω μία ιδέα για το Zino</option>
+                    <option value="abuse">Αναφέρω παραβίαση των Όρων Χρήσης</option>
+                    <option value="biz">Θα ήθελα να συνεργαστούμε</option>
+                    <option value="press">Είμαι δημοσιογράφος</option>
+                    <option value="purge">Θέλω να διαγράψω το λογαριασμό μου</option>
+                   </select>
+                </div>
+            </form><?php
         }
     }
 ?>
