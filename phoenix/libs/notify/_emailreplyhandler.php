fed20e28f4abc3f034745ef8832dc715e7281a41
Petros Aggelatos <petrosagg@gmail.com>
2009-07-27 21:54:18 +0000

    Notify_EmailReplyHandler

--- phoenix/libs/notify/_emailreplyhandler.php
+++ phoenix/libs/notify/_emailreplyhandler.php
@@ -17,11 +17,11 @@
 		
 		$comment = New Comment( $commentid );
 		
-		if( substr( md5( 'beast' . $comment->Created . $comment->Id ), 0, 10 ) != $hash ) {
+		if ( substr( md5( 'beast' . $comment->Created . $comment->Id ), 0, 10 ) != $hash ) {
 			return;
 		}
 		
-		if( $comment->Parentid == 0 ) {
+		if ( $comment->Parentid == 0 ) {
 			switch( $comment->Typeid ) {
 				case TYPE_POLL:
 					$libs->Load( 'poll/poll' );
@@ -34,9 +34,7 @@
 					$userid = $entity->Userid;
 					break;
 				case TYPE_USERPROFILE:
-					$libs->Load( 'user/user' );
-					$entity = New User( $comment->Itemid );
-					$userid = $entity->Userid;
+					$userid = $comment->Itemid;
 					break;
 				case TYPE_JOURNAL:
 					$libs->Load( 'journal/journal' );	
@@ -51,7 +49,7 @@
 		}
 		
 		
-		$pattern = "^.+\n?";
+		$pattern = "^>.+\n?";
 		$text = preg_replace( $pattern, '', $body );
         
         $comment = New Comment();
