dbcfa7c62c174b94cc5efcc7ae8572e5fba1990b
Dionysis Zindros <dionyziz@gmail.com>
2009-07-27 10:58:41 +0000

    css cleanup

--- phoenix/elements/about/info/contact.php
+++ phoenix/elements/about/info/contact.php
@@ -3,16 +3,9 @@
         public function Render() {
             global $page;
             
-            $page->SetTitle( 'Επικοινωνία' );
+            $page->SetTitle( 'Ξ•Ο€ΞΉΞΊΞΏΞΉΞ½Ο‰Ξ½Ξ―Ξ±' );
             
-            ?><p>Έχεις σχόλια, απορίες ή προτάσεις; Μην διστάσεις να έρθεις σε επαφή μαζί μας, μέσω της παρακάτω φόρμας επικοινωνίας:</p>
-            <form action="do/about/contactmail/sendmail" method="post">
-                Email:<br />
-                <input type="text" name="from" style="width:250px"/><br /><br />
-                Σχόλια:<br />
-                <textarea name="text" style="width:400px;height:200px"></textarea><br /><br />
-                <input type="submit" value="Αποστολή" />
-            </form><?php
+            ?><p></p><?php
         }
     }
 ?>
