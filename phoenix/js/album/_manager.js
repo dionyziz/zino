58cd5bd78adc653c39a10121a19a8045fbded3c2
Alex Tzo <chorvus@gmail.com>
2009-07-27 00:33:12 +0000

    photomanager enhancements

--- phoenix/js/album/_manager.js
+++ phoenix/js/album/_manager.js
@@ -16,7 +16,7 @@ function lowerOffset( jqelement ) {
     }
     return parseInt(jqelement.position().top) + parseInt(jqelement.css("marginTop")) + jqelement.outerHeight() - 1;
 }
-function isScrollingElementVisible( eltVisible, eltScrolling ) {
+function isScrollingElementVisible( eltVisible, eltScrolling, pixelsTollerable) {
     //Returns true only if the WHOLE element is visible (accepts a DOM element and the scrolling container
     //WARNING: Usable only for vertical scrolling
     if ( !eltVisible || !eltScrolling ) {
@@ -24,7 +24,7 @@ function isScrollingElementVisible( eltVisible, eltScrolling ) {
     }
     var scrollPos = eltScrolling.scrollTop;
     var scrollHeight = $( eltScrolling ).height();
-    return ( upperOffset( $(eltVisible) ) > scrollPos ) && ( lowerOffset( $(eltVisible) ) < scrollPos + scrollHeight );
+    return ( upperOffset( $(eltVisible) ) - pixelsTollerable > scrollPos ) && ( lowerOffset( $(eltVisible) ) + pixelsTollerable < scrollPos + scrollHeight );
 }
 
 var PhotoManager = {
@@ -34,7 +34,7 @@ var PhotoManager = {
     
     checkEnabledAlbumbs: function() {
         jQuery.each( $("div.albumlist li"), function() {
-            if (isScrollingElementVisible(this,$("div.albumlist").get()[0])) {
+            if (isScrollingElementVisible( this, $("div.albumlist").get()[0]), 10 ) {
                 if (  $(this).hasClass( "ui-droppable-disabled" ) ) { $(this).droppable( 'enable' ); }
             } else {
                 if ( !$(this).hasClass( "ui-droppable-disabled" ) ) { $(this).droppable( 'disable' ); }
