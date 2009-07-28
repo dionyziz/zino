138cfc5105f4be270d760841aaae6de69bfa7606
Chris Pappas <chrispappas12@gmail.com>
2009-07-28 16:22:54 +0000

    css spriting/css optimizing

---
+++ phoenix/elements/album/_list.php
@@ -0,0 +1,105 @@
+<?php
+    class ElementAlbumList extends Element {
+        public function Render( tText $username , tText $subdomain , tInteger $pageno ) {
+            global $page;
+            global $user;
+            global $rabbit_settings;
+            global $water;
+            global $libs;
+            
+            $libs->Load( 'album' );
+            
+            $page->AttachStylesheet( 'css/album/list_ie.css', true );
+            
+            Element( 'user/subdomainmatch' );
+            
+            $username = $username->Get();
+            $subdomain = $subdomain->Get();
+            $finder = New UserFinder();
+            if ( $username != '' ) {
+                if ( strtolower( $username ) == strtolower( $user->Name ) ) {
+                    $theuser = $user;
+                }
+                else {
+                    $theuser = $finder->FindByName( $username );
+                }
+            }
+            else if ( $subdomain != '' ) {
+                if ( strtolower( $subdomain ) == strtolower( $user->Subdomain ) ) {
+                    $theuser = $user;
+                }
+                else {
+                    $theuser = $finder->FindBySubdomain( $subdomain );
+                }
+            }    
+            if ( !isset( $theuser ) || $theuser === false ) {
+                return Element( '404' );
+            }
+            
+            if ( strtoupper( substr( $theuser->Name, 0, 1 ) ) == substr( $theuser->Name, 0, 1 ) ) {
+                $page->SetTitle( $theuser->Name . " Albums" );
+            }
+            else {
+                $page->SetTitle( $theuser->Name . " albums" );
+            }
+
+            $pageno = $pageno->Get();
+            if ( $pageno <= 0 ) {
+                $pageno = 1;
+            }
+            
+            $finder = New AlbumFinder();
+            $limit = 12;
+            $albums = $finder->FindByUser( $theuser, ( $pageno - 1 ) * $limit, $limit, ( $theuser == $user ) );
+            Element( 'user/sections', 'album' , $theuser );
+            ?><ul class="albums"><div class="ads"></div><?php
+                if ( $pageno == 1 && ( $theuser->EgoAlbum->Numphotos != 0 || $theuser->Id != $user->Id ) ) {
+                    ?><li><?php
+                    Element( 'album/small', $theuser->EgoAlbum, false );
+                    ?></li><?php
+                }
+                foreach ( $albums as $album ) {
+                    if ( ( $user->Id == $theuser->Id || $album->Numphotos > 0 ) && $album->Id != $theuser->Egoalbumid ) {
+                        ?><li><?php
+                            Element( 'album/small' , $album , false );
+                        ?></li><?php
+                    }
+                }
+                if ( $theuser->Id == $user->Id ) {
+                    ?><li class="create">
+                        <a href="" class="new"><span class="s1_0048">Δημιουργία album</span></a>
+                    </li><?php
+                }
+            ?></ul><?php
+            if ( $theuser->Id == $user->Id ) {
+                ?><div class="creationmakeup"><?php
+                    Element( 'album/small' , false , true );
+                ?></div>
+                <div class="creating">
+                    <img src="<?php
+                    echo $rabbit_settings[ 'imagesurl' ];
+                    ?>ajax-loader.gif" alt="Παρακαλώ περιμένετε" title="Παρακαλώ περιμένετε" /> Δημιουργία album
+                </div><?php
+            }
+            ?><div class="eof"></div>
+            <div class="pagifyalbums"><?php
+
+            ob_start();
+            Element( 'user/url', $theuser->Id , $theuser->Subdomain );
+            $link = ob_get_clean() . 'albums?pageno=';
+            $total_albums = $albums->TotalCount();
+            $total_pages = ceil( $total_albums / $limit );
+            $text = "( " . $total_albums . " Album";
+            if ( $total_albums > 1 ) {
+            $text .= "s";
+            }
+            $text .= " )";
+            Element( 'pagify', $pageno, $link, $total_pages, $text );
+            ?></div>
+            <?php if ( $user->Id == $theuser->Id ) {
+                ?><div class="managerlink"><a href="?p=photomanager"  >Διαχείριση Φωτογραφιών</a></div><?php
+            }
+            $page->AttachInlineScript( 'AlbumList.OnLoad();' );
+        }
+    }
+?>
