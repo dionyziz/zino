3ef9de265acddadea0b25db9fd78c18ca39aeb58
Chris Pappas <chrispappas12@gmail.com>
2009-07-29 13:44:18 +0000

    css spriting/optimizing

--- phoenix/elements/about/info/_status.php
+++ phoenix/elements/about/info/_status.php
@@ -20,8 +20,8 @@
                 ?><li><?php
                     Element( 'journal/small' , $journal );
                     ?><div class="barfade">
-                        <div class="leftbar"></div>
-                        <div class="rightbar"></div>
+                        <div class="s1_0070 leftbar"></div>
+                        <div class="s1_0071 rightbar"></div>
                     </div>
                 </li><?php
             }
