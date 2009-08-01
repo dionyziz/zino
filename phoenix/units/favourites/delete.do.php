c16e55c5e8200fadf0aade331f84880ad21b5177
Alex Tzo <chorvus@gmail.com>
2009-08-01 07:56:34 +0000

    favourites deletion

--- phoenix/units/favourites/delete.do.php
+++ phoenix/units/favourites/delete.do.php
@@ -9,12 +9,12 @@
         
         $libs->Load( 'favourite' );
         
-        $favid->Get();
+        $favid = $favid->Get();
         
         $favourite = New Favourite( $favid );
         
         if ( $favourite->Exists() ) {
-            ?>alert( "Favourite with id:<?php echo $favid; ?> does not exists" );<?php
+            ?>alert( "Favourite with id:<?php echo $favourite->Id; ?> does not exists" );<?php
             return;
         }
         if ( $favourite->Userid == $user->Id ) {
