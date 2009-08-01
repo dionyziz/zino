2865bafbc696feb94bb90750ce504083ac5d5e12
Alex Tzo <chorvus@gmail.com>
2009-08-01 09:41:44 +0000

    favourites deletion

--- phoenix/js/favourites.js
+++ phoenix/js/favourites.js
@@ -2,8 +2,10 @@ var Favourites = {
     Delete: function( favid ) {
         Coala.Warm( 'favourites/delete', { 'favid': favid } );
         fav = $( "ul.events > li#favourite_" + favid )
-        fav.fadeTo( 300, 0 ).slideUp( 500, function() { fav.remove(); } );
-        $( "ul.events > li:last" ).addClass( "last" ).siblings().removeClass( "last" );
+        fav.fadeTo( 300, 0 ).slideUp( 500, function() {
+            fav.remove();
+            $( "ul.events > li:last" ).addClass( "last" ).siblings().removeClass( "last" );
+        } );
         return false;
     }
 };
