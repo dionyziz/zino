3cfb8043ed327163f60acbe45003931d07fc0c83
Chris Pappas <chrispappas12@gmail.com>
2009-08-03 16:00:47 +0000

    css spriting/optimizing

---
+++ phoenix/js/_banner.js
@@ -0,0 +1,30 @@
+var Banner = {
+    OnLoad : function() {
+        Banner.Lusername = false;
+        Banner.Lpassword = false;
+        $( "#lusername" ).focus( function() {
+            if ( !Banner.Lusername ) {
+                $( this ).css( 'color' , '#000' ).attr( 'value' , '' );
+                Banner.Lusername = true;
+            }
+        } );
+        $( "#lpassword" ).focus( function() {
+            if ( !Banner.Lpassword ) {
+                $( "#lpassword" ).remove();
+                var newinput = document.createElement( 'input' );
+                $( newinput ).addClass( 's2_0008' ).css( {
+                    'color' : '#000',
+                    'margin-left' : '3px'
+                    } ).attr( 
+                    { value : '',
+                    type : 'password',
+                    name : 'password'
+                } );
+                $( "#lusername" ).after( newinput ); 
+                newinput.focus();
+                Banner.Lpassword = true; 
+            }
+
+        } );
+    }
+};
