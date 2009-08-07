4bba1ea47c898fd29ab9f5d4e85bb5c0d3bdc9d6
Dionysis Zindros <dionyziz@gmail.com>
2009-08-07 12:05:32 +0000

    about pages

--- phoenix/elements/about/info/contact.php
+++ phoenix/elements/about/info/contact.php
@@ -23,6 +23,7 @@
                 <div>
                    <label>Επικοινωνώ επειδή:</label>
                    <select name="reason">
+                    <option></option>
                     <option value="support">Έχω τεχνικό πρόβλημα στο Zino</option>
                     <option value="feature">Έχω μία ιδέα για το Zino</option>
                     <option value="abuse">Αναφέρω παραβίαση των Όρων Χρήσης</option>
@@ -34,15 +35,16 @@
                 <div id="contact_support">
                     <div>
                         <label>Σε ποια σελίδα συνέβη το πρόβλημα; (διεύθυνση)</label>
-                        <input type="text" name="url" />
+                        <input type="text" name="url" style="width:100%" />
                     </div>
                     <div>
                         <label>Τι ακριβώς συνέβη; Περιέγραψε με όσες λεπτομέρειες μπορείς.</label>
-                        <textarea cols="50" rows="10" name="description"></textarea>
+                        <textarea cols="70" rows="10" name="description" style="width:100%"></textarea>
                     </div>
                     <div>
                         <label>Τι λειτουργικό σύστημα χρησιμοποιείς;</label>
                         <select name="os">
+                         <option></option>
                          <option value="windows">Windows</option>
                          <option value="linux">Linux</option>
                          <option value="mac">Mac OS</option>
@@ -52,7 +54,8 @@
                     </div>
                     <div>
                         <label>Ποιο browser χρησιμοποιείς;</label>
-                        <select name="os">
+                        <select name="browser">
+                         <option></option>
                          <option value="ie">Internet Explorer</option>
                          <option value="ff">Mozilla Firefox</option>
                          <option value="chrome">Google Chrome</option>
