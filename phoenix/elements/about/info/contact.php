0854076de8e6036e1086fa10d2fd61a6c3f75beb
Dionysis Zindros <dionyziz@gmail.com>
2009-08-07 12:18:45 +0000

    about pages

--- phoenix/elements/about/info/contact.php
+++ phoenix/elements/about/info/contact.php
@@ -53,7 +53,39 @@
                          <option value="linux">Linux</option>
                          <option value="mac">Mac OS</option>
                          <option value="other">Κάποιο άλλο</option>
-                         <option value="dontknow">Δεν γνωρίζω</option>
+                         <option value="dontknow">Δεν ξέρω</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>Ποια έκδοση των Windows χρησιμοποιείς;</label>
+                        <select name="winversion">
+                         <option></option>
+                         <option value="98">Windows 98</option>
+                         <option value="me">Windows Millenium</option>
+                         <option value="2000">Windows 2000</option>
+                         <option value="xp">Windows XP</option>
+                         <option value="vista">Windows Vista</option>
+                         <option value="7">Windows 7</option>
+                         <option value="other">Κάποια άλλη</option>
+                         <option value="dontknow">Δεν ξέρω</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>Ποια διανομή του Linux χρησιμοποιείς;</label>
+                        <select name="linuxdistro">
+                         <option></option>
+                         <option>Ubuntu</option>
+                         <option>OpenSUSE</option>
+                         <option>Fedora</option>
+                         <option>Debian</option>
+                         <option>Mandriva</option>
+                         <option>LinuxMint</option>
+                         <option>PCLinuxOS</option>
+                         <option>Slackware</option>
+                         <option>Gentoo</option>
+                         <option>CentOS</option>
+                         <option>Κάποια άλλη</option>
+                         <option>Δεν ξέρω</option>
                         </select>
                     </div>
                     <div>
@@ -66,13 +98,27 @@
                          <option value="opera">Opera</option>
                          <option value="safari">Safari</option>
                          <option value="other">Κάποιο άλλο</option>
-                         <option value="dontknow">Δεν γνωρίζω</option>
+                         <option value="dontknow">Δεν ξέρω</option>
                         </select>
                     </div>
                 </div>
                 <div id="contact_feature">
+                    <p>Ευχαριστούμε που θέλεις να μοιραστείς την ιδέα σου μαζί μας!</p>
                     <div>
-                        <label>Ευχαριστούμε που θέλεις να μοιραστείς την ιδέα σου μαζί μας!</label>
+                        <label>Τι είναι αυτό που θα σου άρεσε να γίνει στο Zino?</label>
+                        <select name="featurechoice">
+                            <option></option>
+                            <option value="customization">Χρωματικοί συνδιασμοί στο προφίλ μου</option>
+                            <option value="sms">Ενημέρωση μέσω SMS</option>
+                            <option value="music">Μουσική στο προφίλ μου</option>
+                            <option value="purge">Δυνατότητα διαγραφής προφίλ</option>
+                            <option value="rename">Δυνατότητα αλλαγής ονόματος</option>
+                            <option value="newidea">Κάποια άλλη ιδέα (προσδιόρισε)</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>Γράψε μας την ιδέα σου που θα ήθελες να δεις στο Zino:</label>
+                        <textarea cols="70" rows="10" name="featuredescription" style="width:100%"></textarea>
                     </div>
                 </div>
             </form><?php
