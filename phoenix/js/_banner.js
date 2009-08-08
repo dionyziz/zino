4a0a0f9a79f0e2ea927f0bd3a8740aaa95d9b21e
Chris Pappas <chrispappas12@gmail.com>
2009-08-08 22:38:42 +0000

    css spriting/optimizing

--- phoenix/js/_banner.js
+++ phoenix/js/_banner.js
@@ -1,7 +1,6 @@
 var Banner = {
     OnLoad : function() {
         Kamibu.ClickableTextbox( 'lusername' , false , '#000' );
-        Banner.Lpassword = false;
         Kamibu.ClickableTextbox( 'lpassword' , false , '#000' , '' , function() {
             $( '#lpassword' ).focus( function() {
                 $( this ).remove();
@@ -16,23 +15,5 @@ var Banner = {
             } );
 
         } );
-        /*$( "#lpassword" ).focus( function() {
-            if ( !Banner.Lpassword ) {
-                $( "#lpassword" ).remove();
-                var newinput = document.createElement( 'input' );
-                $( newinput ).addClass( 's2_0008' ).css( {
-                    'color' : '#000',
-                    'margin-left' : '3px'
-                    } ).attr( 
-                    { value : '',
-                    type : 'password',
-                    name : 'password'
-                } );
-                $( "#lusername" ).after( newinput ); 
-                newinput.focus();
-                Banner.Lpassword = true; 
-            }
-        } );
-        */
     }
 };
