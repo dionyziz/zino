9a1b3a3669552d529fc15e6e01c46ea7872a2cff
Chris Pappas <chrispappas12@gmail.com>
2009-08-10 11:24:41 +0000

    js optimizing

--- phoenix/js/favourites.js
+++ phoenix/js/favourites.js
@@ -1,7 +1,7 @@
 var Favourites = {
     Delete: function( favid ) {
         Coala.Warm( 'favourites/delete', { 'favid': favid } );
-        fav = $( "ul.events > li#favourite_" + favid )
+        fav = $( "#favourite_" + favid )
         fav.fadeTo( 300, 0 ).slideUp( 500, function() {
             fav.remove();
             $( "ul.events > li:last" ).addClass( "last" ).siblings().removeClass( "last" );
