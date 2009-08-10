24fac4fdb32d4c1eeebbf353d32b1414893d932e
Chris Pappas <chrispappas12@gmail.com>
2009-08-10 10:08:07 +0000

    js optimizing

--- phoenix/js/user/_join.js
+++ phoenix/js/user/_join.js
@@ -141,17 +141,30 @@ var Join = {
         $( 'div a.button' ).click( function() {
             var create = true;
             if ( Join.username.value.length < 4 || Join.username.value.length > 20 ) {
-                Join.HandleError( 'usernameerror' , $( 'form.joinform div > span' )[ 0 ] );
+                Settings.HandleError( 'usernameerror' , $( 'form.joinform div > span' )[ 0 ] );
+                /*if ( !Join.usernameerror ) {
+                    Join.usernameerror = true;
+                    $( $( 'form.joinform div > span' )[ 0 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity : "1" } , 400 );
+                }
+                */
                 Join.username.focus();
                 create = false;
             }
             if ( Join.username.value.length >= 4 && !Kamibu.ValidEmail( Join.username.value ) ) {
-                Join.HandleError( 'invalidusername' , $( 'form.joinform div > span' )[ 2 ] );
+                Settings.HandleError( 'invalidusername' , $( 'form.joinform div > span' )[ 2 ] );
+                /*if ( !Join.invalidusername ) {
+                    Join.invalidusername = true;
+                    $( $( 'form.joinform div > span' )[ 2 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity : "1" } , 400 );
+                }
+                */
                 Join.username.focus();
                 create = false;
             }
             if ( Join.password.value.length < 4 ) {
-                Join.HandleError( 'pwderror' , $( 'form.joinform div > span' )[ 3 ] );
+                if ( !Join.pwderror ) {
+                    Join.pwderror = true;
+                    $( $( 'form.joinform div > span' )[ 3 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
+                }
                 if ( !Join.usernamerror && !Join.invalidusername && !Join.usernameexists ) {
                     //if the username and password are empty then focus the username inputbox
                     Join.password.focus();
@@ -159,14 +172,20 @@ var Join = {
                 create = false;
             }
             if ( Join.password.value != Join.repassword.value && !Join.pwderror ) {
-                Join.HandleError( 'repwderror' , $( 'form.joinform div div > span' )[ 0 ] );
+                if ( !Join.repwderror ) {
+                    Join.repwderror = true;
+                    $( $( 'form.joinform div div > span' )[ 0 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
+                }
                 if ( !Join.usernameerror && !Join.invalidusername && !Join.usernameexists ) {
                     Join.repassword.focus();
                 }
                 create = false;
             }
             if ( Kamibu.ValidEmail( Join.email.value ) ) {
-                Join.HandleError( 'emailerror' , $( 'form.joinform div > span' )[ 5 ] );
+                if ( !Join.emailerror ) {
+                    Join.emailerror = true;
+                    $( $( 'form.joinform div > span' )[ 5 ] ).css( "opacity" , "0" ).css( "display" , "inline" ).animate( { opacity: "1" } , 400 );
+                }
                 if ( !Join.usernameerror && !Join.invalidusername && !Join.usernameexists && !Join.pwderror && !Join.repwderror ) {
                     Join.email.focus();
                 }
