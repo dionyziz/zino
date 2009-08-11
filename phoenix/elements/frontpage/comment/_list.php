95493b3fd748987a551dd90285ad20653c0d00c2
Chris Pappas <chrispappas12@gmail.com>
2009-08-11 22:08:01 +0000

    css optimizing

---
+++ phoenix/elements/frontpage/comment/_list.php
@@ -0,0 +1,21 @@
+<?php
+    class ElementFrontpageCommentList extends Element {
+        protected $mPersistent = array( 'commentseq' );
+        public function Render( $commentseq ) {
+            global $libs;
+
+            $libs->Load( 'comment' );
+            
+            $finder = New CommentFinder();
+            $comments = $finder->FindLatest( 0 , 7 );
+            ?><h2 class="pheading">Σχόλια <span class="small1"(<a href="comments">προβολή όλων</a>)</span></h2>
+            <div class="list"><?php
+                foreach ( $comments as $comment ) {
+                    Element( 'frontpage/comment/view' , $comment );
+                }
+            
+            ?></div>
+            <div class="eof"></div><?php
+        }
+    }
+?>
