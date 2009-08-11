1dbfcebb77c313e0b213e2be62e2c7b87ff46826
Chris Pappas <chrispappas12@gmail.com>
2009-08-11 22:03:20 +0000

    css optimizing

--- phoenix/elements/user/profile/main/_friends.php
+++ phoenix/elements/user/profile/main/_friends.php
@@ -2,7 +2,6 @@
     class ElementUserProfileMainFriends extends Element {
         protected $mPersistent = array( 'userid' );
 
-
         public function Render( $friends , $friendsnum , $userid , $subdomain , $usernorel ) { 
             global $xc_settings;
 
