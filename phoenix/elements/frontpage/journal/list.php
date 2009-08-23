89659cdcce82839520c661c2b576dafec3cb9db6
Dionysis Zindros <dionyziz@gmail.com>
2009-08-23 11:44:50 +0000

    unsticky

--- phoenix/elements/frontpage/journal/list.php
+++ phoenix/elements/frontpage/journal/list.php
@@ -6,7 +6,7 @@
             global $xc_settings;
 			global $libs;
 
-            $sticky = 10155;
+            // $sticky = 10155;
 
             $libs->Load( 'journal/journal' );
 			$libs->Load( 'journal/frontpage' );
