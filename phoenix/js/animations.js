2ff3f7db323083692b196bf96c0f6af3edaa78cf
Dionysis Zindros <dionyziz@gmail.com>
2009-08-30 15:13:51 +0000

    Chorvus + dionyziz friendlist

---
+++ phoenix/js/animations.js
@@ -0,0 +1,15 @@
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutBounce',
+	easeOutBounce: function (x, t, b, c, d) {
+		if ((t/=d) < (1/2.75)) {
+			return c*(7.5625*t*t) + b;
+		} else if (t < (2/2.75)) {
+			return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+		} else if (t < (2.5/2.75)) {
+			return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+		} else {
+			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+		}
+	}
+} );
