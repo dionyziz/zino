3fd31ba00f4c66b02cdea70c8a2c98a42c21df42
Dionysis Zindros <dionyziz@gmail.com>
2009-08-30 15:25:43 +0000

    Chorvus + dionyziz friendlist

--- phoenix/elements/user/relations/row.php
+++ phoenix/elements/user/relations/row.php
@@ -43,7 +43,7 @@
                         echo $data;
                         ?></span><?php
                         if ( !empty( $datalist ) ) {
-                            ?>·<?php
+                            ?> · <?php
                         }
                     }
                 ?><div class="barfade">
