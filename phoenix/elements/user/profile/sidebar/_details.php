6d40ff1fc99eb7492e7f6ccb37a48ee77ecc483f
Dionysis Zindros <dionyziz@gmail.com>
2009-09-01 22:26:12 +0000

    store emblems

--- phoenix/elements/user/profile/sidebar/_details.php
+++ phoenix/elements/user/profile/sidebar/_details.php
@@ -40,8 +40,8 @@
                   Reason: to counter web-crawlers searching for IMs
                   Element( 'user/profile/sidebar/contacts' , $profile->Skype , $profile->Msn , $profile->Gtalk , $profile->Yim ); */
             ?></div><?php
-            // $finder = New StorepurchaseFinder();
-            // $purchases = $finder->FindByUserid( $theuserid );
+            $finder = New StorepurchaseFinder();
+            $purchases = $finder->FindByUserid( $theuserid );
             // var_dump( $purchases );
             // die();
 
