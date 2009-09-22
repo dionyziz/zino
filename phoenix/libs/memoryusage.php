ef13e7d9fcbc13ae4bf165e64ef225e079f143bd
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-09-22 10:58:53 +0000

    memory usage testing

---
+++ phoenix/libs/memoryusage.php
@@ -0,0 +1,29 @@
+<?php
+	function CheckMemoryUsage() {
+		global $user;
+		global $settings;
+
+		$memorySize = memory_get_usage( TRUE );
+		if( $memorySize > 25*1024*1024 ) {
+			$mem = new MemoryUsage();
+			$mem->Size = $memorySize;
+			if( $user->Exists() ) {
+				$mem->Userid = $user->Id;
+			}
+			else {
+				$mem->Userid = -1;
+			}
+			$mem->Url = $settings[ 'url' ];
+			$mem->Save();			
+		}
+		return;
+	}
+
+	class MemoryUsage extends Satori {
+		protected $mDbTableAlias = 'memoryusage';
+
+		protected function LoadDefaults() {
+		    $this->Created = NowDate();
+		}	
+    }
+?>
