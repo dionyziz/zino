85b3f654d4eeea1ef3592bed016c2ce612eb0898
Alex Tzo <chorvus@gmail.com>
2009-09-29 19:20:35 +0000

    comments optimization by dropping oop

--- phoenix/libs/memoryusage.php
+++ phoenix/libs/memoryusage.php
@@ -7,8 +7,9 @@
 		global $settings;
         global $water;
         
-		$memorySize = memory_get_peak_usage( TRUE );
-        $water->Trace( 'Memory peak size: ' . number_format( $memorySize ) . 'bytes' );
+		$memorySize = memory_get_peak_usage( true );
+        $water->Trace( 'Memory peak size: ' . number_format( $memorySize ) . ' bytes' );
+        $water->Trace( 'Memory peak size: ' . number_format( memory_get_usage( true ) ) . ' bytes' );
 		if( $memorySize > 25*1024*1024 && MemoryUsage_LogVisitorOrNot() ) {
 			$mem = new MemoryUsage();
 			$mem->Size = $memorySize;
