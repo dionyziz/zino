06347c941d9ef1fc802763dd1e6ea88204dffeed
Alex Tzo <chorvus@gmail.com>
2009-10-05 07:36:48 +0000

    optimization by dropping oop from comments

--- phoenix/libs/_comment.php
+++ phoenix/libs/_comment.php
@@ -499,7 +499,7 @@
                 foreach ( $commentids as $commentid ) {
                     if ( isset( $comments[ $commentid ] ) ) {
                         $comments[ $commentid ][ 'text' ] = $bulks[ (int) $comments[ $commentid ][ 'comment_bulkid' ] ];
-                        die( var_dump( $comments[ $commentid ] ) );
+                        die( var_dump( $bulks[ (int) $comments[ $commentid ][ 'comment_bulkid' ] ] ) );
                     }
                 }
                 
