cd715c234a35eb49f40dd50f0d2fbae652dab8b9
Alex Tzo <chorvus@gmail.com>
2009-10-31 02:43:25 +0000

    settings redesign

--- phoenix/units/user/settings/_tab.php
+++ phoenix/units/user/settings/_tab.php
@@ -53,7 +53,8 @@
             ?>$( container ).append( <?php
                 alert( 'one more append' );
                 echo w_json_encode( $chunk );
-            ?> );<?php
+            ?> );
+            <?php
         }
         alert( 'appending to the dom tree' );
         ?>$( 'div.settings div.tabs' ).append( $( "form", container ) );
