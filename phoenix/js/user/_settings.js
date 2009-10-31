bb5f06e86ca297519f20aa391235d5b002fb61d7
Alex Tzo <chorvus@gmail.com>
2009-10-31 00:48:03 +0000

    settings redesign

--- phoenix/js/user/_settings.js
+++ phoenix/js/user/_settings.js
@@ -2,7 +2,7 @@ var Settings = {
     OnLoad: function() {
         var arr = ['personal','characteristics','interests','contact','account'];
         $.each( arr, function() {
-            $( "#settingslist li" + this + " a" ).click( function() {
+            $( "#settingslist li " + this + " a" ).click( function() {
                 return function( section ) {
                     alert( section );
                     return false;
