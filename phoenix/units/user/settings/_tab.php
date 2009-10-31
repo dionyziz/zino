987270cb69f70a2fadaf083dd38c8b29ae420644
Alex Tzo <chorvus@gmail.com>
2009-10-31 03:08:27 +0000

    settings redesign

--- phoenix/units/user/settings/_tab.php
+++ phoenix/units/user/settings/_tab.php
@@ -44,7 +44,7 @@
                 return;
         }
         //a little hack to handle the huge tab elements
-        $html = str_split( ob_get_clean(), 512 );
+        $html = str_split( ob_get_clean(), 128 );
         
         ?>$( '#settingsloader' ).fadeOut();
         container = document.createElement( 'form' );
