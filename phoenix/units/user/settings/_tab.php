dc8d75da28ea0126659f94658b60a870c5a579ee
Alex Tzo <chorvus@gmail.com>
2009-10-31 02:34:31 +0000

    settings redesign

--- phoenix/units/user/settings/_tab.php
+++ phoenix/units/user/settings/_tab.php
@@ -1,62 +1,62 @@
 <?php
 /*
-	Masked by: Rhapsody
-	Reason: new ajax loading tabs for settings testing
+    Masked by: Rhapsody
+    Reason: new ajax loading tabs for settings testing
 */
-	function UnitUserSettingsTab( tString $tab ) {
-		global $user;
+    function UnitUserSettingsTab( tString $tab ) {
+        global $user;
         global $libs;
         
         $libs->Load( 'user/profile' );
         $libs->Load( 'user/settings' );
-		
-		ob_start();
+        
+        ob_start();
         
         $tab = $tab->Get();
         
-		switch( $tab ) {
-			case 'personal':
-				?><form id="personalinfo" action="" ><?php
-					Element( 'user/settings/personal/view' );
+        switch( $tab ) {
+            case 'personal':
+                ?><form id="personalinfo" action="" ><?php
+                    Element( 'user/settings/personal/view' );
                 ?></form><?php
-				break;
-			case 'characteristics':
-				?><form id="characteristicsinfo" action="" ><?php
-					Element( 'user/settings/characteristics/view' );
+                break;
+            case 'characteristics':
+                ?><form id="characteristicsinfo" action="" ><?php
+                    Element( 'user/settings/characteristics/view' );
                 ?></form><?php
-				break;
-			case 'interests':
-				?><form onsubmit="return false" id="interestsinfo" action="" ><?php
-					Element( 'user/settings/interests' );
+                break;
+            case 'interests':
+                ?><form onsubmit="return false" id="interestsinfo" action="" ><?php
+                    Element( 'user/settings/interests' );
                 ?></form><?php
-				break;
-			case 'contact':
-				?><form id="contactinfo" action="" ><?php
-					Element( 'user/settings/contact' );
+                break;
+            case 'contact':
+                ?><form id="contactinfo" action="" ><?php
+                    Element( 'user/settings/contact' );
                 ?></form><?php
-				break;
-			case 'settings':
-				?><form id="settingsinfo" action="" ><?php
-					Element( 'user/settings/settings' );
+                break;
+            case 'settings':
+                ?><form id="settingsinfo" action="" ><?php
+                    Element( 'user/settings/settings' );
                 ?></form><?php
-				break;
+                break;
             default:
                 return;
-		}
+        }
         //a little hack to handle the huge tab elements
         $html = str_split( ob_get_clean(), 512 );
-		
-	  ?>$( '#settingsloader' ).fadeOut();
+        
+        ?>$( '#settingsloader' ).fadeOut();
         container = document.createElement( 'form' );
         <?php
-        foreach ( $html as $chunck ) {
+        foreach ( $html as $chunk ) {
             ?>container.append( <?php
                 echo w_json_encode( $chunk );
             ?> );<?php
         }
         ?>$( 'div.settings div.tabs' ).append( $( "form", container ) );
         Settings.LoadProperties( '<?php
-			echo $tab;
-		?>' );<?php
-	}
+            echo $tab;
+        ?>' );<?php
+    }
 ?>
