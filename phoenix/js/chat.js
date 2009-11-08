352ca993b9096ae0cbe5816b82ff1e303adc729c
Aleksis Brezas <abresas@gmail.com>
2009-11-08 04:24:15 +0000

    chat scroll fix

--- phoenix/js/chat.js
+++ phoenix/js/chat.js
@@ -67,6 +67,7 @@ $( function () {
         
         var lis = $( 'ol li' );
         lis[ lis.length - 1 ].scrollIntoView();
+        Frontpage.Shoutbox.BottomScroll = $( 'ol' )[ 0 ].scrollTop();
     };
     f();
     window.onresize = f;
@@ -76,11 +77,17 @@ $( function () {
         if ( Frontpage.Shoutbox.AutoScroll && scrll < Frontpage.Shoutbox.BottomScroll ) { // user scrolled up
             Frontpage.Shoutbox.AutoScroll = false; // disable autoscrolling
             alert( 'auto disabled' );
+            return;
         }
-        else if ( !Frontpage.Shoutbox.AutoScroll && scrll >= Frontpage.Shoutbox.BottomScroll ) { // user scrolled to last known bottom
+        if ( !Frontpage.Shoutbox.AutoScroll && scrll >= Frontpage.Shoutbox.BottomScroll ) { // user scrolled to last known bottom
             Frontpage.Shoutbox.BottomScroll = scrll; // update last known bottom
             Frontpage.Shoutbox.Autoscroll = true;
             alert( 'auto enabled' );
+            return;
+        }
+        if ( scrll > Frontpage.Shoutbox.BottomScroll ) {
+            Frontpage.Shoutbox.BottomScroll = scrll;
+            alert( 'scroll updated' );
         }
     } );
 } );
