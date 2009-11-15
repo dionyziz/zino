13fd266c5d09b5aa31eb2139fe290c5bcecf4b6f
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-11-15 19:57:53 +0000

    spot

--- phoenix/elements/adminpanel/view.php
+++ phoenix/elements/adminpanel/view.php
@@ -64,7 +64,11 @@
                 foreach ( $polls as $poll ) {
                     $domain = str_replace( '*', urlencode( $poll->User->Subdomain ), $xc_settings[ 'usersubdomains' ] );
                     $url = $domain . 'polls/' . $poll->Url;
-	   	    echo "<p>" . $domain . " " . $url . " " . htmlspecialchars( $poll->Question ) . "</p>";
+	   	    echo "<p>" . $url . " " . htmlspecialchars( $poll->Question ) . "</p>";
+		    ?><a href="<?php
+    	            echo $url;
+                    ?>"> - <?php 
+	 	    echo htmlspecialchars( $poll->Question ) . "</p>";
                 }
             ?></div><?php
 
