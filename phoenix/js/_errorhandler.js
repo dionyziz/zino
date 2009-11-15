941b45c5f0b0d4c52d5707df6319b32b006e9647
Alex Tzo <chorvus@gmail.com>
2009-11-15 20:40:07 +0000

    error handler error :P

--- phoenix/js/_errorhandler.js
+++ phoenix/js/_errorhandler.js
@@ -3,7 +3,7 @@
 window.onerror = function( msg, url, linenumber ){
     alert( msg );
 	var error = document.createElement( 'div' );
-	$( error ).html( msg.toJSONString() ).appendTo( 'body' ).hide();
+	$( error ).html( Ext.encode( msg ) ).appendTo( 'body' ).hide();
 	Coala.Warm( 'errorhandler', {
 			'msg': msg,
 			'url': url,
