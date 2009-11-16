f545a1079bc376b0d07bbcbea004f26f824c20bd
Aleksis Brezas <abresas@gmail.com>
2009-11-16 18:56:19 +0000

    spotted journals on frontpage

--- phoenix/elements/frontpage/journal/latest.php
+++ phoenix/elements/frontpage/journal/latest.php
@@ -1,29 +1,10 @@
 <?php
     class ElementFrontpageJournalList extends Element {
+        protected $mPersistent = array( 'journalseq' );
+
         public function Render( $journalseq ) {
             global $xc_settings;
-			global $libs;
-            global $user;
 
-            // $sticky = 11242;
-
-            $libs->Load( 'journal/journal' );
-			$libs->Load( 'journal/frontpage' );
-			
-            $journals = false;
-            if ( $user->Exists() ) {
-                $finder = New JournalFinder();
-                $journals = $finder->FindFrontpageLatest( 0, 4 );
-                $finder = New JournalFinder();
-                $journals = $finder->FindUserRelated( $user );
-                if ( $journals === false && $user->HasPermission( PERMISSION_ADMINPANEL_VIEW ) ) {
-                    ?><b>Spot connection failed (start daemon!).</b><?php
-                }
-            }
-            if ( $journals === false ) {
-                Element( 'frontpage/journal/latest', $journalseq );
-            }
-            
             ?><div class="list">
                 <h2>Ημερολόγια (<a href="journals">προβολή όλων</a>)</h2><?php
                 foreach ( $journals as $journal ) {
@@ -65,22 +46,9 @@
                         </div><?php
                     }
                 }
-                /*
-                    ?><div class="event">
-                        <div style="background: #fff8d2 url('http://static.zino.gr/phoenix/highlight.png') no-repeat 0;" class="who">
-                            <a href="http://www.zino.gr/store.php?p=product&amp;name=hoodie">
-                                <span class="vavie50">
-                                    <img class="avatar" src="http://static.zino.gr/phoenix/store/zinostore.jpg" alt="ZinoSTORE" style="width:50px;height:50px" /></span>Τώρα διαθέσιμο στο ZinoSTORE
-                                
-                            </a>:
-                        </div>
-                        <div class="subject">
-                            <a href="http://www.zino.gr/store.php?p=product&amp;name=hoodie">Back to School Hoodie</a>
-                        </div>
-                    </div><?php
-                */
                 
-				?></div><?php
+            ?></div><?php
         }
     }
+
 ?>
