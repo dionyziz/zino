32af591d8ef632347cf29f9c4993380bfe44dd71
Aleksis Brezas <abresas@gmail.com>
2009-11-19 16:20:21 +0000

    spot update database on production too

--- phoenix/units/backend/spot/comment/created.plasma.php
+++ phoenix/units/backend/spot/comment/created.plasma.php
@@ -3,10 +3,6 @@
         global $libs;
         global $rabbit_settings;
         
-        if ( $rabbit_settings[ 'production' ] ) {
-            return;
-        }
-
         $libs->Load( 'research/spot' );
         
         Spot::CommentCreated( $comment );
