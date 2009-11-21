325c6f17912ebfde4f215a3df1c704fd3cf3eb93
Theodosis Sourgounis <thsourg@gmail.com>
2009-11-21 13:26:19 +0000

    grooveshark

--- phoenix/elements/user/profile/sidebar/_player.php
+++ phoenix/elements/user/profile/sidebar/_player.php
@@ -6,8 +6,8 @@
 				<param name="movie" value="http://listen.grooveshark.com/songWidget.swf"></param> 
 				<param name="wmode" value="window"></param> 
 				<param name="allowScriptAccess" value="always"></param> 
-				<param name="flashvars" value="hostname=cowbell.grooveshark.com&widgetID=16646135&style=metal&p=0"></param> 
-				<embed src="http://listen.grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" width="300" height="40" flashvars="hostname=cowbell.grooveshark.com&widgetID=16646135&style=metal&p=0" allowScriptAccess="always" wmode="window">
+				<param name="flashvars" value="hostname=cowbell.grooveshark.com&amp;idgetID=16646135&amp;style=metal&amp;p=0"></param> 
+				<embed src="http://listen.grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" width="300" height="40" flashvars="hostname=cowbell.grooveshark.com&amp;widgetID=16646135&amp;style=metal&amp;p=0" allowScriptAccess="always" wmode="window">
 				</embed>
 			</object><?php
 	}
