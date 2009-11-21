2b846677c50be3aa5b8af0f81819675ab08623be
Theodosis Sourgounis <thsourg@gmail.com>
2009-11-21 13:32:22 +0000

    grooveshark

--- phoenix/elements/user/profile/sidebar/_player.php
+++ phoenix/elements/user/profile/sidebar/_player.php
@@ -1,15 +1,23 @@
 <?php
     class ElementUserProfileSidebarPlayer extends Element {
         public function Render( $theuser ) {
+		?><div class="mplayer"><?php
+		if( $theuser->Profile->Songwidgetid != -1 ){	
 			?>
 			<object width="300" height="40"> 
 				<param name="movie" value="http://listen.grooveshark.com/songWidget.swf"></param> 
 				<param name="wmode" value="window"></param> 
 				<param name="allowScriptAccess" value="always"></param> 
-				<param name="flashvars" value="hostname=cowbell.grooveshark.com&amp;idgetID=16646135&amp;style=metal&amp;p=0"></param> 
-				<embed src="http://listen.grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" width="300" height="40" flashvars="hostname=cowbell.grooveshark.com&amp;widgetID=16646135&amp;style=metal&amp;p=0" allowScriptAccess="always" wmode="window">
+				<param name="flashvars" value="hostname=cowbell.grooveshark.com&amp;idgetID=<?php
+					echo "16646135";
+				?>&amp;style=metal&amp;p=0"></param> 
+				<embed src="http://listen.grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" width="300" height="40" flashvars="hostname=cowbell.grooveshark.com&amp;widgetID=<?php
+					echo "16646135";
+				?>&amp;style=metal&amp;p=0" allowScriptAccess="always" wmode="window">
 				</embed>
 			</object><?php
+		}
+		?></div><?php
 	}
     }
 ?>
