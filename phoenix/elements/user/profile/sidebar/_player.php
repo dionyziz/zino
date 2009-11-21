54e68f57fbb3eab844f11524aa05945094b75dc5
Theodosis Sourgounis <thsourg@gmail.com>
2009-11-21 13:12:47 +0000

    grooveshark player

--- phoenix/elements/user/profile/sidebar/_player.php
+++ phoenix/elements/user/profile/sidebar/_player.php
@@ -2,9 +2,21 @@
     class ElementUserProfileSidebarPlayer extends Element {
 
         public function Render( $theuser ) {
-		if( $theuser->Profile->Songwidgetid != -1 ){
-			echo $theuser->Profile->Songwidgetid;
-		}
+	//	if( $theuser->Profile->Songwidgetid != -1 ){
+			?>
+			<object width="300" height="40"> 
+				<param name="movie" value="http://listen.grooveshark.com/songWidget.swf"></param> 
+				<param name="wmode" value="window"></param> 
+				<param name="allowScriptAccess" value="always"></param> 
+				<param name="flashvars" value="hostname=cowbell.grooveshark.com&widgetID=<?php
+					echo "16646135";
+				?>&style=metal&p=0"></param> 
+				<embed src="http://listen.grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" width="300" height="40" flashvars="hostname=cowbell.grooveshark.com&widgetID=<?php
+					echo "16646135";
+				?>&style=metal&p=0" allowScriptAccess="always" wmode="window">
+				</embed>
+			</object><?php
+	//	}
 	}
     }
 ?>
