48b82eb48afcd146aad2f2b67558bdf7e21df02c
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-11-21 12:55:18 +0000

    song lib

---
+++ phoenix/libs/music/song.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Song extends Satori {
+		protected $mDbTableAlias = 'song';
+
+		public function LoadDefaults() {
+            global $user;
+
+            $this->Userid = $user->Id;
+			$this->Created = NowDate();
+            return;
+		}
+	}
+
+?>
