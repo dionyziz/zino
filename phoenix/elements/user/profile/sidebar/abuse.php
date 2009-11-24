af0542d64a41b73c28af87ccbcf6d6fddbfb972d
Dionysis Zindros <dionyziz@gmail.com>
2009-11-24 16:30:41 +0000

    Report abuse

--- phoenix/elements/user/profile/sidebar/abuse.php
+++ phoenix/elements/user/profile/sidebar/abuse.php
@@ -9,17 +9,24 @@
                     <p><strong>Η αναφορά αυτή είναι εμπιστευτική.</strong></p>
                     <p>Αν δεν συμφωνείς με τις του, αυτός δεν είναι λόγος για να τον αναφέρεις.
 					Θα ελέγξουμε την περίπτωση παραβίασης και θα παρέμβουμε μόνο αν πραγματικά υπάρχει.</p>
-                    <label for="reportreason">Αιτιολογία:</label>
-                    <select name="reportreason" id="reportreason">
-                        <option>Επέλεξε μία</option>
-                        <option>Ανάρτηση πορνογραφικού υλικού</option>
-                        <option>Ρατσιστική επίθεση</option>
-                        <option>Ανάρτηση διαφημιστικού ή επαναλαμβανόμενου περιεχομένου (spam)</option>
-                        <option>Λογαριασμός που δεν αντιστοιχεί σε πραγματικό πρόσωπο (fake)</option>
-                        <option>Παρενόχληση ανηλίκου</option>
-                    </select>
-                    <label for="reportcomments">Σχόλια:</label>
-                    <textarea name="reportcomments" id="reportcomments"></textarea>
+					
+					<div>
+						<label for="reportreason">Αιτιολογία:</label>
+						<select name="reportreason" id="reportreason">
+							<option>Επέλεξε μία</option>
+							<option>Ανάρτηση πορνογραφικού υλικού</option>
+							<option>Ρατσιστική επίθεση</option>
+							<option>Ανάρτηση διαφημιστικού ή επαναλαμβανόμενου περιεχομένου (spam)</option>
+							<option>Λογαριασμός που δεν αντιστοιχεί σε πραγματικό πρόσωπο (fake)</option>
+							<option>Παρενόχληση ανηλίκου</option>
+						</select>
+					</div>
+					
+					<div>
+						<label for="reportcomments">Σχόλια:</label>
+						<textarea name="reportcomments" id="reportcomments"></textarea>
+					</div>
+					
                     <div class="buttons">
                         <a href="" class="button">Αποστολή Αναφοράς</a>
                         <a href="" class="button">Ακύρωση</a>
