e2ac6ec64db76dda630f0ff3e23ae3dc773b51b3
Petros Aggelatos <petrosagg@gmail.com>
2009-11-24 19:09:48 +0000

    Player back in place

--- phoenix/elements/user/profile/sidebar/details.php
+++ phoenix/elements/user/profile/sidebar/details.php
@@ -15,8 +15,11 @@
             ?></div>
             <div class="social"><?php
                 Element( 'user/profile/sidebar/social/view' , $theuser );
-            ?></div><?php
-			?><div class="aboutme"><?php
+            ?></div>
+			<div class="mplayer"><?php
+				Element( 'user/profile/sidebar/player', $theuser );
+			?></div>
+			<div class="aboutme"><?php
                 Element( 'user/profile/sidebar/aboutme' , $profile->Aboutme );
             ?></div>
             <div class="interests"><?php
