2415c54c67c82f4545d362f927e2f16ac7a699d1
Dionysis Zindros <dionyziz@gmail.com>
2009-12-06 19:32:39 +0000

    private chat history

---
+++ phoenix/units/chat/history.php
@@ -0,0 +1,35 @@
+<?php
+    function UnitChatHistory( tInteger $channelid, tInteger $offset, tCoalaPointer $f ) {
+        global $libs;
+        global $user;
+
+        $libs->Load( 'chat/message' );
+        $libs->Load( 'chat/channel' );
+
+        $channelid = $channelid->Get();
+        $offset = $offset->Get();
+
+        $finder = New ShoutboxFinder();
+        $messages = $finder->FindByChannel( $channelid, $offset, 50 );
+
+        if ( !ChannelFinder::Auth( $channelid, $user->Id ) ) {
+            return;
+        }
+
+        $data = array();
+        foreach ( $messages as $message ) {
+            $data[] = array(
+                'who' => array(
+                    'name' => $message->User->Name
+                ),
+                'text' => $message->Text,
+                'created' => $message->Created
+            );
+        }
+
+        echo $f;
+        ?>(<?php
+        echo w_json_encode( $data );
+        ?>);<?php
+    }
+?>
