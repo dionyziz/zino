754619ed0ddd17b2b3b288ba261055aceea8c766
Dionysis Zindros <dionyziz@gmail.com>
2009-12-06 23:03:40 +0000

    video chat

--- phoenix/units/chat/video/start.do.php
+++ phoenix/units/chat/video/start.do.php
@@ -9,14 +9,17 @@
         $libs->Load( 'chat/channel' );
 
         if ( !$user->Exists() ) { // we must be logged in
+            ?>alert( 'Please login' );<?php
             return;
         }
         if ( !ChannelFinder::Auth( $channelid, $user->Id ) ) { // we must be in the channel
+            ?>alert( 'Please try a different channel' );<?php
             return;
         }
 
         $participants = ChannelFinder::FindParticipantsByChannel( $channelid );
         if ( count( $participants ) != 2 ) {
+            ?>alert( 'Only one-to-one channels are allowed for now' );<?php
             return; // only one-to-one video for now
         }
 
