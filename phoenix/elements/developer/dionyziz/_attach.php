d732f520ef5819744fd776cce653f846bb8bb94e
Dionysis Zindros <dionyziz@gmail.com>
2009-12-07 00:50:11 +0000

    minimal testcase

--- phoenix/elements/developer/dionyziz/_attach.php
+++ phoenix/elements/developer/dionyziz/_attach.php
@@ -3,7 +3,9 @@
         public function Render() {
             global $page;
 
+            ?>This text should be inside the content div.<?php
             $page->AttachInlineScript( 'alert( "Hello" );' );
+            ?> This text should too be inside the content div.<?php
         }
     }
 ?>
