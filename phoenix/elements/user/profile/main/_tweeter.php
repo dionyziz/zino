bade386090fd0d2d8e3eaed52eeac4de96e814ff
Alex Tzo <chorvus@gmail.com>
2009-12-09 10:31:06 +0000

    tweet edit modal test

--- phoenix/elements/user/profile/main/_tweeter.php
+++ phoenix/elements/user/profile/main/_tweeter.php
@@ -53,8 +53,8 @@
                     <i style="float:left;" class="right corner">&nbsp;</i>
                 </div><?php
                 if ( $theuser->Id == $user->Id ) {
-                    ?><div id="tweetedit">
-                        <h3 class="modaltitle">Τι κάνεις τώρα;</h3>
+                    ?><div id="tweetedit" class="modal">
+                        <h3>Τι κάνεις τώρα;</h3>
                         <form>
                             <div class="input"><?php
                                 if ( $theuser->Gender == 'f' ) {
