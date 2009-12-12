a6678c941962171360e78ea14c422cf855c9d0d4
Alex Tzo <chorvus@gmail.com>
2009-12-12 03:19:15 +0000

    settings javascript

--- phoenix/units/user/settings/_tab.php
+++ phoenix/units/user/settings/_tab.php
@@ -44,7 +44,7 @@
                 return;
         }
         //a little hack to handle the huge tab elements
-        $html = str_split( ob_get_clean(), 1500 );
+        $html = str_split( ob_get_clean(), 2048 );
         ?>
         buffer = ""<?php
         foreach ( $html as $chunk ) {
