bdddd01f1d6608b7cd249d302f9d57cc026b5d6b
Alex Tzo <chorvus@gmail.com>
2009-12-18 14:14:44 +0000

    ie settings fix

--- phoenix/js/_modal.js
+++ phoenix/js/_modal.js
@@ -20,6 +20,7 @@
                 } ( this )
             );*/
             _close = document.createElement( 'span' );
+            close = document.createElement( 'span' );
             //close = jQuery.create( "span", { 'class': 'close' } );
             $( _close ).addClass( 'close' );
             this.append( _close );
