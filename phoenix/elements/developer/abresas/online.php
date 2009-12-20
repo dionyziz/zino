381b356d37fd36f1eefe20be4ae5e3a28af0fad3
Aleksis Brezas <abresas@gmail.com>
2009-12-20 18:06:17 +0000

    online now with mood and slogan

--- phoenix/elements/developer/abresas/online.php
+++ phoenix/elements/developer/abresas/online.php
@@ -6,13 +6,14 @@
             global $xc_settings;
 
             $libs->Load( 'user/user' );
+            $libs->Load( 'image/image' );
 
             $finder = New UserFinder();
             $onlineUsers = $finder->FindOnlineWithDetails();
 
             foreach ( $onlineUsers as $onlineUser ) {
                 ?><div id="float: left;"><?php
-                Element( 'user/avatar', $onlineUser[ 'image_id' ], $onlineUser[ 'user_id' ], $onlineUser[ 'image_width' ], $onlineUser[ 'image_height' ], $onlineUser[ 'user_name' ], IMAGE_CROPPED_100x100 );
+                Element( 'user/avatar', $onlineUser[ 'image_id' ], $onlineUser[ 'user_id' ], 100, 100, $onlineUser[ 'user_name' ], IMAGE_CROPPED_100x100, '', '', true, 100, 100 );
                 ?></div><div><a href="<?php
                 Element( 'user/url', $onlineUser[ 'user_id' ], $onlineUser[ 'user_subdomain' ] );
                 ?>"><?php
