82b3e1904a56f8ed7f4b6472f099560429599551
Aleksis Brezas <abresas@gmail.com>
2009-12-20 18:03:45 +0000

    online now with mood and slogan

--- phoenix/elements/developer/abresas/online.php
+++ phoenix/elements/developer/abresas/online.php
@@ -12,7 +12,7 @@
 
             foreach ( $onlineUsers as $onlineUser ) {
                 ?><div id="float: left;"><?php
-                // Element( 'user/avatar', $onlineUser[ 'image_id' ], $onlineUser[ 'user_id' ], $onlineUser[ 'image_width' ], $onlineUser[ 'image_height' ], $onlineUser[ 'user_name' ], IMAGE_CROPPED_100x100 );
+                Element( 'user/avatar', $onlineUser[ 'image_id' ], $onlineUser[ 'user_id' ], $onlineUser[ 'image_width' ], $onlineUser[ 'image_height' ], $onlineUser[ 'user_name' ], IMAGE_CROPPED_100x100 );
                 ?></div><div><a href="<?php
                 Element( 'user/url', $onlineUser[ 'user_id' ], $onlineUser[ 'user_subdomain' ] );
                 ?>"><?php
