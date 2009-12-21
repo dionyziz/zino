b4916bd93e3220f14e2c59fcae9d616a899abaf5
Giorgos Panagiotakos <pagio91i@gmail.com>
2009-12-21 11:19:49 +0000

    content

--- phoenix/libs/content.php
+++ phoenix/libs/content.php
@@ -6,16 +6,43 @@
 
         $libs->Load( 'research/spot' );
         $libs->Load( 'comment' );
+        $libs->Load( 'poll/frontpage' );
+        $libs->Load( 'poll/poll' );
+        $libs->Load( 'journal/journal' );
+		$libs->Load( 'journal/frontpage' );
+        $libs->Load( 'image/image' );
 
         $content = Spot::GetContent( $user, 8, 5, 4 );
+        
+        if ( count( $content ) == 0 ) { // if spot is down , find the newest content    
+                        
+            $finder = New PollFinder();        
+            $polls = $finder->FindFrontpageLatest( 0 , 4 );
+            $finder = New JournalFinder( 0, 5 );
+            $journals = $finder->FindFrontpageLatest(); 
+            $finder = New ImageFinder();
+            $images = $finder->FindFrontpageLatest( 0, 8 );
+            foreach ( $polls as $obj ) {
+                $content[] = $obj;
+            }
+            foreach ( $journals as $obj ) {
+                $content[] = $obj;
+            }
+            foreach ( $images as $obj ) {
+                $content[] = $obj;
+            }
+            $polls = array();
+            $images = array();
+            $journals = array();            
+        }
+
 
         $comments = array();
         $res = array();
         $items = array();
         $commfinder = new CommentFinder();        
         foreach ( $content as $object ) {
-            $comments[ $object->Id ] = $commfinder->FindByTypeidAndItemid( Type_FromObject( $object ), $object->Id, 0, 3 );
-            // comments , Text , User Name            
+            $comments[ $object->Id ] = $commfinder->FindByTypeidAndItemid( Type_FromObject( $object ), $object->Id, 0, 3 );        
             foreach ( $comments[ $object->Id ] as $comment ) {
                 $items[] = $comment;
             }            
