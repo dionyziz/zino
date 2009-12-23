54e691982063fa84e3f3464ed9d0ced10718293f
Dionysis Zindros <dionyziz@gmail.com>
2009-12-23 15:06:35 +0000

    meeting

--- phoenix/elements/frontpage/shoutbox/list.php
+++ phoenix/elements/frontpage/shoutbox/list.php
@@ -4,7 +4,9 @@
             global $user;
 
 
-            ?><h2 class="subheading">Συζήτηση <span class="small1">(<a href="?p=chat">μεγιστοποίηση</a>)</span></h2>
+            ?>
+            <h2 class="subheading" style="color:black"><a href="http://www.zino.gr/?p=journal&amp;id=11690">Zino Meeting στο Θυμωμένο Πορτρέτο στα Γιάννενα</a></h2>
+            <h2 class="subheading">Συζήτηση <span class="small1">(<a href="?p=chat">μεγιστοποίηση</a>)</span></h2>
             <div class="comments"><?php
                 if ( $user->Exists() && $user->HasPermission( PERMISSION_SHOUTBOX_CREATE ) ) {
                     Element( 'shoutbox/reply' , $user->Id , $user->Avatarid , $user );
