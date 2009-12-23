aec04f56490e76c6c00f46f7c8958a2918770a90
Alex Tzo <chorvus@gmail.com>
2009-12-23 07:37:16 +0000

    Store manager

--- phoenix/elements/store/manager.php
+++ phoenix/elements/store/manager.php
@@ -24,27 +24,29 @@
             $purchases = $prcfinder->FindByItemId( $id );
 
             foreach ( $purchases as $purchase ) {
-                ?><tr><td><?php
-                    echo $purchase->User->Name;
-                ?></td><td><?php
-                    echo $purchase->Created;
-                ?></td><td><?php
-                    echo $purchase->User->Profile->Location;
-                ?>-<?php
-                    echo $purchase->User->Profile->Area;
-                ?></td><td><?php
-                    echo $purchase->User->Profile->Address;
-                ?> <?php
-                    echo $purchase->User->Profile->Addressnum;
-                ?> <?php
-                    echo $purchase->User->Profile->Postcode;
-                ?></td><td><?php
-                    echo $purchase->User->Profile->Firstname;
-                ?> <?php
-                    echo $purchase->User->Profile->Lastname;
-                ?></td><td><?php
-                    echo $purchase->User->Profile->Mobile;
-                ?></td></tr><?php
+                if ( $purchase->User->Exists() ) {
+                        ?><tr><td><?php
+                        echo $purchase->User->Name;
+                    ?></td><td><?php
+                        echo $purchase->Created;
+                    ?></td><td><?php
+                        echo $purchase->User->Profile->Location;
+                    ?>-<?php
+                        echo $purchase->User->Profile->Area;
+                    ?></td><td><?php
+                        echo $purchase->User->Profile->Address;
+                    ?> <?php
+                        echo $purchase->User->Profile->Addressnum;
+                    ?> <?php
+                        echo $purchase->User->Profile->Postcode;
+                    ?></td><td><?php
+                        echo $purchase->User->Profile->Firstname;
+                    ?> <?php
+                        echo $purchase->User->Profile->Lastname;
+                    ?></td><td><?php
+                        echo $purchase->User->Profile->Mobile;
+                    ?></td></tr><?php
+                }
             }
             ?></table><?php
         }
