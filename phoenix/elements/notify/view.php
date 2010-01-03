92e9998e9a0b32a30f461b14502d83b7d29d0ae0
Alex Tzo <chorvus@gmail.com>
2010-01-03 15:21:06 +0000

    Dashboard Notifications

--- phoenix/elements/notify/view.php
+++ phoenix/elements/notify/view.php
@@ -4,6 +4,23 @@
             if ( !$notif->Exists() || !$notif->Item->Exists() ) {
                 return;
             }
+            
+            ?><li>
+                <a href="" class="remove" alt="X" title="Απόκρυψη ενημέρωσης">X</a>
+                <?php
+                switch ( $notif->Typeid ) {
+                    case EVENT_COMMENT_CREATED:
+                        Element( 'notify/type/comment', $notif->Item );
+                    case EVENT_FRIENDRELATION_CREATED:
+                        Element( 'notify/type/relation' );
+                    case EVENT_IMAGETAG_CREATED:
+                        Element( 'notify/type/imagetag' );
+                    case EVENT_FAVOURITE_CREATED:
+                        Element( 'notify/type/favourite' );
+                    case EVENT_USER_BIRTHDAY:
+                        Element( 'notify/type/birthday' );
+                }
+            ?></li><?php
         }
     }
 ?>
