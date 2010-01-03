546f76b5398d741e885d38d31ac2a310a694e727
Alex Tzo <chorvus@gmail.com>
2010-01-03 14:52:36 +0000

    Dashboard Notifications

--- phoenix/elements/dashboard/notifications.php
+++ phoenix/elements/dashboard/notifications.php
@@ -1,6 +1,16 @@
 <?php
     class ElementDashboardNotifications extends Element {
         public function Render() {
+            global $user;
+            global $libs;
+            
+            $libs->Load( 'notify/notify' );
+            
+            $finder = New NotificationFinder();
+            $notifs = $finder->FindByUser( $user );
+            
+            $notifycount = $notifs->TotalCount() > 0;
+            
             ?><div id="notifications">
                 <div style="" class="shadow-left"></div>
                 <div style="" class="shadow-right"></div>
@@ -13,81 +23,25 @@
                     <div class="border">
                         <a href="notifications" class="maximize" title="Μεγιστοποίηση"></a>
                         <a href="notifications" class="minimize" title="Ελαχιστοποίηση"></a>
-                        <h2>14 νέες ενημερώσεις</h2>
-
+                        <h2><?php
+                            if ( $notifycount == 0 ) {
+                                echo 'καμία ενημέρωση';
+                            }
+                            elseif ( $notifycount == 1 ) {
+                                echo '1 ενημέρωση';
+                            }
+                            else {
+                                if ( $notifycount <= 10 ) {
+                                    echo $notifycount;
+                                }
+                                else {
+                                    echo '10+';
+                                }
+                                echo ' νέες ενημερώσεις';
+                            }
+                        ?></h2>
                         <ol>
-                            <li>
-                                <a href="" class="remove" alt="X" title="Απόκρυψη ενημέρωσης">X</a>
-                                <a href="" class="item">
-                                    <div class="avatar">
-                                        <div class="tl corner"></div>
-                                        <div class="tr corner"></div>
-                                        <div class="bl corner"></div>
 
-                                        <div class="br corner"></div>
-                                        <img src="http://images2.zino.gr/media/5260/225368/225368_100.jpg" alt="funeral" />
-                                    </div>
-                                    <strong>funeral</strong> Καλά είσαι και βλάκας παιδί μου...
-                                </a>
-                            </li>
-                            <li>
-                                <a href="" class="remove" alt="X" title="Απόκρυψη ενημέρωσης">X</a>
-
-                                <a href="" class="item">
-                                    <div class="avatar">
-                                        <div class="tl corner"></div>
-                                        <div class="tr corner"></div>
-                                        <div class="bl corner"></div>
-                                        <div class="br corner"></div>
-                                        <img src="http://images2.zino.gr/media/4856/184764/184764_100.jpg" alt="ronaldo7" />
-                                    </div>
-                                    <strong>ronaldo7</strong> Nice pic!
-                                </a>
-
-                            </li>
-                            <li>
-                                <a href="" class="remove" alt="X" title="Απόκρυψη ενημέρωσης">X</a>
-                                <a href="" class="item">
-                                    <div class="avatar">
-                                        <div class="tl corner"></div>
-                                        <div class="tr corner"></div>
-                                        <div class="bl corner"></div>
-
-                                        <div class="br corner"></div>
-                                        <img src="http://images2.zino.gr/media/4451/222617/222617_100.jpg" alt="zizou" />
-                                    </div>
-                                    <strong>zizou</strong> OMG! LOL
-                                </a>
-                            </li>
-                            <li>
-                                <a href="" class="remove" alt="X" title="Απόκρυψη ενημέρωσης">X</a>
-
-                                <a href="" class="item">
-                                    <div class="avatar">
-                                        <div class="tl corner"></div>
-                                        <div class="tr corner"></div>
-                                        <div class="bl corner"></div>
-                                        <div class="br corner"></div>
-                                        <img src="http://images2.zino.gr/media/11201/192216/192216_100.jpg" alt="gianniZzZ" />
-                                    </div>
-                                    <strong>gianniZzZ</strong> Τι λέει ρε συ;
-                                </a>
-
-                            </li>
-                            <li>
-                                <a href="" class="remove" alt="X" title="Απόκρυψη ενημέρωσης">X</a>
-                                <a href="" class="item">
-                                    <div class="avatar">
-                                        <div class="tl corner"></div>
-                                        <div class="tr corner"></div>
-                                        <div class="bl corner"></div>
-
-                                        <div class="br corner"></div>
-                                        <img src="http://images2.zino.gr/media/4427/223555/223555_100.jpg" alt="kwNnayO" />
-                                    </div>
-                                    Η <strong>kwNnayO</strong> αγαπάει την φωτογραφία σου.
-                                </a>
-                            </li>
                         </ol>
                     </div>
 
