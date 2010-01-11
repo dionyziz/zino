5d7dc6ddd5d3e8e4552bc36e8154e2ee1f282cfa
Alex Tzo <chorvus@gmail.com>
2010-01-11 11:11:40 +0000

    Dashboard Notifications

--- phoenix/elements/notify/view.php
+++ phoenix/elements/notify/view.php
@@ -8,18 +8,22 @@
             ?><li>
                 <a href="" class="remove" alt="X" title="Απόκρυψη ενημέρωσης">X</a>
                 <?php
-                echo (string) $notif->Typeid . "\n";
                 switch ( $notif->Typeid ) {
                     case EVENT_COMMENT_CREATED:
                         Element( 'notify/type/comment', $notif->Item );
+                        break;
                     case EVENT_FRIENDRELATION_CREATED:
                         Element( 'notify/type/relation', $notif );
+                        break;
                     case EVENT_IMAGETAG_CREATED:
                         Element( 'notify/type/imagetag' );
+                        break;
                     case EVENT_FAVOURITE_CREATED:
                         Element( 'notify/type/favourite' );
+                        break;
                     case EVENT_USER_BIRTHDAY:
                         Element( 'notify/type/birthday' );
+                        break;
                 }
             ?></li><?php 
         }
