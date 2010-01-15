0d7ae7979ce969108c18788dfb5c037a98d0ce86
Dionysis Zindros <dionyziz@gmail.com>
2010-01-15 14:10:48 +0000

    Christmas are over

--- phoenix/elements/footer.php
+++ phoenix/elements/footer.php
@@ -11,10 +11,6 @@
             <div id="copyleft">
                 <span>&copy; 2010</span> <a class="wlink" href="http://www.kamibu.com/">Kamibu</a>
             </div>
-            <div id="xmas1"></div>
-            <div id="xmas2"></div>
-            <div id="xmas3"></div>
-            <div id="xmas4"></div>
             <?php
         }
     }
