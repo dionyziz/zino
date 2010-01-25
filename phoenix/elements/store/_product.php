13326d12d4439533e57a643805da1bc7542836d0
Alex Tzo <chorvus@gmail.com>
2010-01-25 05:32:20 +0000

    Store Actionshots

--- phoenix/elements/store/_product.php
+++ phoenix/elements/store/_product.php
@@ -141,6 +141,7 @@
                 <div class="eof"></div>
                 <?php
                     $shotfinder = New StoreActionshotFinder();
+                    die( 'DIE POTATO!' );
                     $shots = $shotfinder->FindByItemId( $item->Id );
                     
                     ?><ul class="lst ul2 border"><?php
