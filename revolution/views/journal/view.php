e474f9eb333086f531b88182a7f04392b9c7b2e0
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-03-10 13:29:30 +0000

    journals

---
+++ revolution/views/journal/view.php
@@ -0,0 +1,20 @@
+<entry id="<?= $journal[ 'id' ] ?>">
+    <title><?= htmlspecialchars( $journal[ 'title' ] ) ?></title>
+    <author>
+        <name><?= $journal[ 'username' ] ?></name>
+        <subdomain><?= $journal[ 'subdomain' ] ?></subdomain>
+        <gender><?= $journal[ 'gender' ] ?></gender>
+        <avatar>
+            <media url="http://images2.zino.gr/media/<?= $journal[ 'userid' ] ?>/<?= $journal[ 'avatarid' ] ?>/<?= $poll[ 'avatarid' ] ?>_100.jpg" />
+        </avatar>
+    </author>
+    <published><?= $journal[ 'created' ] ?></published>
+    <? include 'views/comment/listing.php'; ?>
+    <? if ( !empty( $favourites ) ): ?>
+    <favourites count="<?= count( $favourites ) ?>">
+        <? foreach ( $favourites as $favourite ): ?>
+        <user><name><?= $favourite[ 'username' ] ?></name></user>
+        <? endforeach; ?>
+    </favourites>
+    <? endif; ?>
+</entry>
