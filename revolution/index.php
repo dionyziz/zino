f017fb3eb59a3b561b6f5c20b9768aed9c0e44de
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-03-10 13:35:48 +0000

    asdsad

--- revolution/index.php
+++ revolution/index.php
@@ -10,7 +10,7 @@
 	unset( $_GET[ 'resource' ], $_GET[ 'method' ] );
 
 	in_array( $resource, array(
-        'photo', 'session', 'comment', 'favourite', 'poll'
+        'photo', 'session', 'comment', 'favourite', 'poll', 'journal'
     ) ) or $resource = 'photo';
 
 	if ( $method == 'create' || $method == 'delete' || $method == 'update' ) {
