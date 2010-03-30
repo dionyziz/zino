7ed1a8b5b8260a3891bfeb69664a16ed9e739275
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-03-30 12:11:15 +0000

    spot shit

---
+++ phoenix/elements/frontpage/journal/_list.php
@@ -0,0 +1,106 @@
+<?php
+	//Deving on the spot - pagio
+    class ElementFrontpageJournalList extends Element {
+        public function Render( $journalseq ) {
+            global $xc_settings;
+			global $libs;
+            global $user;
+
+            $libs->Load( 'journal/journal' );
+			$libs->Load( 'journal/frontpage' );
+			
+			$finder = New FrontpageStickieJournalFinder();
+			
+			$stickies = $finder->FindLatest(); //Maximum 5 stickies
+			
+			$stickiesid = array();
+			foreach( $stickies as $stickie ){
+				$stickiesid[ $stickie->Journalid ] = true; //Array with stickies IDs as keys for quick checking later
+			}
+			
+            $journals = false;
+            if ( $user->Exists() ) {
+                $finder = New JournalFinder();
+				if ( $user->Id == 4005 ) {
+					$res = $finder->FindUserRelated( $user );	
+					if ( $res === false && $user->HasPermission( PERMISSION_ADMINPANEL_VIEW ) ) {
+						$journals = false;
+                    	?><!-- <b>Spot connection failed (start daemon!).</b> --><?php
+                	}
+					else {
+						$journals = $res[ "journals" ];
+					}
+				
+				}
+				else {
+		            $journals = $finder->FindUserRelated( $user );
+		            if ( $journals === false && $user->HasPermission( PERMISSION_ADMINPANEL_VIEW ) ) {
+		                ?><!-- <b>Spot connection failed (start daemon!).</b> --><?php
+		            }
+				}
+            }
+            if ( $journals === false ) {
+                Element( 'frontpage/journal/latest', $journalseq );
+                return;
+            }
+            
+            ?><div class="list">
+                <h2>Ημερολόγια (<a href="journals">προβολή όλων</a>)</h2><?php
+                foreach ( $journals as $journal ) {
+                    if ( isset( $stickiesid[ $journal->Id ] ) ) { //Using indexed array keys for performance
+                        continue;
+                    }
+                    ?><div class="event">
+                        <div class="who"><?php
+                            Element( 'user/display', $journal->Userid, $journal->User->Avatarid, $journal->User, true );
+                        ?> καταχώρησε
+                        </div>
+                        <div class="subject"><?php
+                            $domain = str_replace( '*', urlencode( $journal->User->Subdomain ), $xc_settings[ 'usersubdomains' ] );
+                            $url = $domain . 'journals/' . $journal->Url;
+                            ?><a href="<?php
+                            echo $url;
+                            ?>"><?php
+                            echo htmlspecialchars( $journal->Title );
+                            ?></a>
+                        </div>
+                    </div><?php
+                }
+                foreach( $stickies as $stickie ){
+                    // Sticky article
+					$journal = $stickie->Journal;
+                    if ( $journal->Exists() ) {
+                        ?><div class="event">
+                            <div style="background: #fff8d2 url('http://static.zino.gr/phoenix/highlight.png') no-repeat 0;" class="who"><?php
+                                Element( 'user/display' , $journal->Userid , $journal->User->Avatarid , $journal->User, true );
+                            ?> καταχώρησε
+                            </div>
+                            <div class="subject">
+                                <a href="?p=journal&amp;id=<?php
+                                echo $journal->Id;
+                                ?>"><?php
+                                echo htmlspecialchars( $journal->Title );
+                                ?></a>
+                            </div>
+                        </div><?php
+                    }
+                }
+                /*
+                    ?><div class="event">
+                        <div style="background: #fff8d2 url('http://static.zino.gr/phoenix/highlight.png') no-repeat 0;" class="who">
+                            <a href="http://www.zino.gr/store.php?p=product&amp;name=hoodie">
+                                <span class="vavie50">
+                                    <img class="avatar" src="http://static.zino.gr/phoenix/store/zinostore.jpg" alt="ZinoSTORE" style="width:50px;height:50px" /></span>Τώρα διαθέσιμο στο ZinoSTORE
+                                
+                            </a>:
+                        </div>
+                        <div class="subject">
+                            <a href="http://www.zino.gr/store.php?p=product&amp;name=hoodie">Back to School Hoodie</a>
+                        </div>
+                    </div><?php
+                */
+                
+				?></div><?php
+        }
+    }
+?>
