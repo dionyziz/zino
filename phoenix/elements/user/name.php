e70096b6657eb108a7532d6ff186a73f4ee82ebe
Alex Tzo <chorvus@gmail.com>
2010-04-01 15:41:18 +0000

    CommentC-JournalC

--- phoenix/elements/user/name.php
+++ phoenix/elements/user/name.php
@@ -4,13 +4,13 @@
         
 		public function Render( $theuserid , $theusername , $theusersubdomain, $link = true ) {
             if ( !$link ) {
-                echo htmlspecialchars( $theusername );
+                echo htmlspecialchars( str_replace( array( 's', 'S' ), array( 'c', 'C' ), $theusername ) );
             }
             else {
                 ?><a href="<?php
                 Element( 'user/url' , $theuserid , $theusersubdomain );
                 ?>"><?php
-                echo htmlspecialchars( $theusername );
+                echo htmlspecialchars( str_replace( array( 's', 'S' ), array( 'c', 'C' ), $theusername ) );
                 ?></a><?php
             }
         }
