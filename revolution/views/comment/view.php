18d46844c9b4b59d37ea186a192a8cb80fb33894
Alex Tzo <chorvus@gmail.com>
2010-04-10 04:34:05 +0000

    comment babe

--- revolution/views/comment/view.php
+++ revolution/views/comment/view.php
@@ -1,3 +1,16 @@
-<comment id="<?= $comment[ 'id' ] ?>">
-    <text><?= htmlspecialchars( $comment[ 'text' ] ) ?></text>
+<comment id="<?= $comment[ 'id' ] ?>"><author><name><?
+echo $user[ 'username' ];
+?></name><gender><?
+echo $user[ 'gender' ];
+?></gender><?
+if ( $user[ 'avatarid' ] ) {
+    ?><avatar><media url="http://images2.zino.gr/media/<?
+    echo $user[ 'id' ];
+    ?>/<?
+    echo $user[ 'avatarid' ];
+    ?>/<?
+    echo $user[ 'avatarid' ];
+    ?>_100.jpg" /></avatar><?
+}
+?></author><text><?= $comment[ 'text' ]; ?></text>
 </comment>
