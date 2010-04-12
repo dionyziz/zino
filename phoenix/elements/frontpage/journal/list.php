a463852905363d6bae75980075859f0832bff5b2
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-04-12 19:47:33 +0000

    Spot testers ..

--- phoenix/elements/frontpage/journal/list.php
+++ phoenix/elements/frontpage/journal/list.php
@@ -22,7 +22,7 @@
             $journals = false;
             if ( $user->Exists() ) {
                 $finder = New JournalFinder();
-                if ( $user->Id == 4005 || $user->Id == 5104 || $user->Id == 658 || $user->Id == 1 ) {
+                if ( $user->Id == 4005 || $user->Id == 5104 || $user->Id == 658 || $user->Id == 1 || $user->Id == 5181 || $user->Id == 3890 ) {
                     $res = $finder->FindUserRelated( $user, true );
                     
                     $journals = $res[ 'journals' ];
