e4d0274133c27016f121bd09fd56ce038ea83964
Aleksis Brezas <abresas@gmail.com>
2010-04-17 10:53:39 +0000

    introspection benchmark

--- revolution/etc/benchmarks/introspection.php
+++ revolution/etc/benchmarks/introspection.php
@@ -1,9 +1,16 @@
+#!/usr/bin/php
 <?php
 
     // usage: $ time php introspection.php
     // change the function call on the for loop to change benchmark function
 
-    function testfunc( $color, $iq, $size ) {
+    function usage() {
+        echo "Usage: \n";
+        echo "$ time ./introspection.php type\n";
+        echo "type = intro or call\n";
+    }
+
+    function testfunc( $color, $iq, $size, $parts, $p4, $p5, $p6, $p7, $p8, $p9 ) {
         $magic = 0;
         if ( $color == 'red' ) {
             $magic = $iq * $size;
@@ -11,11 +18,22 @@
         else {
             $magic = $iq / $size;
         }
+        $magic += count( $parts ) + $p4 + $p5 + $p6 - $p7 - $p8 - $p9;
         return $magic;
     }  
 
     function introspect() {
-        $arr = array( 'color' => 'green', 'iq' => 2125, 'size' => 42 );
+        $arr = array( 
+            'color' => 'green', 
+            'iq' => 2125, 
+            'size' => 42, 
+            'parts' => array( 'head', 'hands', 'feet', 'body' ),
+            'p4' => 12,
+            'p5' => 22,
+            'p6' => 27,
+            'p7' => 33,
+            'p8' => -12,
+            'p9' => 42 );
         $func = New ReflectionFunction( 'testfunc' );
         
         $params = array();
@@ -31,13 +49,38 @@
     }
 
     function call() {
-        $arr = array( 'color' => 'green', $iq = 2125, $size = 42 );
+        $arr = array( 
+            'color' => 'green', 
+            'iq' => 2125, 
+            'size' => 42, 
+            'parts' => array( 'head', 'hands', 'feet', 'body' ),
+            'p4' => 12,
+            'p5' => 22,
+            'p6' => 27,
+            'p7' => 33,
+            'p8' => -12,
+            'p9' => 42 );
         return call_user_func_array( 'testfunc', $arr );
     }
 
-    for ( $i = 0; $i < 10000; ++$i ) {
-        introspect();
-        // call(); 
+    if ( $argc < 2 ) {
+        echo "Error: too few arguments\n";
+        usage();
+    }
+
+    if ( $argv[ 1 ] == 'intro' ) {
+        for ( $i = 0; $i < 10000; ++$i ) {
+                introspect();
+        }
+    }
+    else if ( $argv[ 1 ] == 'call' ) {
+        for ( $i = 0; $i < 10000; ++$i ) {
+            call(); 
+        }
+    }
+    else {
+        echo "Error: unknown type\n";
+        usage();
     }
 
 ?>
