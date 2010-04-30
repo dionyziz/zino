0c479595b2ea0dacb98bef5a808eeb02ee570418
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-04-30 22:14:59 +0000

    friend model

---
+++ revolution/models/friend.php
@@ -0,0 +1,25 @@
+<?php
+    class Friend {
+        /*
+            define( 'FRIENDS_NONE', 0 );
+            define( 'FRIENDS_A_HAS_B', 1 );
+            define( 'FRIENDS_B_HAS_A', 2 );
+            define( 'FRIENDS_BOTH', FRIENDS_A_HAS_B | FRIENDS_B_HAS_A );
+        */
+        public static function Create( $userid, $friendid, $typeid ) {
+            is_int( $userid ) or die( 'userid is not an integer' );
+            is_int( $friend ) or die( 'friendid is not an integer' );
+            is_int( $typeid ) or die( 'typeid is not an integer' );
+
+            db( 'INSERT INTO `relations` ( `relation_userid`, `relation_friendid`, `relation_typeid`, `relation_created` )
+                VALUES ( :userid, :friendid, :typeid, NOW() )',
+                compact( 'userid', 'friendid', 'typeid' ) );
+        }
+        public static function Delete( $userid, $friendid ) {
+            return db( 'DELETE FROM `relations` 
+                       WHERE `relation_userid` = :userid 
+                       AND `relation_friendid` = :friendid', 
+                       compact( 'userid', 'friendid' ) );
+        }
+    }
+?>
