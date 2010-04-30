573fb63608d9a06820fe07bed7001fd24c61685e
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-04-30 22:59:56 +0000

    friend things

--- revolution/index.php
+++ revolution/index.php
@@ -10,7 +10,7 @@
 
 	in_array( $resource, array(
         'photo', 'session', 'comment', 'favourite', 'poll', 'journal', 'pollvote', 'news',
-        'user', 'chatmessage', 'tunnel', 'videostream', 'notification'
+        'user', 'chatmessage', 'tunnel', 'videostream', 'notification','friendship'
     ) ) or $resource = 'photo';
 
 	if ( $method == 'create' || $method == 'delete' || $method == 'update' ) {
