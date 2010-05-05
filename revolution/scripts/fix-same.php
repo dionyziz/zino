6d34c6bc9663c30716608e8708496c2ba2a02679
Aleksis Brezas <abresas@gmail.com>
2010-05-05 17:08:13 +0000

    spot samecom fix script

--- revolution/scripts/fix-same.php
+++ revolution/scripts/fix-same.php
@@ -6,17 +6,27 @@
 
     include '../models/db.php';
 
+    ?><html><body><?php
+
+    if ( !isset( $_GET[ 'go' ] ) ) {
+        ?>Tell me to go!<br /><?php
+    }
+
     $tables = array( 'com', 'fav', 'vote' );
 
     foreach ( $table as $tbl ) {
+        echo "Fixing table same$tbl{s} <br />";
         for ( $i = 0; $i < 16; ++$i ) {
+            echo "Fixing " . $i * 1000 . " - " . ($i+1) * 100 . "...";
             db( "INSERT INTO `same{$tbl}s2` ( `same{$tbl}_auserid`, `same{$tbl}_buserid`, `same{$tbl}_count` )
             SELECT `same{$tbl}_auserid` , `same{$tbl}_buserid` , SUM( `same{$tbl}_count` )
             FROM `same{$tbl}s`
             WHERE `same{$tbl}_auserid` >= $i*1000 AND `same{$tbl}_auserid`< ($i+1) *1000
             GROUP BY `same{$tbl}_auserid` , `same{$tbl}_buserid`;" );
+            echo "ok<br />";
         }
     }
 
+    ?></body></html><?php
 
 ?>
