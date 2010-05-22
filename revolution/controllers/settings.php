2c2864963f3acd91a77b3d1f0ab19687b332fd8f
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-05-22 12:13:48 +0000

    user settings

--- revolution/controllers/settings.php
+++ revolution/controllers/settings.php
@@ -1,9 +1,14 @@
 <?php
     class ControllerSettings {
-        public static function View( $userid ) {
-            $userid = ( int )$userid;
-            clude( "models/usersettings.php" );
-            $usersettings = Usersettings::Get( $userid );
+        public static function View() {
+            if ( isset( $_SESSION[ 'user' ] ) ) {
+                $userid = $_SESSION[ 'user' ];
+                clude( "models/usersettings.php" );
+                $usersettings = Usersettings::Get( $userid );
+            }
+            else {
+                //you are not logged in
+            }            
             clude( "views/settings/view.php" );
         }
         public static function Listing() {
