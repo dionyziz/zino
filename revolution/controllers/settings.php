1ed97f2dcd24cc8034e1da6c064a6fc132b80456
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-05-22 12:01:20 +0000

    usersettings

--- revolution/controllers/settings.php
+++ revolution/controllers/settings.php
@@ -4,6 +4,7 @@
             $userid = ( int )$userid;
             clude( "models/usersettings.php" );
             $usersettings = Usersettings::Get( $userid );
+            clude( "views/settings/view.php" );
         }
         public static function Listing() {
             
