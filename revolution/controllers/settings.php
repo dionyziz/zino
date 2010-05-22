20fad5dff4cfdcab2612c6ee00d7891621a5c8b8
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-05-22 12:45:20 +0000

    user settings

--- revolution/controllers/settings.php
+++ revolution/controllers/settings.php
@@ -12,8 +12,24 @@
             }      
             include 'views/settings/view.php';
         }
-        public static function Listing() {
+        public static function Update( $email, $notify ) {
+            if ( isset( $_SESSION[ 'user' ] ) ) {
+                $userid = $_SESSION[ 'user' ][ 'id' ];
+                clude( "models/usersettings.php" );
+            }
+            else {
+                return false;
+            }
+
+            if ( ( $email !== "yes" AND $email !== "no" ) 
+                OR ( $notify !== "yes" AND $notify !== "no" )  ) {
+                return false;
+            }
             
+            $pref = array();
+            $pref[ 'emailprofilecomment'] = $pref[ 'emailphotocomment'] = $pref[ 'emailphototag'] = $pref[ 'emailjournalcomment'] = $pref[ 'emailpollcomment'] = $pref[ 'emailreply'] = $pref[ 'emailfriendaddition'] = $pref[ 'emailfriendjournal'] = $pref[ 'emailfriendpoll'] = $pref[ 'emailfriendphoto'] = $pref[ 'emailfavourite'] = $pref[ 'emailbirthday'] = $email;
+            $pref[ 'notifyprofilecomment'] = $pref[ 'notifyphotocomment'] = $pref[ 'notifyphototag'] = $pref[ 'notifyjournalcomment'] = $pref[ 'notifypollcomment'] = $pref[ 'notifyreply'] = $pref[ 'notifyfriendaddition'] = $pref[ 'notifyfriendjournal'] = $pref[ 'notifyfriendphoto'] = $pref[ 'notifyfriendpoll'] = $pref[ 'notifyfavourite'] = $pref[ 'notifybirthday'] = $notify;
+            return Usersettings::Set( $userid, $pref );
         }
        
     }
