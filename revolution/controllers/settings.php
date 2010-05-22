d9248f3581506ecd7cc8dffffb280e0dac0a9d1e
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-05-22 12:18:50 +0000

    user settings

--- revolution/controllers/settings.php
+++ revolution/controllers/settings.php
@@ -1,6 +1,7 @@
 <?php
     class ControllerSettings {
         public static function View() {
+            $usersettings = array();
             if ( isset( $_SESSION[ 'user' ] ) ) {
                 $userid = $_SESSION[ 'user' ][ 'id' ];
                 clude( "models/usersettings.php" );
