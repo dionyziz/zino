41fe0b9ff6f57c6b7eabbd6c81555515c5e539bf
Petros Aggelatos <petrosagg@gmail.com>
2010-07-18 16:49:02 +0000

    Javascript tidying

---
+++ revolution/js/type.js
@@ -0,0 +1,13 @@
+ var Type = {
+        Poll: 1,
+        Image: 2,
+        Userprofile: 3,
+        Journal: 4,
+        Comment: 5,
+        Shout: 6,
+        School: 7,
+        Storeitem: 8,
+        Album: 9,
+        Song: 10,
+        Status: 11
+};
