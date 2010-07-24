4bfd2ac84e3defec171e13ff77345c8e6ae3b2e4
Dionysis Zindros <dionyziz@gmail.com>
2010-07-24 20:52:09 +0000

    user details

--- revolution/js/user/details.js
+++ revolution/js/user/details.js
@@ -1,9 +1,10 @@
-var Details = {
-    GetString: function( field, value, gender, callback ) {
-        axslt( false, 'call:detailstrings', callback, { 'field': field, 'value': value, 'gender': gender } );
+var UserDetails = {
+    GenderStrings: {},
+    GetString: function( field, value, gender ) {
+        return UserDetails.GenderStrings[ gender ][ field ][ value ];
     },
-    GetMap: function( field ) {
-        
+    GetMap: function( gender, field ) {
+        return UserDetails.GenderStrings[ gender ][ field ];
     },
     AcceptableValues: {
         'gender': [ '-', 'm', 'f' ],
@@ -15,129 +16,127 @@ var Details = {
         'haircolor': [ '-','black','brown','red','blond','highlights','dark','grey','skinhead' ],
         'smoker': [ '-', 'yes', 'no', 'socially' ],
         'drinker': [ '-', 'yes', 'no', 'socially' ]
-    }
-}
-
-function () {
-    var social = {
-        'yes': 'Ναι',
-        'no': 'Όχι',
-        'socially': 'Με παρέα'
-    };
-    var GenderStrings = {
-        m: {
-            smoker: social,
-            drinker: social,
-            relationship: {
-                single: Ελεύθερος,
-                relationship: Σε σχέση,
-                casual: Ελεύθερη Σχέση,
-                engaged: Δεσμευμένος,
-                married: Παντρεμένος,
-                complicated: Μπέρδεμα
+    },
+    Init: function () {
+        var social = {
+            yes: 'Ναι',
+            no: 'Όχι',
+            socially: 'Με παρέα'
+        };
+        var hair = {
+            black: 'Μαύρο,'
+            brown: 'Καστανό,'
+            red: 'Κόκκινο,'
+            blond: 'Ξανθό,'
+            highlights: 'Ανταύγες,'
+            dark: 'Σκούρο,'
+            grey: 'Γκρι,'
+            skinhead: 'Skinhead'
+        };
+        var eyes = {
+            black: 'Μαύρο,'
+            brown: 'Καφέ,'
+            green: 'Πράσινο,'
+            blue: 'Μπλε,'
+            grey: 'Γκρι'
+        };
+        UserDetails.GenderStrings = {
+            m: {
+                smoker: social,
+                drinker: social,
+                relationship: {
+                    single: 'Ελεύθερος,'
+                    relationship: 'Σε σχέση,'
+                    casual: 'Ελεύθερη Σχέση,'
+                    engaged: 'Δεσμευμένος,'
+                    married: 'Παντρεμένος,'
+                    complicated: 'Μπέρδεμα'
+                },
+                religion: {
+                    christian: 'Χριστιανός,'
+                    muslim: 'Ισλαμιστής,'
+                    atheist: 'Άθεος,'
+                    agnostic: 'Αγνωστικιστής,'
+                    nothing: 'Άθρησκος,'
+                    pastafarian: 'Πασταφαριανός,'
+                    pagan: 'Παγανιστής,'
+                    budhist: 'Βουδιστής,'
+                    greekpolytheism: 'Πολυθεϊστής,'
+                    hindu: 'Ινδουιστής'
+                },
+                politics: {
+                    right: 'Δεξιός,'
+                    left: 'Αριστερός,'
+                    center: 'Κεντρώος,'
+                    radical left: 'Ακροαριστερός,'
+                    radical right: 'Ακροδεξιός,'
+                    center left: 'Κεντροαριστερός,'
+                    center right: 'Κεντροδεξιός,'
+                    nothing: 'Τίποτα,'
+                    anarchism: 'Αναρχικός,'
+                    communism: 'Κομμουνιστής,'
+                    socialism: 'Σοσιαλιστής,'
+                    liberalism: 'Φιλελεύθερος,'
+                    green: 'Πράσινος'
+                },
+                sexualorientation: {
+                    straight: 'Straight,'
+                    bi: 'Bisexual,'
+                    gay: 'Gay'
+                },
+                eyecolor: eyes,
+                haircolor: hair
             },
-        },
-        f: {
-            smoker: social,
-            drinker: social,
-            relationship: {
-                single: Ελεύθερη,
-                relationship: Σε σχέση,
-                casual: Ελεύθερη Σχέση,
-                engaged: Δεσμευμένη,
-                married: Παντρεμένη,
-                complicated: Μπέρδεμα
-            },
-            religion: {
+            f: {
+                smoker: social,
+                drinker: social,
+                relationship: {
+                    single: 'Ελεύθερη,'
+                    relationship: 'Σε σχέση,'
+                    casual: 'Ελεύθερη Σχέση,'
+                    engaged: 'Δεσμευμένη,'
+                    married: 'Παντρεμένη,'
+                    complicated: 'Μπέρδεμα'
+                },
+                religion: {
+                    christian: 'Χριστιανή,'
+                    muslim: 'Ισλαμίστρια,'
+                    atheist: 'Άθεη,'
+                    agnostic: 'Αγνωστικίστρια,'
+                    nothing: 'Άθρησκη,'
+                    pastafarian: 'Πασταφαριανή,'
+                    pagan: 'Παγανίστρια,'
+                    budhist: 'Βουδίστρια,'
+                    greekpolytheism: 'Δωδεκαθεΐστρια,'
+                    hindu: 'Ινδουίστρια'
+                },
+                politics: {
+                    right: 'Δεξιά,'
+                    left: 'Αριστερή,'
+                    center: 'Κεντρώα,'
+                    radical left: 'Ακροαριστερή,'
+                    radical right: 'Ακροδεξιά,'
+                    center left: 'Κεντροαριστερή,'
+                    center right: 'Κεντροδεξιά,'
+                    nothing: 'Τίποτα,'
+                    anarchism: 'Αναρχική,'
+                    communism: 'Κομμουνίστρια,'
+                    socialism: 'Σοσιαλίστρια,'
+                    liberalism: 'Φιλελεύθερη,'
+                    green: 'Πράσινη'
+                },
+                sexualorientation: {
+                    straight: 'Straight,'
+                    bi: 'Bisexual,'
+                    gay: 'Λεσβία'
+                },
+                eyecolor: eyes,
+                haircolor: hair
             }
-                        christian: Χριστιανή
-                        muslim: Ισλαμίστρια
-                        atheist: Άθεη
-                        agnostic: Αγνωστικίστρια
-                        nothing: Άθρησκη
-                        pastafarian: Πασταφαριανή
-                        pagan: Παγανίστρια
-                        budhist: Βουδίστρια
-                        greekpolytheism: Δωδεκαθεΐστρια
-                        hindu: Ινδουίστρια
-                <xsl:otherwise>
-                        christian: Χριστιανός
-                        muslim: Ισλαμιστής
-                        atheist: Άθεος
-                        agnostic: Αγνωστικιστής
-                        nothing: Άθρησκος
-                        pastafarian: Πασταφαριανός
-                        pagan: Παγανιστής
-                        budhist: Βουδιστής
-                        greekpolytheism: Πολυθεϊστής
-                        hindu: Ινδουιστής
-                </xsl:otherwise>
-        politics: {
-                <xsl:when test="$gender = 'f'">
-                        right: Δεξιά
-                        left: Αριστερή
-                        center: Κεντρώα
-                        <xsl:when test="$value = 'radical left'">
-                            Ακροαριστερή
-                        <xsl:when test="$value = 'radical right'">
-                            Ακροδεξιά
-                        <xsl:when test="$value = 'center left'">
-                            Κεντροαριστερή
-                        <xsl:when test="$value = 'center right'">
-                            Κεντροδεξιά
-                        nothing: Τίποτα
-                        anarchism: Αναρχική
-                        communism: Κομμουνίστρια
-                        socialism: Σοσιαλίστρια
-                        liberalism: Φιλελεύθερη
-                        green: Πράσινη
-                <xsl:otherwise>
-                        right: Δεξιός
-                        left: Αριστερός
-                        center: Κεντρώος
-                        <xsl:when test="$value = 'radical left'">
-                            Ακροαριστερός
-                        <xsl:when test="$value = 'radical right'">
-                            Ακροδεξιός
-                        <xsl:when test="$value = 'center left'">
-                            Κεντροαριστερός
-                        <xsl:when test="$value = 'center right'">
-                            Κεντροδεξιός
-                        nothing: Τίποτα
-                        anarchism: Αναρχικός
-                        communism: Κομμουνιστής
-                        socialism: Σοσιαλιστής
-                        liberalism: Φιλελεύθερος
-                        green: Πράσινος
-                </xsl:otherwise>
-        sexualorientation: {
-                <xsl:when test="$gender = 'f'">
-                        straight: Straight
-                        bi: Bisexual
-                        gay: Λεσβία
-                <xsl:otherwise>
-                        straight: Straight
-                        bi: Bisexual
-                        gay: Gay
-                </xsl:otherwise>
-        eyecolor: {
-                black: Μαύρο
-                brown: Καφέ
-                green: Πράσινο
-                blue: Μπλε
-                grey: Γκρι
-        haircolor: {
-                black: Μαύρο
-                brown: Καστανό
-                red: Κόκκινο
-                blond: Ξανθό
-                highlights: Ανταύγες
-                dark: Σκούρο
-                grey: Γκρι
-                skinhead: Skinhead
-        },
-        'f': {
-        }
-    };
+        };
+    }
+};
+
+();
 
 //axslt( false, 'call:detailstrings', function() { alert( this ) }, { 'type': 'religion', 'value': 'atheist', 'gender': 'f' } );
