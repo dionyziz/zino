2a4d21cbfb4b2a82afa52bc4994e87bef61feb3a
Dionysis Zindros <dionyziz@gmail.com>
2010-07-24 20:08:52 +0000

    user details js

--- revolution/js/user/details.js
+++ revolution/js/user/details.js
@@ -17,4 +17,127 @@ var Details = {
         'drinker': [ '-', 'yes', 'no', 'socially' ]
     }
 }
+
+function () {
+    var social = {
+        'yes': 'Ναι',
+        'no': 'Όχι',
+        'socially': 'Με παρέα'
+    };
+    var GenderStrings = {
+        m: {
+            smoker: social,
+            drinker: social,
+            relationship: {
+                single: Ελεύθερος,
+                relationship: Σε σχέση,
+                casual: Ελεύθερη Σχέση,
+                engaged: Δεσμευμένος,
+                married: Παντρεμένος,
+                complicated: Μπέρδεμα
+            },
+        },
+        f: {
+            smoker: social,
+            drinker: social,
+            relationship: {
+                single: Ελεύθερη,
+                relationship: Σε σχέση,
+                casual: Ελεύθερη Σχέση,
+                engaged: Δεσμευμένη,
+                married: Παντρεμένη,
+                complicated: Μπέρδεμα
+            },
+            religion: {
+            }
+                        christian: Χριστιανή
+                        muslim: Ισλαμίστρια
+                        atheist: Άθεη
+                        agnostic: Αγνωστικίστρια
+                        nothing: Άθρησκη
+                        pastafarian: Πασταφαριανή
+                        pagan: Παγανίστρια
+                        budhist: Βουδίστρια
+                        greekpolytheism: Δωδεκαθεΐστρια
+                        hindu: Ινδουίστρια
+                <xsl:otherwise>
+                        christian: Χριστιανός
+                        muslim: Ισλαμιστής
+                        atheist: Άθεος
+                        agnostic: Αγνωστικιστής
+                        nothing: Άθρησκος
+                        pastafarian: Πασταφαριανός
+                        pagan: Παγανιστής
+                        budhist: Βουδιστής
+                        greekpolytheism: Πολυθεϊστής
+                        hindu: Ινδουιστής
+                </xsl:otherwise>
+        politics: {
+                <xsl:when test="$gender = 'f'">
+                        right: Δεξιά
+                        left: Αριστερή
+                        center: Κεντρώα
+                        <xsl:when test="$value = 'radical left'">
+                            Ακροαριστερή
+                        <xsl:when test="$value = 'radical right'">
+                            Ακροδεξιά
+                        <xsl:when test="$value = 'center left'">
+                            Κεντροαριστερή
+                        <xsl:when test="$value = 'center right'">
+                            Κεντροδεξιά
+                        nothing: Τίποτα
+                        anarchism: Αναρχική
+                        communism: Κομμουνίστρια
+                        socialism: Σοσιαλίστρια
+                        liberalism: Φιλελεύθερη
+                        green: Πράσινη
+                <xsl:otherwise>
+                        right: Δεξιός
+                        left: Αριστερός
+                        center: Κεντρώος
+                        <xsl:when test="$value = 'radical left'">
+                            Ακροαριστερός
+                        <xsl:when test="$value = 'radical right'">
+                            Ακροδεξιός
+                        <xsl:when test="$value = 'center left'">
+                            Κεντροαριστερός
+                        <xsl:when test="$value = 'center right'">
+                            Κεντροδεξιός
+                        nothing: Τίποτα
+                        anarchism: Αναρχικός
+                        communism: Κομμουνιστής
+                        socialism: Σοσιαλιστής
+                        liberalism: Φιλελεύθερος
+                        green: Πράσινος
+                </xsl:otherwise>
+        sexualorientation: {
+                <xsl:when test="$gender = 'f'">
+                        straight: Straight
+                        bi: Bisexual
+                        gay: Λεσβία
+                <xsl:otherwise>
+                        straight: Straight
+                        bi: Bisexual
+                        gay: Gay
+                </xsl:otherwise>
+        eyecolor: {
+                black: Μαύρο
+                brown: Καφέ
+                green: Πράσινο
+                blue: Μπλε
+                grey: Γκρι
+        haircolor: {
+                black: Μαύρο
+                brown: Καστανό
+                red: Κόκκινο
+                blond: Ξανθό
+                highlights: Ανταύγες
+                dark: Σκούρο
+                grey: Γκρι
+                skinhead: Skinhead
+        },
+        'f': {
+        }
+    };
+
 //axslt( false, 'call:detailstrings', function() { alert( this ) }, { 'type': 'religion', 'value': 'atheist', 'gender': 'f' } );
