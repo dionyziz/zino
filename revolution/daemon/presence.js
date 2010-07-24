57b903583b5ab6c0ea64a1270f6c3e327b941bca
Petros Aggelatos <petrosagg@gmail.com>
2010-07-24 01:48:14 +0000

    Presence server

--- revolution/daemon/presence.js
+++ revolution/daemon/presence.js
@@ -12,57 +12,73 @@ server.on( 'request', function ( req, res ) {
     res.writeHead( 200, {
         'Content-Type': 'text/html'
     } );
-    if( req.url != '/connect' ){
+    if( req.url == '/' ){
         res.end( '<html><head></head><body><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script><script>setTimeout( function(){ $.get( "http://presence.zino.gr:8124/connect" );}, 20 );</script></body></html>' );
         return;
     }
-    if( typeof req.headers.cookie !== 'undefined' ){
-        var cookies = req.headers.cookie.split( '; ' );
-        for( var i = 0; i < cookies.length; ++i ){
-            cookies[ i ] = cookies[ i ].split( '=' );
-            if( cookies[ i ][ 0 ] == 'zino_login_8' ){
-                var credentials = cookies[ i ][ 1 ].split( '%3A' );
-                break;
+    if( req.url == '/users/list' ){
+        for( user in online ){
+            res.write( user + "\n" );
+        }
+        res.end();
+        return;
+    }
+    if( req.url == '/connect' ){
+        if( typeof req.headers.cookie !== 'undefined' ){
+            var cookies = req.headers.cookie.split( '; ' );
+            for( var i = 0; i < cookies.length; ++i ){
+                cookies[ i ] = cookies[ i ].split( '=' );
+                if( cookies[ i ][ 0 ] == 'zino_login_8' ){
+                    var credentials = cookies[ i ][ 1 ].split( '%3A' );
+                    break;
+                }
             }
-        }
-        if( typeof credentials === 'undefined' ){
-            return;
-        }
-        var userid = credentials[ 0 ];
-        req.connection_id = ai_connection++;
-        
-        var body = 'userid=' + credentials[ 0 ] + '&authtoken=' + credentials[ 1 ];
-        var request = php.request( 'POST', '/petros/?resource=presence&method=create', { Host: 'zino.gr', 'Content-Length': body.length } );
-        request.end( body );
-        request.on( 'response', function(response){
-            response.on( 'data', function( data ){
-                var result = libxml.parseXmlString( data.toString().substr( data.toString().indexOf( '<social' ), data.toString().length ));
-                result = result.get( '//result' );
-                if( result.text() == 'SUCCESS' ){
-                    console.log( 'User ' + credentials[ 0 ] + ' connected (Connection id = ' + req.connection_id + ')' );
-                    if( typeof( online[ userid ] ) === 'undefined' ){
-                        online[ userid ] = new Array();
+            if( typeof credentials === 'undefined' ){
+                return;
+            }
+            var userid = credentials[ 0 ];
+            req.connection_id = ai_connection++;
+            
+            var body = 'userid=' + credentials[ 0 ] + '&authtoken=' + credentials[ 1 ];
+            var request = php.request( 'POST', '/petros/?resource=presence&method=create', { Host: 'zino.gr', 'Content-Length': body.length } );
+            request.end( body );
+            request.on( 'response', function(response){
+                response.on( 'data', function( data ){
+                    var result = libxml.parseXmlString( data.toString().substr( data.toString().indexOf( '<social' ), data.toString().length ));
+                    result = result.get( '//result' );
+                    if( result.text() == 'SUCCESS' ){
+                        console.log( 'User ' + credentials[ 0 ] + ' connected (Connection id = ' + req.connection_id + ')' );
+                        if( typeof( online[ userid ] ) === 'undefined' ){
+                            online[ userid ] = new Array();
+                        }
+
+                        online[ userid ].push( req.connection_id );
                     }
-
-                    online[ userid ].push( req.connection_id );
-                }
-                else {
-                    res.end();
-                }
+                    else {
+                        res.end();
+                    }
+                });
             });
-        });
-        
+            
 
-        req.connection.on( 'end', function(){ 
-            setTimeout( function(){
-                if( typeof( online[ userid ] ) !== 'undefined' && online[ userid ].length == 0 ) {
-                    delete online[ userid ];
+            req.connection.on( 'end', function(){ 
+                setTimeout( function(){
+                    if( typeof( online[ userid ] ) !== 'undefined' && online[ userid ].length == 0 ) {
+                        delete online[ userid ];
 
-                    console.log( 'User ' + userid + ' went offline' );
-                }
-            }, 10000);
-            online[ userid ].splice( online[ userid ].indexOf( req.connection_id ), 1 );
-        });
+                        var body = 'userid=' + credentials[ 0 ];
+                        var request = php.request( 'POST', '/petros/?resource=presence&method=delete', { 
+                            'Host': 'zino.gr', 
+                            'Content-Length': body.length 
+                        });
+                        request.end( body );
+
+                        console.log( 'User ' + userid + ' went offline' );
+                    }
+                }, 10000);
+                online[ userid ].splice( online[ userid ].indexOf( req.connection_id ), 1 );
+            });
+        }
     }
 });
 
