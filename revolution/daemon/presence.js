c994bf9c8138e1eb6968775d2465cb8d26fd3cd4
Petros Aggelatos <petrosagg@gmail.com>
2010-07-25 15:35:04 +0000

    Presence server

--- revolution/daemon/presence.js
+++ revolution/daemon/presence.js
@@ -16,14 +16,12 @@ server.on( 'request', function ( req, res ) {
 
     //Iframe request
     if( req.url == '/' ){
-        console.log( 'Sending iframe data..' );
         res.end( '<html><head></head><body><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script><script>setTimeout( function(){ $.get( "http://presence.zino.gr:8124/connect" );}, 20 );</script></body></html>' );
         return;
     }
 
     //User list request
     if( req.url == '/users/list' ){
-        console.log( 'User list request' );
         for( user in online ){
             res.write( user + "\n" );
         }
@@ -33,11 +31,9 @@ server.on( 'request', function ( req, res ) {
 
     //Persistent Connection
     if( req.url == '/connect' ){
-        console.log( 'Persistent connection, checking for cookies' );
         if( typeof req.headers.cookie !== 'undefined' ){
             cookies = querystring.parse( req.headers.cookie, '; ' )
             if( typeof cookies[ 'zino_login_8' ] === 'undefined' ){
-                console.log( 'Cookie zino_login_8 not found, closing connection' );
                 res.end();
                 return;
             }
@@ -49,40 +45,51 @@ server.on( 'request', function ( req, res ) {
             
             console.log( 'Checking authtoken validity. userid=' + userid +  ' authtoken=' + authtoken );
             var body = 'userid=' + userid + '&authtoken=' + authtoken;
-            var request = php.request( 'POST', '/dionyziz/?resource=presence&method=create', { 
+            var request = php.request( 'POST', '/petros/?resource=presence&method=create', { 
                 'Host': 'zino.gr', 
+                'Content-Type': 'application/x-www-form-urlencoded',
                 'Content-Length': body.length 
             });
             request.write( body )
             request.close( body );
             request.on( 'response', function( response ){
-                response.on( 'data', function( data ){
-                    //libxml doesn't like proccessing instructions. Strip them from the XML and parse.
-                    var result = libxml.parseXmlString( data.toString().substr( data.toString().indexOf( '<social' ), data.toString().length ));
-                    result = result.get( '//result' );
-                    if( result.text() == 'SUCCESS' ){
+                var data = '';
+                response.on( 'data', function( chunk ){
+                    data += chunk.toString();
+                });
 
-                        console.log( 'Authtoken valid' );
-                        console.log( 'User ' + userid + ' connected (Connection id = ' + req.connection_id + ')' );
-                        
-                        //User was previously offline
-                        if( typeof online[ userid ]  === 'undefined' ){
-                            online[ userid ] = new Array();
+                response.on( 'end', function( ){
+                    //libxml doesn't like proccessing instructions. Strip them from the XML and parse.
+                    var xml = data.substr( data.indexOf( '<social' ), data.length );
+                    try {
+                        var result = libxml.parseXmlString( xml );
+                        result = result.get( '//result' );
+                        if( result.text() == 'SUCCESS' ){
+
+                            console.log( 'Authtoken valid' );
+                            console.log( 'User ' + userid + ' connected (Connection id = ' + req.connection_id + ')' );
+                            
+                            //User was previously offline
+                            if( typeof online[ userid ]  === 'undefined' ){
+                                online[ userid ] = new Array();
+                            }
+                            
+                            //Add the current connetion id to the user's array of active connections.
+                            online[ userid ].push( req.connection_id );
                         }
-                        
-                        //Add the current connetion id to the user's array of active connections.
-                        online[ userid ].push( req.connection_id );
+                        else {
+                            console.log( 'Invalid authtoken, closing connection' );
+                            res.end();
+                        }
                     }
-                    else {
-                        console.log( 'Invalid authtoken, closing connection' );
-                        res.end();
+                    catch ( e ){
+                        console.log( 'libxml error. argument: ' + data.toString() );
                     }
                 });
             });
             
-
             req.connection.on( 'end', function(){
-                console.log( 'Connection ended, waiting 10s for reconnect' );a
+                console.log( 'Connection ended, waiting 10s for reconnect' );
                 //If a previus timeout is on cancel it
                 if( typeof timeouts[ userid ] !== 'undefined' ){
                     clearTimeout( timeouts[ userid ] );
@@ -99,8 +106,9 @@ server.on( 'request', function ( req, res ) {
                         
                         console.log( 'User ' + userid + ' disconnected for more than 10s. Making API call' );
                         var body = 'userid=' + userid;
-                        var request = php.request( 'POST', '/dionyziz/?resource=presence&method=delete', { 
+                        var request = php.request( 'POST', '/petros/?resource=presence&method=delete', { 
                             'Host': 'zino.gr', 
+                            'Content-Type': 'application/x-www-form-urlencoded',
                             'Content-Length': body.length 
                         });
                         request.end( body );
