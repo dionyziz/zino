9153c775ffe3b698628ad398fce99c82352b4d29
Petros Aggelatos <petrosagg@gmail.com>
2010-07-30 03:28:58 +0000

    Poll Listing view bug

--- revolution/views/poll/listing.php
+++ revolution/views/poll/listing.php
@@ -3,11 +3,11 @@
     <entry type="poll" id="<?= $item[ 'id' ]; ?>">
         <published><?= $item[ 'created' ] ?></published>
         <author>
-            <name><?= $item[ 'username' ]; ?></name>
-            <subdomain><?= $item[ 'subdomain' ]; ?></subdomain>
-            <gender><?= $item[ 'gender' ]; ?></gender>
+            <name><?= $user[ 'name' ]; ?></name>
+            <subdomain><?= $user[ 'subdomain' ]; ?></subdomain>
+            <gender><?= $user[ 'gender' ]; ?></gender>
             <avatar>
-                <media url="http://images2.zino.gr/media/<?= $item[ 'userid' ] ?>/<?= $item[ 'avatarid' ] ?>/<?= $item[ 'avatarid' ] ?>_100.jpg" />
+                <media url="http://images2.zino.gr/media/<?= $user[ 'id' ] ?>/<?= $user[ 'avatarid' ] ?>/<?= $user[ 'avatarid' ] ?>_100.jpg" />
             </avatar>
         </author>
         <url><?= $item[ 'url' ]; ?></url>
