368d356e3cf39b079920e22c34b15818ad2b2c9f
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-07-31 15:05:30 +0000

    status test

---
+++ revolution/tests/status.php
@@ -0,0 +1,41 @@
+<?php
+
+    class TestActivity extends ModelTestcase {
+		protected $mUsers;
+		protected $mStatusData;
+
+
+		public function SetUp() {
+            clude( 'models/user.php' );
+			clude( 'models/status.php' );
+
+            $this->mUsers = $this->GenerateTestUsers( 1 );
+			$userid = $this->mUsers[ 0 ][ 'id' ];
+			$this->mStatusData = array( 
+				array( $userid, "Aoua?" ),
+				array( $userid, "What?" )
+			);
+        }
+        public function TearDown() {
+            $this->DeleteTestUsers();
+        }
+		public function PreConditions() {
+            $this->AssertClassExists( 'Status' );
+            $this->AssertMethodExists( 'Status', 'Create' );
+        }
+        /**
+         * @dataProvider GetStatusData
+         */
+        public function TestCreate( $userid, $text ) {			
+			Status::Create( $userid, $text );
+			$details = User::ItemDetails( $userid );
+			$this->AssertEquals( $details[ "status" ], $text, "Status : Wrong Text" );
+        }
+
+		public function GetStatusData() {
+			return $this->mStatusData;	
+		}
+	}
+	
+	return New TestActivity();
+?>
