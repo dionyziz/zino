af59329dc54cd20623394279556cafa7512c35c7
Dionysis Zindros <dionyziz@gmail.com>
2010-07-31 11:55:41 +0000

    favourites

--- revolution/js/favourite.js
+++ revolution/js/favourite.js
@@ -1,5 +1,16 @@
 var Favourite = {
     Init: function () {
-        alert( 'INit' );
+        var $items = $( '.itemstream ul li' );
+        var $item, url;
+
+        for ( var i = 0; i < $items.length; ++i ) {
+            $item = $( $items[ i ] );
+            url = $item.find( 'a' ).attr( 'href' );
+            $item.click( ( function ( url ) {
+                return function() {
+                    location.href = url;
+                };
+            } )( url ) );
+        }
     }
 };
--- revolution/views/favourite/listing.php
+++ revolution/views/favourite/listing.php
@@ -11,36 +11,34 @@
     case TYPE_JOURNAL: 
     ?>
 
-    <entry id="<?= $favourite[ "id" ] ?>" type="journal">
+    <entry id="<?= $favourite[ 'data' ][ "id" ] ?>" type="journal">
 		<title><?= htmlspecialchars( $favourite[ "data" ][ "title" ] ) ?></title>
 		<text><?= WYSIWYG_PresentAndSubstr( $favourite[ "data" ][ "text" ], 400 ) ?></text>
 		<created><?= $favourite[ "data" ][ "created" ] ?></created>
 		<url><?= $favourite[ "data" ][ "url" ] ?></url>
 		<numcomments><?= $favourite[ "data" ][ "numcomments" ] ?></numcomments>
-		<itemid><?= $favourite[ "data" ][ "id" ] ?></itemid>
 	<?
 	break;
     case TYPE_PHOTO: 
     ?>
 
-    <entry id="<?= $favourite[ "id" ] ?>" type="photo">
+    <entry id="<?= $favourite[ 'data' ][ "id" ] ?>" type="photo">
 		<title><?= htmlspecialchars( $favourite[ "data" ][ "title" ] ) ?></title>
 		<created><?= $favourite[ "data" ][ "created" ] ?></created>
 		<numcomments><?= $favourite[ "data" ][ "numcomments" ] ?></numcomments>
-		<itemid><?= $favourite[ "data" ][ "id" ] ?></itemid>
         <media url="http://images2.zino.gr/media/<?= $favourite[ 'data' ][ 'userid' ] ?>/<?= $favourite[ 'data' ][ 'id' ] ?>/<?= $favourite[ 'data' ][ 'id' ] ?>_150.jpg" />
 	<?
 	break;
 	case TYPE_POLL: 
     ?>
 
-    <entry id="<?= $favourite[ "id" ] ?>" type="poll">
+    <entry id="<?= $favourite[ 'data' ][ "id" ] ?>" type="poll">
 	<?
 	break;
 	case TYPE_STOREITEM: 
     ?>
 
-    <entry id="<?= $favourite[ "id" ] ?>" type="storeitem">
+    <entry id="<?= $favourite[ 'data' ][ "id" ] ?>" type="product">
 	<?
 	break;
 	endswitch; ?>
