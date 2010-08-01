f96e1814b1f94d3244d1258fcc293f9c62b391a5
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-01 11:31:48 +0000

    poll tests

---
+++ revolution/tests/poll.php
@@ -0,0 +1,78 @@
+<?php
+
+    class TestPoll extends ModelTestcase {
+		protected $mUsers;
+		protected $mData;
+
+
+		public function SetUp() {
+            clude( 'models/journal.php' );
+            clude( 'models/types.php' );
+            clude( 'models/user.php' );
+
+
+            $this->mUsers = $this->GenerateTestUsers( 1 );
+			$userid = $this->mUsers[ 0 ][ 'id' ];
+			$this->mData = array( 
+				array( $userid, "Pws kai gt", array( "Na fygoume", "Na pame", "gt?" ) ),
+				array( $userid, "Dwse mia stigmh", array( "Alla kai gt", "jaf", "aslfh" ) )
+			);
+        }
+        public function TearDown() {
+            $this->DeleteTestUsers();
+        }
+		public function PreConditions() {
+            $this->AssertClassExists( 'Poll' );
+            $this->AssertMethodExists( 'Poll', 'Create' );
+            $this->AssertMethodExists( 'Poll', 'Delete' );
+        }
+        /**
+         * @dataProvider GetData
+         */
+        public function TestCreate( $userid, $question, $optiontexts ) {	
+			$info = Poll::Create( $userid, $question, $optiontexts );
+			$this->AssertIsArray( $info );
+			$id = $info[ 'id' ];
+			$item = Poll::Item( $id );
+			$this->AssertIsArray( $item );
+			$this->AssertEquals( $item[ "question" ], $question, "Wrong Title" );
+			$success = false;
+			foreach ( $item[ 'options' ] as $option ) {
+				foreach ( $optiontexts as $text ) {
+					if ( $text == $option ) {
+						$success = true;
+					}
+				}
+				if ( $success == false ) {
+					break;
+				}
+			}
+			$this->Assert( $success, "Poll options didn't match" );
+			$this->AssertEquals( ( int )$item[ "user" ][ "id" ], $userid, "Wrong Creator" );
+
+			return $item;
+        }
+		/**
+         * @producer TestCreate
+         */
+        public function TestDelete( $info ) {
+            $id = (int)$info[ 'id' ];
+            $success = Poll::Delete( $id );
+            $this->Assert( $success, 'Poll::Delete failed' );
+
+            $poll = Poll::Item( $id );
+            $this->Called( "Poll::Item" );
+            $this->AssertFalse( $poll, 'Poll::Item should return false on deleted item' );
+
+            // test on invalid album
+            $success = Poll::Delete( 0 );
+            $this->Assert( $success, 'Poll::Delete didnt succeeded on non-existing poll' );
+        }
+
+		public function GetData() {
+			return $this->mData;	
+		}
+	}
+	
+	return New TestPoll();
+?>
