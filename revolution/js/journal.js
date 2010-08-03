69ee28457109c840fe76febc8a70f222d3b77594
Alex Tzo <chorvus@gmail.com>
2010-08-03 22:35:41 +0000

    journal back

--- revolution/js/journal.js
+++ revolution/js/journal.js
@@ -36,7 +36,7 @@ var Journal = {
                 } );
                 return false;
             } );
-            $screen.find( '.toolbox .button.big' ).click( function() { window.location.href = 'news'; return false; } );
+            $screen.find( '.toolbox .linkbutton' ).click( function() { window.location.href = 'news'; return false; } );
         } );
         return false;
     }
