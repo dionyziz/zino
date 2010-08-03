d3e5a696276b2c1e32278b3615849a9a75a628f7
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-03 16:51:08 +0000

    comment tests

--- revolution/tests/comment.php
+++ revolution/tests/comment.php
@@ -15,6 +15,9 @@
 			$userid = $this->mUsers[ 0 ][ 'id' ];
 			$this->mData = array( 
 				array( $userid, "Eftase hwra?", TYPE_PHOTO, 1245, 0 ),
+				array( $userid, "Eftase hwra?", TYPE_JOURNAL, 1245, 0 ),
+				array( $userid, "Eftase hwra?", TYPE_POLL, 1245, 0 ),
+				array( $userid, "Eftase hwra?", TYPE_USERPROFILE, 1245, 0 )				
 			);
         }
         /*public function TearDown() {
@@ -34,7 +37,6 @@
 			$this->AssertIsArray( $info );
 			$item = Comment::Item( $id );
 			$this->AssertIsArray( $item );
-			var_dump( $item );
 			$this->AssertEquals( ( int )$item[ "id" ], $id, "wrong comment id" );
 			$this->AssertEquals( $item[ "text" ], $text2, "wrong comment text" );
 			$this->AssertEquals( ( int )$item[ "typeid" ], $typeid, "wrong comment type" );
