c680fd40658197a3891d3266f70864a1266e70a6
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-03 16:26:57 +0000

    comment tests

---
+++ revolution/tests/comment.php
@@ -0,0 +1,47 @@
+<?php
+
+    class TestComment extends ModelTestcase {
+		protected $mUsers;
+		protected $mData;
+
+
+		public function SetUp() {
+            clude( 'models/comment.php' );
+            clude( 'models/types.php' );
+            clude( 'models/user.php' );
+
+
+            $this->mUsers = $this->GenerateTestUsers( 1 );
+			$userid = $this->mUsers[ 0 ][ 'id' ];
+			$this->mData = array( 
+				array( $userid, "Eftase hwra?", TYPE_PHOTO, 1245, 0 ),
+			);
+        }
+        public function TearDown() {
+            $this->DeleteTestUsers();
+        }
+		public function PreConditions() {
+            $this->AssertClassExists( 'Comment' );
+            $this->AssertMethodExists( 'Comment', 'Create' );
+        }
+        /**
+         * @dataProvider GetData
+         */
+        public function TestCreate( $userid, $text, $typeid, $itemid, $parentid ) {	
+			$info = Comment::Create( $userid, $text, $typeid, $itemid, $parentid );
+			$this->AssertIsArray( $info );
+			$item = Comment::Item( $info[ "id" ] );
+			$this->AssertIsArray( $item );
+			$this->AssertEquals( ( int )$item[ $info[ "id" ] ][ "id" ], ( int )$info[ "id" ], "wrong comment id" );
+			$this->AssertEquals( $item[ $info[ "id" ] ][ "text" ], $info[ "text" ], "wrong comment text" );
+			$this->AssertEquals( ( int )$item[ $info[ "id" ] ][ "typeid" ], $typeid, "wrong comment text" );
+			
+			return $item;
+        }
+		public function GetData() {
+			return $this->mData;	
+		}
+	}
+	
+	return New TestComment();
+?>
