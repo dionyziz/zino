a4f2aaf6e069c0719f328846b27d0f92b7070851
Dionysis Zindros <dionyziz@gmail.com>
2010-08-03 23:22:48 +0000

    activity

--- revolution/views/activity/itemview.php
+++ revolution/views/activity/itemview.php
@@ -1,7 +1,7 @@
 <? switch ( $activity[ 'item' ][ 'typeid' ] ):
     case TYPE_JOURNAL:
         ?><journal id="<?= $activity[ 'item' ][ 'id' ] ?>">
-            <title><?= $activity[ 'item' ][ 'title' ] ?></title>
+            <title><?= htmlspecialchars( $activity[ 'item' ][ 'title' ] ) ?></title>
             <url><?= $activity[ 'item' ][ 'url' ] ?></url>
             <? if ( isset( $activity[ 'item' ][ 'text' ] ) ): ?>
             <text><?= $activity[ 'item' ][ 'text' ] ?></text>
@@ -10,13 +10,13 @@
         break;
     case TYPE_POLL:
         ?><poll id="<?= $activity[ 'item' ][ 'id' ] ?>">
-            <question><?= $activity[ 'item' ][ 'title' ] ?></question>
+            <question><?= htmlspecialchars( $activity[ 'item' ][ 'title' ] ) ?></question>
             <url><?= $activity[ 'item' ][ 'url' ] ?></url>
         </poll><?
         break;
     case TYPE_PHOTO:
         ?><photo id="<?= $activity[ 'item' ][ 'id' ] ?>">
-            <title><?= $activity[ 'item' ][ 'title' ] ?></title>
+            <title><?= htmlspecialchars( $activity[ 'item' ][ 'title' ] ) ?></title>
             <url><?= $activity[ 'item' ][ 'url' ] ?></url> 
         </photo><?
         break;
