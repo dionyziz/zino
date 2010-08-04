c76d98a221e14f7c5026c64748769ca6acb4c466
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-04 11:02:53 +0000

    user test

---
+++ revolution/tests/user.php
@@ -0,0 +1,54 @@
+<?php
+
+    class TestUser extends ModelTestcase {
+		protected $mData;
+
+
+		public function SetUp() {
+            clude( 'models/journal.php' );
+            clude( 'models/types.php' );
+            clude( 'models/user.php' );
+
+
+			$this->mData = array( 
+				array( "mitsaras223", "apap@rigo.com", "hellsbell" ),
+			);
+        }
+        public function TearDown() {
+            $this->DeleteTestUsers();
+        }
+		public function PreConditions() {
+            $this->AssertClassExists( 'Journal' );
+            $this->AssertMethodExists( 'Journal', 'Create' );
+        }
+        /**
+         * @dataProvider GetData
+         */
+        public function TestCreate( $name, $email, $password ) {	
+			$info = User::Create( $name, $email, $password );
+			$this->Assert( $info, "User could not be created" );
+			$userid = $info;
+			$user = User::Item( $userid );
+			$this->AssertIsArray( $user, "User::Item should return an array." );
+			$this->AssertEquals( $user[ 'name' ], $name, "User::Create.Wrong Name." );
+
+			return $user;
+        }
+		/**
+         * @producer TestCreate
+         */
+        public function TestDelete( $user ) {
+            $userid = $user[ 'id' ];
+			User::Delete( $userid );
+			$user = User::Item( $userid );
+			$this->AssertIsArray( $user, "User::Item should return an array." );
+			$this->AssertEquals( ( bool )$user[ 'userdeleted' ], true, "User::Delete.User should have been deleted." );
+        }
+
+		public function GetData() {
+			return $this->mData;	
+		}
+	}
+	
+	return New TestUser();
+?>
