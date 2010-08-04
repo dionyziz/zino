9ab177a5589dfad8909d1158c295290601c14490
Aleksis Brezas <abresas@gmail.com>
2010-08-04 17:42:16 +0000

    model undefined index

--- revolution/models/activity.php
+++ revolution/models/activity.php
@@ -86,7 +86,9 @@
                         $activity[ 'item' ][ 'bulkid' ] = $row[ 'activity_bulkid' ];
                         $activity[ 'item' ][ 'title' ] = $row[ 'activity_text' ];
                         $activity[ 'item' ][ 'url' ] = $row[ 'activity_url' ];
-                        $activity[ 'item' ][ 'text' ] = $bulk[ $row[ 'activity_bulkid' ] ];
+                        if ( $row[ 'activity_bulkid' ] != 0 ) {
+                            $activity[ 'item' ][ 'text' ] = $bulk[ $row[ 'activity_bulkid' ] ];
+                        }
                         break;
                     default:
                         die( 'unknown activity type' );
