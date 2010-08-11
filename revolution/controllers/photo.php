1042a95dd76f952d3a30675389882212f6e405fe
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-11 13:29:24 +0000

    comment fix

--- revolution/controllers/photo.php
+++ revolution/controllers/photo.php
@@ -111,7 +111,6 @@
                 }
                 $tempname = $uploadimage[ 'tmp_name' ];
             }
-			die( "DIE BITCJ" );
             
             $photo = Photo::Create( $userid, $albumid, $tempname );
             $photo[ 'userid' ] = $userid;
