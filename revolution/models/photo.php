62e94dd4171effe4f775d7ce4273fb669ad01818
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-11 14:16:45 +0000

    upload fix

--- revolution/models/photo.php
+++ revolution/models/photo.php
@@ -85,8 +85,6 @@
 
                 WHERE
                     i.`image_id` = :id AND
-					next.`image_delid` = 0 AND
-					previous.`image_delid` = 0
                 GROUP BY
                     i.`image_id`
                 LIMIT 1;', array( 'id' => $id )
