6b513c3b50651391346275943ea02448d479eed7
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-12 15:19:52 +0000

    patry api modifications

--- revolution/etc/zinoparty/zinoparty.php
+++ revolution/etc/zinoparty/zinoparty.php
@@ -1,19 +1,19 @@
 <?php
 	$data = file( "http://www.zino.gr/songs" );
 	var_dump( $data );
-
+	$p = xml_parser_create();
+	xml_parse_into_struct($p, $data, $vals, $index);
+	var_dump( $vals );
+	var_dump( $index );
 	
 ?>
+<!--
 <html>
 	<head>
 		<title>Zino Party!</title>
 	</head>
 	<body>
-	</body>
-	
 
-
-<xsl:template match="song">
     <div class="mplayer">
         <div class="player">
             <object>
@@ -21,15 +21,21 @@
                 <param name="wmode" value="opaque"></param>
                 <param name="allowScriptAccess" value="always"></param>
                 <param name="flashvars">
+
                     <xsl:attribute name="value">hostname=cowbell.grooveshark.com&amp;songID=<xsl:value-of select="@id" />&amp;style=metal</xsl:attribute>
+
                 </param>
                 <embed src="http://listen.grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" flashvars="" allowScriptAccess="always" wmode="opaque">
+
+			
                 <xsl:attribute name="flashvars">hostname=cowbell.grooveshark.com&amp;songID=<xsl:value-of select="@id" />&amp;style=metal</xsl:attribute>
+
                 </embed>
             </object>
         </div>
     </div>
 </xsl:template>
 
-
+	</body>
 </html>
+--!>
