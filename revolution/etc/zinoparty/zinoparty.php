6ce6d9025aaeb860370af1ba8cf5461125bad719
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-12 15:35:40 +0000

    patry api modifications

--- revolution/etc/zinoparty/zinoparty.php
+++ revolution/etc/zinoparty/zinoparty.php
@@ -1,41 +1,30 @@
 <?php
-	$data = file( "http://www.zino.gr/songs" );
+	$data = file( "http://www.zino.gr/randomsongs" );
 	var_dump( $data );
 	$p = xml_parser_create();
 	xml_parse_into_struct($p, $data, $vals, $index);
 	var_dump( $vals );
 	var_dump( $index );
-	
+	$songid = 1787526;	
 ?>
-<!--
+
 <html>
 	<head>
 		<title>Zino Party!</title>
 	</head>
 	<body>
 
-    <div class="mplayer">
-        <div class="player">
-            <object>
-                <param name="movie" value="http://listen.grooveshark.com/songWidget.swf"></param>
-                <param name="wmode" value="opaque"></param>
-                <param name="allowScriptAccess" value="always"></param>
-                <param name="flashvars">
-
-                    <xsl:attribute name="value">hostname=cowbell.grooveshark.com&amp;songID=<xsl:value-of select="@id" />&amp;style=metal</xsl:attribute>
-
-                </param>
-                <embed src="http://listen.grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" flashvars="" allowScriptAccess="always" wmode="opaque">
-
-			
-                <xsl:attribute name="flashvars">hostname=cowbell.grooveshark.com&amp;songID=<xsl:value-of select="@id" />&amp;style=metal</xsl:attribute>
-
-                </embed>
-            </object>
-        </div>
-    </div>
-</xsl:template>
+		<div class="mplayer">
+		    <div class="player">
+		        <object>
+		            <param name="movie" value="http://listen.grooveshark.com/songWidget.swf"></param>
+		            <param name="wmode" value="opaque"></param>
+		            <param name="allowScriptAccess" value="always"></param>
+		            <param name="flashvars" value="hostname=cowbell.grooveshark.com&amp;songID=<?php echo $songid ?>&amp;style=metal"></param>
+		            <embed src="http://listen.grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" flashvars="hostname=cowbell.grooveshark.com&amp;songID=<?php echo $songid ?>&amp;style=metal" allowScriptAccess="always" wmode="opaque"></embed>
+		        </object>
+		    </div>
+		</div>
 
 	</body>
 </html>
---!>
