b62e260802cc908eb908d5cadec515c56553ae02
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-14 23:15:45 +0000

    ban system

--- revolution/views/ban/listing.php
+++ revolution/views/ban/listing.php
@@ -1,14 +1,13 @@
 <banned>
     <?php foreach ( $banned as $user ): ?>
 	<ban id ="<?= $user[ 'id' ]; ?>">
-		<user>
-			<id><?= $user[ 'userid' ]; ?></id>
-			<rights><?= $user[ 'rights' ]; ?></rights>
-			<started><?= $user[ 'started' ]; ?></started>
-			<expire><?= $user[ 'expire' ]; ?></expire>
-			<reason><?= $user[ 'reason' ]; ?></reason>
-			<admin><?= $user[ 'admin' ]; ?></admin>
+		<user id ="<?= $user[ 'userid' ]; ?>">
+			<name><?= $user[ 'name' ]; ?></name>			
 		</user>
+		<started><?= $user[ 'started' ]; ?></started>
+		<expire><?= $user[ 'expire' ]; ?></expire>
+		<reason><?= $user[ 'reason' ]; ?></reason>
+		<admin><?= $user[ 'admin' ]; ?></admin>
 	</ban>
 	<?php endforeach; ?>
 </banned>
