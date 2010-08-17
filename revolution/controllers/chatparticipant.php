3e782cb67ba9b6e60d5736525278b8bbb8945531
Dionysis Zindros <dionyziz@gmail.com>
2010-08-17 22:36:33 +0000

    chat typing

--- revolution/controllers/chatparticipant.php
+++ revolution/controllers/chatparticipant.php
@@ -7,7 +7,13 @@
         public static function Create() { // add person to group chat
         }
         public static function Update( $channelid, $typing ) {
+            // warning: no authentication here
+            // anyone can claim they are typing in any channel
+            // it remains for the client to validate that
+            // the claimed participant is a member of the
+            // channel they claim they are typing in
             $typing = ( bool )$typing;
+            $channelid = ( int )$channelid;
             isset( $_SESSION[ 'user' ] ) or die( 'You must be logged in to access the chat application.' );
             clude( 'models/comet.php' );
             clude( 'models/db.php' );
