d57a923ca999f26329586038e970c08166e19afe
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-18 15:09:58 +0000

    password requests

---
+++ revolution/controllers/passwordrequest.php
@@ -0,0 +1,27 @@
+<?php
+    class ControllerPasswordRequest {
+		public static function Create( $username ) {
+			clude( 'models/db.php' );
+			clude( 'models/user.php' );
+			clude( 'models/email.php' );
+			
+			$user = User::ItemByName( $username );
+			if ( empty( $user ) ) {
+				throw New Exception( "This username doesn't belong to a registered user" );	
+			}
+			$id = $user[ 'id' ];
+			$details = User::ItemDetails( $id );
+			if ( empty( $details[ 'profile' ][ 'email' ] ) ) {
+				throw New Exception( "The user has to have set an email account" );	
+			}
+			$request = PasswordRequest::Create( $userid );
+			//sent email
+
+			ob_start();
+			echo $settings[ 'base' ];
+            $subject = PasswordRequest::Mail();
+        	$message = ob_get_clean();
+        	Email( $user[ 'name' ], $details[ 'profile' ][ 'email' ], $subject, $message, 'Zino', 'info@zino.gr' );
+		}
+	}
+?>
---
+++ revolution/models/passwordrequest.php
@@ -0,0 +1,47 @@
+<?php
+	global $settings;
+    class PasswordRequest {
+		public static function Create( $userid ) {
+			clude( 'models/hashstring.php' );
+			clude( 'models/agent.php' );
+			$userid = ( int )$userid;
+			$hash = GenerateRandomHash();
+			$ip = (string)UserIp();
+			db( 'INSERT INTO `passwordrequests`
+                 ( `request_userid`, `request_hash`, `request_used`, `request_host`, `request_created` )
+                 VALUES ( :userid, :hash, 0, :ip, NOW() )',
+				compact( 'userid', 'hash', 'ip' ) 
+			);
+			
+			return array( 
+					'userid' => $userid, 
+					'hash' => $hash, 
+					'used' => 0,
+					'host' => $ip,
+			);
+		}
+		public static function Item( $id, $hashvalue ) {
+
+		}
+        public function Mail( $requestid, $hash ) {
+            ?>Είμαστε έτοιμοι να αλλάξεις τον κωδικό στον λογαριασμό σου στο Zino.
+
+Για να το κάνεις, ακολούθησε τον παρακάτω σύνδεσμο:
+
+<?php
+            echo $settings[ 'base' ];
+            ?>/forgot/recover/<?php
+            echo $requestid;
+            ?>?hash=<?php
+            echo $hash;
+            ?>
+
+Αν ο σύνδεσμός δεν λειτουργεί, δοκίμασε να τον αντιγράψεις στην γραμμή διευθύνσεων.
+
+Αν δεν έχεις ζητήσει να αλλάξεις τον κωδικό σου, μπορείς να αγνοήσεις αυτό το μήνυμα.<?php
+            //Element( 'email/footer', false );
+            
+            return 'Zino: Επαναφορά κωδικού πρόσβασης';
+        }
+	}
+?>
