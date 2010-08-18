8ffa7d5a12922cebf0d3da04795deb68cdd07a45
Dionysis Zindros <dionyziz@gmail.com>
2010-08-18 19:19:01 +0000

    doctype HTML 1.0 strict

--- revolution/xslt/global.php
+++ revolution/xslt/global.php
@@ -25,7 +25,7 @@ foreach ( $list as $num=>$line ) {
         //Generate viewable error
         echo '<?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    <xsl:output method="text" encoding="utf-8" indent="yes" />
+    <xsl:output method="html" encoding="utf-8" indent="yes" doctype-public="-//W3C//DTD XHTML 1.0 Strict//EN" doctype-system="http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd" />
     <xsl:template match="/">';
         echo "global.xsl: File \"$line\" does not exist
     </xsl:template>
