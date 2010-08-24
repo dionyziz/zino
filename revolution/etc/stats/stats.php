e00ffbd22663f03e8ccf02363410f75d04e5b2b0
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-24 16:57:14 +0000

    stats

--- revolution/etc/stats/stats.php
+++ revolution/etc/stats/stats.php
@@ -1,14 +1,25 @@
 <?php
+	function clude( $path ) {
+        static $included = array();
+        if ( !isset( $included[ $path ] ) ) {
+            $included[ $path ] = true;
+            return include $path;
+        }
+        return true;
+    }
+	
+
 	set_include_path( '../../:./' );
+
 
 	session_start();
     error_reporting( E_ERROR | E_WARNING | E_PARSE | E_NOTICE );    
-    include 'models/water.php';
+    clude( 'models/water.php' );
     global $settings;
     $settings = include 'settings.php';
 
-	include "models/libchart-1.2.1/libchart/classes/libchart.php";
-	include "models/db.php";
+	clude( "models/libchart-1.2.1/libchart/classes/libchart.php" );
+	clude( "models/db.php" );
 	$chart = new VerticalBarChart(500, 250);
 	$dataSet = new XYDataSet();
 	$dataSet->addPoint(new Point("Jan 2005", 273));
