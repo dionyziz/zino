0c118a90a71786fc294fae85b07c07d43e5493a4
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-08-24 16:54:48 +0000

    stats

--- revolution/etc/stats/stats.php
+++ revolution/etc/stats/stats.php
@@ -1,6 +1,12 @@
 <?php
 	set_include_path( '../../:./' );
 
+	session_start();
+    error_reporting( E_ERROR | E_WARNING | E_PARSE | E_NOTICE );    
+    clude( 'models/water.php' );
+    global $settings;
+    $settings = include 'settings.php';
+
 	include "models/libchart-1.2.1/libchart/classes/libchart.php";
 	include "models/db.php";
 	$chart = new VerticalBarChart(500, 250);
