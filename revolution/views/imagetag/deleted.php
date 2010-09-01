9463baf75dbdc296cbb5e134ac6d89fe5e4b68e9
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-09-01 20:19:31 +0000

    dumpass php

--- revolution/views/imagetag/deleted.php
+++ revolution/views/imagetag/deleted.php
@@ -1,13 +1,1 @@
-<operation resource="imagetag" method="delete">
-    <imagetag id="<?= $tag[ 'id' ] ?>">
-        <image id="<?= $tag[ 'image' ][ 'id' ] ?>" />
-        <person id="<?= $tag[ 'personid' ] ?>" />
-        <owner id="<?= $tag[ 'ownerid' ] ?>" />
-        <geometry>
-            <left><?= $tag[ 'tagleft' ] ?></left>
-            <top><?= $tag[ 'tagtop' ] ?></top>
-            <width><?= $tag[ 'width' ] ?></width>
-            <height><?= $tag[ 'height' ] ?></height>
-        </geometry>
-    </imagetag>
-</operation>
+<operation resource="imagetag" method="delete"></operation>
