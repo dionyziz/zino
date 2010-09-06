4ab40222f9aa1b7e116f2bf6da2f78eb945f4723
Aleksis Brezas <abresas@gmail.com>
2010-09-06 18:41:04 +0000

    debugging stuff

--- revolution/models/water.php
+++ revolution/models/water.php
@@ -4,4 +4,20 @@
             throw New Exception( $description );
         }
     }
+    class Water {
+        public static function ProfileStart( $description ) {
+        }
+        public static function ProfileEnd() {
+        }
+        public static function QueryStart( $sql ) {
+        }
+        public static function QueryEnd() {
+        }
+        public static function Trace( $description, $data ) {
+        }
+        public static function Init() {
+        }
+        public static function TotalTime() {
+        }
+    }
 ?>
