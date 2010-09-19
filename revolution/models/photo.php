39d0b14842b113e0429520f982df60f0b699d3da
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-09-19 14:55:26 +0000

    photo size

--- revolution/models/photo.php
+++ revolution/models/photo.php
@@ -262,7 +262,7 @@
 
             global $settings;
 
-            if ( filesize( $tempfile ) > 4 * 1024 * 1024 ) { // 4 MB
+            if ( filesize( $tempfile ) > 7 * 1024 * 1024 ) { // 7 MB
                 return -1;
             }
 
