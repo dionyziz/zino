57d14e234defca6d0d78c2a518f285319c72081c
Giorgos Panagiotakos <pagio91i@gmail.com>
2010-09-26 18:37:04 +0000

    song view

--- revolution/views/song/listing.php
+++ revolution/views/song/listing.php
@@ -3,15 +3,6 @@
     <? foreach ( $songs as $song ): ?>
         <song id="<?= $song[ 'SongID' ] ?>">
             <name><?= htmlspecialchars( $song[ 'SongName' ] ) ?></name>
-            <!-- 
-            <media url="<?= $song[ 'image' ][ 'small' ] ?>" />
-            <duration><?= $song[ 'estDurationSecs' ] ?></duration>
-            <? if ( $song[ 'genreID' ] != 0 ): ?>
-            <genre id="<?= $song[ 'genreID' ] ?>">
-                <name><?= htmlspecialchars( $song[ 'genreName' ] ) ?></name>
-            </genre>
-            <? endif; ?> 
-            -->
             <artist id="<?= $song[ 'ArtistID' ] ?>">
                 <name><?= htmlspecialchars( $song[ 'ArtistName' ] ) ?></name>
             </artist>
